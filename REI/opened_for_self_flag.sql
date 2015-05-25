cde6cf6b4b46ee13194889cd7d45899ee2320b54
Shamna P <shamna.puthalath@numerify.com>
2015-05-25 12:45:59 +0530

    customizations

---
+++ REI/opened_for_self_flag.sql
@@ -0,0 +1,8 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+FROM (select count(*) as cnt from rei_workdb.dwh_d_incident AA
+INNER JOIN rei_mdsdb.incident_final BB ON AA.row_id COLLATE utf8_general_ci =BB.sys_id AND AA.source_id=BB.sourceinstance
+where
+opened_for_self_flag <> case when BB.u_self_service_incident = 1 then 'Y' else 'N' end)c
+ 
