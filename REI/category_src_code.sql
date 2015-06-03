e01d2acba73a538db808ae05399b832642f72e91
Srinivas Vedagiri <srinivas@numerify.com>
2015-06-03 18:52:42 +0530

    fixed REI sql scripts.

--- REI/category_src_code.sql
+++ REI/category_src_code.sql
@@ -1,7 +1,6 @@
 SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
-FROM ( select count(*) as cnt from rei_workdb.dwh_f_incident AA INNER JOIN rei_mdsdb.incident_final BB
-ON AA.row_id COLLATE utf8_general_ci =BB.sys_id AND AA.source_id=BB.sourceinstance
-where category_src_code collate utf8_general_ci <> BB.u_online_store_incident_catego)c
- 
+FROM ( select count(1) as cnt from rei_workdb.f_incident AA INNER JOIN rei_mdsdb.incident_final BB
+ON AA.row_id =BB.sys_id AND AA.source_id=BB.sourceinstance
+where category_src_code  <> BB.u_online_store_incident_catego)c
