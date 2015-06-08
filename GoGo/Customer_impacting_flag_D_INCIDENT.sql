42be4a1ae63510cc08dd52c855947d798adb0809
Bhavana Jayaprakash <bhavana@numerify.com>
2015-06-08 16:35:27 +0530

    files deleted

--- GoGo/Customer_impacting_flag_D_INCIDENT.sql
+++
@@ -1,7 +0,0 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM ( select count(1) as cnt from gogo_mdsdb.incident_final a
-inner join gogo_mdwdb.d_incident  b on a.sys_id =b.row_id a.sourceinstance=b.source_id and b.row_key <100
-where case when a.u_customer_impacting ='1' then 'Y' else 'N' end <> b.customer_impacting_c_flag
- ) c;
--- GoGo/state_src_code_F_INCIDENT.sql
+++
@@ -1,8 +0,0 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM ( select count(1) as cnt
-from gogo_mdsdb.incident_final a
-inner join gogo_mdwdb.f_incident b on a.sys_id =b.row_id and a.sourceinstance=b.source_id and b.row_key <100
-where a.incident_state<>b.state_src_code
- ) c;
