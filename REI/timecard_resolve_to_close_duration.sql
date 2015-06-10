c86ec32c8e884c6d830ab421366b05c2224f106b
Bhavana Jayaprakash <bhavana@numerify.com>
2015-06-10 11:34:09 +0530

    rei_timecard changes

---
+++ REI/timecard_resolve_to_close_duration.sql
@@ -0,0 +1,9 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+FROM (select count(*) as cnt from rei_mdwdb.f_incident a
+inner join rei_mdwdb.d_incident b on a.row_id = b.row_id and a.source_id = b.source_id
+where
+resolve_to_close_duration <> TIMESTAMPDIFF(SECOND,last_resolved_on,closed_on) or
+open_to_resolve_duration <> TIMESTAMPDIFF(SECOND,opened_on,last_resolved_on))c;
+
