dcd3f8a664d60aef77466a3eaf1bd718eff56eca
Srinivas Vedagiri <srinivas@numerify.com>
2015-06-17 10:22:47 +0530

    Updated the query

--- MATSGENERIC/Check_row_id_Duplicates_mdwdb.sql
+++ MATSGENERIC/Check_row_id_Duplicates_mdwdb.sql
@@ -4,5 +4,5 @@
 
 
 select case when count(distinct row_id)<> count( row_id) then 'FAILURE' else 'SUCCESS' end as Result,
-case when count(distinct row_id)<> count( row_id) then 'ROW_ID has duplicates in #TABLE_NAME ' else '' end as Message
+case when count(distinct row_id)<> count( row_id) then 'ROW_ID has duplicates in <<tablename>> ' else '' end as Message
 from <<tenant>>_mdwdb.<<tablename>>;
