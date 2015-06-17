7911b31acbbc9c1389b045417daf0934731d30a2
Madhura_Patwardhan <madhura@numerify.com>
2015-06-17 17:44:00 +0530

    Mats corrected syntax errors

--- MATS/Check_reassignment_flag_t_task_activity.sql
+++ MATS/Check_reassignment_flag_t_task_activity.sql
@@ -2,7 +2,7 @@
 
 SELECT CASE WHEN COUNT(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 , CASE WHEN COUNT(1) > 0 THEN 'reassigned_flag not set correctly in t_task_activity' ELSE 'SUCCESS' END AS Message
-from  <<tenant>>_mdwdb.t_task_activity ta 
+from  <<tenant>>_mdwdb.f_t_task_activity ta 
 where CASE WHEN ta.task_attribute_wh_name= 'assignment_group'
 and ta.task_wh_type= 'incident'
 and ta.task_attribute_wh_old_value is not null  
