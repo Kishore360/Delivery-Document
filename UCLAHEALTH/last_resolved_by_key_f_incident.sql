c96a38195e6bcfbac17f4ec12496b275cc79a053
prabhuram <prabhuram.sridharan@numerify.com>
2015-06-19 02:21:42 +0530

    Pushing one file after test

--- UCLAHEALTH/last_resolved_by_key_f_incident.sql
+++ UCLAHEALTH/last_resolved_by_key_f_incident.sql
@@ -5,10 +5,12 @@ FROM (
 select count(1) as cnt from(
 SELECT A.SYS_ID,B.ROW_ID,A.last_resolved_by_key as  A_last_resolved_by_key,B.last_resolved_by_key as  B_last_resolved_by_key FROM
 (SELECT SYS_ID,sourceinstance, y.row_key AS   last_resolved_by_key
-FROM uclahealth_mdsdb.incident_final x join uclahealth_mdwdb.d_internal_contact y on 
-u_resolved_by=REPLACE(row_id,'INTERNAL_CONTACT~','')
+FROM uclahealth_mdsdb.incident_final x join 
+(select row_key,row_id  from uclahealth_mdwdb.d_internal_contact
+ )y on 
+u_resolved_by=REPLACE(row_id,'INTERNAL_CONTACT~','' )
 )A
-LEFT OUTER JOIN  
-(SELECT  REPLACE(last_resolved_by_key,'INTERNAL_CONTACT~','') AS last_resolved_by_key,source_id,ROW_ID FROM  uclahealth_mdwdb.f_incident
+ JOIN  
+(SELECT  last_resolved_by_key AS last_resolved_by_key,source_id,ROW_ID FROM  uclahealth_mdwdb.f_incident
   )B on A.sourceinstance=B.source_id AND B.ROW_ID=SYS_ID)H
 WHERE A_last_resolved_by_key<> B_last_resolved_by_key)E; 
