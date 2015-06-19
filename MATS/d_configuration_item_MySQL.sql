cd9b29974120a5bd6e5ded988aabb80b59e128c9
Madhura_Patwardhan <madhura@numerify.com>
2015-06-19 18:03:27 +0530

    modified Mats

--- MATS/d_configuration_item_MySQL.sql
+++ MATS/d_configuration_item_MySQL.sql
@@ -1,4 +1,4 @@
 
-
-
-SELECT COUNT(1) FROM <<tenant>>_mdwdb.d_configuration_item 
+select case when count(1)>0 then 'Success' else 'Failure' end as Result,
+case when count(1)<0 then 'No count' else 'Success' end as Message from (
+SELECT COUNT(1) FROM <<tenant>>_mdwdb.d_configuration_item ) a
