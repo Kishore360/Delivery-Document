739a315dbad69adfecd43f093caa901993ea9401
Madhura_Patwardhan <madhura@numerify.com>
2015-06-22 13:28:26 +0530

    mats added files

--- MATS/LOCATION REGION CLASSIFICATION_SN_LEVEL.sql
+++ MATS/LOCATION REGION CLASSIFICATION_SN_LEVEL.sql
@@ -36,9 +36,4 @@ LEFT JOIN  <<tenant>>_mdwdb.d_location level_5
 ON a.s5 = level_5.row_id) b ;
 
 
-select case when count(1)<1 then 'Failure' else 'Success' end as Result,
-case when count(1)<1 then 'No records found' else 'Records found' end as Message from
-(SELECT row_id,lev_0_id,lev_0_key,lev_1_id,lev_1_key,lev_2_id,lev_2_key,
-lev_3_id,lev_3_key,lev_4_id,lev_4_key,lev_5_id,lev_5_key,source_id
-FROM <<tenant>>_mdwdb.d_h_all_hierarchies
-WHERE hierarchy_class ='LOCATION REGION CLASSIFICATION' ) c;
+
