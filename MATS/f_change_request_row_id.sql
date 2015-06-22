baa8fd40be0555e8c5f1e0dfe3475bb417bedf65
Bhavana Jayaprakash <bhavana@numerify.com>
2015-06-22 10:30:05 +0530

    fixed errors

--- MATS/f_change_request_row_id.sql
+++ MATS/f_change_request_row_id.sql
@@ -6,4 +6,4 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  LEFT JOIN <<tenant>>_mdwdb.f_change_request TRGT 
  ON (convert(SRC.sys_id using utf8) =convert(TRGT.row_id using utf8) 
  AND convert(SRC.sourceinstance using utf8)= convert(TRGT.source_id using utf8) )
- WHERE COALESCE( SRC.sys_id using utf8),'')<> COALESCE(TRGT.row_id using utf8),'')
+ WHERE COALESCE( SRC.sys_id ,'')<> COALESCE(TRGT.row_id ,'')
