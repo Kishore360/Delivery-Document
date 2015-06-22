0587dab8946bdcd4cd063bd94c16ac9ceb29a0d0
Bhavana Jayaprakash <bhavana@numerify.com>
2015-06-22 16:33:02 +0530

    mats fixed timing execution

--- MATS/f_response.sql
+++ MATS/f_response.sql
@@ -1,3 +1,3 @@
 select case when count(1)<1 then 'Failure' else 'Success' end as Result,
 case when count(1)<1 then 'No records found' else 'Records found' end as Message from
-(SELECT  row_key, row_id, source_id, etl_run_number, response_value, response, updated_count, survey_instance_key, question_key, soft_deleted_flag, created_by, changed_by, created_on, changed_on, dw_inserted_on, dw_updated_on FROM <<tenant>>_mdwdb.f_response ORDER BY  row_key, row_id, source_id, etl_run_number, response_value, response, updated_count, survey_instance_key, question_key, soft_deleted_flag, created_by, changed_by, created_on, changed_on, dw_inserted_on, dw_updated_on)a ;
+(SELECT  row_key, row_id, source_id, etl_run_number, response_value, response, updated_count, survey_instance_key, question_key, soft_deleted_flag, created_by, changed_by, created_on, changed_on, dw_inserted_on, dw_updated_on FROM <<tenant>>_mdwdb.f_response )a ;
