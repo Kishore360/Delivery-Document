03223b8f555b4cdcb792ca2d5feb9a485215aff0
Madhura_Patwardhan <madhura@numerify.com>
2015-06-22 12:19:52 +0530

    Mats fixed syntax errors

--- MATS/ASSIGNMENT GROUP TIER_HIER_SN_LEVEL.sql
+++ MATS/ASSIGNMENT GROUP TIER_HIER_SN_LEVEL.sql
@@ -2,7 +2,6 @@ select case when count(1)<1 then 'Failure' else 'Success' end as Result,
 case when count(1)<1 then 'No records found' else 'Records found' end as Message from
 
 (
-
 SELECT CONCAT(a.s0,'~1~ASSIGNMENT GROUP TIER') AS row_id,
 a.s0 AS lev_0_id,IFNULL(level_0.row_key ,0) AS lev_0_key,
 a.s1 AS lev_1_id,IFNULL(level_1.row_key ,0) AS lev_1_key,
@@ -37,7 +36,7 @@ LEFT JOIN  <<tenant>>_mdwdb.d_internal_organization level_4
 ON a.s4 = level_4.row_id
 LEFT JOIN  <<tenant>>_mdwdb.d_internal_organization level_5
 ON a.s5 = level_5.row_id 
-)a;
+)b;
 
 
 
@@ -50,4 +49,4 @@ SELECT row_id,lev_0_id,lev_0_key,lev_1_id,lev_1_key,lev_2_id,lev_2_key,
 lev_3_id,lev_3_key,lev_4_id,lev_4_key,lev_5_id,lev_5_key
 FROM <<tenant>>_mdwdb.d_h_all_hierarchies
 WHERE hierarchy_class ='ASSIGNMENT GROUP TIER' 
-)b;
+)c;
