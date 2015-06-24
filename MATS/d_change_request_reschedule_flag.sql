f81519117a2f1d5565a48dc67df55800b3c11e07
Madhura_Patwardhan <madhura@numerify.com>
2015-06-24 16:02:48 +0530

    modified

--- MATS/d_change_request_reschedule_flag.sql
+++ MATS/d_change_request_reschedule_flag.sql
@@ -1,6 +1,6 @@
 
 
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+#SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_request.rescheduled_flag' ELSE 'SUCCESS' END as Message
  FROM <<tenant>>_mdsdb.change_request_final SRC
  LEFT JOIN <<tenant>>_mdwdb.d_change_request TRGT 
