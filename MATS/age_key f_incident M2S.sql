3fc3bb02d608770fdea8a44cfdd9b51beb43ed09
Prabhuram Sridharan <Prabhuram_Sridharan>
2015-06-24 17:08:33 +0000

    age_key f_incident M2S.sql edited online with Bitbucket

--- MATS/age_key f_incident M2S.sql
+++ MATS/age_key f_incident M2S.sql
@@ -1,14 +1,13 @@
-
-
 select case when count(1) > 1 then 'FAILURE' else 'SUCCESS' end as Result,
 case when count(1) > 1 then CONCAT( CAST(COUNT(1) AS CHAR(10)), ' invalid aging_key set for f_incident') 
-else 'SUCCESS' end as Message
- from <<tenant>>_mdwdb.d_lov a , 
-					(select  TIMESTAMPDIFF(DAY,c.calendar_date,i.dw_updated_on) as age_in_days, i.row_key,i.age_key
-					from <<tenant>>_mdwdb.f_incident i
-					inner join <<tenant>>_mdwdb.d_calendar_date c
-					on c.row_key = i.opened_on_key 
-					where c.source_id=0
-					and i.state_src_code not in(6,7) ) as x 
-where x.age_in_days between a.lower_range_value and a.upper_range_value 
-and dimension_class like '%AGEBUCKET%'
+else 'SUCCESS' end as Message from(
+select count(1) from
+(
+select i.row_id,i.age,c.lower_range_value,c.upper_range_value,i.age_key,c.row_key
+from <<tenant>>_mdwdb.f_incident i
+left join <<tenant>>_mdwdb.d_lov c
+on c.dimension_class='AGEBUCKET_WH~INCIDENT'
+where  i.age between c.lower_range_value and c.upper_range_value
+and i.age_key<>c.row_key
+)h
+)e
