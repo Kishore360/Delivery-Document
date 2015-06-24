3e10581e8b33e0e096d38c19be2bc4a3fbf837d7
Prabhuram Sridharan <Prabhuram_Sridharan>
2015-06-24 17:07:47 +0000

    Check_reassignment_flag_t_task_activity.sql edited online with Bitbucket

--- MATS/Check_reassignment_flag_t_task_activity.sql
+++ MATS/Check_reassignment_flag_t_task_activity.sql
@@ -1,10 +1,15 @@
 
-
-SELECT CASE WHEN COUNT(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-, CASE WHEN COUNT(1) > 0 THEN 'reassigned_flag not set correctly in t_task_activity' ELSE 'SUCCESS' END AS Message
-from  <<tenant>>_mdwdb.f_t_task_activity ta 
-where CASE WHEN ta.task_attribute_wh_name= 'assignment_group'
-and ta.task_wh_type= 'incident'
-and ta.task_attribute_wh_old_value is not null  
-and ta.task_attribute_wh_new_value is not null 
-THEN 'Y' ELSE 'N' END <> reassigned_flag
+select case when count(1) > 1 then 'FAILURE' else 'SUCCESS' end as Result,
+case when count(1) > 1 then CONCAT( CAST(COUNT(1) AS CHAR(10)), ' invalid aging_key set for f_incident') 
+else 'SUCCESS' end as Message from(
+select count(1) from
+(
+select (case when task_attribute_wh_new_value is not null and task_attribute_wh_old_value is not null then 'Y' else 'N' 
+end),ta.reassigned_flag 
+from <<tenant>>_mdwdb.f_t_task_activity ta
+where task_attribute_wh_name= 'assignment_group'
+and task_wh_type= 'incident'
+and (case when task_attribute_wh_new_value is not null and task_attribute_wh_old_value is not null then 'Y' else 'N' end)
+<>ta.reassigned_flag
+)h
+)e
