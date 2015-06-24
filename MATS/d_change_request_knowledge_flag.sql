5437fcbee0be9bf23b9dc07cab68e070aac7fe0a
Madhura_Patwardhan <madhura@numerify.com>
2015-06-24 14:12:19 +0530

    modified 'convert' clause

--- MATS/d_change_request_knowledge_flag.sql
+++ MATS/d_change_request_knowledge_flag.sql
@@ -4,6 +4,6 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_request.knowledge_flag' ELSE 'SUCCESS' END as Message
  FROM <<tenant>>_mdsdb.change_request_final SRC 
  LEFT JOIN <<tenant>>_mdwdb.d_change_request TRGT 
- ON (convert(SRC.sys_id using utf8) =convert(TRGT.row_id using utf8) 
- AND convert(SRC.sourceinstance using utf8)= convert(TRGT.source_id using utf8) )
+ ON (SRC.sys_id=TRGT.row_id) 
+ AND (SRC.sourceinstance = TRGT.source_id  )
  WHERE COALESCE( CASE WHEN  SRC.knowledge= TRUE THEN 'Y' ELSE 'N' END,'' )<> COALESCE(TRGT.knowledge_flag,'' )
