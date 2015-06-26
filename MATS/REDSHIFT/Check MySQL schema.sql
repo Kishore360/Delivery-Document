bc5c71c50c2ebba2c6ac9266e617bca61078b22b
Madhura_Patwardhan <madhura@numerify.com>
2015-06-26 15:35:01 +0530

    corrected

--- MATS/REDSHIFT/Check MySQL schema.sql
+++ MATS/REDSHIFT/Check MySQL schema.sql
@@ -7,7 +7,7 @@ case when count(1)<1 then 'No records found' else 'Records found' end as Message
 
 (select table_name,column_name,column_type 
 from information_schema.columns
-where table_schema ='pgi_mdwdb'
+where table_schema ='<<tenant>>_mdwdb'
 and (table_name like 'fs_%' or 'ds%'
 or table_name like 'f_%' or 'd_%')
 order by 1,2,3) a;
