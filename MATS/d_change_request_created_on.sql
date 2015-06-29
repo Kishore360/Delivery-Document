dfdf9512b483576a8697f620d29d62501c027c5b
Madhura_Patwardhan <madhura@numerify.com>
2015-06-29 16:59:16 +0530

    for checking

--- MATS/d_change_request_created_on.sql
+++ MATS/d_change_request_created_on.sql
@@ -1,9 +1,8 @@
-SELECT SRC.sourceinstance, TRGT.source_id ,SRC.cab_date, created_on
-,COALESCE(CONVERT_TZ(SRC.sys_created_on,"GMT","America/Los_Angeles"),''),
-SRC.sys_created_on
- FROM <<tenant>>_mdsdb.change_request_final SRC 
- LEFT JOIN rei_mdwdb.d_change_request TRGT 
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_request.approved_on' ELSE 'SUCCESS' END as Message
+ FROM gogo_mdsdb.change_request_final SRC 
+ LEFT JOIN gogo_mdwdb.d_change_request TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
-where COALESCE(CONVERT_TZ(SRC.sys_created_on,<<source_time_zone>>,<<target_time_zone>>),'')
+where COALESCE(CONVERT_TZ(SRC.sys_created_on,"GMT","America/Chicago"),'')
 <> TRGT.created_on;
