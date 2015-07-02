6d22958aef9c7c3ed30368338799fad23ace9697
Bhavana Jayaprakash <bhavana@numerify.com>
2015-07-02 14:07:52 +0530

    fixed mats syntax errors

--- MATS/Executes/t_task_activity_update_count.sql
+++ MATS/Executes/t_task_activity_update_count.sql
@@ -1,9 +1,9 @@
 
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for t_task_activity.update_count' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_t_task_activity.update_count' ELSE 'SUCCESS' END as Message
  FROM <<tenant>>_mdsdb.sys_audit_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.t_task_activity TRGT 
+ LEFT JOIN <<tenant>>_mdwdb.f_t_task_activity TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
  WHERE COALESCE( SRC.record_checkpoint,'')<> COALESCE(TRGT.update_count ,'')
