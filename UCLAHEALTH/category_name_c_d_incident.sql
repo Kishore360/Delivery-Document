2c1b4ce07ee3b1a0f70b993fa287c0574eb2c43d
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-02 11:07:37 +0530

    changes

--- UCLAHEALTH/category_name_c_d_incident.sql
+++ UCLAHEALTH/category_name_c_d_incident.sql
@@ -3,8 +3,11 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ELSE 'Data Matched' END AS Message 
 FROM (
 select count(1) as cnt 
-FROM uclahealth_mdsdb.incident_final 
+FROM 
+uclahealth_mdsdb.incident_final 
  JOIN  
-  uclahealth_mdwdb.d_incident B on sourceinstance=B.source_id AND B.ROW_ID=SYS_ID
-and u_category_name<>B.category_name_c
+  uclahealth_mdwdb.d_incident B 
+on sourceinstance=B.source_id 
+AND B.ROW_ID=SYS_ID
+where  u_category_name<>B.category_name_c
 )E;
