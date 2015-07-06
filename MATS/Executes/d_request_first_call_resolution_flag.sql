c7aef5ff2c9e47b1345126ed8730ec607120cebe
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-06 20:06:42 +0530

    no message

--- MATS/Executes/d_request_first_call_resolution_flag.sql
+++ MATS/Executes/d_request_first_call_resolution_flag.sql
@@ -1,5 +1,7 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_request.first_call_resolution_flag' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_request.first_call_resolution_flag' ELSE 'SUCCESS' END as Message from (
+ select  CASE WHEN  TIMESTAMPDIFF(MINUTE,TRGT.opened_on,TRGT.closed_on)<30
+AND  LM.dimension_wh_code IN('RESOLVED','CLOSED') THEN 'Y' ELSE 'N' END abc, COALESCE(TRGT.first_call_resolution_flag ,'') def
  FROM <<tenant>>_mdsdb.sc_request_final SRC 
  LEFT JOIN <<tenant>>_mdwdb.d_request TRGT 
  ON (SRC.sys_id =TRGT.row_id  
@@ -9,7 +11,6 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  AND TRGTF.source_id =TRGT.source_id)
 
  LEFT JOIN <<tenant>>_mdwdb.d_lov_map LM ON TRGTF.state_src_key=LM.src_key
-LEFT JOIN <<tenant>>_mdwdb.d_o_data_freshness FRESH  ON(FRESH.source_id=SRC.sourceinstance) 
- WHERE  CASE WHEN  TIMESTAMPDIFF(MINUTE,TRGT.opened_on,TRGT.closed_on)<30
-AND  LM.dimension_wh_code IN('RESOLVED','CLOSED') THEN 'Y' ELSE 'N' END <> COALESCE(TRGT.first_call_resolution_flag ,'')
+LEFT JOIN <<tenant>>_mdwdb.d_o_data_freshness FRESH  ON(FRESH.source_id=SRC.sourceinstance) )
+ WHERE abc<>def
 
