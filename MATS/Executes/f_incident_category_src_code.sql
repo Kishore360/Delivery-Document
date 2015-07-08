129f6db6c212411e58efc014ac0d44964f0d3f44
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 21:17:32 +0530

    no message

--- MATS/Executes/f_incident_category_src_code.sql
+++ MATS/Executes/f_incident_category_src_code.sql
@@ -1,9 +1,9 @@
 
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.source_id' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.category_src_code' ELSE 'SUCCESS' END as Message
  FROM <<tenant>>_mdsdb.incident_final SRC 
  LEFT JOIN <<tenant>>_mdwdb.f_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.sourceinstance,'')<> COALESCE(TRGT.source_id ,'')
+ WHERE COALESCE( SRC.category,'')<> COALESCE(TRGT.category_src_code ,'')
