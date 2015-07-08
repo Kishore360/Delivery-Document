58758a33c27622b4a3007bd20676eb74e52128a8
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 21:05:38 +0530

    no message

--- MATS/Executes/f_problem_priority_src_code.sql
+++
@@ -1,9 +0,0 @@
-
-
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_problem.priority_src_code' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.problem_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.f_problem TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.priority,'')<> COALESCE(TRGT.priority_src_code ,'')
