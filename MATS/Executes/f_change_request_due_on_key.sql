be7a5926d6328b8f381f435ef8f0d1c5a42b1c94
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 21:38:35 +0530

    no message

--- MATS/Executes/f_change_request_due_on_key.sql
+++
@@ -1,9 +0,0 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.due_on_key' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.change_request_final SRC
- LEFT JOIN <<tenant>>_mdwdb.f_change_request TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  ) LEFT JOIN <<tenant>>_mdwdb.d_calendar_date LKP 
-on (LKP.row_id = date_format(convert_tz(SRC.due_date,<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>),'%Y%m%d') and LKP.source_id=0 
-)
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.due_date IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.due_on_key,'')
