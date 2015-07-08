859b263f6f8e8f97448cf2e4da692a541a843b51
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 18:29:52 +0530

    no message

--- MATS/Executes/f_request_item_opened_by_department_key.sql
+++ MATS/Executes/f_request_item_opened_by_department_key.sql
@@ -1,9 +1,10 @@
 
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request.opened_by_department_key' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.sc_request_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.f_request TRGT 
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_item.opened_by_department_key' ELSE 'SUCCESS' END as Message
+ 
+ FROM <<tenant>>_mdsdb.sc_req_item_final SRC 
+ LEFT JOIN <<tenant>>_mdwdb.f_request_item TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
 LEFT JOIN  <<tenant>>_mdwdb.d_internal_contact LKP1 
@@ -21,3 +22,4 @@ and LKP.row_id  is null
  
  WHERE COALESCE(LKP.row_key,LKP2.row_key,CASE WHEN (SRC.opened_by is not null and LKP1.department_code IS NULL 
  or SRC.opened_by is  null) THEN 0 else '-1' end)<> COALESCE(TRGT.opened_by_department_key,'')
+
--- MATS/Executes/f_request_item_opened_time_key.sql
+++ MATS/Executes/f_request_item_opened_time_key.sql
@@ -1,9 +1,9 @@
 
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request.opened_time_key' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.sc_request_final SRC
- LEFT JOIN <<tenant>>_mdwdb.f_request TRGT 
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_item.opened_time_key' ELSE 'SUCCESS' END as Message
+ FROM <<tenant>>_mdsdb.sc_req_item_final SRC
+ LEFT JOIN <<tenant>>_mdwdb.f_request_item TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
 
