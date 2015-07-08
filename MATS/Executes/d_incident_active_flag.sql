41a00a8ece1182ea66277f4cbc8a55eb0782f7d7
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 17:52:25 +0530

    no message

--- MATS/Executes/d_incident_active_flag.sql
+++
@@ -1,9 +0,0 @@
-
-
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.active_flag' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.incident_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( CASE WHEN SRC.active= 1 then 'Y' else 'N' END,'')<> COALESCE(TRGT.active_flag ,'')
--- MATS/Executes/f_incident_impact_src_code.sql
+++
@@ -1,9 +0,0 @@
-
-
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.impact_src_code' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.incident_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.f_incident TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.impact,'')<> COALESCE(TRGT.impact_src_code ,'')
