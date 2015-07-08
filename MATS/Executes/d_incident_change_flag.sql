e17b6a6eb35f9d92e0f95c2ce196e1e8fc6fe5ce
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 22:16:53 +0530

    no message

--- MATS/Executes/d_incident_change_flag.sql
+++ MATS/Executes/d_incident_change_flag.sql
@@ -1,7 +1,10 @@
+
+
+
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.met_sla_flag' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.change_flag' ELSE 'SUCCESS' END as Message
  FROM <<tenant>>_mdsdb.incident_final SRC 
  LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( CASE WHEN made_sla = 1 then 'Y' else 'N' END,'')<> COALESCE(TRGT.met_sla_flag ,'')
+ WHERE COALESCE( CASE WHEN SRC.rfc is not null then 'Y' else 'N' END,'')<> COALESCE(TRGT.change_flag ,'')
