fdcab6c26598ec3d7e2372715325db1cbe052e20
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 18:35:24 +0530

    no message

--- MATS/Executes/f_request_item_business_duration.sql
+++ MATS/Executes/f_request_item_business_duration.sql
@@ -1,9 +1,9 @@
 
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_item.open_to_close_duration' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_item.business_duration' ELSE 'SUCCESS' END as Message
  FROM <<tenant>>_mdsdb.sc_req_item_final SRC
  LEFT JOIN <<tenant>>_mdwdb.f_request_item TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
-WHERE  TIMESTAMPDIFF(second,SRC.closed_at,SRC.opened_at)<>  COALESCE(TRGT.open_to_close_duration,'') 
+WHERE  TIMESTAMPDIFF(second,'1970-01-01 00:00:00',SRC.business_duration)<>  COALESCE(TRGT.business_duration,'') 
