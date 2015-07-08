35450b5b434c5d5a2144c657fccabf852dd2911f
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 20:14:31 +0530

    no message

--- MATS/Executes/f_request_assignment_group_key.sql
+++ MATS/Executes/f_request_assignment_group_key.sql
@@ -1,12 +1,14 @@
 
 
+
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_item.assignment_group_key' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.sc_req_item_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.f_request_item TRGT 
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request.assignment_group_key' ELSE 'SUCCESS' END as Message
+ FROM <<tenant>>_mdsdb.sc_request_final SRC
+
+ LEFT JOIN <<tenant>>_mdwdb.f_request TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
  LEFT JOIN <<tenant>>_mdwdb.d_internal_organization LKP 
- ON ( SRC.assignment_group= substr(LKP.row_id,instr(LKP.row_id,'~')+1,length(LKP.row_id))
+ ON ( SRC.assignment_group= substr(LKP.row_id,instr(LKP.row_id,'~')+1,LENGTH(LKP.row_id))
 AND SRC.sourceinstance= LKP.source_id )
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.assignment_group IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.assignment_group_key,'')
