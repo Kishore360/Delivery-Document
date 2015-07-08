04d7d0e78806942e1c9d2a0151a989a160f7a037
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 21:59:41 +0530

    no message

---
+++ MATS/Executes/d_problem_linked_incident_flag.sql
@@ -0,0 +1,9 @@
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.linked_incident_flag' ELSE 'SUCCESS' END as Message
+ FROM <<tenant>>_mdsdb.problem_final SRC 
+ LEFT JOIN <<tenant>>_mdwdb.d_problem TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+ WHERE COALESCE( CASE WHEN SRC.related_incidents>0 then 'Y' else 'N' END,'')<> COALESCE(TRGT.linked_incident_flag ,'')
