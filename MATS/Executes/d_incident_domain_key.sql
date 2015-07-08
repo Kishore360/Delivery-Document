93c1f79ed426e3e7582656e0ea360586aba25507
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-08 22:11:14 +0530

    no message

--- MATS/Executes/d_incident_domain_key.sql
+++ MATS/Executes/d_incident_domain_key.sql
@@ -1,9 +1,10 @@
 
 
+
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.domain_key' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.problem_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.d_problem TRGT 
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.domain_key' ELSE 'SUCCESS' END as Message
+ FROM <<tenant>>_mdsdb.incident_final SRC 
+ LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
  LEFT JOIN <<tenant>>_mdwdb.d_domain LKP 
--- MATS/Executes/d_incident_met_sla_flag.sql
+++ MATS/Executes/d_incident_met_sla_flag.sql
@@ -1,9 +1,7 @@
-
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.source_id' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.met_sla_flag' ELSE 'SUCCESS' END as Message
  FROM <<tenant>>_mdsdb.incident_final SRC 
  LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.sourceinstance,'')<> COALESCE(TRGT.source_id ,'')
+ WHERE COALESCE( CASE WHEN made_sla = 1 then 'Y' else 'N' END,'')<> COALESCE(TRGT.met_sla_flag ,'')
