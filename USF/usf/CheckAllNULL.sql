d60f69a98289b7dbc400b6fc0e849993ededad82
Devesh <devesh.agewal@numerify.com>
2015-07-20 18:40:51 +0530

    Revert "USF cust for ITAA"
    This reverts commit e3b38cbacb51ffe64c65bf14076ea731e11cf5d1.

--- USF/usf/CheckAllNULL.sql
+++
@@ -1,4 +0,0 @@
-SELECT CASE WHEN ColCount = 0 AND TotalCount >0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result,
- CASE WHEN ColCount = 0 AND TotalCount >0 THEN ' #COLUMN_NAME is all NULL for <<tablename>>' ELSE 'SUCCESS' END AS Message
-FROM (SELECT COUNT(DISTINCT #COLUMN_NAME) AS ColCount , COUNT(1) TotalCount
-FROM <<tenant>>_mdwdb.<<tablename>>) A
--- USF/usf/Check_data.sql
+++
@@ -1,3 +0,0 @@
-
-
-SELECT #SELECT_COLS FROM <<tablename>> ORDER BY #ORDER_COLS
