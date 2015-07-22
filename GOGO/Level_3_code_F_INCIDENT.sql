53c883e6577d1914ab3440fd44ba95ae1a4ace22
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-22 17:26:41 +0530

    no message

--- GOGO/Level_3_code_F_INCIDENT.sql
+++ GOGO/Level_3_code_F_INCIDENT.sql
@@ -4,4 +4,4 @@ ELSE 'Data Matched' END AS Message
 FROM ( select count(1) as cnt from gogo_mdsdb.incident_final s
 inner join gogo_mdwdb.f_incident i on s.sys_id=i.row_id 
 and s.sourceinstance = i.source_id
-where coalesce(s.u_level_3,' ')=coalesce(i.level_3_src_code_c ,' '))c;
+where coalesce(s.u_level_3,' ')<>coalesce(i.level_3_src_code_c ,' '))c;
--- GOGO/last_resolved_on_F_INCIDENt.sql
+++ GOGO/last_resolved_on_F_INCIDENt.sql
@@ -2,9 +2,11 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 FROM ( select count(1) as cnt from gogo_mdsdb.incident_final s
+left outer join gogo_mdwdb.d_calendar_date j on 
+s.u_resolved=row_id
 inner join gogo_mdwdb.f_incident i on s.sys_id=i.row_id 
 and s.sourceinstance=i.source_id
-where  coalesce(DATE_FORMAT(CONVERT_TZ(s.u_resolved,'GMT','America/Chicago'),'%Y%m%d'),'0')
-<>coalesce(i.last_resolved_on_key,'0')
+where  j.row_key
+<>coalesce(j.last_resolved_on_key,'0')
 ) c;
  
