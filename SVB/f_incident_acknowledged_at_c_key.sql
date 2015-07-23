299a1dc7979c0734483df26452f23f5b1d273679
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-23 07:41:31 +0530

    no message

--- SVB/f_incident_acknowledged_at_c_key.sql
+++
@@ -1,9 +0,0 @@
-SELECT CaSE WHEN cnt > 0 THEN 'FaILURE' ELSE 'SUCCESS' END aS Result
-,CaSE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END aS Message 
-FROM (
-select count(1) as cnt  from svb_mdwdb. f_incident b
- inner  JOIN  svb_mdsdb.incident_final a
-on  b.ROW_ID=SYS_ID and sourceinstance=b.source_id
-left join svb_mdwdb.d_incident i on a. u_acknowledged_at =i.acknowledged_at_c
- where i.row_key <> b. acknowledged_at_c_key)c
