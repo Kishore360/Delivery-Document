e70aa1d0a2072968912972fc6d083d2883c971d3
prabhuram <prabhuram.sridharan@numerify.com>
2015-07-23 18:51:10 +0530

    no message

--- SVB/d_incident_acknowledged_at_c_id.sql
+++ SVB/d_incident_acknowledged_at_c_id.sql
@@ -4,7 +4,8 @@ SELECT CaSE WHEN cnt > 0 THEN 'FaILURE' ELSE 'SUCCESS' END aS Result
 ,CaSE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END aS Message 
 FROM (
-select count(1) as cnt  from svb_workdb. fs_incident b
+select count(1) as cnt  from svb_mdwdb. d_incident b
  inner  JOIN  svb_mdsdb.incident_final a
 on  b.ROW_ID=SYS_ID and sourceinstance=b.source_id
- where date_format(convert_tz(a.u_acknowledged_at,'GMT','America/Los_Angeles'),'%Y%m%d') <> b. acknowledged_at_c_id)c
+ where date_format(a.u_acknowledged_at,'%Y%m%d') <> 
+date_format(b. acknowledged_at_c,'%Y%m%d'))c
