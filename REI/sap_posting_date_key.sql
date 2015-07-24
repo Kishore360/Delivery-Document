c037ad2db85e7d84b7eb0d77877e29c215a8f7f6
Madhura_Patwardhan <madhura@numerify.com>
2015-07-24 12:10:52 +0530

    Committing the new SAP cube test cases foe REI

---
+++ REI/sap_posting_date_key.sql
@@ -0,0 +1,11 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
+ FROM
+
+rei_mdsdb.us_sap_actuals_final a
+left join rei_mdwdb.d_calendar_date b
+on b.row_id=date_format(a.posting_date,'%Y%m%d~2')
+left join rei_mdwdb.f_sap_actuals_c c
+on MD5(a.row_id)=c.row_id
+where b.row_key<>c.sap_posting_date_key
+;
