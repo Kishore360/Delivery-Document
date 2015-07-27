a5669a7f614050a3b73c10bd4e3cf22761444f25
Bhavana Jayaprakash <bhavana@numerify.com>
2015-07-27 16:35:48 +0530

    fixed errors.

--- PGI/last_resolved_on_d_incident.sql
+++ PGI/last_resolved_on_d_incident.sql
@@ -5,4 +5,4 @@ FROM (
 select count(1) as cnt from pgi_mdwdb.d_incident dfi
 join pgi_mdsdb.incident_final incf on dfi.row_id = incf.sys_id and dfi.source_id= incf.sourceinstance
 where
-dfi.last_resolved_on <> CONVERT_TZ(incf.closed_at,"GMT","America/Los_Angeles"))c
+dfi.last_resolved_on <> CONVERT_TZ(incf.closed_at,"GMT","UTC"))c;
