47e3f3b1159f9f1d8c78cd9b106b4901cac1b048
prabhuram <prabhuram.sridharan@numerify.com>
2015-08-03 12:06:33 +0530

    Query tuned to run fast

--- SVB/ITSA/f_incident_last_resolved_on_key_mysql.sql
+++ SVB/ITSA/f_incident_last_resolved_on_key_mysql.sql
@@ -1,13 +1,21 @@
 
-
-SELECT CaSE WHEN cnt > 0 THEN 'FaILURE' ELSE 'SUCCESS' END aS Result
-,CaSE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END aS Message 
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
 FROM (
-select count(1) as cnt  from
-svb_mdsdb.incident_final a
-left outer join svb_mdwdb.d_incident b on
-a. u_resolved=b.row_id
-left join svb_mdwdb. f_incident c 
-on b.row_id=c.row_id and b.source_id=c.source_id
-where b.row_key<>c.  last_resolved_on_key)c
+select COUNT(*) cnt from(
+SELECT A.SYS_ID,B.ROW_ID,
+B.  last_resolved_on_key B_last_resolved_on_key ,
+case when year <2000 then A.last_resolved_on_key+20000000 else A.last_resolved_on_key 
+end as  A_last_resolved_on_key from
+(SELECT SYS_ID,sourceinstance, 
+DATE_FORMAT(CONVERT_TZ(u_resolved,'GMT','America/Los_Angeles'),'%Y')  AS   year,
+DATE_FORMAT(CONVERT_TZ(u_resolved,'GMT','America/Los_Angeles'),'%Y%m%d')  AS   last_resolved_on_key
+FROM svb_mdsdb.incident_final 
+#where sys_id='092bf5f2f547b9c00f4bd5728aaf9afc'
+)A
+ JOIN  
+(SELECT  last_resolved_on_key,source_id,ROW_ID FROM  svb_mdwdb.f_incident
+where state_src_code>4
+ )B on A.sourceinstance=B.source_id AND B.ROW_ID=SYS_ID)H
+WHERE A_last_resolved_on_key<> B_last_resolved_on_key)E;
