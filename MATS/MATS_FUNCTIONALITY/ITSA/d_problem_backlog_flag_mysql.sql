af8d3b15e979456912cf9703ea764be1b76cb1bd
prabhuram <prabhuram.sridharan@numerify.com>
2015-08-06 04:25:48 +0530

    committing back after checking for index queries (t_task_activity)

---
+++ MATS/MATS_FUNCTIONALITY/ITSA/d_problem_backlog_flag_mysql.sql
@@ -0,0 +1,13 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.backlog_flag' ELSE 'SUCCESS' END as Message
+ FROM <<tenant>>_mdsdb.problem_final SRC 
+ LEFT JOIN <<tenant>>_mdwdb.d_problem TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+  JOIN  <<tenant>>_mdwdb.f_problem TRGTF 
+ ON (TRGTF.problem_key =TRGT.row_key
+ AND TRGTF.source_id =TRGT.source_id)
+LEFT JOIN <<tenant>>_mdwdb.d_lov_map LM
+ on TRGTF.state_src_key = LM.src_key 
+WHERE COALESCE( CASE WHEN LM.dimension_wh_code NOT IN('RESOLVED','CLOSED') THEN 'Y' ELSE 'N' END,'')<> COALESCE(TRGT.backlog_flag ,'')
+
