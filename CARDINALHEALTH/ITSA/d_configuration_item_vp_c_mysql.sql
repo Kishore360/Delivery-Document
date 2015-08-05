61adedcf3b2264629818ddf5263acb34a7f9fc2c
Madhura_Patwardhan <madhura@numerify.com>
2015-08-05 12:32:43 +0530

    no message

--- CARDINALHEALTH/ITSA/d_configuration_item_vp_c_mysql.sql
+++ CARDINALHEALTH/ITSA/d_configuration_item_vp_c_mysql.sql
@@ -1,8 +1,11 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
-from
-cardinalhealth_mdsdb.sys_user_final a
-left join 
-cardinalhealth_mdwdb.d_configuration_item b 
-on a.sys_id=b.row_id and b.source_id=a.sourceinstance
-where b.vp_c <> CONCAT_WS(' ',a.first_name,a.last_name);
+
+from cardinalhealth_mdsdb.cmdb_ci_final a
+join cardinalhealth_mdwdb.d_configuration_item b
+on a.sys_id=b.row_id and a.sourceinstance=b.source_id
+left join cardinalhealth_mdsdb.sys_user_final suo 
+ON a.owned_by=suo.sys_id and a.sourceinstance=suo.sourceinstance  
+LEFT JOIN cardinalhealth_mdsdb.sys_user_final svp 
+ON suo.u_vp=svp.sys_id and svp.sourceinstance=suo.sourceinstance 
+where b.vp_c <> CONCAT_WS(' ',svp.first_name, svp.last_name);
