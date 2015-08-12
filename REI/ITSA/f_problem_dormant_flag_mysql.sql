7c4c90044f80dde833ddd47b5d2ba5847dcf709f
Bhavana Jayaprakash <bhavana@numerify.com>
2015-08-12 12:07:38 +0530

    rei cust

---
+++ REI/ITSA/f_problem_dormant_flag_mysql.sql
@@ -0,0 +1,28 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.dormant_flag' ELSE 'SUCCESS' END as Message
+
+FROM rei_mdsdb.problem_final SRC 
+
+ LEFT JOIN rei_mdwdb.d_problem TRGT 
+ 
+ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+ 
+LEFT JOIN rei_mdwdb.f_problem TRGTF
+ 
+ON (TRGTF.problem_key =TRGT.row_key  
+ AND TRGTF.source_id= TRGT.source_id  )
+
+ 
+LEFT JOIN rei_mdwdb.d_lov_map LM 
+ON TRGTF.state_src_key=LM.src_key
+
+LEFT JOIN rei_mdwdb.d_o_data_freshness FRESH  
+ON(FRESH.source_id=SRC.sourceinstance) 
+ AND 
+
+FRESH.lastupdated= (select lastupdated from rei_mdwdb.d_o_data_freshness
+
+where etl_run_number = ( select max(etl_run_number) from rei_mdwdb.d_o_data_freshness)) WHERE 
+CASE WHEN timestampdiff(DAY,TRGT.changed_on,FRESH.lastupdated)>14 
+AND  LM.dimension_wh_code='OPEN' THEN 'Y' ELSE 'N' END <> COALESCE(TRGT.dormant_flag ,'');
