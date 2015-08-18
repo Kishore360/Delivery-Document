200859ffdd015003ead5d71d73811ca6969a8c40
Madhura_Patwardhan <madhura@numerify.com>
2015-08-18 10:26:18 +0530

    no message

---
+++ HOMEDEPOT/ITSA/f_incident_urgency_src_key_mysql.sql
@@ -0,0 +1,12 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,					
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.urgency_src_key' 					
+ELSE 'SUCCESS' END as Message					
+select COALESCE(LKP.row_key,CASE WHEN SRC.urgency IS NULL THEN 0 else '-1' end), COALESCE(TRGT.urgency_src_key,'')				
+ FROM homedepot_mdsdb.incident_final SRC 					
+ LEFT JOIN homedepot_mdwdb.f_incident TRGT 					
+ ON (SRC.sys_id =TRGT.row_id  					
+ AND SRC.sourceinstance= TRGT.source_id  )					
+LEFT JOIN homedepot_mdwdb.d_lov LKP 					
+ ON ( concat('URGENCY','~','TASK','~','~','~','A~',upper(urgency))= LKP.src_rowid 					
+AND SRC.sourceinstance= LKP.source_id )					
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.urgency IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.urgency_src_key,'')		
