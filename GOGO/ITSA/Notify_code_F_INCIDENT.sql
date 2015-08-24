39872ef8f781d03364f12c1233ebaed4fc495bde
bhavana jayaprakash <bhavana@numerify.com>
2015-08-24 14:43:21 +0530

    fixed gogo error

--- GOGO/ITSA/Notify_code_F_INCIDENT.sql
+++ GOGO/ITSA/Notify_code_F_INCIDENT.sql
@@ -1,5 +1,8 @@
-select s.sys_id, s.u_notify,i.row_id, i.notify_src_code_c 
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result,
+CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+from(select count(1) as cnt
 from gogo_mdsdb.incident_final s
 inner join gogo_mdwdb.f_incident i on s.sys_id =i.row_id 
 and s.sourceinstance = i.source_id
-where coalesce(s.u_notify ,' ')<>coalesce(i.notify_src_code_c ,' ')
+where coalesce(s.u_notify ,' ')<>coalesce(i.notify_src_code_c ,' '))a;
