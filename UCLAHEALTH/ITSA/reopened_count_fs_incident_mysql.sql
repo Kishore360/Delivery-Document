22d0a157a889cc10556b2e18cbcacb0d687ae358
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-01 07:28:34 +0530

    seperating staged files for testing

--- UCLAHEALTH/ITSA/reopened_count_fs_incident_mysql.sql
+++
@@ -1,8 +0,0 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM (
-select count(1) as cnt from uclahealth_mdsdb.incident_final 
-JOIN  
-  uclahealth_workdb.fs_incident B on sourceinstance=B.source_id AND B.ROW_ID=SYS_ID
-WHERE u_reopen_count<> B.reopened_count)E;
--- UCLAHEALTH/ITSA/room_c_ds_incident_mysql.sql
+++
@@ -1,7 +0,0 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message from (select count(1) as cnt 
-FROM uclahealth_mdsdb.incident_final A
- JOIN  
-  uclahealth_workdb.ds_incident B on sourceinstance=B.source_id AND B.ROW_ID=SYS_ID
-WHERE u_room<>B.room_c)E;
