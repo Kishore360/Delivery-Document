a5fa536e25b3be231a5fded99554f8880ec7eb5f
Prabhuram Sridharan <Prabhuram_Sridharan>
2015-09-02 13:30:27 +0000

    d_incident_dormant_flag_mysql.sql edited online with Bitbucket

--- MATS/MATS_FUNCTIONALITY/ITSA/d_incident_dormant_flag_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/d_incident_dormant_flag_mysql.sql
@@ -10,5 +10,5 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  
 LEFT JOIN <<tenant>>_mdwdb.d_lov_map LM ON TRGTF.state_src_key=LM.src_key
 LEFT JOIN <<tenant>>_mdwdb.d_o_data_freshness FRESH  ON(FRESH.source_id=SRC.sourceinstance and FRESH.etl_run_number=TRGTF.etl_run_number) 
- WHERE  CASE WHEN timestampdiff(DAY,TRGT.changed_on,FRESH.lastupdated)>30
+ WHERE  CASE WHEN timestampdiff(DAY,TRGT.changed_on,FRESH.lastupdated)>30 and  LM.dimension_class = 'STATE~INCIDENT'
 AND  LM.dimension_wh_code='OPEN' THEN 'Y' ELSE 'N' END <> COALESCE(TRGT.dormant_flag ,'')
