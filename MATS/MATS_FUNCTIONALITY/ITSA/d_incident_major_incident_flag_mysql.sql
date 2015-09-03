33745b6822267b4149034b759ff2de6283ecab86
bhavana jayaprakash <bhavana@numerify.com>
2015-09-03 17:07:53 +0530

    commiting changes

--- MATS/MATS_FUNCTIONALITY/ITSA/d_incident_major_incident_flag_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/d_incident_major_incident_flag_mysql.sql
@@ -1,15 +1,13 @@
 SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message FROM
-(select count(1) as cnt 
-from <<tenant>>_mdwdb.d_incident a
-join <<tenant>>_mdsdb.incident_final b
-on a.row_id=b.sys_id 
-
-and a.source_id=b.sourceinstance
- 
-where a.major_incident_flag<>case when b.u_major_incident=1 then 'Y' else 'N' end) a;
-
-
-
-
+ELSE 'Data Matched' END AS Message 
+FROM (
+select count(1) as cnt from(
+SELECT A.SYS_ID,B.ROW_ID,A.MAJOR_INCIDENT_FLAG_SRC A_MAJOR_INCIDENT_FLAG_SRC,
+B.MAJOR_INCIDENT_FLAG B_MAJOR_INCIDENT_FLAG_SRC FROM
+(SELECT SYS_ID,sourceinstance,CASE WHEN PRIORITY IN (1,0) THEN 'Y' else 'N'END AS MAJOR_INCIDENT_FLAG_SRC 
+FROM <<tenant>>_mdsdb.incident_final )A
+ JOIN  
+(SELECT MAJOR_INCIDENT_FLAG,source_id,ROW_ID FROM  <<tenant>>_mdwdb.d_incident
+ )B on A.sourceinstance=B.source_id AND B.ROW_ID=SYS_ID)H
+WHERE A_MAJOR_INCIDENT_FLAG_SRC<>B_MAJOR_INCIDENT_FLAG_SRC)E;
