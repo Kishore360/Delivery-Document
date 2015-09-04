a68ca1784cb67f1c26d0bf973ffcf07cddff83b0
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-04 16:27:00 +0530

    Committing Netflix CUST Queries

---
+++ NETFLIX/ITSA/d_problem_task_source_id_mysql.sql
@@ -0,0 +1,4 @@
+
+SELECT CASE WHEN count(1) = 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) =0 THEN 'MDS to DWH data validation failed for d_problem_task.source_id' ELSE 'SUCCESS' END as Message
+
---
+++ NETFLIX/ITSA/f_problem_task_category_src_key_mysql.sql
@@ -0,0 +1,4 @@
+
+SELECT CASE WHEN count(1) = 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) =0 THEN 'MDS to DWH data validation failed for f_problem_task.category_src_key' ELSE 'SUCCESS' END as Message
+
