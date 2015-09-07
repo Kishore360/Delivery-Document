f20742f102954ff97886c40a2648e3e2c58af6ee
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-07 14:31:07 +0530

    Adding to test only incident in STARWOOD

--- MATS/MATS_FUNCTIONALITY/ITSA/f_incident_change_request_key_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/f_incident_change_request_key_mysql.sql
@@ -1,12 +1,44 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_incident.active_flag' ELSE 'SUCCESS' END as Message
+ FROM
+(
+select count(1) as cnt from
+(SELECT SRC.sys_id,TRGT.row_id, COALESCE(LKP.row_key,CASE WHEN SRC.rfc IS NULL THEN 0 else '-1' end)abc, COALESCE(TRGT.change_request_key,'')def
 
 
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.change_request_key' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.incident_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.f_incident TRGT 
+
+
+FROM  starwood_mdsdb.task_final a
+inner join starwood_mdsdb.incident_final SRC 
+on a.sys_id=SRC.sys_id
+inner join starwood_mdsdb.cmdb_ci_service_final d
+on a.u_service=d.sys_id 
+inner  JOIN  starwood_mdsdb.request_category_final b
+on b.sys_id=SRC.u_request_category   
+LEFT JOIN starwood_mdwdb.f_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- LEFT JOIN <<tenant>>_mdwdb.d_change_request LKP 
+
+LEFT JOIN starwood_mdwdb.d_change_request LKP 
  ON ( SRC.rfc= LKP.row_id 
 AND SRC.sourceinstance= LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.rfc IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.change_request_key,'')
+ 
+-- LEFT JOIN starwood_mdwdb.d_calendar_date LKP 
+-- on (LKP.row_id = date_format(convert_tz(SRC.closed_at,"GMT","America/New_York"),'%Y%m%d')  and LKP.source_id=0)
+where 
+d.name IN ('Booking.com','Central 
+Reservation','DirectConnect','EZYield','HBSi','Hotwire','Orbitz','Priceline/Travelweb','PRSnet','Rational Dynamic Pricing','Saratoga',
+'Valhalla','Valhalla Availability','Valhalla Booking Engine','Valhalla Data Quality Engine(DQE)','Valhalla Inventory',
+'Valhalla Availability','Wholesaler Direct Connect (WDC)','Agoda','ISAC - Integrated Sales & Catering')
+OR 
+ d.name like 
+('Branded%') 
+OR d.name like ('Expedia%') 
+OR d.name like ('IPS%') 
+OR d.name like ('Property Mgt%') 
+OR d.name like ('Revenue Management%')
+OR d.name like ('Opera-PMS%') 
+  OR b.u_name  IN ('Valhalla','Central Reservation Systems')  
+)abc
+  where abc<>def
+)i
