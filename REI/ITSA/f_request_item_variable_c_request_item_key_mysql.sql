839edd7083e7c0e4886d023c89c2d730ab8d1680
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-07 17:31:33 +0530

    Adding the long running queries after index applied for REI

---
+++ REI/ITSA/f_request_item_variable_c_request_item_key_mysql.sql
@@ -0,0 +1,25 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+FROM(select count(1) as cnt
+from rei_mdsdb.sc_item_option_mtom_final a
+
+join rei_mdsdb.sc_item_option_final d
+
+on a.sc_item_option=d.sys_id
+
+and a.sourceinstance=d.sourceinstance
+ 
+left join rei_mdwdb.f_request_item_variable_c b
+
+on d.sys_id=b.row_id and
+
+d.sourceinstance=b.source_id
+
+left join rei_mdwdb.d_request_item c
+
+on c.row_id=a.request_item
+
+where c.row_key<>b.request_item_key
+
+and d.item_option_new='2676a6f90a0a3c1f00824b465e408ed0') x;
