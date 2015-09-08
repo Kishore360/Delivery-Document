7d732c71c8d283c58eef7c27a538bda5b22056c7
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-08 21:20:52 +0530

    Pushing the UCLA Cust Modifications

--- UCLAHEALTH/ITSA/d_incident_last_resolved_on_mysql.sql
+++ UCLAHEALTH/ITSA/d_incident_last_resolved_on_mysql.sql
@@ -13,7 +13,7 @@ DATE_FORMAT(CONVERT_TZ(u_resolved_date,'GMT','America/Los_Angeles'),'%Y')  AS   
  AS last_resolved_on
 FROM uclahealth_mdsdb.incident_final )A
 JOIN  
-(SELECT DATE_FORMAT(last_resolved_on,'%Y%m%d%h%m%s%p') last_resolved_on ,source_id,ROW_ID FROM
+(SELECT DATE_FORMAT(last_resolved_on,'%Y%m%d%h%m%s') last_resolved_on ,source_id,ROW_ID FROM
 uclahealth_mdwdb.d_incident
  )B on A.sourceinstance=B.source_id AND B.ROW_ID=SYS_ID)H
 WHERE A_last_resolved_on<>B_last_resolved_on  )E
--- UCLAHEALTH/ITSA/f_incident_parent_incident_key_mysql.sql
+++ UCLAHEALTH/ITSA/f_incident_parent_incident_key_mysql.sql
@@ -5,6 +5,6 @@ FROM (
 select count(1) as cnt from
 uclahealth_mdsdb.incident_final x LEFT join uclahealth_mdwdb.d_incident y on
 x.u_parent_ticket=y.row_id
-left join uclahealth_mdwdb.f_incident B on y.source_id=B.source_id AND B.ROW_ID=y.ROW_ID
+left join uclahealth_mdwdb.f_incident B on x.sourceinstance=B.source_id AND B.ROW_ID=x.sys_id
 WHERE y.row_key<> B.parent_incident_key
 )E;
