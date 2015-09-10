1d4c6f78630afc3b9d413f0535a4ac948908b186
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-10 11:27:19 +0530

    Modified the query in MATS

--- MATS/MATS_FUNCTIONALITY/ITSA/f_change_request_actual_duration_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/f_change_request_actual_duration_mysql.sql
@@ -1,8 +1,8 @@
 select CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.actual_duration' ELSE 'SUCCESS' END as Message
-     from weillcornell_mdsdb.change_request_final SRC left join
-        weillcornell_mdwdb.f_change_request fi on SRC.sys_id=fi.row_id and SRC.sourceinstance=fi.source_id
-		    left join weillcornell_mdwdb.d_lov_map lm 
+     from <<tenant>>_mdsdb.change_request_final SRC left join
+        <<tenant>>_mdwdb.f_change_request fi on SRC.sys_id=fi.row_id and SRC.sourceinstance=fi.source_id
+		    left join <<tenant>>_mdwdb.d_lov_map lm 
 		ON (lm.src_key = fi.state_src_key) and lm.dimension_wh_code in ('OPEN') 
      
 	where case when lm.dimension_wh_code in ('OPEN') 
