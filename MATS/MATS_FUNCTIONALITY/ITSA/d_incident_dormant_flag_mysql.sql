b48a4b740625786a491eacd9cad8fea25faa79a3
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-12 09:57:37 +0530

    Modifying query changes

--- MATS/MATS_FUNCTIONALITY/ITSA/d_incident_dormant_flag_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/d_incident_dormant_flag_mysql.sql
@@ -1,16 +1,11 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.dormant_flag' ELSE 'SUCCESS' END as Message
- 
+ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_incident.dormant_flag' ELSE 'SUCCESS' END as Message from (
+ select count(1) as cnt 
   FROM 
-usf_mdwdb.d_incident di
-  JOIN usf_mdwdb.f_incident fi ON di.row_key = fi.incident_key
-  JOIN usf_mdwdb.d_lov_map dlm ON fi.state_src_key = dlm.src_key
-  JOIN (
-                                                SELECT lastupdated, 
-                                                                   source_id
-                                                  FROM usf_mdwdb.d_o_data_freshness
-                                                WHERE etl_run_number in (select max(etl_run_number) from usf_mdwdb.d_o_data_freshness)
-                ) df ON di.source_id = df.source_id
+<<tenant>>_mdwdb.d_incident di
+  JOIN <<tenant>>_mdwdb.f_incident fi ON di.row_key = fi.incident_key
+  JOIN <<tenant>>_mdwdb.d_lov_map dlm ON fi.state_src_key = dlm.src_key
+  JOIN  <<tenant>>_mdwdb.d_o_data_freshness FRESH on fi.etl_run_number=FRESH.etl_run_number										
   where dlm.dimension_class = 'STATE~INCIDENT'
   AND dlm.dimension_wh_code = 'OPEN'
-  AND (CASE WHEN timestampdiff(DAY,di.changed_on, df.lastupdated)>30 THEN 'Y' ELSE 'N' END) <> di.dormant_flag;
+  AND  (CASE WHEN timestampdiff(DAY,di.changed_on, FRESH.lastupdated)>30 THEN 'Y' ELSE 'N' END) <> di.dormant_flag)a;
--- MATS/MATS_FUNCTIONALITY/ITSA/f_incident_category_src_key_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/f_incident_category_src_key_mysql.sql
@@ -1,12 +1,14 @@
 
 
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.category_src_key' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.incident_final SRC 
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.category_src_key' ELSE 'SUCCESS' END as Message from (
+select count(1) as cnt   
+FROM <<tenant>>_mdsdb.incident_final SRC 
+LEFT JOIN <<tenant>>_mdwdb.d_lov LKP 
+ ON LKP.dimension_class like '%CATEGORY~INCIDENT%' and
+  concat('CATEGORY~INCIDENT~~~',upper(category ))=  LKP.src_rowid 
+AND SRC.sourceinstance= LKP.source_id 
  LEFT JOIN <<tenant>>_mdwdb.f_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
-LEFT JOIN <<tenant>>_mdwdb.d_lov LKP 
- ON (coalesce( concat('CATEGORY','~','INCIDENT','~','~','~',upper(category )),'-99')= coalesce(LKP.src_rowid,'-99') 
-AND SRC.sourceinstance= LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.category IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.category_src_key,'')
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.category IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.category_src_key,''))a
