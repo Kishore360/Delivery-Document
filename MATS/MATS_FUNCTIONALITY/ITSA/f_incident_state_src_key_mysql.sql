5862c6b914a6335a5d22afd2693ec2c234600f2e
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-12 10:54:04 +0530

    modifying changes

--- MATS/MATS_FUNCTIONALITY/ITSA/f_incident_state_src_key_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/f_incident_state_src_key_mysql.sql
@@ -7,10 +7,7 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
 LEFT JOIN <<tenant>>_mdwdb.d_lov LKP 
-<<<<<<< HEAD
- ON ( concat('STATE','~','INCIDENT','~','~','~',upper(SRC.state))= LKP.src_rowid 
-=======
- ON ( concat('STATE','~','INCIDENT','~','~','~',upper(state))= LKP.src_rowid 
->>>>>>> origin/master
+ ON  LKP.dimension_class like '%STATE~INCIDENT%' and
+( concat('STATE~INCIDENT~~~',upper(SRC.state))= LKP.src_rowid 
 AND SRC.sourceinstance= LKP.source_id )
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.state IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.state_src_key,'')
