13129de494049ca4c495a8229ed8e9514b74dce7
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-14 17:35:11 +0530

    modified the query in nmats

--- MATS/MATS_FUNCTIONALITY/ITSA/d_request_task_over_due_flag_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/d_request_task_over_due_flag_mysql.sql
@@ -9,8 +9,8 @@ select
 COALESCE( CASE WHEN TRGT.active_flag = 'Y' and TRGT.due_on < FRESH.lastupdated then 'Y' else 'N' END,''),
  COALESCE(TRGT.over_due_flag ,'')
 from
-svb_mdwdb.d_request_task TRGT
-LEFT JOIN svb_mdwdb.d_o_data_freshness FRESH  ON(FRESH.source_id=TRGT.source_id
+<<tenant>>_mdwdb.d_request_task TRGT
+LEFT JOIN <<tenant>>_mdwdb.d_o_data_freshness FRESH  ON(FRESH.source_id=TRGT.source_id
 and TRGT.etl_run_number=FRESH.etl_run_number) 
 WHERE due_on is not null and 
 COALESCE( CASE WHEN TRGT.active_flag = 'Y' and TRGT.due_on < FRESH.lastupdated then 'Y' else 'N' END,'')<>
