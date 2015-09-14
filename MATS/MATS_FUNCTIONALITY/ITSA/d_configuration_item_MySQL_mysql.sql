0d2a7178769d92fa064f946039dfb46fbe7cfd33
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-15 01:25:07 +0530

    Pushing back after testing the testing of 426 queries

--- MATS/MATS_FUNCTIONALITY/ITSA/d_configuration_item_MySQL_mysql.sql
+++
@@ -1,4 +0,0 @@
-
-
-
-SELECT COUNT(1) FROM <<tenant>>_mdwdb.d_configuration_item 
---
+++ MATS/MATS_FUNCTIONALITY/ITSA/d_request_multiple_assignment_flag_mysql.sql
@@ -0,0 +1,9 @@
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_request.multiple_assignment_flag' ELSE 'SUCCESS' END as Message
+ FROM <<tenant>>_mdsdb.sc_request_final SRC 
+ LEFT JOIN <<tenant>>_mdwdb.d_request TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+ WHERE COALESCE( CASE WHEN SRC.reassignment_count >= 1 then 'Y' else 'N' END,'')<> COALESCE(TRGT.multiple_assignment_flag ,'')
