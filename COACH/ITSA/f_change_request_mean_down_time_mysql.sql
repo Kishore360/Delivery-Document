3d6054f0870877b4c105b08177f7d0fcd902aab3
veena.k <veena.karkala@numerify.com>
2015-09-15 12:52:29 +0530

    updated the couch - cust queries

--- COACH/ITSA/f_change_request_mean_down_time_mysql.sql
+++ COACH/ITSA/f_change_request_mean_down_time_mysql.sql
@@ -2,7 +2,7 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 FROM (
-select b.mean_down_time_c , timestampdiff(SECOND,a.u_outage_start,a.u_outage_end)  as x
+select count(1) as cnt
 from coach_mdsdb.change_request_final a
 left  JOIN  coach_mdwdb.f_change_request b
 on  a.SYS_ID= b.ROW_ID and a.sourceinstance=b.source_id
