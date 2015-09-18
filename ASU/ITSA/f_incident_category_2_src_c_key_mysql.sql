df56eae4269cfd871788968aeac96da88594be86
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-18 12:49:15 +0530

    Modified d_calendar_date related queries

--- ASU/ITSA/f_incident_category_2_src_c_key_mysql.sql
+++ ASU/ITSA/f_incident_category_2_src_c_key_mysql.sql
@@ -3,10 +3,10 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 FROM (
-select count(1) cnt from asu_mdsdb.incident_final a
- left join asu_mdwdb.d_lov b on 
-COALESCE(CONCAT('CATEGORY~INCIDENT~~~',a.u_category_2),'UNSPECIFIED)'=b.src_rowid
-left join asu_mdwdb.f_incident c on a.sourceinstance=c.source_id and a.sys_id=c.row_id
+select count(1) cnt from <<tenant>>_mdsdb.incident_final a
+ left join <<tenant>>_mdwdb.d_lov b on 
+COALESCE(CONCAT('U_CATEGORY~INCIDENT~~~',a.u_category_2),'UNSPECIFIED')=b.row_id
+left join <<tenant>>_mdwdb.f_incident c on a.sourceinstance=c.source_id and a.sys_id=c.row_id
 where COALESCE(b.row_key,-1)<>c.category_2_src_c_key
 
 )i; 
--- ASU/ITSA/f_incident_category_src_c_key_mysql.sql
+++ ASU/ITSA/f_incident_category_src_c_key_mysql.sql
@@ -5,7 +5,7 @@ ELSE 'Data Matched' END AS Message
 FROM (
 select count(1) cnt from asu_mdsdb.incident_final a
  left join asu_mdwdb.d_lov b on 
-COALESCE(CONCAT('CATEGORY~INCIDENT~~~',a.u_category),'UNSPECIFIED')=b.src_rowid
+COALESCE(CONCAT('U_CATEGORY~INCIDENT~~~',a.u_category),'UNSPECIFIED')=b.src_rowid
 left join asu_mdwdb.f_incident c on a.sourceinstance=c.source_id and a.sys_id=c.row_id
 where COALESCE(b.row_key,-1)<>c.category_src_c_key
 
