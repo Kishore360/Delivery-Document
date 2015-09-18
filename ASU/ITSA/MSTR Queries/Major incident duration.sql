3d65ad3aa096be8e4c22b2841d3cf5131e4d2416
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-18 19:06:15 +0530

    no message

---
+++ ASU/ITSA/MSTR Queries/Major incident duration.sql
@@ -0,0 +1,7 @@
+Select sum(a11.major_incident_duration_c) from ldb.f_incident a11 
+join	ldb.d_calendar_date	A14
+	  on 	(a11.last_resolved_on_key = a14.row_key)
+	  join	ldb.d_calendar_month	a15
+	  on 	(a14.month_start_date_key = a15.row_key)
+where	a15.lagging_count_of_month between 0 and 11 
+group by 1;
