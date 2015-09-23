947f1b73ccdb85ca5c852f8e02b37a10eed5ed12
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-23 16:04:06 +0530

    prodverificationofunknowns

--- MATS/CUBE_MSTR/ITSA/UnknownUNSPECIFIED_mysql.sql
+++ MATS/CUBE_MSTR/ITSA/UnknownUNSPECIFIED_mysql.sql
@@ -6,15 +6,16 @@ SELECT concat(
                  FROM (',group_concat('SELECT ''',cols.table_name,''' as tName,', cols.column_name,' as Cname ,', 'count(*) as totalrecs, COUNT(CASE WHEN ', cols.column_name,' = ''-1'' THEN 1 ELSE NULL END) as UNKNOWN, COUNT(CASE WHEN ', cols.column_name,' = ''0'' THEN 1 ELSE NULL END) as UNSPECIFIED ' ' FROM ',cols.table_schema,'.',cols.table_name separator ' UNION ALL '),')a 
                  WHERE UNKNOWN>0)b') INTO @sql
 FROM information_schema.COLUMNS cols
-INNER JOIN information_schema.TABLES tbl ON cols.TABLE_SCHEMA = '<<tenant>>_mdwdb'
+INNER JOIN information_schema.TABLES tbl ON  cols.table_name = tbl.TABLE_NAME
+AND cols.TABLE_SCHEMA=tbl.table_schema
+where tbl.TABLE_ROWS>2
+AND cols.table_name LIKE 'f\_%' 
 AND cols.column_name LIKE '%\_key'
 AND cols.column_name NOT LIKE 'row\_%key'
-AND cols.table_name = tbl.TABLE_NAME
-AND cols.TABLE_SCHEMA=tbl.table_schema
-AND tbl.TABLE_ROWS>2
 AND tbl.TABLE_TYPE = 'BASE TABLE'
-AND cols.table_name LIKE 'f\_%';
+AND cols.TABLE_SCHEMA = '<<tenant>>_mdwdb';
 
 PREPARE stmt FROM @sql;
 EXECUTE stmt;
 DEALLOCATE PREPARE stmt;
+
