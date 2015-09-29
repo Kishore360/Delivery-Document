a8965197d7536e13ed343155d4323282803f94db
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-29 22:57:29 +0530

    Replacing back the older queries

---
+++ MATS/MATS_FUNCTIONALITY/ITSA/d_incident_over_due_flag_mysql.sql
@@ -0,0 +1,14 @@
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.over_due_flag' ELSE 'SUCCESS' END as Message from (
+ select COALESCE( CASE WHEN SRC.active = 1 and due_date < FRESH.lastupdated then 'Y' else 'N' END,'')abc, COALESCE(TRGT.over_due_flag ,'')def
+ FROM <<tenant>>_mdsdb.incident_final SRC 
+LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+ LEFT JOIN <<tenant>>_mdwdb.d_o_data_freshness FRESH  ON(FRESH.source_id=TRGT.source_id  and FRESH.etl_run_number=TRGT.etl_run_number) 
+WHERE due_date is not null
+ )a
+ WHERE abc<>def
+
