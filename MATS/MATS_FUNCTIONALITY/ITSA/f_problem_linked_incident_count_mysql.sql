71ac7ec5ed74bee7514455d7a12905a250401d21
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-29 08:29:41 +0530

    pushing back the MATS Queries

---
+++ MATS/MATS_FUNCTIONALITY/ITSA/f_problem_linked_incident_count_mysql.sql
@@ -0,0 +1,9 @@
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_problem.linked_incident_count' ELSE 'SUCCESS' END as Message
+ FROM <<tenant>>_mdsdb.problem_final SRC 
+ LEFT JOIN <<tenant>>_mdwdb.f_problem TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+ WHERE COALESCE( SRC.related_incidents,'')<> COALESCE(TRGT.linked_incident_count ,'')
---
+++ MATS/MATS_FUNCTIONALITY/ITSA/f_request_task_open_to_close_duration_mysql.sql
@@ -0,0 +1,9 @@
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_task.open_to_close_duration' ELSE 'SUCCESS' END as Message
+ FROM <<tenant>>_mdsdb.sc_task_final SRC
+ LEFT JOIN <<tenant>>_mdwdb.f_request_task TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+WHERE  TIMESTAMPDIFF(second,SRC.opened_at,SRC.closed_at)<>  COALESCE(TRGT.open_to_close_duration,'') 
