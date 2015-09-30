5fa7f07e4d48f4ffcb9b12d06b1b0cb472362940
prabhuram <prabhuram.sridharan@numerify.com>
2015-09-30 22:42:20 +0530

    testing the new run queries

--- MATS/MATS_FUNCTIONALITY/ITSA/d_problem_change_flag_mysql.sql
+++
@@ -1,9 +0,0 @@
-
-
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.change_flag' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.problem_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.d_problem TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( CASE WHEN rfc is not null then 'Y' else 'N' END,'')<> COALESCE(TRGT.change_flag ,'')
--- MATS/MATS_FUNCTIONALITY/ITSA/f_request_item_opened_by_department_key_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/f_request_item_opened_by_department_key_mysql.sql
@@ -1,15 +1,7 @@
  SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_item.opened_by_department_key' ELSE 'SUCCESS' END as Message
- from 
-  <<tenant>>_mdsdb.sc_req_item_final SRC 
-left join  <<tenant>>_mdsdb.sys_user_final scu on SRC.opened_by = scu.sys_id
- LEFT JOIN <<tenant>>_mdwdb.f_request_item TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
-left join <<tenant>>_mdwdb.d_internal_organization LKP
-on  LKP.row_key =  TRGT.opened_by_department_key AND LKP.source_id =  TRGT.source_id 
- WHERE COALESCE(LKP.row_key ,CASE WHEN scu.department is  null 
- THEN 0 else '-1' end)<> COALESCE(TRGT.opened_by_department_key,'')
-
-
-
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_request.priority_escalated_flag' ELSE 'SUCCESS' END as Message from
+<<tenant>>_mdsdb.sc_req_item_final SRC
+left join <<tenant>>_mdsdb.sys_user_final scu on SRC.opened_by = scu.sys_id
+LEFT JOIN <<tenant>>_mdwdb.f_request_item TRGT ON SRC.sys_id=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id
+left join <<tenant>>_mdwdb.d_internal_organization LKP on LKP.row_id =concat('DEPARTMENT~',scu.department) 
+WHERE COALESCE(LKP.row_key ,CASE WHEN scu.department is null THEN 0 else '-1' end)<> COALESCE(TRGT.opened_by_department_key,'')
