d90cab19c2cccd956441b4064460a8b7f96c88fb
prabhuram <prabhuram.sridharan@numerify.com>
2015-10-01 01:05:10 +0530

    placing back the queries

--- MATS/MATS_FUNCTIONALITY/ITSA/d_configuration_item_category_mysql.sql
+++
@@ -1,8 +0,0 @@
-
-SELECT  CASE WHEN COUNT(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result,
-CASE WHEN COUNT(1) > 0 THEN 'MDS to DWH data validation failed between cmdb_ci_final and d_configuration_item' ELSE 'SUCCESS' END AS Message
-FROM <<tenant>>_mdsdb.cmdb_ci_final S
-
-left join  <<tenant>>_mdwdb.d_configuration_item t
- on S.sys_id = t.row_id and S.sourceinstance =t.source_id
-where coalesce(t.category,'') <> coalesce(S.category,'');
