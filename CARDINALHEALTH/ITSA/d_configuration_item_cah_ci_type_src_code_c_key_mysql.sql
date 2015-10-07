f1878cfb2afcb1d94d52ba7409b1c9d0aed87eff
Shubha <Shubha.rao@numerify.com>
2015-10-07 12:19:30 +0530

    Updating cust queries for cardinal

--- CARDINALHEALTH/ITSA/d_configuration_item_cah_ci_type_src_code_c_key_mysql.sql
+++ CARDINALHEALTH/ITSA/d_configuration_item_cah_ci_type_src_code_c_key_mysql.sql
@@ -7,8 +7,5 @@ on a.row_id = b.sys_id AND a.source_id = b.sourceinstance
 LEFT OUTER JOIN cardinalhealth_mdwdb.d_lov c 
 ON c.row_id = CASE WHEN b.u_ci_type IS NULL THEN 'UNSPECIFIED' ELSE UPPER(CONCAT('U_CI_TYPE~CMDB_CI','~','','~','','~',COALESCE(b.u_ci_type,''))) END 
 AND c.source_id = CASE WHEN b.u_ci_type IS NULL THEN 0 ELSE b.sourceinstance END 
-<<<<<<< HEAD
-where a.cah_ci_type_src_code_c_key <> (c.row_key); 
-=======
-where a.cah_ci_type_src_code_c_key <> COALESCE(c.row_key,''); 
->>>>>>> b24a4dfda5506de5959fa3ef85cf0d2c8e75a5ef
+
+where a.cah_ci_type_src_code_c_key <> (c.row_key);
