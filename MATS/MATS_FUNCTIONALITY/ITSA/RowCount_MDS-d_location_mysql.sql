64e557e3d7eed6ee519d83aeb8b665129bbfd909
veena.k <veena.karkala@numerify.com>
2015-10-07 17:20:18 +0530

    syntax errors in MATS fixed

--- MATS/MATS_FUNCTIONALITY/ITSA/RowCount_MDS-d_location_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/RowCount_MDS-d_location_mysql.sql
@@ -7,6 +7,6 @@ SELECT CASE WHEN  (count_mds = count_dwh) THEN 'SUCCESS' ELSE 'FAILURE' END AS R
 ,CASE WHEN (count_mds =count_dwh) THEN 'Data Matched'
 ELSE CONCAT( 'MDS-DWH RowCount did not Match for ', DWH.Table_DWH , '. MDS:',count_mds,'DWH : ' ,DWH.Count_DWH)
 END AS Message
-FROM (SELECT (SELECT COUNT(1) FROM <<tenant>>_mdsdb.cmn_location_final) + AS count_mds) MDS,
+FROM (SELECT (SELECT COUNT(1) FROM <<tenant>>_mdsdb.cmn_location_final)  AS count_mds) MDS,
 (SELECT COUNT(1) Count_DWH, 'd_location'  Table_DWH 
 FROM <<tenant>>_mdwdb.d_location WHERE row_key not in (0,-1)) DWH
