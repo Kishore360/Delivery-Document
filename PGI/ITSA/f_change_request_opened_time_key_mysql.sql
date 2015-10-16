9b3eb34dc8881499f8fd7b86e2c914f64ee31b27
vijay rao <vijay.rao@numerify.com>
2015-10-16 15:25:49 +0530

    Putting the f_change_request query to PGI Cust

--- PGI/ITSA/f_change_request_opened_time_key_mysql.sql
+++ PGI/ITSA/f_change_request_opened_time_key_mysql.sql
@@ -7,7 +7,7 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
  LEFT JOIN <<tenant>>_mdwdb.d_calendar_time LKP 
-on (LKP.row_id = COALESCE(date_format(convert_tz(SRC.opened_at,<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>),'%H%i') ,'UNSPECIFIED') and LKP.source_id=0
+on (LKP.row_id = COALESCE(date_format(convert_tz(SRC.sys_created_on,<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>),'%H%i') ,'UNSPECIFIED') and LKP.source_id=0
 
  
 )
