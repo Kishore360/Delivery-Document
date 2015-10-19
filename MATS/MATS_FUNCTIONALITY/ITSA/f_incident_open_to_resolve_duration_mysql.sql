f91cc3524d38d4204f481a190a1d8f14ccaba8b4
prabhuram <prabhuram.sridharan@numerify.com>
2015-10-19 12:07:56 +0530

    Committing query change

--- MATS/MATS_FUNCTIONALITY/ITSA/f_incident_open_to_resolve_duration_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/f_incident_open_to_resolve_duration_mysql.sql
@@ -1,9 +1,10 @@
-
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.open_to_resolve_duration' ELSE 'SUCCESS' END as Message
  FROM <<tenant>>_mdsdb.incident_final SRC
  LEFT JOIN <<tenant>>_mdwdb.f_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
-WHERE  TIMESTAMPDIFF(second, SRC.opened_at, SRC.resolved_at)<>  COALESCE(TRGT.open_to_resolve_duration,'') 
+ LEFT JOIN <<tenant>>_mdwdb.d_lov_map br 
+ ON TRGT.state_src_key = br.src_key
+WHERE  case when br.dimension_wh_code IN ('OPEN') THEN NULL
+ELSE TIMESTAMPDIFF(SECOND,SRC.opened_at,SRC.resolved_at) END <>  COALESCE(TRGT.open_to_resolve_duration,'') 
