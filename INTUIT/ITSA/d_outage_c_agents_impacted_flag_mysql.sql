982e278e5249a6262f67aa52fb45d95f6cb55b48
veena.k <veena.karkala@numerify.com>
2015-10-21 12:21:43 +0530

    Syntax errors corrected

--- INTUIT/ITSA/d_outage_c_agents_impacted_flag_mysql.sql
+++ INTUIT/ITSA/d_outage_c_agents_impacted_flag_mysql.sql
@@ -5,9 +5,5 @@ FROM (
 select count(1) as cnt
 from intuit_mdsdb.cmdb_ci_outage_final i
 LEFT JOIN intuit_mdsdb.incident_final inf ON i.task_number=inf.sys_id
-left join intuit_mdwdb.d_outage_c t
-on i.sys_id=t.row_id and i.sourceinstance = t.source_id
-WHERE t.agents_impacted_flag <> CASE
-   WHEN i.u_agents_impacted = 1 THEN 'Y'
-   ELSE 'N'
-  END AS agents_impacted_flag) temp 
+left join intuit_mdwdb.d_outage_c t on i.sys_id=t.row_id and i.sourceinstance = t.source_id
+WHERE (t.agents_impacted_flag) <> CASE WHEN i.u_agents_impacted = 1 THEN 'Y'  ELSE 'N' END  )temp
