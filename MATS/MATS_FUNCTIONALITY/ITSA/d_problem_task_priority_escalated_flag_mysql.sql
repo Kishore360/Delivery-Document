4d1ee883898c881a767e5b5f9f1bed72d9a5b44d
veena.k <veena.karkala@numerify.com>
2015-10-29 14:16:40 +0530

    MATS query corrected

--- MATS/MATS_FUNCTIONALITY/ITSA/d_problem_task_priority_escalated_flag_mysql.sql
+++ MATS/MATS_FUNCTIONALITY/ITSA/d_problem_task_priority_escalated_flag_mysql.sql
@@ -1,19 +1,11 @@
-
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem_task.priority_escalated_flag' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.problem_task_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.d_problem_task TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- JOIN <<tenant>>_mdwdb.f_t_task_activity TA 
- ON (SRC.sys_id =TA.task_row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- AND TA.task_wh_type = 'problem_task' 
-AND TA.task_attribute_wh_name =  'priority' 
-and TA.task_attribute_wh_old_value IS NOT NULL 
-AND TA.task_attribute_wh_new_value IS NOT NULL
- WHERE  CASE WHEN 
-  TA.task_attribute_wh_new_value < TA.task_attribute_wh_old_value 
-THEN 'Y' ELSE 'N' END
-<> COALESCE(TRGT.priority_escalated_flag,'')
+FROM <<tenant>>_mdsdb.problem_task_final SRC
+LEFT JOIN <<tenant>>_mdwdb.d_problem_task TRGT
+ ON SRC.sys_id =TRGT.row_id    AND SRC.sourceinstance= TRGT.source_id
+LEFT JOIN <<tenant>>_mdsdb.sys_audit_final TA 
+ON SRC.sys_id =TA.documentkey AND SRC.sourceinstance= TA.sourceinstance
+AND  TABLENAME = 'problem_task' AND FIELDNAME = 'priority'
+WHERE CASE   WHEN TA.NEWVALUE < TA.OLDVALUE THEN 'Y'
+          ELSE 'N'
+      END <> COALESCE(TRGT.priority_escalated_flag,'');
