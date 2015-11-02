dcf318e90da48f4444dd7b63cc7cbb3c7173e6bf
veena.k <veena.karkala@numerify.com>
2015-11-02 15:32:58 +0530

    Intuit Cust queries corrected

--- INTUIT/ITSA/d_problem_task_upcoming_over_due_c_flag_mysql.sql
+++ INTUIT/ITSA/d_problem_task_upcoming_over_due_c_flag_mysql.sql
@@ -5,7 +5,7 @@ FROM (
 select count(1) as cnt 
 from intuit_mdwdb.d_problem_task a 
  inner join intuit_mdwdb.f_problem_task b on a.row_id=b.row_id and a.source_id=b.source_id 
- left join intuit_mdwdb.o_data_freshness c on a.source_id = c.source_id
+ left join intuit_mdwdb.d_o_data_freshness c on a.source_id = c.source_id and a.etl_run_number = c.etl_run_number
 where a.upcoming_over_due_c_flag <>
-(case when a.due_on between c.lastupdated and date_add(c.lastupdated,INTERVAL 7 DAY) then 'Y' else  'N' end)
- and  b.state_src_code not in(3,4,7) ) temp
+case when a.due_on between c.lastupdated and date_add(c.lastupdated,INTERVAL 7 DAY)  and  b.state_src_code not in(3,4,7) then 'Y' else  'N' end
+  )temp
