e74fbdce5f37793355791a6bb2032028f17d39ca
prabhuram <prabhuram.sridharan@numerify.com>
2015-11-03 17:55:21 +0530

    Removed Unspecified/Unknown for verifying the query again

--- SVB/ITSA/f_incident_state_src_key_mysql.sql
+++ SVB/ITSA/f_incident_state_src_key_mysql.sql
@@ -2,12 +2,12 @@
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.state_src_key' ELSE 'SUCCESS' END as Message
- FROM <<tenant>>_mdsdb.incident_final SRC
-LEFT JOIN <<tenant>>_mdwdb.d_lov LKP 
+ FROM svb_mdsdb.incident_final SRC
+LEFT JOIN svb_mdwdb.d_lov LKP 
  ON LKP.dimension_class like '%STATE~INCIDENT%' and 
 ( concat('STATE~INCIDENT~~~',upper(incident_state))= LKP.src_rowid 
 AND SRC.sourceinstance= LKP.source_id ) 
- LEFT JOIN <<tenant>>_mdwdb.f_incident TRGT 
+ LEFT JOIN svb_mdwdb.f_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.state IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.state_src_key,'')
