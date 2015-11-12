3d15a01182906f9038a32cb69970fc07ae602c34
prabhuram <prabhuram.sridharan@numerify.com>
2015-11-12 23:47:47 +0530

    Verified the changes

--- INTUIT/ITSA/Request_Task_redshift.sql
+++ INTUIT/ITSA/Request_Task_redshift.sql
@@ -3,7 +3,7 @@ SELECT CASE WHEN max_count<>min_count THEN 'FAILURE' ELSE 'SUCCESS' END as Resul
  select max(Row_Count) max_count,Min(Row_Count) min_count from (
 select'ldb.f_request_task' as Table_Name, count(a11.row_key) Row_Count
  from  ldb.f_request_task             a11 
-union
+
 /*select'ldb.d_task_contacttype' as Table_Name, count(a11.row_key) Row_Count
  from  ldb.f_request_task             a11 
 cross join ldb.d_task_contacttype         a12
