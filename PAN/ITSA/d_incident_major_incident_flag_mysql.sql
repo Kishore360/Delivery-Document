e362468be6ac10b58527c122c445fd98e7c339cf
vijay rao <vijay.rao@numerify.com>
2015-11-17 11:09:03 +0530

    PAN MAP attributes test queries

--- PAN/ITSA/d_incident_major_incident_flag_mysql.sql
+++ PAN/ITSA/d_incident_major_incident_flag_mysql.sql
@@ -1,10 +1,10 @@
 
-SELECT CaSE WHEN cnt > 0 THEN 'FaILURE' ELSE 'SUCCESS' END aS Result
-,CaSE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END aS Message 
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
 FROM (
-select count(1) as cnt  from pan_mdwdb. d_incident b
- inner  JOIN  pan_mdsdb.incident_final a
-on  b.ROW_ID=SYS_ID and sourceinstance=b.source_id
- where case when a. u_major_incident=1 then 'Y' else 'N' end
-<> b. major_incident_flag)c
+select count(1) as cnt
+from pan_mdsdb.incident_final a
+left join pan_mdwdb.d_incident c 
+on  a.sys_id = c.ROW_ID and a.sourceinstance=c.source_id
+where c.major_incident_flag <> CASE WHEN a.u_major_incident=1 THEN 'Y' ELSE 'N'END) g
