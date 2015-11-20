3bf96f29cbf5598c31d481cf562fc4ac15fa5069
veena.k <veena.karkala@numerify.com>
2015-11-20 15:56:08 +0530

    REI - Added CUST Queries d_incident_met_sla_flag f-change_request_type_scr_code

--- REI/ITSA/f_change_request_actual_duration_mysql.sql
+++ REI/ITSA/f_change_request_actual_duration_mysql.sql
@@ -1,18 +1,10 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM(
-select count(1) as cnt
-from rei_mdsdb.change_request_final a
-
-
-left join rei_mdwdb.f_change_request b
-
-
-on a.sys_id=b.row_id and
-
-
-a.sourceinstance=b.source_id
-
-
-where timestampdiff(second,a.u_work_start_date,a.u_qa_deployment_date)<>b.actual_duration) z ;
+ select CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.actual_duration' ELSE 'SUCCESS' END as Message
+     from rei_mdsdb.change_request_final SRC left join
+        rei_mdwdb.f_change_request fi on SRC.sys_id=fi.row_id and SRC.sourceinstance=fi.source_id
+      left join rei_mdwdb.d_lov_map lm   ON (lm.src_key = fi.state_src_key) and lm.dimension_wh_code in ('OPEN') 
+  where CASE WHEN  SRC.u_qa_deployment_date <  SRC.u_work_start_date THEN NULL 
+WHEN TIMESTAMPDIFF(SECOND,SRC.u_work_start_date, SRC.u_qa_deployment_date) > 214748364 THEN NULL 
+WHEN lm.dimension_wh_code='OPEN' THEN 0
+ELSE TIMESTAMPDIFF(SECOND,SRC.u_work_start_date, SRC.u_qa_deployment_date
+) end <> fi.actual_duration
