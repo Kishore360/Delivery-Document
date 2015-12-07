669134a3b76922a7606a0f14460f15e99f246fdf
prabhuram <prabhuram.sridharan@numerify.com>
2015-12-07 11:52:50 +0530

    Query change

--- OOTB/ITSA/MATS/ootb_INCIDENT_detailed_redshift.sql
+++ OOTB/ITSA/MATS/ootb_INCIDENT_detailed_redshift.sql
@@ -1,3 +1,9 @@
+ select'f_incident_closed' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_incident_closed             a11 
+union
+ select'f_incident_resolved' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_incident_resolved             a11 
+ union
  select'f_incident' as Table_Name, count(a11.row_key) Row_Count
  from  ldb.f_incident             a11 
  union
