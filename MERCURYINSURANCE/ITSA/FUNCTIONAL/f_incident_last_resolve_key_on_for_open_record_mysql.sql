db315de6006254c0e8113379cdef73b2c9515f49
veena.k <veena.karkala@numerify.com>
2015-12-11 15:15:00 +0530

    Uploading Mercury Insurance Cust queries

---
+++ MERCURYINSURANCE/ITSA/FUNCTIONAL/f_incident_last_resolve_key_on_for_open_record_mysql.sql
@@ -0,0 +1,15 @@
+
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+FROM (
+select count(1) cnt
+ from mercuryinsurance_mdsdb.incident_final a 
+left join  mercuryinsurance_mdwdb.f_incident b
+on (a.sys_id=b.row_id and a.sourceinstance=b.source_id)
+left join  mercuryinsurance_mdwdb.d_calendar_date c
+on 
+DATE_FORMAT(CONVERT_TZ(a.u_resolved,'GMT','America/Los_Angeles'),'%Y%m%d') = c.row_id
+where 
+0<>  b.last_resolved_on_key
+AND a.state NOT IN (6,7))b
