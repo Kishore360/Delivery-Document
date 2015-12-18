90baee3d110e96537a0f364a1dec0004317de571
veena.k <veena.karkala@numerify.com>
2015-12-18 16:41:00 +0530

    corrected cust query

--- WOW/ITSA/FUNCTIONAL/f_sweep_nodes_c_swp_node_amplifier_c_mysql.sql
+++ WOW/ITSA/FUNCTIONAL/f_sweep_nodes_c_swp_node_amplifier_c_mysql.sql
@@ -3,7 +3,7 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ELSE 'Data Matched' END AS Message 
 FROM (
 select count(1) as cnt  
-from wow_mdsdb.u­_sweep_nodes_final a
+from wow_mdsdb.u_sweep_nodes_final a
 left join wow_mdwdb.f_sweep_nodes_c b
 on a.sys_id=b.row_id and a.sourceinstance=b.source_id 
 where  a.u_swp_node_amplifier <> b.swp_node_amplifier_c)g
