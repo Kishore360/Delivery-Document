6e68387d20aa4e6b793dbf4db11f1cbc0f2c8109
veena.k <veena.karkala@numerify.com>
2015-12-18 13:05:35 +0530

    updated missing query in MATS cust - change

--- ASU/ITSA/MATS/change_request_cust_cube_detailed_redshift.sql
+++ ASU/ITSA/MATS/change_request_cust_cube_detailed_redshift.sql
@@ -17,3 +17,8 @@ select  'd_lov1' as Table_Name,count(a.row_key) Row_Count
 from ldb.f_change_request    a
 join  asu_mdwdb.d_lov   b
 on  (a.closed_status_src_c_key = b.row_key)
+union
+select  'd_request_item' as Table_Name,count(a.row_key) Row_Count
+from ldb.f_change_request    a
+join  asu_mdwdb.d_request_item   b
+on  (a.change_document_c_key = b.row_key)
