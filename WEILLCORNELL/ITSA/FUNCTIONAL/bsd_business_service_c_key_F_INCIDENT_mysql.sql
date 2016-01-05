95dc8bdcdbe60e3dc9e5ae6b4e1e9ac3b5e6a1d4
vijay rao <vijay.rao@numerify.com>
2016-01-05 11:23:30 +0530

    Cleaned up WeillCornell syntax query issues

--- WEILLCORNELL/ITSA/FUNCTIONAL/bsd_business_service_c_key_F_INCIDENT_mysql.sql
+++ WEILLCORNELL/ITSA/FUNCTIONAL/bsd_business_service_c_key_F_INCIDENT_mysql.sql
@@ -4,7 +4,7 @@ ELSE 'Data Matched' END AS Message
 FROM ( select count(*) as cnt from weillcornell_mdsdb.incident_final a
 inner join weillcornell_mdwdb.f_incident  b on a.sys_id collate utf8_unicode_ci=b.row_id  and b.row_key <100
 left outer join weillcornell_mdwdb.d_configuration_item c on a.u_bsd_business_service=c.row_id where a.u_bsd_business_service is not null
- where case when  a.u_bsd_business_service is null then '0' 
+ and case when  a.u_bsd_business_service is null then '0' 
  else case when c.row_key is null then '-1' else c.row_key end end <>b.bsd_business_service_c_key
   ) c;
  
