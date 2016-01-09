841ed72c0b782cdcc45cf3da0ea42abcc6684d59
prabhuram <prabhuram.sridharan@numerify.com>
2016-01-09 22:17:56 +0530

    pushing PGI Changes

--- PGI/ITSA/FUNCTIONAL/d_incident_last_resolved_on_d_incident_mysql.sql
+++ PGI/ITSA/FUNCTIONAL/d_incident_last_resolved_on_d_incident_mysql.sql
@@ -5,4 +5,4 @@ FROM (
 select count(1) as cnt from pgi_mdwdb.d_incident dfi
 join pgi_mdsdb.incident_final incf on dfi.row_id = incf.sys_id and dfi.source_id= incf.sourceinstance
 where
-dfi.last_resolved_on <> CONVERT_TZ(incf.u_resolved,"GMT","UTC"))c;
+dfi.last_resolved_on <> CONVERT_TZ(incf.resolved_at,"GMT","UTC"))c;
