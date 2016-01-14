b78572c226f9ede2b5250856f1aa11d02f4e5c1a
vijay rao <vijay.rao@numerify.com>
2016-01-14 09:54:10 +0530

    WOW CUST Changed query

--- WOW/ITSA/FUNCTIONAL/f_incident_category_src_key_mysql.sql
+++ WOW/ITSA/FUNCTIONAL/f_incident_category_src_key_mysql.sql
@@ -9,8 +9,11 @@ on s.u_category = uc.sys_id and s.sourceinstance = uc.sourceinstance
 left join wow_mdwdb.f_incident t
 on s.sys_id=t.row_id and s.sourceinstance=t.source_id
 left join wow_mdwdb.d_lov l 
-on (concat('category~incident~~~',upper(uc.u_name))= l.row_id 
+on (concat('CATEGORY~INCIDENT~~~',upper(uc.u_name))= l.row_id 
  and s.sourceinstance= l.source_id )
-WHERE COALESCE(l.row_key,CASE WHEN s.subcategory IS NULL THEN 0 else '-1' end)<> COALESCE(t.sub_category_src_key,'')
+inner join wow_mdsdb.incident_final src 
+ on t.row_id = src.sys_id
+ and t.source_id = src.sourceinstance
+WHERE COALESCE(l.row_key,CASE WHEN src.u_category IS NULL THEN 0 else '-1' end) <> COALESCE(t.category_src_key,'')
 )
-temp
+temp;
