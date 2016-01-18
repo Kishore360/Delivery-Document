2f65d1e1af6a00e5a15cb07d2e75d956da439c1d
prabhuram <prabhuram.sridharan@numerify.com>
2016-01-18 16:05:05 +0530

    Flag Data type query modified

--- OOTB/ITSA/FUNCTIONAL/Check all Flag DataType_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/Check all Flag DataType_mysql.sql
@@ -5,7 +5,7 @@ select case when count(1) <> 0 then 'FAILURE' else 'SUCCESS' end as Result
 else 'SUCCESS' end as Result
 from (select concat(table_name,'.',column_name) as InvalidCols,column_type
 from information_schema.columns
-where table_schema ='<<tenant>>_workdb' and table_name IN (SELECT 	table_name
- from information_schema.TABLES where table_schema ='<<tenant>>_workdb' and table_type ='BASE TABLE')
+where table_schema  in ('<<tenant>>_workdb','<<tenant>>_mdsdb','<<tenant>>_mdwdb') and table_name IN (SELECT 	table_name
+ from information_schema.TABLES where table_schema  in ('<<tenant>>_workdb','<<tenant>>_mdsdb','<<tenant>>_mdwdb') and table_type ='BASE TABLE')
 and column_name like '%flag'
 and column_type <> 'char(1)') A
