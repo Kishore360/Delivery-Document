f59b22285eaa3fa1076a3ae23d601e4cc2f9cdf9
prabhuram <prabhuram.sridharan@numerify.com>
2016-01-18 16:14:33 +0530

    Pushing the query change for d_problem_over_due_flag

--- OOTB/ITSA/FUNCTIONAL/d_problem_over_due_flag_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/d_problem_over_due_flag_mysql.sql
@@ -1,12 +1,9 @@
-
-
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_request_task.over_due_flag' ELSE 'SUCCESS' END as Message FROM
  (
-
 select 
-COALESCE( CASE WHEN TRGT.active_flag = 'Y' and TRGT.due_on < FRESH.lastupdated then 'Y' else 'N' END,''),
+COALESCE( CASE WHEN TRGT.active_flag = 'Y' and TRGT.due_on < (SELECT MAX(lastupdated) AS lastupdated
+FROM <<tenant>>_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%') then 'Y' else 'N' END,''),
  COALESCE(TRGT.over_due_flag ,'')
 from
 <<tenant>>_mdwdb.d_problem TRGT
@@ -15,4 +12,3 @@ COALESCE( CASE WHEN TRGT.active_flag = 'Y' and TRGT.due_on < (SELECT MAX(lastupd
 FROM <<tenant>>_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%') then 'Y' else 'N' END,'')<>
  COALESCE(TRGT.over_due_flag ,'')
 )a
-
