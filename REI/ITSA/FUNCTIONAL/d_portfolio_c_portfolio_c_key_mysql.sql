9b1bf019ab6ea18e3405615f7df761a41dff0111
veena.k <veena.karkala@numerify.com>
2016-01-25 16:03:19 +0530

    updated the cust queries

--- REI/ITSA/FUNCTIONAL/d_portfolio_c_portfolio_c_key_mysql.sql
+++ REI/ITSA/FUNCTIONAL/d_portfolio_c_portfolio_c_key_mysql.sql
@@ -1,9 +1,8 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.dormant_flag' ELSE 'SUCCESS' END as Message
-
 from rei_mdsdb.pm_project_final s
 left join rei_mdwdb.d_portfolio_c l
 on s.pm_portfolio=l.row_id and s.sourceinstance=l.source_id  
 left join rei_mdwdb.d_project t
 on s.sys_id=t.row_id and s.sourceinstance=t.source_id
-where  l.row_key=t.portfolio_c_key);
+where  l.row_key=t.portfolio_c_key
--- REI/ITSA/FUNCTIONAL/d_portfolio_c_portfolio_owner_key_mysql.sql
+++ REI/ITSA/FUNCTIONAL/d_portfolio_c_portfolio_owner_key_mysql.sql
@@ -1,9 +1,9 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.dormant_flag' ELSE 'SUCCESS' END as Message
 from rei_mdsdb.pm_portfolio_final s
-left join rei_mdwdb.d_internal_contact l
-on  concat('INTERNAL_CONTACT~',s.u_portfolio_owner )= l.row_id
- and s.sourceinstance=l.source_id
 left join rei_mdwdb.d_portfolio_c   t
 on s.sys_id=t.row_id and s.sourceinstance=t.source_id
-WHERE t.portfolio_owner_key = l.row_key);
+left join rei_mdwdb.d_internal_contact l
+on  concat('INTERNAL_CONTACT~',s.u_portfolio_owner )= l.row_id
+ and s.sourceinstance=l.source_id and (s.sys_created_on between l.effective_to and l.effective_from)
+WHERE t.portfolio_owner_key <> l.row_key
