88e359a7840312772596e4761e453fb886d057bb
Shubha <Shubha.rao@numerify.com>
2016-01-27 17:25:43 +0530

    Updating mats queries

--- OOTB/ITSA/FUNCTIONAL/f_request_task_closed_by_key_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/f_request_task_closed_by_key_mysql.sql
@@ -1,5 +1,4 @@
-
- SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_task.closed_by_key' ELSE 'SUCCESS' END as Message
  FROM <<tenant>>_mdsdb.sc_task_final SRC 
  LEFT JOIN <<tenant>>_mdwdb.f_request_task TRGT 
@@ -8,6 +7,5 @@
  LEFT JOIN <<tenant>>_mdwdb.d_internal_contact LKP 
  ON ( concat('INTERNAL_CONTACT~',SRC.closed_by)= LKP.row_id 
 AND SRC.sourceinstance= LKP.source_id
-and COALESCE(CONVERT_TZ (SRC.opened_at,<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>), CONVERT_TZ (SRC.closed_at,<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>),'UNSPECIFIED') 
-BETWEEN effective_from AND effective_to)
+and TRGT.pivotdate BETWEEN LKP.effective_from AND LKP.effective_to)
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.closed_by IS NULL THEN 0 else -1 end)<> (TRGT.closed_by_key)
