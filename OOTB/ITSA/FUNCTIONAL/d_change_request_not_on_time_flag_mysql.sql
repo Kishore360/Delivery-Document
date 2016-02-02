2f50d2ff1ea9bcf39989e9d37d6a5171b8da61ea
unknown <avishek.gupta@numerify.com>
2016-02-02 15:48:27 +0530

    updated the ootb queries

--- OOTB/ITSA/FUNCTIONAL/d_change_request_not_on_time_flag_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/d_change_request_not_on_time_flag_mysql.sql
@@ -6,4 +6,4 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  LEFT JOIN <<tenant>>_mdwdb.d_change_request TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE ( CASE WHEN (SRC.active= TRUE AND SRC.closed_at > SRC.end_date) THEN 'Y' ELSE 'N' END )<> (TRGT.not_on_time_flag )
+ WHERE ( CASE WHEN ( SRC.closed_at > SRC.end_date) THEN 'Y' ELSE 'N' END )<> (TRGT.not_on_time_flag )
