7329fcbcf331180aac0a0e5ea405e31bdf485c04
unknown <avishek.gupta@numerify.com>
2016-02-03 12:08:19 +0530

    modification in OOTB functional queries.

--- OOTB/ITSA/FUNCTIONAL/f_incident_age_open_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/f_incident_age_open_mysql.sql
@@ -2,17 +2,17 @@
  CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.age' ELSE 'SUCCESS' END as Message
 
 from
-(select count(1) cnt FROM tivo_mdsdb.incident_final SRC 
-  join tivo_mdwdb.f_incident f ON (SRC.sys_id =f.row_id  
+(select count(1) cnt FROM <<tenant>>_mdsdb.incident_final SRC 
+  join <<tenant>>_mdwdb.f_incident f ON (SRC.sys_id =f.row_id  
  AND SRC.sourceinstance= f.source_id and f.soft_deleted_flag='N' )
 
-JOIN tivo_mdwdb.d_lov_map br ON f.state_src_key = br.src_key
+JOIN <<tenant>>_mdwdb.d_lov_map br ON f.state_src_key = br.src_key
 AND br.dimension_wh_code = 'OPEN' and br.dimension_class = 'STATE~INCIDENT'
-JOIN tivo_mdwdb.d_incident a ON a.row_key = f.incident_key
+JOIN <<tenant>>_mdwdb.d_incident a ON a.row_key = f.incident_key
 AND f.source_id = a.source_id 
 
 WHERE TIMESTAMPDIFF(DAY,a.opened_on,(SELECT MAX(lastupdated) AS lastupdated
 
-FROM tivo_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%'))<> f.age
+FROM <<tenant>>_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%'))<> f.age
 
  )A
