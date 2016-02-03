ce83c4d40e9505ab5ef9a3859953d548fbd5f7d1
unknown <avishek.gupta@numerify.com>
2016-02-03 11:09:51 +0530

    OOTB QUERY update

--- OOTB/ITSA/FUNCTIONAL/f_incident_age_open_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/f_incident_age_open_mysql.sql
@@ -1,14 +1,18 @@
  select CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.age' ELSE 'SUCCESS' END as Message
+
 from
-(select count(1) cnt FROM <<tenant>>_mdsdb.incident_final SRC 
-  join <<tenant>>_mdwdb.f_incident f ON (SRC.sys_id =f.row_id  
+(select count(1) cnt FROM tivo_mdsdb.incident_final SRC 
+  join tivo_mdwdb.f_incident f ON (SRC.sys_id =f.row_id  
  AND SRC.sourceinstance= f.source_id and f.soft_deleted_flag='N' )
-JOIN <<tenant>>_mdwdb.d_lov_map br ON f.state_src_key = br.src_key
+
+JOIN tivo_mdwdb.d_lov_map br ON f.state_src_key = br.src_key
 AND br.dimension_wh_code = 'OPEN' and br.dimension_class = 'STATE~INCIDENT'
-JOIN <<tenant>>_mdwdb.d_incident a ON a.row_key = f.incident_key
-AND f.source_id = a.source_id
-WHERE DATEDIFF((SELECT MAX(lastupdated) AS lastupdated
-FROM <<tenant>>_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%'),a.opened_on)<> f.age
+JOIN tivo_mdwdb.d_incident a ON a.row_key = f.incident_key
+AND f.source_id = a.source_id 
+
+WHERE TIMESTAMPDIFF(DAY,a.opened_on,(SELECT MAX(lastupdated) AS lastupdated
+
+FROM tivo_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%'))<> f.age
 
  )A
