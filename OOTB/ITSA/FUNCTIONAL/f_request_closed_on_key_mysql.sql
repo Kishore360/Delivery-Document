6f0c4818237977340f5f0f9dad937778a7ed4adc
veena.k <veena.karkala@numerify.com>
2016-02-03 19:08:17 +0530

    closed on key population needed lookup on d_lov- OOTb query updated

--- OOTB/ITSA/FUNCTIONAL/f_request_closed_on_key_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/f_request_closed_on_key_mysql.sql
@@ -4,7 +4,9 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  LEFT JOIN <<tenant>>_mdwdb.f_request TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
+  JOIN <<tenant>>_mdwdb.d_lov_map dlm 
+ON TRGT.state_src_key = dlm.src_key
 LEFT JOIN <<tenant>>_mdwdb.d_calendar_date LKP 
 on (LKP.row_id = date_format(convert_tz(SRC.closed_at,<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>),'%Y%m%d')  and LKP.source_id=0
 )
-WHERE coalesce(LKP.row_key,-99) <> coalesce(TRGT.closed_on_key,-99)
+WHERE case when dlm.dimension_wh_code <> 'OPEN' then (LKP.row_key) else null end <> (TRGT.closed_on_key)
