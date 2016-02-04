1d96b080e93d96f718844cd7291f4914fa0c5ca6
unknown <avishek.gupta@numerify.com>
2016-02-04 12:30:23 +0530

    Modified the  last resolved on attribute  for d_incident.

--- OOTB/ITSA/FUNCTIONAL/d_incident_last_resolved_on_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/d_incident_last_resolved_on_mysql.sql
@@ -1,2 +1,13 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.last_resolved_on' ELSE 'SUCCESS' END as Message  FROM <<tenant>>_mdsdb.incident_final SRC  LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT  ON (SRC.sys_id =TRGT.row_id   AND SRC.sourceinstance= TRGT.source_id  ) WHERE 
-coalesce(convert_tz(SRC.resolved_at,<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>),-99)<> coalesce(TRGT.last_resolved_on,-99)
+SELECT CASE WHEN cnt> 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_incident.last_resolved_on' ELSE 'data matched' END as
+ Message  from (
+select count(1) as cnt  FROM <<tenant>>_mdsdb.incident_final SRC  LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT  
+ON (SRC.sys_id =TRGT.row_id   AND SRC.sourceinstance= TRGT.source_id  )
+left join <<tenant>>_mdwdb.f_incident f on f.incident_key=TRGT.row_key
+JOIN <<tenant>>_mdwdb.d_lov_map dlm 
+ON f.state_src_key = dlm.src_key
+ WHERE 
+case when
+ dlm.dimension_wh_code <> 'OPEN' then 
+ convert_tz(SRC.resolved_at,'GMT','America/Los_Angeles') else null end 
+<> TRGT.last_resolved_on)x
