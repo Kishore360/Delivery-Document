72823131ed0cba8120b8f3ec9c0c9e537fc4b40a
veena.k <veena.karkala@numerify.com>
2016-02-04 12:00:16 +0530

    OOTB Query updated to consider date instead of time duration in d_change_request_not_on_time_flag_mysql.sql

--- OOTB/ITSA/FUNCTIONAL/d_change_request_not_on_time_flag_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/d_change_request_not_on_time_flag_mysql.sql
@@ -6,4 +6,4 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  LEFT JOIN <<tenant>>_mdwdb.d_change_request TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE ( CASE WHEN ( SRC.closed_at > SRC.end_date) THEN 'Y' ELSE 'N' END )<> (TRGT.not_on_time_flag )
+ WHERE (CASE WHEN (date_format(SRC.closed_at,'%Y%m%d') > date_format(SRC.end_date,'%Y%m%d')) THEN 'Y' ELSE 'N' END )<> (TRGT.not_on_time_flag )
