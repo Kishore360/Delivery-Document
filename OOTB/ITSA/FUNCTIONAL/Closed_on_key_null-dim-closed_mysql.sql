355c89bdac4dcdb62fa055cc307f8bbe7a7b1224
unknown <avishek.gupta@numerify.com>
2016-02-08 12:08:20 +0530

    no message

--- OOTB/ITSA/FUNCTIONAL/Closed_on_key_null-dim-closed_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/Closed_on_key_null-dim-closed_mysql.sql
@@ -1,7 +1,7 @@
 select CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.state_src_key' ELSE 'SUCCESS' END as Message
  FROM(
-select lov.dimension_name,f.opened_on_key,f.last_resolved_on_key,f.closed_on_key
+select count(1) cnt
 from <<tenant>>_mdwdb.f_incident f
  join <<tenant>>_mdwdb.d_lov_map d on src_key=state_src_key
  join <<tenant>>_mdwdb.d_lov lov on lov.dimension_class=d.dimension_class and lov.dimension_code=d.dimension_code 
