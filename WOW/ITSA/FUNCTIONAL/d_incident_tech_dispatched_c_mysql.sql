5a2adf2d576f57608b6d23c019f6e61d813ae546
vijay rao <vijay.rao@numerify.com>
2016-02-09 11:57:00 +0530

    WOW Outage ETL test cases

---
+++ WOW/ITSA/FUNCTIONAL/d_incident_tech_dispatched_c_mysql.sql
@@ -0,0 +1,7 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
+from
+wow_mdsdb.u_incident_outage_final a
+left join wow_mdwdb.d_incident b
+on a.sys_id=b.row_id and a.sourceinstance=b.source_id
+where CONVERT_TZ(a.u_tech_dispatched,'GMT','US/Mountain')<>b.tech_dispatched_c;
