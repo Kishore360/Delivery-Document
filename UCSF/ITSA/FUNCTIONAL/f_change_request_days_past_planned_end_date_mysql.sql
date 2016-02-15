07cfea365936e000e332e368fbfc689ed158469c
prabhuram <prabhuram.sridharan@numerify.com>
2016-02-15 18:30:49 +0530

    Edited files of UCSF

--- UCSF/ITSA/FUNCTIONAL/f_change_request_days_past_planned_end_date_mysql.sql
+++ UCSF/ITSA/FUNCTIONAL/f_change_request_days_past_planned_end_date_mysql.sql
@@ -6,5 +6,8 @@ join ucsf_mdwdb.d_lov b
 on a.state_src_key=b.row_key and b.dimension_code='OPEN'
 left join ucsf_mdwdb.d_change_request c
 on c.row_key=a.change_request_key and c.source_id=a.source_id
-where a.days_past_planned_end_date<>datediff(convert_tz((SELECT MAX(lastupdated) AS lastupdated
-FROM ucsf_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%')),'GMT','America/LosAngeles'),c.planned_end_on);
+where a.days_past_planned_end_date<>
+
+datediff(convert_tz(
+(SELECT MAX(lastupdated) AS lastupdated FROM ucsf_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%'),'GMT','America/LosAngeles')
+,c.planned_end_on);
