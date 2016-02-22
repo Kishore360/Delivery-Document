785f8e7e9563eaa42ea97d19e23549a52e816a56
prabhuram <prabhuram.sridharan@numerify.com>
2016-02-22 17:09:09 +0530

    ASU Items modified and uploaded

--- ASU/ITSA/FUNCTIONAL/f_hr_case_c_dormancy_age_opened_mysql.sql
+++ ASU/ITSA/FUNCTIONAL/f_hr_case_c_dormancy_age_opened_mysql.sql
@@ -12,17 +12,8 @@ AND br.dimension_wh_code = 'OPEN'
 JOIN ( select source_id,max(lastupdated) as lastupdated,soft_deleted_flag  from asu_mdwdb.d_o_data_freshness )as df 
 ON f.source_id = df.source_id
 and df.soft_deleted_flag='N'   
-WHERE timestampdiff(day,convert_tz(a.changed_on,),df.lastupdated)<> f.dormancy_age
+WHERE timestampdiff(day,convert_tz(a.changed_on,'GMT','US/Mountain'),df.lastupdated)<> f.dormancy_age
 
-union
 
-select count(1) FROM asu_mdsdb.hr_case_final SRC 
-  join asu_mdwdb.f_hr_case_c f ON (SRC.sys_id =f.row_id  
- AND SRC.sourceinstance= f.source_id  )
-JOIN asu_mdwdb.d_hr_case_c a ON a.row_key = f.hr_case_c_key
-AND f.source_id = a.source_id
-JOIN asu_mdwdb.d_lov_map br ON a.state_src_key = br.src_key
-AND br.dimension_wh_code IN ('CLOSED','RESOLVED')
-WHERE
-0 <> f.dormancy_age
+
 )A
