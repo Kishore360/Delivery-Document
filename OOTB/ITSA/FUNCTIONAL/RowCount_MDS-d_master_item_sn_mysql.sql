53b5774bd58b374bc2738cdc52c8d27cf4723630
prabhuram <prabhuram.sridharan@numerify.com>
2016-03-15 07:41:02 +0530

    Uploading few files

--- OOTB/ITSA/FUNCTIONAL/RowCount_MDS-d_master_item_sn_mysql.sql
+++ OOTB/ITSA/FUNCTIONAL/RowCount_MDS-d_master_item_sn_mysql.sql
@@ -8,6 +8,6 @@ ELSE CONCAT( 'MDS-DWH RowCount did not Match for ', DWH.Table_DWH , '. MDS:',cou
 END AS Message
 FROM (SELECT (SELECT COUNT(1) FROM <<tenant>>_mdsdb.sc_cat_item_final) AS count_mds) MDS,
 (SELECT COUNT(1) Count_DWH, 'd_master_item'  Table_DWH 
-FROM <<tenant>>_mdwdb.d_master_item WHERE row_key NOT IN (0,-1) and soft_deleted_flag='N')  DWH
+FROM <<tenant>>_mdwdb.d_master_item WHERE row_key NOT IN (0,-1) )  DWH
 
 
