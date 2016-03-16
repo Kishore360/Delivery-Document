325510ca95d9865bb99eec0a282897ae711871c2
prabhuram <prabhuram.sridharan@numerify.com>
2016-03-16 12:51:49 +0530

    Applying the changes to MI and general queries

--- OOTB/ITSA/FUNCTIONAL/Dimclass not configued in LSMLS Table.sql
+++ OOTB/ITSA/FUNCTIONAL/Dimclass not configued in LSMLS Table.sql
@@ -1,7 +1,9 @@
 -- Please Ignore the dimension classes related to Asset,Finance
--- APP 1870 is the defect raised for not having LSM LS Dim class not configured 
+-- APP 1870 is the defect raised for not having LSM LS Dim class not configured
+--Ignore Category and Subcategory for TJX(Populated using M2SI ,Not configured using LSM LS Sytem variable)
+
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'Dimension classes not configured in LSM LS System Variables' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'Dimension classes not configured in LSM LS System Variables:Ignore Category and Subcategory for TJX' ELSE 'SUCCESS' END as Message
  FROM(
 
 select distinct dimension_class,source_id
