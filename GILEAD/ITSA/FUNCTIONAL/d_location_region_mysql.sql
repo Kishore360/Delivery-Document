675962c64e7a610a0a1f6fd243c5f939d5490330
vijay rao <vijay.rao@numerify.com>
2016-03-29 12:34:26 +0530

    changed Gilead region test query

--- GILEAD/ITSA/FUNCTIONAL/d_location_region_mysql.sql
+++ GILEAD/ITSA/FUNCTIONAL/d_location_region_mysql.sql
@@ -2,8 +2,17 @@
 ,CaSE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END aS Message 
 FROM (
-select count(1) as cnt  from gilead_mdsdb.cmn_location_final a 
-left join 
-gilead_mdwdb.d_location b on 
-a.sys_id=b.row_id and a.sourceinstance=b.source_id
-where coalesce(a.u_region,'UNKNOWN')<>b.region_c)c 
+select count(1) as cnt  
+FROM gilead_mdsdb.cmn_location_final SRC
+join gilead_mdwdb.d_location a
+ ON (SRC.sys_id =a.row_id  
+ AND SRC.sourceinstance= a.source_id  )
+left join  gilead_mdwdb.d_location b 
+on a.site_c_key = b.row_key 
+and a.site_c_key not in (0,-1) 
+and (b.country_code is not null or b.region_c is not null )
+where a.region_c<>b.region_c
+)c 
+
+
+ 
