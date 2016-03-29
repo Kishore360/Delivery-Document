720f7c596c45bd2778aa1bc6771c0597eb0bc3bd
vijay rao <vijay.rao@numerify.com>
2016-03-29 12:18:03 +0530

    country_code Gilead cust changed query

--- GILEAD/ITSA/FUNCTIONAL/d_location_country_code_mysql.sql
+++ GILEAD/ITSA/FUNCTIONAL/d_location_country_code_mysql.sql
@@ -1,7 +1,11 @@
- SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_survey_question.survey_key' ELSE 'SUCCESS' END as Message
  FROM gilead_mdsdb.cmn_location_final SRC
-join gilead_mdwdb.d_location TRGT
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
-where SRC.country<>TRGT.country_code
+join gilead_mdwdb.d_location a
+ ON (SRC.sys_id =a.row_id  
+ AND SRC.sourceinstance= a.source_id  )
+left join  gilead_mdwdb.d_location b 
+on a.site_c_key = b.row_key 
+and a.site_c_key not in (0,-1) 
+and (b.country_code is not null or b.region_c is not null )
+where a.country_code<>b.country_code
