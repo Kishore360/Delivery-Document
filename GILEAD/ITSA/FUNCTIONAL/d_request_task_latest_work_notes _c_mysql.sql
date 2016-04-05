dc8272ba96e3f7f5e49b3ab826b5f5085190b34b
prabhuram <prabhuram.sridharan@numerify.com>
2016-04-05 15:19:10 +0530

    Removing the Manual Queries as of now

--- GILEAD/ITSA/FUNCTIONAL/d_request_task_latest_work_notes _c_mysql.sql
+++
@@ -1,7 +0,0 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_request_task.description' ELSE 'SUCCESS' END as Message
- FROM gilead_mdsdb.sc_task_final SRC 
- LEFT JOIN gilead_mdwdb.d_request_task TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.u_latest_work_notes    ,'')<> COALESCE(TRGT.latest_work_notes _c ,'')
