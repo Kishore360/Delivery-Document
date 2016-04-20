83ebe020e7a2d48d9e46915ea5922857b9c28520
veena.k <veena.karkala@numerify.com>
2016-04-20 18:44:55 +0530

    Fixed OOTB - Synatx errors/ d_lov related queries modified according to Apps 3.0

--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/f_request_item_closed_on_key_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/f_request_item_closed_on_key_mysql.sql
@@ -7,11 +7,8 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 JOIN <<tenant>>_mdwdb.d_lov_map dlm 
 ON TRGT.state_src_key = dlm.src_key
 LEFT JOIN <<tenant>>_mdwdb.d_calendar_date LKP 
-<<<<<<< HEAD
 on (LKP.row_id = date_format(convert_tz(coalesce(SRC.closed_at,SRC.sys_updated_on),<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>),'%Y%m%d')  and LKP.source_id=0
-=======
 on (LKP.row_id = date_format(convert_tz(coalesce(SRC.closed_at,SRC.sys_updated_on),'<<TENANT_SSI_TIME_ZONE>>','<<DW_TARGET_TIME_ZONE>>'),'%Y%m%d')  and LKP.source_id=0
->>>>>>> f1fc2cd6f47dd2f514d6f92abcbce7c893ff8b62
 )
 WHERE case when dlm.dimension_wh_code = 'CLOSED' then (LKP.row_key) else null end <> (TRGT.closed_on_key)
 
--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/f_request_task_open_to_close_duration_open_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/f_request_task_open_to_close_duration_open_mysql.sql
@@ -1,10 +1,10 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_task.open_to_close_duration' ELSE 'SUCCESS' END as Message
- FROM nbcu_mdsdb.sc_task_final SRC
- LEFT JOIN nbcu_mdwdb.f_request_task TRGT 
+ FROM <<tenant>>_mdsdb.sc_task_final SRC
+ LEFT JOIN <<tenant>>_mdwdb.f_request_task TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id)
-left join nbcu_mdwdb.d_lov_map p
+left join <<tenant>>_mdwdb.d_lov_map p
 on TRGT.state_src_key=p.src_key
 where dimension_wh_code IN ('OPEN') and 
 case WHEN dimension_wh_code IN ('OPEN') then -99 end
