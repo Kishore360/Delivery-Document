5ff37a88e2c6861402410e13cca178316ec907e1
veena.k <veena.karkala@numerify.com>
2016-04-21 13:07:24 +0530

    Tivo - query correction done

--- Apps 3.0/TIVO/ITSA/FUNCTIONAL/d_problem_task_communicated_to_MSO_c_flag_mysql.sql
+++ Apps 3.0/TIVO/ITSA/FUNCTIONAL/d_problem_task_communicated_to_MSO_c_flag_mysql.sql
@@ -7,7 +7,7 @@ select count(1) as cnt
 from tivo_mdsdb.problem_task_final s
 left join tivo_mdwdb.d_problem_task t
 on s.sys_id=t.row_id and s.sourceinstance=t.source_id
-WHERE case when s.u_communicated_to_MSO = 0 then 'N' else 'Y' end = t.communicated_to_MSO_c_flag
+WHERE case when s.u_communicated_to_MSO = 0 then 'N' else 'Y' end <> t.communicated_to_MSO_c_flag
 ) temp
 
 
