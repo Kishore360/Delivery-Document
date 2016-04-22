db80c3bb6d294d609acccf68978611c99db26103
prabhuram <prabhuram.sridharan@numerify.com>
2016-04-22 18:40:46 +0530

    Edited the o_data_freshness validation

--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/Entries in D_o_data_freshness_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/Entries in D_o_data_freshness_mysql.sql
@@ -1,5 +1,5 @@
-SELECT CASE WHEN count(1) > 1 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN count(1) > 1 THEN 'Data did not Match.' 
+SELECT CASE WHEN count(1) < 1 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN count(1) < 1 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 FROM(
 select distinct sourcename from <<tenant>>_mdwdb.d_o_data_freshness
