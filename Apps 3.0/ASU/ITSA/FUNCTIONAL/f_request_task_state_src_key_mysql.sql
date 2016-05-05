3ddfc413b4d3ff3f7d2800a9c51217fc87eeb55c
vijay rao <vijay.rao@numerify.com>
2016-05-05 16:49:33 +0530

    no message

--- Apps 3.0/ASU/ITSA/FUNCTIONAL/f_request_task_state_src_key_mysql.sql
+++ Apps 3.0/ASU/ITSA/FUNCTIONAL/f_request_task_state_src_key_mysql.sql
@@ -7,7 +7,7 @@ from asu_mdsdb.sc_task_final s
 left join asu_mdwdb.f_request_task   t
 on s.sys_id=t.row_id and s.sourceinstance=t.source_id
 LEFT JOIN asu_mdwdb.d_lov l 
- ON  l.dimension_class like 'STATE~TASK' and
+ ON  l.dimension_class like 'STATE~SC_TASK' and
 ( concat('STATE~SC_TASK~~~',upper(s.state))= l.src_rowid 
 AND s.sourceinstance= l.source_id )
  WHERE COALESCE(l.row_key,CASE WHEN s.state IS NULL THEN 0 else '-1' end)<> COALESCE(t.state_src_key,'')
