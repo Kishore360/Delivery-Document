875f6167ffb48224f79f212cb5767fa6815f555d
vijay rao <vijay.rao@numerify.com>
2016-05-07 20:29:03 +0530

    task_time_worked queries

---
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_task_opened_on_c_mysql.sql
@@ -0,0 +1,7 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
+from
+nbcu_mdsdb.task_final a
+join nbcu_mdwdb.d_task b
+on a.sys_id=b.row_id and a.sourceinstance=b.source_id
+where b.opened_on_c<> CONVERT_TZ(a.opened_at,'GMT','America/New_York');
