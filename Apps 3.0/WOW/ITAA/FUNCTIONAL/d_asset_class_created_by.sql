9ad1cc8e2f5787d553de6aa09f8c3a6f8597d398
vijay rao <vijay.rao@numerify.com>
2016-05-27 10:04:53 +0530

    Changing ITAA queries

--- Apps 3.0/WOW/ITAA/FUNCTIONAL/d_asset_class_created_by.sql
+++ Apps 3.0/WOW/ITAA/FUNCTIONAL/d_asset_class_created_by.sql
@@ -1,8 +1,7 @@
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_asset_class.created_by' ELSE 'SUCCESS' END as Message
- FROM wow_mdsdb.sys_db_object_final  SRC 
- LEFT JOIN wow_mdwdb.d_asset_class TRGT 
- ON (concat('ASSET~',SRC.sys_id)=TRGT.row_id 
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_asset_class.changed_by' ELSE 'SUCCESS' END as Message
+FROM wow_mdsdb.sys_db_object_final  SRC 
+ JOIN wow_mdwdb.d_asset_class TRGT 
+ ON (SRC.sys_id=TRGT.row_id 
  AND SRC.sourceinstance=TRGT.source_id )
- WHERE COALESCE( SRC.sys_created_by ,'')<> COALESCE(TRGT.created_by ,'')
+WHERE COALESCE( SRC.sys_updated_by ,'')<> COALESCE(TRGT.changed_by ,'');
