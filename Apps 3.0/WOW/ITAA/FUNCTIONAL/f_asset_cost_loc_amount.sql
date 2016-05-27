43b6d963a187adde6c1fab8eb190ccd8b2481e91
vijay rao <vijay.rao@numerify.com>
2016-05-27 11:25:36 +0530

    no message

--- Apps 3.0/WOW/ITAA/FUNCTIONAL/f_asset_cost_loc_amount.sql
+++ Apps 3.0/WOW/ITAA/FUNCTIONAL/f_asset_cost_loc_amount.sql
@@ -1,6 +1,8 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset.cost_loc_amount' ELSE 'SUCCESS' END as Message
  FROM wow_mdsdb.fx_currency_instance_final  SRC 
- LEFT JOIN wow_mdwdb.f_asset TRGT ON (SRC.sys_id = TRGT.row_id 
- AND SRC.sourceinstance=TRGT.source_id )
- WHERE COALESCE( SRC.amount ,'')<> COALESCE(TRGT.cost_loc_amount ,'');
+ JOIN wow_mdwdb.f_asset TRGT 
+ ON SRC.id = TRGT.row_id 
+ AND SRC.sourceinstance=TRGT.source_id
+ AND SRC.field='cost'  
+ WHERE COALESCE( SRC.amount ,'')<> COALESCE(ROUND(TRGT.cost_loc_amount,2) ,'');
