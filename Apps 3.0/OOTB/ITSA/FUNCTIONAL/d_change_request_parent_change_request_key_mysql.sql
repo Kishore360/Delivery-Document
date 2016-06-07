40227835116f7b3ec37ab6f2ef3ea0945b47ff76
Prabhuram Sridharan <Prabhuram_Sridharan>
2016-06-07 14:56:43 +0000

    d_change_request_parent_change_request_key_mysql.sql edited online with Bitbucket

--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/d_change_request_parent_change_request_key_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/d_change_request_parent_change_request_key_mysql.sql
@@ -2,7 +2,7 @@ SELECT  CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE
 -- COALESCE(PARENT.row_key,CASE WHEN SRC.parent IS NULL THEN 0 ELSE -1 END ) , TRGT.parent_change_request_key
  FROM <<tenant>>_mdsdb.change_request_final SRC
   LEFT JOIN (select row_key,row_id,parent_change_request_key from <<tenant>>_mdwdb.d_change_request) PARENT 
- ON coalesce(SRC.u_parent_change,'UNSPECIFIED')= PARENT.row_id 
+ ON coalesce(SRC.parent,'UNSPECIFIED')= PARENT.row_id 
  JOIN <<tenant>>_mdwdb.d_change_request TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  ) 
