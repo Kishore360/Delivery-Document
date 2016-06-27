8097dbee70e634f39b1391655229ef464368fbe6
prabhuram <prabhuram.sridharan@numerify.com>
2016-06-27 12:34:57 +0530

    Gilead - Adding the Cust Queries

--- Apps 3.0/GILEAD/ITSA/FUNCTIONAL/d_internal_org_owner_c_key_mysql.sql
+++ Apps 3.0/GILEAD/ITSA/FUNCTIONAL/d_internal_org_owner_c_key_mysql.sql
@@ -1,9 +1,11 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN count(1) > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message from
-gilead_mdwdb.d_internal_organization a 
-left join  gilead_mdsdb.sys_user_group_final b
+ 
+  gilead_mdsdb.sys_user_group_final b left join 
+gilead_mdwdb.d_internal_organization a
 on (a.row_id=(CONCAT('GROUP~',b.sys_id)) AND a.source_id=b.sourceinstance)
 left join gilead_mdwdb.d_internal_contact c
 ON COALESCE(CONCAT('INTERNAL_CONTACT~',b.u_owner),'UNSPECIFIED')=c.row_id
-where a.owner_c_key<>COALESCE(c.row_key);
+where 
+COALESCE(c.row_key,CASE WHEN b.u_owner IS NULL THEN 0 else -1 end)<>a.owner_c_key
