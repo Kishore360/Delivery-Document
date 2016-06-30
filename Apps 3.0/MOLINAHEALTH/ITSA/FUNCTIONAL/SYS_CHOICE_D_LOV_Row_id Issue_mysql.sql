848822977931bc36d5416ead8f2a0ad107a54557
prabhuram <prabhuram.sridharan@numerify.com>
2016-06-30 16:53:29 +0530

    Query change to the CUST

--- Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/SYS_CHOICE_D_LOV_Row_id Issue_mysql.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/SYS_CHOICE_D_LOV_Row_id Issue_mysql.sql
@@ -16,6 +16,10 @@ and SRC.row_id not like '%SUBCATEGORY%'
 and SRC.row_id not like '%GOAL~~~%'
 and SRC.row_id not like '%PROJECT~~~%'
 and SRC.row_id not like '%PORTFOLIO~~~%'
+and TGT.row_id like '%SUBCATEGORY%' 
+and TGT.row_id not like '%GOAL~~~%'
+and TGT.row_id not like '%PROJECT~~~%'
+and TGT.row_id not like '%PORTFOLIO~~~%'
 UNION ALL
 select TGT.row_id a1 , SRC.row_id a2
 from (
@@ -33,40 +37,9 @@ and SRC.row_id like '%SUBCATEGORY%'
 and SRC.row_id not like '%GOAL~~~%'
 and SRC.row_id not like '%PROJECT~~~%'
 and SRC.row_id not like '%PORTFOLIO~~~%'
- )a;
-select case when count(1) > 0 then 'FAILURE' else 'SUCCESS' end as Result,
-case when count(1) > 0 then 'sys_choice-d_lov row_di does not exist' else 'SUCCESS' end as Message
-from (
-select TGT.row_id a1 , SRC.row_id a2
-from (select UPPER(concat(conf.class_value,'~~~',scf.value)) as row_id,scf.sourceinstance AS source_id
-from molinahealth_workdb.lsm_ls_system_variables conf
-join molinahealth_mdsdb.sys_choice_final scf on conf.table_value= scf.name and conf.column_value = scf.element
-where conf.enable_flag = 'Y'
-and scf.inactive = 0
-and scf.language='en') SRC
-left join molinahealth_mdwdb.d_lov TGT
-ON TGT.row_id = SRC.row_id
-and TGT.source_id = SRC.source_id
-where coalesce(TGT.row_id,-99)  <> coalesce(SRC.row_id,-99) 
-and SRC.row_id not like '%SUBCATEGORY%' 
-and SRC.row_id not like '%GOAL~~~%'
-and SRC.row_id not like '%PROJECT~~~%'
-and SRC.row_id not like '%PORTFOLIO~~~%'
-UNION ALL
-select TGT.row_id a1 , SRC.row_id a2
-from (
-select UPPER(concat(conf.class_value,'~',scf.label,'~~',scf.value)) as row_id,scf.sourceinstance AS source_id
-from molinahealth_workdb.lsm_ls_system_variables conf
-join molinahealth_mdsdb.sys_choice_final scf on conf.table_value= scf.name and conf.column_value = scf.element
-where conf.enable_flag = 'Y'
-and scf.inactive = 0
-and scf.language='en') SRC
-left join molinahealth_mdwdb.d_lov TGT
-ON TGT.row_id = SRC.row_id
-and TGT.source_id = SRC.source_id
-where coalesce(TGT.row_id,-99)  <> coalesce(SRC.row_id,-99)
-and SRC.row_id like '%SUBCATEGORY%' 
-and SRC.row_id not like '%GOAL~~~%'
-and SRC.row_id not like '%PROJECT~~~%'
-and SRC.row_id not like '%PORTFOLIO~~~%'
+and TGT.row_id like '%SUBCATEGORY%' 
+and TGT.row_id not like '%GOAL~~~%'
+and TGT.row_id not like '%PROJECT~~~%'
+and TGT.row_id not like '%PORTFOLIO~~~%'
+
  )a;
