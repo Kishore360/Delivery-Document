f98b76a24cc95076a0fe08f873b11376a4058cdb
prabhuram <prabhuram.sridharan@numerify.com>
2016-07-01 23:08:57 +0530

    Committing Molina related Query

--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/D_LOV_DUPLICATE_CHECK_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/D_LOV_DUPLICATE_CHECK_mysql.sql
@@ -1,7 +1,7 @@
 select case when count(1) > 0 then 'FAILURE' else 'SUCCESS' end as Result,
 case when count(1) > 0 then 'multiple source_id, src_rowid exists in d_lov' else 'SUCCESS' end as Message
 from (
-select row_id,source_id,dimension_class
+select row_id,source_id,dimension_class,count(1)
 from <<tenant>>_mdwdb.d_lov 
 where row_id not  in ('STATE~GEO~~~~VA')
 group by 1,2,3
