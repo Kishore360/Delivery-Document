02350f47709aacbc4177b5a9e3336c9a97c3afba
unknown <avishek.gupta@numerify.com>
2016-07-01 14:58:31 +0530

    jira 2477 query change

--- TIVO/ITSA/FUNCTIONAL/d_change_request_lab_environment_type_c_flag_mysql.sql
+++ TIVO/ITSA/FUNCTIONAL/d_change_request_lab_environment_type_c_flag_mysql.sql
@@ -1,10 +1,15 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.work_start_on_key' ELSE 'SUCCESS' END as Message
- FROM tivo_mdsdb.change_request_final SRC
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.work_start_on_key' 
+ELSE 'SUCCESS' END as Message from 
+(select a.sys_id  ,a.sourceinstance,group_concat(d.u_environment_type) as u_environment_type
+ FROM tivo_mdsdb.change_request_final a
+ LEFT JOIN tivo_mdsdb.task_ci_final b ON a.sys_id = b.task
+ JOIN tivo_mdsdb.cmdb_ci_final c ON b.ci_item = c.sys_id 
+ Join tivo_mdsdb.cmdb_ci_environment_final d on d.sys_id=c.sys_id 
+ group by  a.sys_id,a.sourceinstance)x
+
  LEFT  JOIN tivo_mdwdb.d_change_request TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id )
-LEFT JOIN  tivo_mdsdb.cmdb_ci_environment LKP on 
-SRC.cmdb_ci=LKP.sys_id
+ ON (x.sys_id =TRGT.row_id  
+ AND x.sourceinstance= TRGT.source_id )
  
-where  case when LKP.u_environment_type like 'Lab%' THEN 'Y' else 'N' END <>TRGT.lab_environment_type_c_flag
+where case when x.u_environment_type like '%lab%' then 'Y' else 'N' end<> TRGT.lab_environment_type_c_flag
