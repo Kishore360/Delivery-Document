0ebbbc2f69bad8ebb82f328b6279598f1b9f5716
unknown <avishek.gupta@numerify.com>
2016-07-13 10:59:02 +0530

    Updated the UCSF queries.

--- Apps 3.0/UCSF/ITSA/FUNCTIONAL/d_change_request_failure_flag_mysql.sql
+++ Apps 3.0/UCSF/ITSA/FUNCTIONAL/d_change_request_failure_flag_mysql.sql
@@ -1,8 +1,8 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
-from
-
-ucsf_mdsdb.change_request_final a
-left join ucsf_mdwdb.d_change_request b
-on a.sys_id=b.row_id and a.sourceinstance=b.source_id
-where b.failure_flag<>case when a.u_change_result='Successful' then 'N' else 'Y' end
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_lov' ELSE 'SUCCESS' END as Message
+from ucsf_mdwdb.d_change_request cr 
+JOIN  ucsf_mdsdb.change_request_final crf ON crf.sys_id=cr.row_id 
+  AND crf.sourceinstance=cr.source_id
+where 
+	
+	CASE WHEN crf.u_change_result='Successful' THEN 'N' ELSE 'Y'  END<>cr.failure_flag
