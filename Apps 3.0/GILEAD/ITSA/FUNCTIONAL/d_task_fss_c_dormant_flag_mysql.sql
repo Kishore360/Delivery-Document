fe6901fd03ce9273c1b36159adbcb235fd9adf05
ashokvelu <ashok.velu@numerify.com>
2016-07-21 18:34:47 +0530

    FCR metric validation

--- Apps 3.0/GILEAD/ITSA/FUNCTIONAL/d_task_fss_c_dormant_flag_mysql.sql
+++ Apps 3.0/GILEAD/ITSA/FUNCTIONAL/d_task_fss_c_dormant_flag_mysql.sql
@@ -1,5 +1,5 @@
  SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_incident.dormant_flag' ELSE 'SUCCESS' END as Message from (
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_task_fss_c.dormant_flag' ELSE 'SUCCESS' END as Message from (
  select count(1) as cnt 
   FROM 
 gilead_mdwdb.d_task_fss_c di
