9e77d903ad24a4800da7ea420df871494313d721
ashokvelu <ashok.velu@numerify.com>
2016-08-01 12:45:13 +0530

    Molinahealth  small changes

--- Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/D_LOV-LSM_LS_Enable_flag validation_mysql.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/D_LOV-LSM_LS_Enable_flag validation_mysql.sql
@@ -3,8 +3,8 @@ case when count(1) > 0 then 'Distinct dimension classes which are not loaded int
 from (
 select TGT.dimension_class as d1 , SRC.dimension_class as d2
 from (select class_value as dimension_class
-from <<tenant>>_workdb.lsm_ls_system_variables conf
+from molinahealth_workdb.lsm_ls_system_variables conf
 where conf.enable_flag = 'Y') SRC
-left join <<tenant>>_mdwdb.d_lov TGT
+left join molinahealth_mdwdb.d_lov TGT
 ON TGT.dimension_class = SRC.dimension_class
 where TGT.row_id is null and SRC.dimension_class not in ('CATEGORY~FACILITIES','CATEGORY~HR_CHANGE','STATE~HR_CHANGE'))a;
--- Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_response_question_key_mysql.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_response_question_key_mysql.sql
@@ -6,6 +6,6 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  AND SRC.sourceinstance= TRGT.source_id  )
 left join molinahealth_mdwdb.d_survey_question LKP
 on (COALESCE(SRC.metric,'UNSPECIFIED')= LKP.row_id and SRC.sourceinstance=LKP.source_id)
-WHERE COALESCE(LKP.row_key,CASE WHEN SRC.metric IS NULL THEN 0 else -1 end)=TRGT.question_key
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.metric IS NULL THEN 0 else -1 end)<>TRGT.question_key
 
 
