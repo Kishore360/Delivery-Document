8641f6947310cda6545be8d3f35664c1245faf0c
prabhuram <prabhuram.sridharan@numerify.com>
2016-08-01 19:15:46 +0530

    Intuit Query updated

--- Apps 3.0/INTUIT/ITSA/FUNCTIONAL/Request_Item_REFERENCE_C_KEY_check_mysql.sql
+++ Apps 3.0/INTUIT/ITSA/FUNCTIONAL/Request_Item_REFERENCE_C_KEY_check_mysql.sql
@@ -5,7 +5,7 @@ select count(1) as cnt from(
 select a.reference_c_key aa,b.value,c.row_key bb
 from intuit_mdwdb.f_request_item_variable_c a
 inner join intuit_mdsdb.sc_item_option_final b
-on a.row_id=b.sys_id
+on left(a.row_id,32)=b.sys_id
 and a.source_id=b.sourceinstance
 left join intuit_mdwdb.d_variable_lov_c c
 on b.value=c.value
