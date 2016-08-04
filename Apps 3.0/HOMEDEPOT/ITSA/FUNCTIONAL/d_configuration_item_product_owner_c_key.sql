bef95fa43f587180f9ca21c574f5261653fca768
Rajashekhar <Rajashekhar>
2016-08-04 15:49:54 +0530

    Change in d_configuration_item_product_owner_c_key

--- Apps 3.0/HOMEDEPOT/ITSA/FUNCTIONAL/d_configuration_item_product_owner_c_key.sql
+++ Apps 3.0/HOMEDEPOT/ITSA/FUNCTIONAL/d_configuration_item_product_owner_c_key.sql
@@ -7,11 +7,11 @@ from homedepot_mdsdb.cmdb_ci_service_final a
 left JOIN homedepot_mdwdb.d_configuration_item b
 on a.sys_id= b.row_id and a.sourceinstance=b.source_id
 left join homedepot_mdwdb.d_internal_contact c
-on concat('INTERNAL_CONTACT~',a.u_vice_president) = c.row_id
+on concat('INTERNAL_CONTACT~',a.u_product_owner) = c.row_id
 and a.sourceinstance = c.source_id
  where 
-case when u_vice_president is NULL then 0
-when c.row_id is NULL and u_vice_president is not NULL then -1
+case when u_product_owner is NULL then 0
+when c.row_id is NULL and u_product_owner is not NULL then -1
 else
-c.row_key <> b.vice_president_c_key end ) b
+c.row_key <> b.product_owner_c_key end ) b
 
