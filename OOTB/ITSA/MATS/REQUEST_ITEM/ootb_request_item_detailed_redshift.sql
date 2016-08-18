8cf8b78f7442d526fd0205a7e57beef65f05637a
prabhuram <prabhuram.sridharan@numerify.com>
2016-08-18 19:37:34 +0530

    Commit OOTB Queries for MATS

---
+++ OOTB/ITSA/MATS/REQUEST_ITEM/ootb_request_item_detailed_redshift.sql
@@ -0,0 +1,129 @@
+ select'f_request_item' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ union
+ select'd_calendar_date' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_calendar_date         a12
+on (a11.opened_on_key = a12.row_key)
+union
+ select'd_calendar_week' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_calendar_date         a12
+on (a11.opened_on_key = a12.row_key)
+left outer join  ldb.d_calendar_month         a14
+on (a12.week_start_date_key  = a14.row_key)
+ union
+  select'd_calendar_quarter' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_calendar_date         a12
+on (a11.opened_on_key = a12.row_key)
+left outer join  ldb.d_calendar_quarter                         a14
+on (a12.quarter_start_date_key  = a14.row_key)
+union
+  select'd_calendar_year' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_calendar_date         a12
+on (a11.opened_on_key = a12.row_key)
+left outer join  ldb.d_calendar_year                         a14
+on (a12.year_start_date_key  = a14.row_key)
+union
+ select'd_change_request' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_change_request         a12
+on (a11.change_c_key  = a12.row_key)
+union
+ select'd_hr_change_category' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_hr_change_category                  a12
+on (a11.hr_category_src_key   = a12.row_key)
+union
+ select'd_sc_req_item_approval' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_sc_req_item_approval                       a12
+on (a11.approval_state_src_key    = a12.row_key)
+union
+ select'd_sc_req_item_urgency' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ join  ldb.d_sc_req_item_urgency                       a12
+on (a11.urgency_state_src_key    = a12.row_key)
+union
+ select'd_hr_change' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_hr_change                  a12
+on (a11.hr_change_key    = a12.row_key)
+union
+ select'd_calendar_date_closed' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_calendar_date_closed         a13
+on (a11.closed_on_key = a13.row_key)
+union
+ select'd_calendar_month' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_calendar_date         a12
+on (a11.opened_on_key = a12.row_key)
+left outer join  ldb.d_calendar_month         a14
+on (a12.month_start_date_key = a14.row_key)
+union
+select'd_calendar_month_closed' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_calendar_date_closed         a13
+on (a11.closed_on_key = a13.row_key)
+left outer join  ldb.d_calendar_month_closed         a15
+on (a13.month_start_date_key = a15.row_key)
+union
+ select'd_internal_contact' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_internal_contact         a16
+on (a11.opened_by_key = a16.row_key)
+union
+select'd_internal_contact_mdm' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ left outer join  ldb.d_internal_contact         a16
+on (a11.opened_by_key = a16.row_key)
+left outer join  ldb.d_internal_contact_mdm         a17
+on (a16.row_current_key = a17.row_current_key)
+union
+select'd_internal_contact_requested_for' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ left outer join  ldb.d_internal_contact_requested_for         a18
+on (a11.requested_for_key = a18.row_key)
+union
+select'd_internal_contact_task_closed_by' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ left outer join  ldb.d_internal_contact_task_closed_by         a19
+on (a11.closed_by_key = a19.row_key)
+union
+select'd_internal_organization_department' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ left outer join  ldb.d_internal_organization_department         a110
+on (a11.opened_by_department_key  = a110.row_key)
+union
+select'd_internal_organization_group' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ left outer join  ldb.d_internal_organization_group         a110
+on (a11.assignment_group_key = a110.row_key)
+union
+select'd_master_item' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ left outer join  ldb.d_master_item         a111
+on (a11.catalog_item_key = a111.row_key)
+union
+select'd_request' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ left outer join  ldb.d_request         a112
+on (a11.request_key = a112.row_key)
+union
+select'd_request_item' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ left outer join  ldb.d_request_item         a113
+on (a11.request_item_key = a113.row_key)
+union
+select'd_request_item_state' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+ left outer join  ldb.d_request_item_state         a115
+on (a11.state_src_key = a115.row_key)
+union
+ select'd_internal_contact_assigned' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+left outer join  ldb.d_internal_contact         a116
+on (a11.assigned_to_key = a116.row_key)
