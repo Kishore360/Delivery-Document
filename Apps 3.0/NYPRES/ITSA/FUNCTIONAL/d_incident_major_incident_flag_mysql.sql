e6f2add1a8393e7eaa0bca61488d45be316a4ebf
prabhuram <prabhuram.sridharan@numerify.com>
2016-08-18 17:55:35 +0530

    Committing thre Nypres Major Incident

--- Apps 3.0/NYPRES/ITSA/FUNCTIONAL/d_incident_major_incident_flag_mysql.sql
+++ Apps 3.0/NYPRES/ITSA/FUNCTIONAL/d_incident_major_incident_flag_mysql.sql
@@ -5,5 +5,8 @@ ELSE 'Data Matched' END AS Message from
 select count(1) as cnt
 FROM nypres_mdsdb.incident_final A
 left join  nypres_mdwdb.d_incident B on A.sourceinstance=B.source_id AND B.ROW_ID=SYS_ID
-WHERE Case when Impact in ( 'Limited' , 'Large' , 'Widespread') AND Urgency in ( 'High' , 'Critical' ) then 'Y' else 'N' end  <> B.major_incident_flag)A
+ join (select label,value from nypres_mdsdb.sys_choice_final where name = 'incident' and element = 'impact') as imp on A.impact = imp.value
+ join (select label,value from nypres_mdsdb.sys_choice_final where name = 'incident' and element = 'urgency') as urg on A.urgency = urg.value
+WHERE Case when imp.label in ( 'Limited' , 'Large' , 'Widespread') AND urg.label in ( 'High' , 'Critical' ) then 'Y' else 'N' end  <> B.major_incident_flag)A; 
 
+ 
