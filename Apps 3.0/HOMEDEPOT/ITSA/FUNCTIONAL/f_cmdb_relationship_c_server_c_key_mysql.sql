80820959196a51b2e00b7b92f30c56889079c222
vijay rao <vijay.rao@numerify.com>
2016-09-02 06:42:12 +0530

    Updating CMDB_CI hierarchy queries

---
+++ Apps 3.0/HOMEDEPOT/ITSA/FUNCTIONAL/f_cmdb_relationship_c_server_c_key_mysql.sql
@@ -0,0 +1,15 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+FROM (SELECT count(1) as cnt
+FROM homedepot_mdsdb.cmdb_rel_ci_final SRC 
+ JOIN homedepot_mdsdb.cmdb_ci_final LKP 
+ ON SRC.child= LKP.sys_id 
+AND SRC.sourceinstance= LKP.sourceinstance 
+join homedepot_mdwdb.d_configuration_item LKP2
+on LKP.sys_id= LKP2.row_id
+and LKP.sourceinstance=LKP2.source_id
+ JOIN homedepot_mdwdb.f_cmdb_relationship_c TRGT 
+ ON (LKP2.row_id =SUBSTRING(TRGT.row_id,100,32) 
+ AND LKP2.source_id= TRGT.source_id  )
+WHERE COALESCE(LKP2.row_key,CASE WHEN SRC.child IS NULL THEN 0 else -1 end)<>TRGT.server_c_key)temp; 
