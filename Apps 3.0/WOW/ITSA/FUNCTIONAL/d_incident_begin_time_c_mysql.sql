90d362555056e92f12fbe1a5ddc495967e40e40b
vijay rao <vijay.rao@numerify.com>
2016-09-07 13:06:58 +0530

    Refreshing wow test queries for Outage

--- Apps 3.0/WOW/ITSA/FUNCTIONAL/d_incident_begin_time_c_mysql.sql
+++ Apps 3.0/WOW/ITSA/FUNCTIONAL/d_incident_begin_time_c_mysql.sql
@@ -4,4 +4,4 @@ from
 wow_mdsdb.u_incident_outage_final a
 left join wow_mdwdb.d_incident b
 on a.sys_id=b.row_id and a.sourceinstance=b.source_id
-where CONVERT_TZ(a.u_begin_time,'GMT','US/Central')<>b.begin_time_c;
+where CONVERT_TZ(a.u_begin_time,'GMT','US/Mountain')<>b.begin_time_c;
