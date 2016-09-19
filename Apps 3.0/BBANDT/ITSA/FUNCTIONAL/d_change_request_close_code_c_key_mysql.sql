e510c86166a435c023381cd135ec2146756b02be
Rajashekhar <Rajashekhar>
2016-09-19 18:48:45 +0530

    correction to the queries(BBANDT)

--- Apps 3.0/BBANDT/ITSA/FUNCTIONAL/d_change_request_close_code_c_key_mysql.sql
+++ Apps 3.0/BBANDT/ITSA/FUNCTIONAL/d_change_request_close_code_c_key_mysql.sql
@@ -3,5 +3,5 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  FROM bbandt_mdsdb.change_request_final SRC 
  LEFT JOIN bbandt_mdwdb.d_change_request TRGT ON (SRC.sys_id =TRGT.row_id AND SRC.sourceinstance= TRGT.source_id)
 LEFT JOIN bbandt_mdwdb.d_lov LKP 
- ON  LKP.dimension_class =  'CLOSE_CODE_C~CHANGE_REQUEST' and (concat('CLOSE_CODE_C~CHANGE_REQUEST~',SRC.state,'~~',upper(SRC.u_close_code))= LKP.row_id AND SRC.sourceinstance= LKP.source_id )
+ ON  LKP.dimension_class =  'CLOSE_CODE_C~CHANGE_REQUEST' and (concat('CLOSE_CODE_C~CHANGE_REQUEST~~~',upper(SRC.u_close_code))= LKP.row_id AND SRC.sourceinstance= LKP.source_id )
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_close_code IS NULL THEN 0 else -1 end)<> (TRGT.close_code_c_key);
