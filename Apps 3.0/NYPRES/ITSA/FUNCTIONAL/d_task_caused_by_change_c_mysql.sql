cb8b1338df99406b66602e1465bb22c8da12f120
ashokvelu <ashok.velu@numerify.com>
2016-09-21 15:25:57 +0530

    performance improvement

--- Apps 3.0/NYPRES/ITSA/FUNCTIONAL/d_task_caused_by_change_c_mysql.sql
+++ Apps 3.0/NYPRES/ITSA/FUNCTIONAL/d_task_caused_by_change_c_mysql.sql
@@ -2,9 +2,12 @@ SELECT CASE WHEN COUNT(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN COUNT(1) >0 THEN 'MDS to DWH data validation failed for d_task.category_c_key' ELSE 'SUCCESS' END as Message
 FROM nypres_mdwdb.d_task TRGT
 JOIN nypres_mdsdb.task_final SRC  
-ON SRC.sys_id =TRGT.row_id   AND SRC.sourceinstance= TRGT.source_id  AND TRGT.task_type='incident'
-JOIN nypres_mdsdb.incident_final SRC_INC
-ON TRGT.row_id=SRC_INC.sys_id AND TRGT.source_id=SRC_INC.sourceinstance
+ON SRC.sys_id =TRGT.row_id   AND SRC.sourceinstance= TRGT.source_id  AND TRGT.task_type='incident' AND SRC.sys_class_name='incident'
+JOIN(
+select COALESCE(LKP.row_key,CASE WHEN SRC_INC.caused_by IS NULL THEN 0 ELSE -1 END) as che,SRC_INC.sys_id,SRC_INC.sourceinstance FROM 
+nypres_mdsdb.incident_final SRC_INC
 JOIN nypres_mdwdb.d_change_request LKP
-ON LKP.row_id = SRC_INC.caused_by
-WHERE COALESCE(LKP.row_key,CASE WHEN SRC_INC.caused_by IS NULL THEN 0 ELSE -1 END) <> TRGT.caused_by_change_c_key
+ON LKP.row_id = SRC_INC.caused_by and LKP.source_id=SRC_INC.sourceinstance) vals
+ON TRGT.source_id = vals.sourceinstance and TRGT.row_id = vals.sys_id
+WHERE vals.che <>TRGT.caused_by_change_c_key
+
