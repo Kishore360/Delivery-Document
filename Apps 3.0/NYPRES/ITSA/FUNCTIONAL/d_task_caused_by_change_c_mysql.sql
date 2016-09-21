503389807e2636147fad81e13c9faf60446d77fc
ashokvelu <ashok.velu@numerify.com>
2016-09-21 15:09:46 +0530

    no message

--- Apps 3.0/NYPRES/ITSA/FUNCTIONAL/d_task_caused_by_change_c_mysql.sql
+++ Apps 3.0/NYPRES/ITSA/FUNCTIONAL/d_task_caused_by_change_c_mysql.sql
@@ -1,5 +1,5 @@
 SELECT CASE WHEN COUNT(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN COUNT(1) >0 THEN 'MDS to DWH data validation failed for d_task.category_c_key' ELSE 'SUCCESS' END as Message  FROM
+CASE WHEN COUNT(1) >0 THEN 'MDS to DWH data validation failed for d_task.category_c_key' ELSE 'SUCCESS' END as Message
 FROM nypres_mdwdb.d_task TRGT
 JOIN nypres_mdsdb.task_final SRC  
 ON SRC.sys_id =TRGT.row_id   AND SRC.sourceinstance= TRGT.source_id  AND TRGT.task_type='incident'
