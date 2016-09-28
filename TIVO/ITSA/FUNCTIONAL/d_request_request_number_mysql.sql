7dc350d26b6bb92497d3ee6c3acdb2dfe69b7d6c
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-09-28 17:38:28 +0530

    tivo - Rollout -Queries-DELV-3234,35

--- TIVO/ITSA/FUNCTIONAL/d_request_request_number_mysql.sql
+++ TIVO/ITSA/FUNCTIONAL/d_request_request_number_mysql.sql
@@ -2,8 +2,14 @@
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_request.request_number' ELSE 'SUCCESS' END as Message
- FROM tivo_mdsdb.sc_request_final SRC 
+from
+ (select sys_id,number,sourceinstance from
+ tivo_mdsdb.u_tech_ops_request_final where sys_id not in(select sys_id from tivo_mdsdb.sc_request_final)
+union 
+select sys_id,number,sourceinstance from  tivo_mdsdb.sc_req_item_final
+union select sys_id,number,sourceinstance from  tivo_mdsdb.sc_request_final
+) s 
  LEFT JOIN tivo_mdwdb.d_request TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.number,'')<> COALESCE(TRGT.request_number ,'')
+ ON (s.sys_id =TRGT.row_id  
+ AND s.sourceinstance= TRGT.source_id  )
+ WHERE COALESCE(s.number,'') <> COALESCE(TRGT.request_number ,'')
--- TIVO/ITSA/FUNCTIONAL/f_request_stage_src_key_mysql.sql
+++ TIVO/ITSA/FUNCTIONAL/f_request_stage_src_key_mysql.sql
@@ -1,12 +1,17 @@
 
-
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request.stage_src_key' ELSE 'SUCCESS' END as Message
- FROM tivo_mdsdb.sc_req_item_final SRC 
- LEFT JOIN tivo_mdwdb.f_request TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
-LEFT JOIN tivo_mdwdb.d_lov LKP 
- ON ( concat('STAGE','~','SC_REQ_ITEM','~','~','~',upper(stage))= LKP.src_rowid 
-AND SRC.sourceinstance= LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.stage IS NULL THEN 0 else -1 end)<> (TRGT.stage_src_key)
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN count(1) > 0 THEN 'Data did not Match' 
+ELSE 'Data Matched' END AS Message 
+from (select sys_id,null as stage,sourceinstance,'STAGE~TECH_OPS_REQUEST~~~' as join_cond from tivo_mdsdb.u_tech_ops_request_final
+where sys_id not in(select sys_id from tivo_mdsdb.sc_request_final)
+union 
+select sys_id,stage as stage,sourceinstance,'STAGE~SC_REQ_ITEM~~~' as join_cond from   tivo_mdsdb.sc_req_item_final
+union 
+select sys_id, stage as stage ,sourceinstance,'STAGE~SC_REQUEST~~~' as join_cond from  tivo_mdsdb.sc_request_final
+ )
+ s
+left join   tivo_mdwdb.f_request t
+on s.sys_id=t.row_id and s.sourceinstance=t.source_id
+left join tivo_mdwdb.d_lov l
+on concat(s.join_cond,upper(s.stage))=l.row_id and s.sourceinstance=l.source_id
+WHERE COALESCE(l.row_key,CASE WHEN s.stage IS NULL THEN 0 else -1 end) <> t.stage_src_key
