e55ba43dcd38d34cfd894667e5ec792532b884cf
prabhuram <prabhuram.sridharan@numerify.com>
2016-10-04 00:05:48 +0530

    Committing the RI Cube Query

--- Apps 3.0/MOLINAHEALTH/ITSA/MATS/RequestItem_detailed_redshift.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/MATS/RequestItem_detailed_redshift.sql
@@ -10,3 +10,11 @@ union
  from  ldb.f_request_item             a11 
 join        ldb.d_request_item_hr_category_src_c                  a111
                   on         (a11.category_src_c_key  = a111.row_key)
+				  union
+				   select'd_business_unit_c' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_request_item             a11 
+join        ldb.d_configuration_item            a12
+                  on         (a11.configuration_item_key = a12.row_key)
+                join        ldb.d_business_unit_c   a13
+                  on         (a12.business_unit_c_key = a13.row_key)
+ 
