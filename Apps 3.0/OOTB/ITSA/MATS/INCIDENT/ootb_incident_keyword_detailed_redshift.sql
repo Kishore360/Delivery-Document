8ffc5267a63cf11a13efeb71f1a1a2c2b4017302
prabhuram <prabhuram.sridharan@numerify.com>
2016-10-12 16:51:49 +0530

    Committing

--- Apps 3.0/OOTB/ITSA/MATS/INCIDENT/ootb_incident_keyword_detailed_redshift.sql
+++ Apps 3.0/OOTB/ITSA/MATS/INCIDENT/ootb_incident_keyword_detailed_redshift.sql
@@ -43,28 +43,28 @@ join ldb.d_incident       a17
 on (a11.incident_key = a17.row_key)
 
  union
-select'd_incident_category' as Table_Name, count(table_row_key) Row_Count
-from  ldb.f_incident_keyword             a11 
+select'd_incident_category' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_incident            a11 
 join ldb.d_incident_category         a12
 on (a11.category_src_key = a12.row_key)
 union
-select'd_incident_close_code' as Table_Name, count(table_row_key) Row_Count
-from  ldb.f_incident_keyword             a11
+select'd_incident_close_code' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_incident        a11
 join ldb.d_incident_close_code         a12
 on (a11.close_code_src_key = a12.row_key)
 union
-select'd_incident_severity' as Table_Name, count(table_row_key) Row_Count
-from  ldb.f_incident_keyword             a11 
+select'd_incident_severity' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_incident             a11 
 join ldb.d_incident_severity       a13
 on (a11.severity_src_key = a13.row_key)
 union
-select'd_incident_state' as Table_Name, count(table_row_key) Row_Count
-from  ldb.f_incident_keyword             a11 
+select'd_incident_state' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_incident             a11 
 join ldb.d_incident_state     a14
 on (a11.state_src_key = a14.row_key)
 union
-select'd_incident_subcategory' as Table_Name, count(table_row_key) Row_Count
-from  ldb.f_incident_keyword             a11
+select'd_incident_subcategory' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_incident             a11
 join ldb.d_incident_subcategory       a14
 on (a11.sub_category_src_key = a14.row_key)
 union
