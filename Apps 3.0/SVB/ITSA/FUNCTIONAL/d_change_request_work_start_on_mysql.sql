bef3417818f78187f9c094ec45fb63c79679f135
prabhuram <prabhuram.sridharan@numerify.com>
2016-10-14 13:38:49 +0530

    svb  uploaded

--- Apps 3.0/SVB/ITSA/FUNCTIONAL/d_change_request_work_start_on_mysql.sql
+++ Apps 3.0/SVB/ITSA/FUNCTIONAL/d_change_request_work_start_on_mysql.sql
@@ -1,5 +1,6 @@
 
-SELECT  * 
+ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed ' ELSE 'SUCCESS' END as Message
  FROM  svb_mdsdb.change_request_final SRC 
  LEFT JOIN  svb_mdwdb.d_change_request TRGT 
  ON (SRC.sys_id =TRGT.row_id  
