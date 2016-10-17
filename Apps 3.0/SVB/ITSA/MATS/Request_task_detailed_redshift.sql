fed34a84b959e47f15ca0dcb552ec6368b7b6676
prabhuram <prabhuram.sridharan@numerify.com>
2016-10-17 13:29:28 +0530

    Request Task Uploaded after query correction in svb

--- Apps 3.0/SVB/ITSA/MATS/Request_task_detailed_redshift.sql
+++ Apps 3.0/SVB/ITSA/MATS/Request_task_detailed_redshift.sql
@@ -4,6 +4,6 @@
  select'd_internal_contact_requested_for_manager_c' as Table_Name, count(a11.row_key) Row_Count
  from ldb.f_request_task a11
  join           ldb.d_internal_contact_requested_for   a111
-                  on          (a11.request_task_key) = a111.row_key)
+                  on          (a11.requested_for_key = a111.row_key)
 join           ldb.d_internal_contact_requested_for_manager_c               a120
                   on          (a111.manager_c_key = a120.row_key)
