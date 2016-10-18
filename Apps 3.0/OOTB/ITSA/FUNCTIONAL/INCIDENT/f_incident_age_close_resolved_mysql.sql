5cc5b403d85cb10da0ec3c0bea1ffe92817225c5
Rajashekhar <Rajashekhar>
2016-10-18 15:21:33 +0530

    Updated OOTB query

--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/f_incident_age_close_resolved_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/f_incident_age_close_resolved_mysql.sql
@@ -5,12 +5,7 @@ this is not an issue or data mismatch else investigate.
  CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.age' ELSE 'SUCCESS' END as Message
 from
 (
-<<<<<<< HEAD
 select count(1) as cnt FROM (select * from <<tenant>>_mdsdb.incident_final where opened_at < coalesce(resolved_at,closed_at)) SRC 
-=======
-
-select count(1) as cnt FROM (select sys_id,sourceinstance,opened_at,resolved_at,closed_at from <<tenant>>_mdsdb.incident_final where opened_at < coalesce(resolved_at,closed_at)) SRC 
->>>>>>> c8fd627169326ceddb3b80bb47c00b9f1ed293dc
   join <<tenant>>_mdwdb.f_incident f ON (SRC.sys_id =f.row_id  
  AND SRC.sourceinstance= f.source_id  )
 JOIN <<tenant>>_mdwdb.d_lov_map br ON f.state_src_key = br.src_key
