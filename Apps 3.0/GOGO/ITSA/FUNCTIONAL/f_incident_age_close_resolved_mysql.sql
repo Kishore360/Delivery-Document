eb9698b69ad8b93132bba81a9c70cacb3d43dd07
Shubha <Shubha.rao@numerify.com>
2016-10-18 14:22:54 +0530

    Mats and Cust queries updation

--- Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_incident_age_close_resolved_mysql.sql
+++ Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_incident_age_close_resolved_mysql.sql
@@ -5,12 +5,12 @@ this is not an issue or data mismatch else investigate.
  CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.age' ELSE 'SUCCESS' END as Message
 from
 (
-select count(1) as cnt FROM (select * from <<tenant>>_mdsdb.incident_final where opened_at < coalesce(resolved_at,closed_at)) SRC 
-  join <<tenant>>_mdwdb.f_incident f ON (SRC.sys_id =f.row_id  
+select count(1) as cnt FROM (select * from gogo_mdsdb.incident_final where opened_at < coalesce(resolved_at,closed_at)) SRC 
+  join gogo_mdwdb.f_incident f ON (SRC.sys_id =f.row_id  
  AND SRC.sourceinstance= f.source_id  )
-JOIN <<tenant>>_mdwdb.d_lov_map br ON f.state_src_key = br.src_key
+JOIN gogo_mdwdb.d_lov_map br ON f.state_src_key = br.src_key
 AND br.dimension_wh_code IN ('RESOLVED','CLOSED')
-JOIN <<tenant>>_mdwdb.d_incident a ON a.row_key = f.incident_key
+JOIN gogo_mdwdb.d_incident a ON a.row_key = f.incident_key
 AND f.source_id = a.source_id
 WHERE
 timestampdiff(DAY, SRC.opened_at, coalesce(SRC.u_resolved, SRC.closed_at)) <> f.age
--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_request_item_requestor_c_key_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_request_item_requestor_c_key_mysql.sql
@@ -7,5 +7,5 @@ from  nbcu_mdwdb.f_request_item a11
 left join nbcu_mdsdb.sc_req_item_final a12
 on a11.row_id=a12.sys_id and a11.source_id=a12.sourceinstance
 left join nbcu_mdwdb.d_internal_contact a13
-on concat('INTERNAL_CONTACT~',a12.u_requestor)=a13.row_id
+on concat('INTERNAL_CONTACT~',a12.u_requestor)=a13.row_id and a12.sourceinstance=a13.source_id
 where a13.row_key<>a11.requestor_c_key)a
