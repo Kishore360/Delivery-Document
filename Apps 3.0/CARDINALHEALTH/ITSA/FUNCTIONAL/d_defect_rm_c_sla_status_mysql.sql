a298b7e8ee878bee7b3f8d4a18f62de411b09b46
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-10-18 17:58:48 +0530

    query change of sla status on defect module

--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_defect_rm_c_sla_status_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_defect_rm_c_sla_status_mysql.sql
@@ -1,11 +1,33 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM (
-select count(1) as cnt 
-from cardinalhealth_mdwdb.f_defect_rm_c a
-left  JOIN  cardinalhealth_mdwdb.d_defect_rm_c b
-on  b.ROW_ID = a.row_id and a.source_id=b.source_id
-left join cardinalhealth_mdwdb.d_lov_map br 
-on a.status_rm_src_c_key = br.src_key 
-where CASE WHEN br.dimension_wh_code = 'OPEN' THEN 'OPEN' WHEN br.dimension_wh_code <> 'OPEN' AND b.due_date>b.closed_date THEN 'ACHIEVED' ELSE 'BREACHED' END<>b.sla_status)c
+SELECT 
+    CASE
+        WHEN cnt > 0 THEN 'FAILURE'
+        ELSE 'SUCCESS'
+    END AS Result,
+    CASE
+        WHEN cnt > 0 THEN 'Data did not Match.'
+        ELSE 'Data Matched'
+    END AS Message
+FROM
+    (select 
+        count(1) as cnt
+    from
+       
+     cardinalhealth_mdwdb.d_defect_rm_c b  JOIN
+ cardinalhealth_mdwdb.f_defect_rm_c a ON 
+b.row_key = a.defect_c_key
+    left join cardinalhealth_mdwdb.d_lov_map br ON a.status_rm_src_c_key = br.src_key 
+AND br.dimension_class = 'STATUS_C~TASK'
+    where
+        CASE
+            WHEN br.dimension_wh_code = 'OPEN' THEN 'PENDING'
+            WHEN
+              
+                    date(b.due_date) >= date(b.closed_date)
+            THEN
+                'ACHIEVED'
+            ELSE 'BREACHED'
+        END <> b.sla_status) c
+
+
+
+
