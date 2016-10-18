13f88fd5e4852c7f4296feb42a9500b4b40568b4
prabhuram <prabhuram.sridharan@numerify.com>
2016-10-18 11:37:00 +0530

    Updating Molinahealth Query

---
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_change_request_parent_c_key_mysql.sql
@@ -0,0 +1,7 @@
+SELECT 
+ CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.parent_c_key' ELSE 'SUCCESS' END as Message
+FROM molinahealth_mdsdb.change_request_final src 
+LEFT JOIN molinahealth_mdwdb.f_change_request trgt ON src.sys_id = trgt.row_id AND src.sourceinstance = trgt.source_id
+LEFT JOIN molinahealth_mdwdb.d_task lkp ON src.parent = lkp.row_id AND src.sourceinstance = lkp.source_id
+WHERE COALESCE(lkp.row_key,CASE WHEN src.parent IS NULL THEN 0 else -1 end)<> (trgt.parent_c_key) ;
