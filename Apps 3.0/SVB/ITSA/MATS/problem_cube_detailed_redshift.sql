f6e5bef6ecff9dabc2cff7a8ec1589df5e0e22fc
prabhuram <prabhuram.sridharan@numerify.com>
2016-10-19 11:46:05 +0530

    Uploading the problem cube query updated

--- Apps 3.0/SVB/ITSA/MATS/problem_cube_detailed_redshift.sql
+++ Apps 3.0/SVB/ITSA/MATS/problem_cube_detailed_redshift.sql
@@ -1,7 +1,7 @@
  select'f_problem' as Table_Name, count(a11.row_key) Row_Count
  from  ldb.f_problem             a11 
  union
- select'd_calendar_date' as Table_Name, count(a11.row_key) Row_Count
+ select'd_problem_state_c' as Table_Name, count(a11.row_key) Row_Count
  from  ldb.f_problem             a11 
 join ldb.d_problem_state_c         a12
-on (a11.state_src_key = a12.row_key)
+on (a11.state_state_src_key  = a12.row_key)
