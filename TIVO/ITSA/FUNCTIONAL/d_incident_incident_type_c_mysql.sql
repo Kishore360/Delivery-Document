1786bf6702afe58d8921c5258651203ae4d3970c
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-10-19 12:14:34 +0530

    TIVO corrected SQL Syntax error of OOTB and CUST

--- TIVO/ITSA/FUNCTIONAL/d_incident_incident_type_c_mysql.sql
+++ TIVO/ITSA/FUNCTIONAL/d_incident_incident_type_c_mysql.sql
@@ -1,9 +1,7 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN count(1) > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
-FROM (select
-count(*) as cnt
 from  tivo_mdwdb.d_incident a11
 left join tivo_mdsdb.incident_final a12
 on a11.row_id=a12.sys_id and a11.source_id=a12.sourceinstance
-where a12.u_incident_type <>a11.incident_type_c)a
+where coalesce(a12.u_incident_type,'UNSPECIFIED')<>a11.incident_type_c
--- TIVO/ITSA/FUNCTIONAL/d_incident_knowledge_number_c_mysql.sql
+++ TIVO/ITSA/FUNCTIONAL/d_incident_knowledge_number_c_mysql.sql
@@ -7,5 +7,5 @@ from tivo_mdsdb.incident_final a11
 left join tivo_mdwdb.d_incident a12
 on a11.sys_id=a12.row_id and a11.sourceinstance=a12.source_id
 left join tivo_mdsdb.kb_knowledge_final a13
-on a12.u_knowledge_utilized=a13.sys_id
+on a11.u_knowledge_utilized=a13.sys_id
 where coalesce(a13.number,'')<>a12.knowledge_number_c)a
