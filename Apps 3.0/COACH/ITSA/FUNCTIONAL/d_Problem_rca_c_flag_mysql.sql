1da7404febc5fe5b646edb4bfbb303e4206704c5
vijay rao <vijay.rao@numerify.com>
2016-10-19 09:31:28 +0530

    Coach CUST query updated

--- Apps 3.0/COACH/ITSA/FUNCTIONAL/d_Problem_rca_c_flag_mysql.sql
+++ Apps 3.0/COACH/ITSA/FUNCTIONAL/d_Problem_rca_c_flag_mysql.sql
@@ -3,7 +3,6 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ELSE 'Data Matched' END AS Message 
 FROM (
 select count(1) as cnt from coach_mdsdb.problem_final a
- left  JOIN  coach_mdwdb.d_problem b
+left  JOIN  coach_mdwdb.d_problem b
 on  a.SYS_ID= b.ROW_ID and a.sourceinstance=b.source_id
- where case when a.u_rca = 1 then 'Y'else 'N'end <>b.rca_c_flag )b
-
+where CASE WHEN a.u_rca IS NOT NULL AND a.u_rca <> '0' THEN 'Y' ELSE 'N' END <>b.rca_c_flag )b;
