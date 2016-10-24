8c802e2dc47abcdcddef370f96d96edae0261940
prabhuram <prabhuram.sridharan@numerify.com>
2016-10-24 18:02:58 +0530

    Committing the Query

---
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/d_hr_case_c_employee_title_mysql.sql
@@ -0,0 +1,17 @@
+select 
+case 
+when sum(failures_cnt) > 0 and length(group_concat(distinct failures)) >= 1024 then concat('Failed. Data does not match for ',sum(failures_cnt),' records. Few Record Ids are : ',substring_index(group_concat(distinct failures),',',-31))
+
+when sum(failures_cnt) > 0 and length(group_concat(distinct failures)) < 1024
+then concat('Failed. Data does not match for ',sum(failures_cnt),' records. Record Ids : ',substring_index(group_concat(distinct failures),',',-sum(failures_cnt)))
+
+else 'Success. All warehouse records are matching with source.' end Result
+from 
+(select 
+
+case when COALESCE(src.employee_title,'UNSPECIFIED') <> COALESCE(trgt.employee_title,'UNSPECIFIED') then src.record_id else '' end as failures,
+
+case when COALESCE(src.employee_title,'UNSPECIFIED') <> COALESCE(trgt.employee_title,'UNSPECIFIED') then 1 else 0 end as failures_cnt
+
+from molinahealth_mdsdb.u_hr_case_ext_final src
+left join molinahealth_mdwdb.d_hr_case_c trgt on trgt.row_id = src.record_id and trgt.source_id = src.sourceinstance) fnl ;
