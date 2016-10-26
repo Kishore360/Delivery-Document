768cadfd4f431718c8d6e0b837cdade7c56c9e84
prabhuram <prabhuram.sridharan@numerify.com>
2016-10-26 17:35:11 +0530

    committing the TIVO Queries

--- Apps 3.0/TIVO/ITSA/FUNCTIONAL/f_incident_granularity_check_mysql.sql
+++ Apps 3.0/TIVO/ITSA/FUNCTIONAL/f_incident_granularity_check_mysql.sql
@@ -1,8 +1,8 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN count(1) > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 FROM (select
-incident_number,count(*) as cnt
+incident_number,count(1) as cnt
 from  tivo_mdwdb.f_incident a12
 group by 1
-having count(*)>1)a;
+having count(1)>1)a;
