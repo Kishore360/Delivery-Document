67f1232bc20b5f89190db591f5c59c5e13247f56
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-10-27 18:23:51 +0530

    MercuryIns QA  cleanup

--- Apps 3.0/MERCURYINS/ITSA/FUNCTIONAL/d_incident_first_call_resolution_flag_mysql.sql
+++ Apps 3.0/MERCURYINS/ITSA/FUNCTIONAL/d_incident_first_call_resolution_flag_mysql.sql
@@ -1,11 +1,6 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.first_call_resolution_flag' ELSE 'SUCCESS' END as Message
- FROM  mercuryins_mdwdb.d_incident TRGT
-   JOIN  mercuryins_mdwdb.f_incident TRGTF 
- ON (TRGTF.incident_key =TRGT.row_key
- AND TRGTF.source_id =TRGT.source_id  and TIMESTAMPDIFF(MINUTE,TRGT.opened_on,TRGT.last_resolved_on)<30)
- 
- JOIN mercuryins_mdwdb.d_lov_map LM ON TRGTF.state_src_key=LM.src_key AND LM.dimension_class = 'STATE~INCIDENT'
-AND  LM.dimension_wh_code IN('RESOLVED','CLOSED')
-WHERE  CASE WHEN  TIMESTAMPDIFF(MINUTE,TRGT.opened_on,TRGT.last_resolved_on)<30
-AND  LM.dimension_wh_code IN('RESOLVED','CLOSED') THEN 'Y' ELSE 'N' END <> (TRGT.first_call_resolution_flag )
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident_asc_c.assignment_group_key' ELSE 'SUCCESS' END as Message
+  FROM  
+mercuryins_mdsdb.incident_final a left  join
+mercuryins_mdwdb.d_incident TRGT on a.sys_id=TRGT.row_id and a.sourceinstance=TRGT.source_id
+ where case when a.u_fcr=1 then 'Y' else 'N' end  <> (TRGT.first_call_resolution_flag ) 
