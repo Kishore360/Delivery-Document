4b4dbfa06f02f2c629f820f9c4924b60a722b499
vijay rao <vijay.rao@numerify.com>
2016-10-28 15:43:46 +0530

    Adding source_c_key test queries for NBCU

---
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_incident_source_src_c_key_mysql.sql
@@ -0,0 +1,10 @@
+ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.source_src_c_key' ELSE 'SUCCESS' END as Message
+  FROM nbcu_mdsdb.incident_final SRC
+ LEFT JOIN nbcu_mdwdb.f_incident TRGT 
+ ON (SRC.sys_id ) =(TRGT.row_id ) 
+ AND (SRC.sourceinstance )=(TRGT.source_id ) 
+ LEFT JOIN nbcu_mdwdb.d_lov LKP 
+ ON (CONCAT('SOURCE_C~TASK~~~',UPPER(SRC.u_source)))= (LKP.src_rowid )
+ AND SRC.sourceinstance= LKP.source_id
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_source IS NULL THEN 0 else -1 end)<> (TRGT.source_src_c_key) ;
