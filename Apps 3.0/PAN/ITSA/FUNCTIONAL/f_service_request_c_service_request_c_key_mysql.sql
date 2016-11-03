20175686b9df299a82fa67f8604a93c608f02cc8
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-11-03 18:30:12 +0530

    Pan missed cust queries

--- Apps 3.0/PAN/ITSA/FUNCTIONAL/f_service_request_c_service_request_c_key_mysql.sql
+++ Apps 3.0/PAN/ITSA/FUNCTIONAL/f_service_request_c_service_request_c_key_mysql.sql
@@ -1,5 +1,5 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_service_request_c.service_request_c_key' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_service_request_c.service_request_c_key' ELSE 'SUCCESS' END as Message
  FROM pan_mdsdb.u_service_request_final SRC 
  LEFT JOIN pan_mdwdb.f_service_request_c TRGT 
  ON (SRC.sys_id =TRGT.row_id  
