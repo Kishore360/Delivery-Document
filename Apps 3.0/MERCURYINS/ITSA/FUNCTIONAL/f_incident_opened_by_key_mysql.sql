1ddf2d1b01df93400be16ef8934aa86845090bdb
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-11-04 13:09:19 +0530

    correctiong MI cust failures

--- Apps 3.0/MERCURYINS/ITSA/FUNCTIONAL/f_incident_opened_by_key_mysql.sql
+++ Apps 3.0/MERCURYINS/ITSA/FUNCTIONAL/f_incident_opened_by_key_mysql.sql
@@ -7,10 +7,10 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
 LEFT JOIN mercuryins_mdwdb.d_internal_contact LKP 
- ON ( concat('INTERNAL_CONTACT~',u_caller)= LKP.row_id 
+ ON ( concat('INTERNAL_CONTACT~',opened_by)= LKP.row_id 
 AND SRC.sourceinstance= LKP.source_id ) AND 
 COALESCE(CONVERT_TZ (SRC.opened_at,'GMT','America/Los_Angeles'), CONVERT_TZ (coalesce(SRC.closed_at,SRC.sys_updated_on),'GMT','America/Los_Angeles'),'UNSPECIFIED') 
 BETWEEN effective_from AND effective_to
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_caller IS NULL THEN 0 else -1 end)<> (TRGT.opened_by_key)
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.opened_by IS NULL THEN 0 else -1 end)<> (TRGT.opened_by_key)
  
  
