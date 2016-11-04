c21da98775dad9470234dd181e6d75cd1e86d5bc
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-11-04 13:17:08 +0530

    cust query Mi

--- Apps 3.0/MERCURYINS/ITSA/FUNCTIONAL/ASC_d_incident_last_resolved_on_mysql.sql
+++ Apps 3.0/MERCURYINS/ITSA/FUNCTIONAL/ASC_d_incident_last_resolved_on_mysql.sql
@@ -3,9 +3,9 @@ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_incident_asc_c.la
  Message  from (
 select count(1) as cnt  FROM mercuryins_mdsdb.u_asc_ticket_final SRC  LEFT JOIN mercuryins_mdwdb.d_incident_asc_c TRGT  
 ON (SRC.sys_id =TRGT.row_id   AND SRC.sourceinstance= TRGT.source_id  )
-left join mercuryins_mdwdb.f_incident_asc_c f on f.incident_key=TRGT.row_key
+left join   mercuryins_mdwdb.f_incident_asc_c f on f.incident_asc_c_key=TRGT.row_key
 JOIN mercuryins_mdwdb.d_lov_map dlm 
-ON f.state_src_key = dlm.src_key
+ON f.asc_incident_state_c_key = dlm.src_key
  WHERE 
 case when
  dlm.dimension_wh_code = 'RESOLVED' then 
