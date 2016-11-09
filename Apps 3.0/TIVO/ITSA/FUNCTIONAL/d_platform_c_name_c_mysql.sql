027e42a047f9f4b8840fcc21a8e49a2e2a2dc463
prabhuram <prabhuram.sridharan@numerify.com>
2016-11-09 18:40:44 +0530

    Added TIVO related CUST Queries

---
+++ Apps 3.0/TIVO/ITSA/FUNCTIONAL/d_platform_c_name_c_mysql.sql
@@ -0,0 +1,12 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+FROM (
+select count(1) as cnt  
+from tivo_mdsdb.u_platform_final a
+ left  JOIN   tivo_mdwdb.d_platform_c b
+on  b.ROW_ID=a.sys_id and a.sourceinstance=b.source_id
+ where a.u_name <> b.name_c) temp;
+ 
+ 
+ 
