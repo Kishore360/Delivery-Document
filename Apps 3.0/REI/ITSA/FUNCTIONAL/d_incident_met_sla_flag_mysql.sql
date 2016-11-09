c1ada55dbe34fc21908f5ae62443db5462883de6
Shubha <Shubha.rao@numerify.com>
2016-11-09 18:35:59 +0530

    correcting the cust queries for rei

--- Apps 3.0/REI/ITSA/FUNCTIONAL/d_incident_met_sla_flag_mysql.sql
+++ Apps 3.0/REI/ITSA/FUNCTIONAL/d_incident_met_sla_flag_mysql.sql
@@ -1,7 +1,7 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_task.open_to_close_duration' ELSE 'SUCCESS' END as Message
- FROM rei_mdsdb.incident_final SRC 
+FROM rei_mdsdb.task_sla_final SRC 
  LEFT JOIN rei_mdwdb.d_incident TRGT 
-ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE  IF(SRC.made_sla = TRUE,'Y','N')<>TRGT.met_sla_flag 
+ON (SRC.task =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  ) 
+ WHERE  case when SRC.stage  = 'breached' then 'N' else 'Y' end<>TRGT.met_sla_flag 
