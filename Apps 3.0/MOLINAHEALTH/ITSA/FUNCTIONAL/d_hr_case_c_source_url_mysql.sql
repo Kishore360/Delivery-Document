07c2de9d84cf7c73b2564b8c9d7490246db08bc0
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2016-11-14 18:30:51 +0530

    Uploading the updated queries as part of Molina Regression

--- Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/d_hr_case_c_source_url_mysql.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/d_hr_case_c_source_url_mysql.sql
@@ -1,10 +1,12 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.open_to_close_duration' ELSE 'SUCCESS' END as Message
- FROM molinahealth_mdsdb.u_hr_case_final SRC
- LEFT JOIN molinahealth_mdwdb.d_hr_case_c TRGT 
+SELECT 
+CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_hr_task_c.source_url' ELSE 'SUCCESS' END as Message
+  from 
+ (select SRC.sys_id,TRGT.source_url,CONCAT('<a href=\"https://molina.service-now.com/nav_to.do?uri=u_hr_case.do?sys_id=',SRC.sys_id,'\" target=\"_blank\">',SRC.number,'</a>') as src_url,
+case when CONCAT('<a href=\"https://molina.service-now.com/u_hr_case.do?sys_id=',SRC.sys_id,'\" target=\"_blank\">',SRC.number,'</a>') =  source_url then 1
+when CONCAT('<a href=\"https://molina.service-now.com/nav_to.do?uri=u_hr_case.do?sys_id=',SRC.sys_id,'\" target=\"_blank\">',SRC.number,'</a>') = source_url then 1 else 0 end as fail_cnt
+FROM molinahealth_mdsdb.u_hr_case_final SRC
+LEFT JOIN molinahealth_mdwdb.d_hr_case_c TRGT 
  ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- where
-CONCAT('<a href=\"https://molina.service-now.com/u_hr_case.do?sys_id=',sys_id,'\" target=\"_blank\">',number,'</a>') <>source_url
-
-
+ AND SRC.sourceinstance= TRGT.source_id  ) ) fnl
+where fail_cnt = 0;
