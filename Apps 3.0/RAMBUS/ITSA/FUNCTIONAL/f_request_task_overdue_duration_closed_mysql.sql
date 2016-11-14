57bb3c6aec7f4c83cb6772798078cc39257cb443
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-11-14 21:39:18 +0530

    DELV-3769 rambus

---
+++ Apps 3.0/RAMBUS/ITSA/FUNCTIONAL/f_request_task_overdue_duration_closed_mysql.sql
@@ -0,0 +1,9 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_task.OPEN_TO_CLOSE_DURATION' ELSE 'SUCCESS' END as Message
+ from
+ rambus_mdwdb.f_request_task f 
+join rambus_mdwdb.d_request_task d on d.row_id = f.row_id
+JOIN rambus_mdwdb.d_lov_map br ON f.state_src_key = br.src_key and br.dimension_wh_code='CLOSED'
+join rambus_mdwdb.d_o_data_freshness dat on f.source_id = dat.source_id
+where f.overdue_duration <>  case when d.closed_on > d.due_on then TIMESTAMPDIFF(SECOND,d.due_on,d.closed_on) else 0 end
+ ;
