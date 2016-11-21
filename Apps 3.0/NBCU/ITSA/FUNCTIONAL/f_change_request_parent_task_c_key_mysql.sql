e54355e21d9c1ebab6fbdddfc3ec2a0bc87b2f0b
vijay rao <vijay.rao@numerify.com>
2016-11-21 12:02:16 +0530

    NBCU Phase 4 cust queries

---
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_change_request_parent_task_c_key_mysql.sql
@@ -0,0 +1,9 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
+from
+nbcu_mdsdb.change_request_final a
+join nbcu_mdwdb.f_change_request b
+on a.sys_id=b.row_id and a.sourceinstance=b.source_id
+join nbcu_mdwdb.d_task c
+on a.sys_id=c.row_id and a.sourceinstance=c.source_id
+where coalesce(c.row_key, case when a.sys_id is null then 0 else -1 end)<> b.parent_task_c_key;
