c595a5fd14e330fe279be75d745323a4871d3642
prabhuram <prabhuram.sridharan@numerify.com>
2016-11-23 17:19:59 +0530

    intuit variable queries updated

--- Apps 3.0/INTUIT/ITSA/FUNCTIONAL/d_request_task_time_to_assign_catalog_task_c_mysql.sql
+++ Apps 3.0/INTUIT/ITSA/FUNCTIONAL/d_request_task_time_to_assign_catalog_task_c_mysql.sql
@@ -3,7 +3,6 @@
  FROM(
 select count(1) as cnt from intuit_mdwdb.d_request_task a
 LEFT JOIN intuit_mdsdb.sys_audit_final b ON a.row_id = b.documentkey
-	AND b.tablename='sc_task'
+where  b.tablename='sc_task'
 	AND b.fieldname='assgined_to'
-	AND b.oldvalue IS NULL
-where  a.time_to_assign_catalog_task_c<> COALESCE(CONVERT_TZ(b.sys_created_on,'GMT' ,'America/Los_Angeles'),a.created_on))b;
+	AND b.oldvalue IS NULL AND a.time_to_assign_catalog_task_c<> COALESCE(CONVERT_TZ(b.sys_created_on,'GMT' ,'America/Los_Angeles'),a.created_on))b;
--- Apps 3.0/INTUIT/ITSA/FUNCTIONAL/f_incident_variable_c_datetime_value_mysql.sql
+++ Apps 3.0/INTUIT/ITSA/FUNCTIONAL/f_incident_variable_c_datetime_value_mysql.sql
@@ -2,21 +2,21 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 FROM(select count(1) as cnt 
-from rei_mdsdb.sc_item_option_final qa  
-inner join  rei_mdsdb.sc_item_option_mtom_final b 
+from intuit_mdsdb.sc_item_option_final qa  
+inner join  intuit_mdsdb.sc_item_option_mtom_final b 
 on qa.sys_id=b.sc_item_option and qa.sourceinstance = b.sourceinstance 
-left join rei_mdsdb.question_final q 
+left join intuit_mdsdb.question_final q 
 on qa.item_option_new=q.sys_id and qa.sourceinstance=q.sourceinstance 
 and q.sys_id  in 
 (select  row_id from  
-rei_workdb.lsm_ls_variable_list lvl where lvl.table_name='request_item' and lvl.variable_type!='Reference') 
-join rei_mdsdb.sys_choice_final c 
+intuit_workdb.lsm_ls_variable_list lvl where lvl.table_name='incident' and lvl.variable_type!='Reference') 
+join intuit_mdsdb.sys_choice_final c 
 on q.type=c.value and c.element='type' and c.name like 'question' and c.label !='Reference' and  c.sourceinstance=q.sourceinstance
-left join rei_mdsdb.question_choice_final qc 
+left join intuit_mdsdb.question_choice_final qc 
 on qa.value=qc.value and qa.item_option_new=qc.question and qa.sourceinstance=qc.sourceinstance 
-left join rei_workdb.lsm_ls_variable_datatype lvd on c.label = lvd.variable_type 
+left join intuit_workdb.lsm_ls_variable_datatype lvd on c.label = lvd.variable_type 
 join
-rei_mdwdb.f_request_item_variable_c trgt
+intuit_mdwdb.f_incident_variable_c trgt
 on qa.sys_id=trgt.row_id and qa.sourceinstance=trgt.source_id
 where case when  (c.label !='Reference' and lvd.variable_type!='Reference') then
 Case when lvd.data_type ='DATETIME' THEN 
