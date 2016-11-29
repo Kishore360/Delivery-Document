ffa588e47345373c7078c8d5e1852eda98c8a5b4
prabhuram <prabhuram.sridharan@numerify.com>
2016-11-29 16:14:45 +0530

    committing the tjx deleted queries

--- Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_incident_age_key_mysql.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_incident_age_key_mysql.sql
@@ -5,5 +5,6 @@ from molinahealth_mdwdb.f_incident f
 LEFT JOIN molinahealth_mdwdb.d_lov L  
 ON ((f.age div 86400 BETWEEN L.lower_range_value AND L.upper_range_value)
 	AND L.dimension_class = 'AGEBUCKET_WH~INCIDENT' )
-WHERE coalesce(L.row_key,-1 )  <> f.age_key
+	join molinahealth_mdwdb.d_lov_map map on f.state_src_key=map.src_key
+WHERE dimension_wh_code<>'CANCELLED' and coalesce(L.row_key,case when age is null then 0 else -1 end )  <> f.age_key
 
