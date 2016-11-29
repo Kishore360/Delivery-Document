7d6a5716e4b2e4ef3a4618162cd561e48c7ba39b
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-11-29 23:46:12 +0530

    wow cust syntax error correction

--- Apps 3.0/WOW/ITSA/FUNCTIONAL/d_incident_task_c_DUE_ON_mysql.sql
+++ Apps 3.0/WOW/ITSA/FUNCTIONAL/d_incident_task_c_DUE_ON_mysql.sql
@@ -7,5 +7,5 @@ select count(1) as cnt
 from wow_mdsdb.u_incident_task_final s
 left join wow_mdwdb.d_incident_task_c t
 on s.sys_id=t.row_id and s.sourceinstance = t.source_id where
-CONVERT_TZ(i.due_date,'GMT','US/Central') <> t.DUE_ON)a
+CONVERT_TZ(s.due_date,'GMT','US/Central') <> t.DUE_ON)a
 
