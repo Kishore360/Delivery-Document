0617a9bd1f9df43115312bc0bdf0b3ca5e44224c
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2016-11-30 16:42:35 +0530

    tesm Prod query

---
+++ TESM/ITSA/FUNCTIONAL/d_x_tori2_opd_incoming_alerts_final_c_u_object_type_c_mysql.sql
@@ -0,0 +1,9 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.'
+ELSE 'Data Matched' END AS Message
+FROM (
+select count(1) as cnt 
+from tesm_mdsdb.x_tori2_opd_incoming_alerts_final a
+left  JOIN   tesm_mdwdb.d_x_tori2_opd_incoming_alerts_final_c b
+on  b.ROW_ID=a.SYS_ID and a.sourceinstance=b.source_id
+where a.u_object_type <> b.u_object_type_c) temp;
