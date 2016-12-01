39119aef8697ab53a1d62aac12c1b5434acc6a3c
vijay rao <vijay.rao@numerify.com>
2016-12-01 18:18:13 +0530

    location_name cust query fix for REI

--- Apps 3.0/REI/ITSA/FUNCTIONAL/d_location_location_number_mysql.sql
+++ Apps 3.0/REI/ITSA/FUNCTIONAL/d_location_location_number_mysql.sql
@@ -6,5 +6,6 @@ FROM
 from rei_mdwdb.d_location a 
 join rei_mdsdb.cmn_location_final b 
 ON a.row_id = b.sys_id AND a.source_id = b.sourceinstance
+join rei_mdwdb.d_location_clean_c src on src.row_id = a.location_name
+where src.target_location_name<>a.location_number )c ;
 
-where b.name<>a.location_number)c ;
