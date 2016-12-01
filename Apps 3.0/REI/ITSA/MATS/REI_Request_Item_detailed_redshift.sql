59fc3a99217eb2415f917f4838557091cd310db2
vijay rao <vijay.rao@numerify.com>
2016-12-01 11:00:47 +0530

    Updating cube queries for REI

--- Apps 3.0/REI/ITSA/MATS/REI_Request_Item_detailed_redshift.sql
+++ Apps 3.0/REI/ITSA/MATS/REI_Request_Item_detailed_redshift.sql
@@ -1,35 +1,35 @@
 select 'ldb.f_request_item a11 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
 union
-select 'ldb.d_request_item a12 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact a12 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
-join ldb.d_request_item a12 
-on (a11.request_item_key = a12.row_key) 
+join ldb.d_internal_contact a12 
+on (a11.opened_by_key = a12.row_key) 
 union
-select 'ldb.d_request_item_variable_requested_completion_date_c a13 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_master_item a13 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
-join ldb.d_request_item_variable_requested_completion_date_c a13 
-on (a11.request_item_key = a13.row_key) 
+join ldb.d_master_item a13 
+on (a11.catalog_item_key = a13.row_key) 
 union
-select 'ldb.d_internal_contact a14 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_requested_for_c a14 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
-join ldb.d_internal_contact a14 
-on (a11.opened_by_key = a14.row_key) 
+join ldb.d_internal_contact_requested_for_c a14 
+on (a11.requested_for_key = a14.row_key) 
 union
-select 'ldb.d_master_item a15 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_request_item a15 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
-join ldb.d_master_item a15 
-on (a11.catalog_item_key = a15.row_key) 
+join ldb.d_request_item a15 
+on (a11.request_item_key = a15.row_key) 
 union
-select 'ldb.d_internal_contact_requested_for_c a16 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_calendar_date a16 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
-join ldb.d_internal_contact_requested_for_c a16 
-on (a11.requested_for_key = a16.row_key) 
+join ldb.d_calendar_date a16 
+on (a11.opened_on_key = a16.row_key) 
 union
-select 'ldb.d_calendar_date a17 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_request_item_variable_requested_completion_date_c a17 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
-join ldb.d_calendar_date a17 
-on (a11.opened_on_key = a17.row_key) 
+join ldb.d_request_item_variable_requested_completion_date_c a17 
+on (a11.request_item_key = a17.row_key) 
 union
 select 'ldb.d_internal_contact_assigned_to a18 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
@@ -43,10 +43,10 @@ on (a11.assignment_group_key = a19.row_key)
 union
 select 'ldb.d_internal_contact_mdm a110 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
-join ldb.d_internal_contact a14 
-on (a11.opened_by_key = a14.row_key) 
+join ldb.d_internal_contact a12 
+on (a11.opened_by_key = a12.row_key) 
 join ldb.d_internal_contact_mdm a110 
-on (a14.row_current_key = a110.row_current_key) 
+on (a12.row_current_key = a110.row_current_key) 
 union
 select 'ldb.d_request a111 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
@@ -75,10 +75,10 @@ on (a11.requested_for_key = a115.row_key)
 union
 select 'ldb.d_internal_organization_department_c a116 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
-join ldb.d_internal_contact_requested_for_c a16 
-on (a11.requested_for_key = a16.row_key) 
+join ldb.d_internal_contact_requested_for_c a14 
+on (a11.requested_for_key = a14.row_key) 
 join ldb.d_internal_organization_department_c a116 
-on (a16.department_c_key = a116.row_key) 
+on (a14.department_c_key = a116.row_key) 
 union
 select 'ldb.d_internal_contact_task_closed_by a117 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
@@ -87,8 +87,7 @@ on (a11.closed_by_key = a117.row_key)
 union
 select 'ldb.d_calendar_month a118 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_request_item a11 
-join ldb.d_calendar_date a17 
-on (a11.opened_on_key = a17.row_key) 
+join ldb.d_calendar_date a16 
+on (a11.opened_on_key = a16.row_key) 
 join ldb.d_calendar_month a118 
-on (a17.month_start_date_key = a118.row_key) 
- 
+on (a16.month_start_date_key = a118.row_key) 
--- Apps 3.0/REI/ITSA/MATS/REI_Request_task_Backlog_detailed_redshift.sql
+++ Apps 3.0/REI/ITSA/MATS/REI_Request_task_Backlog_detailed_redshift.sql
@@ -1,59 +1,60 @@
-select'ldb.f_request_task' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
+select 'ldb.f_request_task a11 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
 union
-select'ldb.d_calendar_date' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_calendar_date         a12
-on (a11.opened_on_key=a12.row_key)
+select 'ldb.d_calendar_date a12 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_calendar_date a12 
+on (a11.opened_on_key = a12.row_key) 
 union
-select'ldb.d_request_item_variable_requested_completion_date_c' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_request_item_variable_requested_completion_date_c         a13
-on (a11.request_item_key=a13.row_key)
+select 'ldb.d_master_item a13 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_master_item a13 
+on (a11.catalog_item_key = a13.row_key) 
 union
-select'ldb.d_master_item' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_master_item         a14
-on (a11.catalog_item_key=a14.row_key)
+select 'ldb.d_request_task a14 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_request_task a14 
+on (a11.request_task_key = a14.row_key) 
 union
-select'ldb.d_request_task' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_request_task         a15
-on (a11.request_task_key=a15.row_key)
+select 'ldb.d_request_item_variable_requested_completion_date_c a15 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_request_item_variable_requested_completion_date_c a15 
+on (a11.request_item_key = a15.row_key) 
 union
-select'ldb.d_internal_contact_assigned_to' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_internal_contact_assigned_to         a16
-on (a11.assigned_to_key=a16.row_key)
+select 'ldb.d_internal_contact_assigned_to a16 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_internal_contact_assigned_to a16 
+on (a11.assigned_to_key = a16.row_key) 
 union
-select'ldb.d_internal_organization_group' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_internal_organization_group         a17
-on (a11.assignment_group_key=a17.row_key)
+select 'ldb.d_internal_organization_group a17 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_internal_organization_group a17 
+on (a11.assignment_group_key = a17.row_key) 
 union
-select'ldb.d_internal_organization_department' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_internal_organization_department         a18
-on (a11.opened_by_department_key=a18.row_key)
+select 'ldb.d_internal_organization_department a18 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_internal_organization_department a18 
+on (a11.opened_by_department_key = a18.row_key) 
 union
-select'ldb.d_request_item' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_request_item         a19
-on (a11.request_item_key=a19.row_key)
+select 'ldb.d_request_item a19 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_request_item a19 
+on (a11.request_item_key = a19.row_key) 
 union
-select'ldb.d_sc_task_priority' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_sc_task_priority         a110
-on (a11.priority_src_key=a110.row_key)
+select 'ldb.d_sc_task_priority a110 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_sc_task_priority a110 
+on (a11.priority_src_key = a110.row_key) 
 union
-select'ldb.d_sc_task_state' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11 
-join ldb.d_sc_task_state         a111
-on (a11.state_src_key=a111.row_key)
+select 'ldb.d_sc_task_state a111 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_sc_task_state a111 
+on (a11.state_src_key = a111.row_key) 
 union
-select'ldb.d_calendar_month' as Table_Name, count(a11.row_key) Row_Count
- from  ldb.f_request_task             a11
-join ldb.d_calendar_date         a12
-on (a11.opened_on_key=a12.row_key) 
-join ldb.d_calendar_month         a112
-on (a12.month_start_date_key=a112.row_key)
+select 'ldb.d_calendar_month a112 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_request_task a11 
+join ldb.d_calendar_date a12 
+on (a11.opened_on_key = a12.row_key) 
+join ldb.d_calendar_month a112 
+on (a12.month_start_date_key = a112.row_key) 
+ 
