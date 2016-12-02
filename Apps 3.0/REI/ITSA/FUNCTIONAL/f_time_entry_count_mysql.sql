2a43e22d32efec4183d0c8cd83128d4749d750f4
vijay rao <vijay.rao@numerify.com>
2016-12-02 10:27:46 +0530

    Final Update on REI sql cust query fixes

--- Apps 3.0/REI/ITSA/FUNCTIONAL/f_time_entry_count_mysql.sql
+++ Apps 3.0/REI/ITSA/FUNCTIONAL/f_time_entry_count_mysql.sql
@@ -3,4 +3,4 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ELSE 'Data Matched' END AS Message 
 FROM (select count(*) as cnt from rei_mdsdb.time_card_final where sys_id  not in(
 select substring(row_id,1,32) from rei_mdwdb.f_time_entry
-))c;
+) and year(week_starts_on) >= 1970)c; 
