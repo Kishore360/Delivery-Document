64a3932639d9c3740757dee5d7093fc9152588d4
Rajashekhar <Rajashekhar>
2016-12-05 11:35:50 +0530

    QA query currection

--- Apps 3.0/GILEAD/ITSA/FUNCTIONAL/d_request_task_closure_code_c_key_mysql.sql
+++ Apps 3.0/GILEAD/ITSA/FUNCTIONAL/d_request_task_closure_code_c_key_mysql.sql
@@ -4,10 +4,10 @@ ELSE 'Data Matched' END AS Message
 FROM(
 select count(*) as cnt 
 from gilead_mdwdb.d_request_task a
-inner join gilead_mdsdb.sc_task_final b
+inner join (select sys_id,sourceinstance,CONCAT('CLOSURE_CODE','~','SC_TASK','~','~','~',UPPER(u_closure_code)) as u_closure_code from gilead_mdsdb.sc_task_final) b
 on a.row_id=b.sys_id
 and a.source_id=b.sourceinstance
 left join gilead_mdwdb.d_lov c
-on CONCAT('CLOSURE_CODE','~','SC_TASK','~','~','~',UPPER(b.u_closure_code)=c.row_id
+on b.u_closure_code=c.row_id
 AND c.source_id=b.sourceinstance
 where c.row_key<>a.closure_code_c_key) c;
