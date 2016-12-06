a27a7c2689f61667ad2315171d17e8dc16c586de
prabhuram <prabhuram.sridharan@numerify.com>
2016-12-06 17:32:30 +0530

    concat spelling corrected in bbandt

--- Apps 3.0/BBANDT/ITSA/FUNCTIONAL/f_change_request_group_coordinated_with_c_key_mysql.sql
+++ Apps 3.0/BBANDT/ITSA/FUNCTIONAL/f_change_request_group_coordinated_with_c_key_mysql.sql
@@ -11,6 +11,6 @@ on a.sys_id=b.row_id and
 a.sourceinstance=b.source_id
 
 left join bbandt_mdwdb.d_internal_contact d
-on concta('INTERNAL_CONTACT~',a.u_group_coordinated_with)=d.row_id 
+on concat('INTERNAL_CONTACT~',a.u_group_coordinated_with)=d.row_id 
 where coalesce(d.row_key,case when u_group_coordinated_with is null then 0 else -1 end) <>b.group_coordinated_with_c_key) a;
 
