7b94e20864d03ed5c189adba78e08c6207565af8
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2016-12-07 19:41:50 +0530

    commit and push changes

--- Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/d_hr_task_c_short_description_mysql.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/d_hr_task_c_short_description_mysql.sql
@@ -1,17 +1,5 @@
-select 
-case 
-when sum(failures_cnt) > 0 and length(group_concat(distinct failures)) >= 1024 then concat('Failed. Data does not match for ',sum(failures_cnt),' records. Few sys ids are : ',substring_index(group_concat(distinct failures),',',-31))
-
-when sum(failures_cnt) > 0 and length(group_concat(distinct failures)) < 1024
-then concat('Failed. Data does not match for ',sum(failures_cnt),' records. Sys Ids : ',substring_index(group_concat(distinct failures),',',-sum(failures_cnt)))
-
-else 'Success. All warehouse records are matching with source.' end Result
-from 
-(select 
-
-case when COALESCE(src.short_description,'UNSPECIFIED') <> COALESCE(trgt.short_description,'UNSPECIFIED') then src.sys_id else '' end as failures,
-
-case when COALESCE(src.short_description,'UNSPECIFIED') <> COALESCE(trgt.short_description,'UNSPECIFIED') then 1 else 0 end as failures_cnt
-
-from molinahealth_mdsdb.u_hr_task_final src
-left join molinahealth_mdwdb.d_hr_task_c trgt on trgt.row_id = src.sys_id and trgt.source_id = src.sourceinstance) fnl ;
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.short_description' ELSE 'SUCCESS' END as Message
+ FROM  molinahealth_mdsdb.u_hr_task_final src
+left join molinahealth_mdwdb.d_hr_task_c trgt on trgt.row_id = src.sys_id and trgt.source_id = src.sourceinstance
+WHERE length(src.short_description)<=255 and src.short_description <> trgt.short_description;
