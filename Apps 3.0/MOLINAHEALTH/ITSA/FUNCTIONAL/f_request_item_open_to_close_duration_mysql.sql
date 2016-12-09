3d119f0817941e11b19790222a03b3933941b3bd
Siva Challa <siva.challa@numerify.com>
2016-12-09 12:27:07 +0000

    f_request_item_open_to_close_duration_mysql.sql created online with Bitbucket

---
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_request_item_open_to_close_duration_mysql.sql
@@ -0,0 +1,31 @@
+select 
+case 
+
+when sum(src_not_null_count) = 0 then 'Success. Source does not have any data. All are nulls.'
+
+when sum(failures_cnt) > 0 and length(group_concat(distinct failures)) >= 1024 then concat('Failed. Data does not match for ',sum(failures_cnt),' records. Few sys ids are : ',substring_index(group_concat(distinct failures),',',-31))
+
+when sum(failures_cnt) > 0 and length(group_concat(distinct failures)) < 1024
+then concat('Failed. Data does not match for ',sum(failures_cnt),' records. Sys Ids : ',substring_index(group_concat(distinct failures),',',-sum(failures_cnt)))
+
+else CONCAT('Success. All warehouse records are matching with source. Source Count : ',sum(src_not_null_count),' ; Target Count : ',sum(trgt_not_null_count)) end Result
+
+from 
+(select 
+case when src.open_to_close_duration is not null then 1 else 0 end as src_not_null_count,
+
+case when trgt.open_to_close_duration is not null then 1 else 0 end as trgt_not_null_count,
+
+case when COALESCE(src.open_to_close_duration,0) <> COALESCE(trgt.open_to_close_duration,0) then src.sys_id else '' end as failures,
+
+case when COALESCE(src.open_to_close_duration,0) <> COALESCE(trgt.open_to_close_duration,0) then 1 else 0 end as failures_cnt
+from 
+(select 
+ri.sys_id,ri.sourceinstance,timestampdiff(second,ri.opened_at,coalesce(sa_src.fulfilled_on,ri.closed_at)) as open_to_close_duration
+from molinahealth_mdsdb.sc_req_item_final ri
+left join (select su.documentkey,su.sourceinstance,max(su.sys_created_on) as fulfilled_on
+from molinahealth_mdsdb.sys_audit_final su 
+join molinahealth_mdsdb.sys_choice_final sc on su.newvalue = sc.value
+where su.tablename = 'sc_req_item' and su.fieldname = 'state' and sc.name = 'sc_req_item' and sc.element = 'state' and sc.label = 'Fulfilled'
+group by su.documentkey,su.sourceinstance) sa_src on ri.sys_id = sa_src.documentkey) src 
+left join molinahealth_mdwdb.f_request_item trgt on trgt.row_id = src.sys_id and trgt.source_id = src.sourceinstance) fnl ;
