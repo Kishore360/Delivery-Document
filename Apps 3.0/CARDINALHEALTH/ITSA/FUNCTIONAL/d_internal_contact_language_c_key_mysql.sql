98c9fc8a42ec934133a75cccfc26bcad1bd733a7
Shubha <Shubha.rao@numerify.com>
2016-12-12 12:19:37 +0530

    Creating New tenant Folded for Wirlpool

--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_internal_contact_language_c_key_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_internal_contact_language_c_key_mysql.sql
@@ -1,6 +1,10 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
-from cardinalhealth_mdwdb.d_internal_contact a
-JOIN cardinalhealth_mdsdb.sys_user_final b ON a.row_id=concat('INTERNAL_CONTACT~',b.sys_id) AND a.source_id=b.sourceinstance
-JOIN cardinalhealth_mdwdb.d_lov l ON l.src_rowid=COALESCE(CONCAT('PREFERRED_LANGUAGE_C~SYS_USER~~~',b.preferred_language),'UNSPECIFIED') AND l.source_id=a.source_id
-where  a.language_c_key <> l.row_key ;
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
+ FROM cardinalhealth_mdsdb.sys_user_final SRC 
+  JOIN cardinalhealth_mdwdb.d_internal_contact TRGT 
+ ON (SRC.sys_id =RIGHT(TRGT.row_id, 32)  
+ AND SRC.sourceinstance= TRGT.source_id  )
+LEFT JOIN cardinalhealth_mdwdb.d_lov LKP 
+ ON ( COALESCE(CONCAT('PREFERRED_LANGUAGE_C~SYS_USER~~~',SRC.preferred_language),'PREFERRED_LANGUAGE_C~SYS_USER~~~en'))= LKP.row_id 
+AND SRC.sourceinstance= LKP.source_id 
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.preferred_language IS NULL THEN 0 else -1 end)<> (TRGT.language_c_key)
--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_project_manager_c_key_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_project_manager_c_key_mysql.sql
@@ -1,10 +1,10 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
-
-from cardinalhealth_mdwdb.d_project d
-JOIN cardinalhealth_mdsdb.pm_project_final i
-on d.row_id=i.sys_id and d.source_id=i.sourceinstance
-join
-cardinalhealth_mdwdb.d_internal_contact e 
-ON (COALESCE(i.project_manager,'UNSPECIFIED')=RIGHT(e.row_id,32) AND e.current_flag='Y') 
-where coalesce(e.row_key,case when i.project_manager is null then 0 else -1 end) <> d.manager_c_key
+  SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
+ FROM cardinalhealth_mdsdb.pm_project_final SRC 
+ LEFT JOIN cardinalhealth_mdwdb.d_project TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+LEFT JOIN cardinalhealth_mdwdb.d_internal_contact LKP 
+ ON ( COALESCE(CONCAT('INTERNAL_CONTACT~',SRC.project_manager),'UNSPECIFIED')= LKP.row_id 
+AND SRC.sourceinstance= LKP.source_id )
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.project_manager IS NULL THEN 0 else -1 end)<> (TRGT.manager_c_key)
