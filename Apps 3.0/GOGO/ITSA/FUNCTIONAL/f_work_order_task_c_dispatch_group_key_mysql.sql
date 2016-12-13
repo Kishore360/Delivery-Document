d293aee53c65c356f81c5082e6f0b57a03b09c09
prabhuram <prabhuram.sridharan@numerify.com>
2016-12-13 14:06:03 +0530

    committing gogo new queries

---
+++ Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_work_order_task_c_dispatch_group_key_mysql.sql
@@ -0,0 +1,15 @@
+
+ 
+ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.work_start_on_key' ELSE 'SUCCESS' END as Message
+ FROM gogo_mdsdb.wm_task_final SRC
+ LEFT JOIN gogo_mdwdb.f_work_order_task_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  ) 
+ LEFT JOIN gogo_mdwdb.d_internal_organization LKP 
+on (LKP.row_id =  concat('GROUP~',SRC.dispatch_group)) )
+ WHERE COALESCE(LKP.row_key,case when SRC.dispatch_group is null then 0 else -1 end )<>(TRGT.dispatch_group_key)
+ AND DATE_FORMAT(TRGT.pivot_date, '%Y-%m-%d %H:%i:%s') 
+									BETWEEN effective_from AND effective_to
+ 
+ 
---
+++ Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_work_order_task_c_urgency_src_key_mysql.sql
@@ -0,0 +1,12 @@
+
+ 
+ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.work_start_on_key' ELSE 'SUCCESS' END as Message
+ FROM gogo_mdsdb.wm_task_final SRC
+ LEFT JOIN gogo_mdwdb.f_work_order_task_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  ) LEFT JOIN gogo_mdwdb.d_lov LKP 
+on (LKP.row_id = concat('URGENCY','~','TASK','~','~','~',UPPER(i.urgency)) )
+ WHERE COALESCE(LKP.row_key,case when SRC.urgency is null then 0 else -1 end )<>(TRGT.urgency_src_key)
+ 
+ 
