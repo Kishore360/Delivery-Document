830dab5012a957a0ac55c66559683ef1e7f930a5
prabhuram <prabhuram.sridharan@numerify.com>
2016-12-13 14:32:12 +0530

    committing the tjx new queries

--- Apps 3.0/TJX/ITSA/FUNCTIONAL/f_customer_service_c_corporate_division_id_c_mysql.sql
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/f_customer_service_c_corporate_division_id_c_mysql.sql
@@ -1,3 +1,5 @@
+
+
 SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
@@ -9,6 +11,4 @@ on trt.row_id=src.sys_id and trt.row_id=src.sourceinstance
 left join tjx_mdsdb.u_company_corporate_division_final lkp
 on src.u_chain=lkp.sys_id and src.sourceinstance=lkp.sourceinstance
 where trt.corporate_division_id_c <> lkp.u_corporate_division_id ) g; 
-
-
 
---
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/f_customer_service_c_source_c_key_mysql.sql
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_customer_service_c.ca_standards_c_key' ELSE 'SUCCESS' END as Message
+FROM tjx_mdsdb.u_customer_service_final SRC
+LEFT JOIN tjx_mdwdb.d_lov LKP 
+ ON LKP.dimension_class like '%U_SOURCE_C~U_CUSTOMER_SERVICE%' and 
+( concat('U_SOURCE_C~U_CUSTOMER_SERVICE~~~',UPPER(SRC.u_source))= LKP.src_rowid 
+AND SRC.sourceinstance= LKP.source_id ) 
+ LEFT JOIN tjx_mdwdb.f_customer_service_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_source IS NULL THEN 0 else -1 end)<> (TRGT.source_c_key);
+
