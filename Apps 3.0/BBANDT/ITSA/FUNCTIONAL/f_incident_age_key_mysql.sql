b484e922e2be80b7c234655de7ea5683b2a0d254
prabhuram <prabhuram.sridharan@numerify.com>
2016-12-14 08:19:05 +0530

    age key query modified

--- Apps 3.0/BBANDT/ITSA/FUNCTIONAL/f_incident_age_key_mysql.sql
+++ Apps 3.0/BBANDT/ITSA/FUNCTIONAL/f_incident_age_key_mysql.sql
@@ -3,6 +3,6 @@
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.age_key' ELSE 'SUCCESS' END as Message
 from bbandt_mdwdb.f_incident f  
 LEFT JOIN bbandt_mdwdb.d_lov L  
-ON ((f.age BETWEEN L.lower_range_value AND L.upper_range_value)
+ON ((f.age div 86400 BETWEEN L.lower_range_value AND L.upper_range_value)
 	AND L.dimension_class = 'AGEBUCKET_WH~INCIDENT' )
-WHERE (L.row_key )  <> f.age_key
+	WHERE  coalesce(L.row_key,case when age is null then 0 else -1 end )  <> f.age_key
