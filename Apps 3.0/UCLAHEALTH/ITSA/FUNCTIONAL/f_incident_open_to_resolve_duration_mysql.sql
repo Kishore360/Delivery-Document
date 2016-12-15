b5268824381242365a8f8de16238208b73b02e0f
Rajashekhar <Rajashekhar>
2016-12-15 10:52:09 +0530

    UCLA Health Query modification

--- Apps 3.0/UCLAHEALTH/ITSA/FUNCTIONAL/f_incident_open_to_resolve_duration_mysql.sql
+++ Apps 3.0/UCLAHEALTH/ITSA/FUNCTIONAL/f_incident_open_to_resolve_duration_mysql.sql
@@ -1,12 +1,12 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM (
-select count(1) as cnt from(
-SELECT A.SYS_ID,B.ROW_ID,A.  open_to_resolve_duration A_open_to_resolve_duration,B.  open_to_resolve_duration B_open_to_resolve_duration FROM
-(SELECT SYS_ID,sourceinstance, TIMESTAMPDIFF(SECOND,'1970-01-01 00:00:00',business_duration )  AS   open_to_resolve_duration
-FROM uclahealth_mdsdb.incident_final p )A
- JOIN  
-(SELECT  open_to_resolve_duration,source_id,ROW_ID FROM  uclahealth_mdwdb.f_incident
- )B on A.sourceinstance=B.source_id AND B.ROW_ID=SYS_ID)H
-WHERE A_open_to_resolve_duration<> B_open_to_resolve_duration)E;
+-- If the query FAILs, please check DST
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.open_to_resolve_duration' ELSE 'SUCCESS' END as Message
+ FROM uclahealth_mdsdb.incident_final SRC
+  JOIN uclahealth_mdwdb.f_incident TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+ LEFT JOIN uclahealth_mdwdb.d_lov_map br 
+ ON TRGT.state_src_key = br.src_key
+WHERE   br.dimension_wh_code IN ('CLOSED','RESOLVED')
+AND TIMESTAMPDIFF(SECOND,SRC.opened_at,coalesce(SRC.u_resolved_date,SRC.closed_at,SRC.sys_updated_on))  <> TRGT.open_to_resolve_duration
