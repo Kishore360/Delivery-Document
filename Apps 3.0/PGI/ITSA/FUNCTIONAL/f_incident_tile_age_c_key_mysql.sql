08f1adfdcc72467776414f87c30f7711c81996ad
ashokvelu <ashok.velu@numerify.com>
2016-12-18 14:09:07 +0530

    prod small changes

--- Apps 3.0/PGI/ITSA/FUNCTIONAL/f_incident_tile_age_c_key_mysql.sql
+++ Apps 3.0/PGI/ITSA/FUNCTIONAL/f_incident_tile_age_c_key_mysql.sql
@@ -2,7 +2,7 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 from(select count(1) as cnt from pgi_mdwdb.f_incident a
-LEFT OUTER JOIN pgi_mdwdb.d_lov x ON a.age BETWEEN x.lower_range_value AND x.upper_range_value and x.dimension_class = 'TILEAGEBUCKET_WH~INCIDENT'
+LEFT OUTER JOIN pgi_mdwdb.d_lov x ON floor(a.age/86400) BETWEEN x.lower_range_value AND x.upper_range_value and x.dimension_class = 'TILEAGEBUCKET_WH~INCIDENT'
 WHERE (a.tile_age_c_key <> x.row_key ))b ; 
 
 
