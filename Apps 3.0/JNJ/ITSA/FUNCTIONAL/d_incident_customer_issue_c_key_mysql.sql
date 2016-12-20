ff1cce5c9736d9dd8ac08faea122a9820a9aef1b
vijay rao <vijay.rao@numerify.com>
2016-12-20 19:11:13 +0530

    Incident JNJ cust queries

--- Apps 3.0/JNJ/ITSA/FUNCTIONAL/d_incident_customer_issue_c_key_mysql.sql
+++ Apps 3.0/JNJ/ITSA/FUNCTIONAL/d_incident_customer_issue_c_key_mysql.sql
@@ -2,8 +2,8 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.state_src_key' ELSE 'SUCCESS' END as Message
  FROM jnj_mdsdb.incident_final SRC
 LEFT JOIN jnj_mdwdb.d_lov LKP 
- ON LKP.dimension_class like '%STATE~INCIDENT%' and 
-( concat('STATE~INCIDENT~~~',u_customer_issue)= LKP.src_rowid 
+ ON LKP.dimension_class like '%CUSTOMER_ISSUE_C%' and 
+( concat('CUSTOMER_ISSUE_C~INCIDENT~~~',u_customer_issue)= LKP.src_rowid 
 AND SRC.sourceinstance= LKP.source_id ) 
  LEFT JOIN jnj_mdwdb.d_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
--- Apps 3.0/JNJ/ITSA/FUNCTIONAL/f_incident_customer_time_c_mysql.sql
+++ Apps 3.0/JNJ/ITSA/FUNCTIONAL/f_incident_customer_time_c_mysql.sql
@@ -6,7 +6,4 @@ select count(1) as cnt
 from jnj_mdsdb.incident_final s
 left join jnj_mdwdb.f_incident t
 on s.sys_id=t.row_id and s.sourceinstance = t.source_id
-where t.customer_time_c <> s.u_customer_time) temp
-
-
-
+where t.customer_time_c <> TIMESTAMPDIFF(second, '1970-01-01 00:00:00', s.u_customer_time)) temp
