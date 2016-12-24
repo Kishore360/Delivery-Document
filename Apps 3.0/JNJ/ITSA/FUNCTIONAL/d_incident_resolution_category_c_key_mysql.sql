f9ec571b51cc71a28f9c18c43aa3b843345e83c0
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2016-12-24 18:39:18 +0530

    commit and push

--- Apps 3.0/JNJ/ITSA/FUNCTIONAL/d_incident_resolution_category_c_key_mysql.sql
+++ Apps 3.0/JNJ/ITSA/FUNCTIONAL/d_incident_resolution_category_c_key_mysql.sql
@@ -1,7 +1,8 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.state_src_key' ELSE 'SUCCESS' END as Message
- FROM jnj_mdsdb.incident_final SRC 
+ FROM jnj_mdsdb.u_resolution_category_choices_final LKP 
+ JOIN  jnj_mdsdb.incident_final SRC 
+ ON (SRC.u_resolution_category = LKP.sys_id )
  LEFT JOIN jnj_mdwdb.d_incident TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE coalesce(SRC.u_resolution_category,'UNSPECIFIED') <> (TRGT.resolution_category_c)
+ ON (SRC.sys_id =TRGT.row_id   AND SRC.sourceinstance= TRGT.source_id  )
+ WHERE LKP.u_value <> TRGT.resolution_category_c;
