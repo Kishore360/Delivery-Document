981144d8a127caf60b49444589109b5e96cc0431
Kishore Mantha <kishore.mantha@numerify.com>
2016-12-26 11:57:25 +0530

    updated the d_internal_ contact cost_ center  key query

--- Apps 3.0/BBANDT/ITSA/FUNCTIONAL/d_internal_contact_cost_center_key_mysql.sql
+++ Apps 3.0/BBANDT/ITSA/FUNCTIONAL/d_internal_contact_cost_center_key_mysql.sql
@@ -1,13 +1,12 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_internal_contact_cost_center_c_key' ELSE 'SUCCESS' END as Message
 FROM bbandt_mdsdb.sys_user_final SRC 
-LEFT JOIN bbandt_mdwdb.d_cost_center LKP
-ON ( (CONCAT('INTERNAL_CONTACT~',SRC.sys_id))= LKP.row_id AND SRC.sourceinstance= LKP.source_id )
 LEFT JOIN bbandt_mdwdb.d_internal_contact TRGT 
-ON (SRC.sys_id =TRGT.row_id AND SRC.sourceinstance= TRGT.source_id )
-WHERE COALESCE(LKP.row_key,CASE WHEN SRC.cost_center IS NULL THEN 0 else -1 end)<> 
-(TRGT.cost_center_key); 
-
+ON (concat('INTERNAL_CONTACT~',SRC.sys_id) =TRGT.row_id AND SRC.sourceinstance= TRGT.source_id )
+JOIN bbandt_mdwdb.d_cost_center LKP
+ON ( SRC.cost_center= LKP.row_id AND SRC.sourceinstance= LKP.source_id )
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.cost_center IS NULL THEN 0 else -1 end)<>
+(TRGT.cost_center_key);  
 
 
 
