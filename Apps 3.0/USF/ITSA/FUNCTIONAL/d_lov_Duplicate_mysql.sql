c9a7f755b60cf816327b625a8ec16307df3f8f03
Kishore Mantha <kishore.mantha@numerify.com>
2017-01-02 17:53:59 +0530

    Updated the USF_d_lov_Duplicate_mysql_ QA Query

--- Apps 3.0/USF/ITSA/FUNCTIONAL/d_lov_Duplicate_mysql.sql
+++ Apps 3.0/USF/ITSA/FUNCTIONAL/d_lov_Duplicate_mysql.sql
@@ -4,6 +4,6 @@ FROM( Select count(cnt) as cnt
 from( 
 select row_id,source_id,count(1) as cnt 
 from  
-usf_mdwdb.d_lov  
+usf_mdwdb.d_lov  where row_id not like '%ASSET%' and row_id not like '%EXPENSE_ITEM%'  
 group by 1,2 
 having count(1)>1)c )a 
