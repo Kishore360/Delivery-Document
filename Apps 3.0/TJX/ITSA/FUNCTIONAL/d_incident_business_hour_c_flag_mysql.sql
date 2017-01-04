32174810313ad9f6af08f67e8ed90651b0245a27
prabhuram <prabhuram.sridharan@numerify.com>
2017-01-04 17:26:40 +0530

    business hours flag updated in tjx

--- Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_business_hour_c_flag_mysql.sql
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_business_hour_c_flag_mysql.sql
@@ -1,5 +1,3 @@
-
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_survey_question.survey_key' ELSE 'SUCCESS' END as Message
 FROM tjx_mdsdb.incident_final SRC
@@ -7,8 +5,10 @@ join tjx_mdwdb.d_incident TRGT
 ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
 
-where case when time(SRC.opened_on)  between '08-00-00' and  '18-00-00' then 'Y' else 'N' end  <>TRGT. business_hour_c_flag;
-
-
-
-
+where case when time(CONVERT_TZ(SRC.opened_at,
+		'GMT',
+		'America/New_York'))  between '08-00-00' and  '18-00-00' 
+		 and DAYNAME(DATE(CONVERT_TZ(SRC.opened_at,
+		'GMT',
+		'America/New_York'))) in('Monday' ,'Tuesday','Wednesday','Thursday','Friday') then 'Y' else 'N' end
+		 <> TRGT. business_hour_c_flag; 
