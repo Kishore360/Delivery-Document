1f17adc9ef3e4f61963b34eb8c9dc0b272c0e6ee
prabhuram <prabhuram.sridharan@numerify.com>
2017-01-04 14:33:57 +0530

    Updating NBCU Queries

--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_task_c_assignment_group_key_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_task_c_assignment_group_key_mysql.sql
@@ -1,9 +1,9 @@
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_task_fss_c.assignment_group_key' ELSE 'SUCCESS' END as Message
-FROM rambus_mdsdb.task_final SRC 
-  JOIN rambus_mdwdb.f_task_c TRGT  ON (SRC.sys_id =TRGT.row_id   AND SRC.sourceinstance= TRGT.source_id  )
- JOIN rambus_mdwdb.d_internal_organization LKP  ON ( coalesce(concat('GROUP~',assignment_group),'UNSPECIFIED')= LKP.row_id AND SRC.sourceinstance= LKP.source_id )  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.assignment_group IS NULL THEN 0 else -1 end)<> (TRGT.assignment_group_key);
+FROM nbcu_mdsdb.task_final SRC 
+  JOIN nbcu_mdwdb.f_task_c TRGT  ON (SRC.sys_id =TRGT.row_id   AND SRC.sourceinstance= TRGT.source_id  )
+ JOIN nbcu_mdwdb.d_internal_organization LKP  ON ( coalesce(concat('GROUP~',assignment_group),'UNSPECIFIED')= LKP.row_id AND SRC.sourceinstance= LKP.source_id )  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.assignment_group IS NULL THEN 0 else -1 end)<> (TRGT.assignment_group_key);
  
  
  
