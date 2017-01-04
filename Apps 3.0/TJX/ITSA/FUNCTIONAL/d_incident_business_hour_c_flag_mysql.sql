e65d25e12ee73cdb67216793aeea6ec4a69615b8
prabhuram <prabhuram.sridharan@numerify.com>
2017-01-04 14:32:09 +0530

    Updating NBCU Queries

---
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_business_hour_c_flag_mysql.sql
@@ -0,0 +1,14 @@
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_survey_question.survey_key' ELSE 'SUCCESS' END as Message
+FROM tjx_mdsdb.incident_final SRC
+join tjx_mdwdb.d_incident TRGT
+ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+
+where case when time(SRC.opened_on)  between '08-00-00' and  '18-00-00' then 'Y' else 'N' end  <>TRGT. business_hour_c_flag;
+
+
+
+
--- Apps 3.0/UCLAHEALTH/ITSA/FUNCTIONAL/f_a_incident_monthly_age_bucket1_mysql.sql
+++ Apps 3.0/UCLAHEALTH/ITSA/FUNCTIONAL/f_a_incident_monthly_age_bucket1_mysql.sql
@@ -9,10 +9,11 @@ from
        open_task_fss.assignment_group_key,
        open_task_fss.assigned_to_key,
        SUM(CASE WHEN (open_task_fss.opened_on_key BETWEEN mon.month_start_date_key AND mon.month_end_date_key) and 
-       (open_task_fss.closed_on_key is null) and (open_task_fss.age between 0 and 30) then 1 else 0 END ) AS 'age_bucket1'
+       (open_task_fss.closed_on_key is null) 
+	   and (open_task_fss.age between 0 and 30) then 1 else 0 END ) AS 'age_bucket1'
                    FROM uclahealth_mdwdb.d_calendar_date mon
-JOIN uclahealth_mdwdb.f_incident open_task_fss  ON (open_task_fss.opened_on_key  BETWEEN
-mon.month_start_date_key AND mon.month_end_date_key
+JOIN uclahealth_mdwdb.f_incident open_task_fss  ON 
+(open_task_fss.opened_on_key  BETWEEN mon.month_start_date_key AND mon.month_end_date_key
 OR open_task_fss.closed_on_key BETWEEN mon.month_start_date_key AND mon.month_end_date_key
 )
 AND open_task_fss.soft_deleted_flag = 'N'
