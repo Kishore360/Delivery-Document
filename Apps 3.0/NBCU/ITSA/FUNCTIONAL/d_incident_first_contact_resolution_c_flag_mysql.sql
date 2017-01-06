aff8eb9fc20097cb9639a9d5657082945d6b76fe
Kishore Mantha <kishore.mantha@numerify.com>
2017-01-06 18:08:53 +0530

    Updated the QA Query for nbcu_d_incident_first_contact_resolution_c_flag

--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_incident_first_contact_resolution_c_flag_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_incident_first_contact_resolution_c_flag_mysql.sql
@@ -1,10 +1,11 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.opened_on_key' ELSE 'SUCCESS' END as Message
 from
 nbcu_mdsdb.incident_final a
 join nbcu_mdwdb.d_incident b
-on a.sys_id=b.row_id and a.sourceinstance=b.source_id
-where 
-case 
-when a.reopen_count=0 and a.reassignment_count=0
-AND TIMESTAMPDIFF(HOUR,a.opened_at,COALESCE(a.resolved_at,a.closed_at,'2099-01-01 00:00:00'))<=24 then 'Y' else 'N' end<> b.first_contact_resolution_c_flag;
+on a.sys_id=b.row_id and a.sourceinstance=b.source_id 
+JOIN nbcu_mdwdb.d_lov_map dlm ON b.state_src_key = dlm.src_key  
+where (case when a.reopen_count=0 and a.reassignment_count=0
+AND TIMESTAMPDIFF(HOUR,a.opened_at,COALESCE(a.resolved_at,a.closed_at,'2099-01-01 00:00:00'))<=24
+ then 'Y' else 'N' end)<>b.first_contact_resolution_c_flag AND dlm.dimension_class = 'STATE~INCIDENT'
+  AND dlm.dimension_wh_code ='CLOSED';
