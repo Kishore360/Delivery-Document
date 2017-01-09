698a1191991136aab1caee20dfcb250e352d7b3d
Shubha <Shubha.rao@numerify.com>
2017-01-09 12:22:31 +0530

    Updating the QA queries for whirlpool

--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_request_item_fullfilled_at_c_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_request_item_fullfilled_at_c_mysql.sql
@@ -3,8 +3,8 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ELSE 'Data Matched' END AS Message 
 FROM (select
 count(*) as cnt
-from  nbcu_mdwdb.d_request_item a11
-left join nbcu_mdsdb.sc_req_item_final a12
+from  nbcu_mdsdb.sc_req_item_final a12
+left join nbcu_mdwdb.d_request_item a11
 on a11.row_id=a12.sys_id and a11.source_id=a12.sourceinstance
-where coalesce(convert_tz(a12.u_fulfilled_at,'GMT','America/New_York'),-99)<>coalesce(fulfilled_on_c,-99)
+where convert_tz(a12.u_fulfilled_at,'GMT','America/New_York')<>a11.fulfilled_on_c
 )a
--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_request_task_req_item_state_src_code_c_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_request_task_req_item_state_src_code_c_mysql.sql
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request.assigned_to_key' ELSE 'SUCCESS' END as Message
- FROM nbcu_mdwdb.f_request_task stg
- JOIN nbcu_mdsdb.sc_task_final src ON stg.row_id = src.sys_id AND stg.source_id = src.sourceinstance
-join nbcu_mdsdb.sc_req_item_final ri on ri.sys_id=src.request_item  and ri.sourceinstance=src.sourceinstance
-where COALESCE(ri.state ,'UNSPECIFIED')<>stg.req_item_state_src_code_c
-=======
-
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_item.state_src_code' ELSE 'SUCCESS' END as Message
  FROM nbcu_mdsdb.sc_task_final SRC 
@@ -16,13 +6,4 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  LEFT JOIN nbcu_mdwdb.f_request_task TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.state,'')<> (TRGT.req_item_state_src_code_c) 
-
- 
- 
- 
- 
-
-	
-	 
->>>>>>> aaa1643d087f22d304cecb7a9e916801886b10e5
+ WHERE COALESCE( ri.state,'')<> (TRGT.req_item_state_src_code_c) 
