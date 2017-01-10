e6ef7b77a0baf89539db386d996019d2c0e5168d
Kishore Mantha <kishore.mantha@numerify.com>
2017-01-10 16:36:36 +0530

    Done Changes to intuit

--- Apps 3.0/INTUIT/ITSA/FUNCTIONAL/f_incident_age_open_mysql.sql
+++ Apps 3.0/INTUIT/ITSA/FUNCTIONAL/f_incident_age_open_mysql.sql
@@ -1,6 +1,9 @@
 
 
-select count(1) as cnt FROM (select sys_updated_on,sys_id,sourceinstance,opened_at,resolved_at,closed_at 
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt>0 THEN 'PASSTHROUGH NOT MATCHING ON DATA' ELSE 'SUCCESS' END as Message
+ FROM
+ (select count(1) as cnt FROM (select sys_updated_on,sys_id,sourceinstance,opened_at,resolved_at,closed_at 
 from intuit_mdsdb.incident_final where opened_at < coalesce(resolved_at,closed_at)) SRC 
   join intuit_mdwdb.f_incident f ON (SRC.sys_id =f.row_id  
  AND SRC.sourceinstance= f.source_id  )
@@ -12,4 +15,4 @@ WHERE
 timestampdiff(DAY, convert_tz(convert_tz(SRC.opened_at,"GMT","America/Los_Angeles"),"America/Los_Angeles","GMT"), 
 coalesce(convert_tz(convert_tz(SRC.resolved_at,"GMT","America/Los_Angeles"),"America/Los_Angeles","GMT"), 
 convert_tz(convert_tz(SRC.closed_at,"GMT","America/Los_Angeles"),"America/Los_Angeles","GMT"),
-convert_tz(convert_tz(SRC.sys_updated_on,"GMT","America/Los_Angeles"),"America/Los_Angeles","GMT"))) <> f.age
+convert_tz(convert_tz(SRC.sys_updated_on,"GMT","America/Los_Angeles"),"America/Los_Angeles","GMT"))) <> f.age) Temp; 
