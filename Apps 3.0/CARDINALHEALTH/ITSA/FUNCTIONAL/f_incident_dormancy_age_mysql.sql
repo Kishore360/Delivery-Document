6f39f7a30b53eb42c4f839d763dffccfee26f89f
Shubha <Shubha.rao@numerify.com>
2017-01-18 15:45:45 +0530

    Updating the queries

--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_incident_dormancy_age_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_incident_dormancy_age_mysql.sql
@@ -8,6 +8,6 @@ FROM cardinalhealth_mdsdb.incident_final SRC
   ON (lm.src_key = TRGT.state_src_key)
 where lm.dimension_class = 'STATE~INCIDENT'
 AND   lm.dimension_wh_code = 'OPEN' and
-timestampdiff(DAY,CONVERT_TZ(TRGT.changed_on,'America/New_York','GMT'),convert_tz((SELECT MAX(lastupdated) AS lastupdated
+timestampdiff(second,CONVERT_TZ(TRGT.changed_on,'America/New_York','GMT'),convert_tz((SELECT MAX(lastupdated) AS lastupdated
 FROM cardinalhealth_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%'),'America/New_York','GMT')) <> 
 TRGT.dormancy_age; 
