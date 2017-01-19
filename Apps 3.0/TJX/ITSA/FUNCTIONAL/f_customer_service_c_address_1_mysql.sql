6966641a8d785ed3b4060f6cb1dd694ec01f45c1
prabhuram <prabhuram.sridharan@numerify.com>
2017-01-19 14:35:56 +0530

    TJX Updated the Query

--- Apps 3.0/TJX/ITSA/FUNCTIONAL/f_customer_service_c_address_1_mysql.sql
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/f_customer_service_c_address_1_mysql.sql
@@ -1,6 +1,6 @@
 SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message, cnt
+ELSE 'Data Matched' END AS Message
 FROM (
 select count(1) as cnt
 from tjx_mdsdb.u_customer_service_final src
--- Apps 3.0/TJX/ITSA/FUNCTIONAL/f_customer_service_c_closed_by_key_mysql.sql
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/f_customer_service_c_closed_by_key_mysql.sql
@@ -1,5 +1,5 @@
  SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_customer_service.closed_by_key' ELSE 'SUCCESS' END as Message,count(1)
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_customer_service.closed_by_key' ELSE 'SUCCESS' END as Message
  FROM tjx_mdsdb.u_customer_service_final SRC 
  LEFT JOIN tjx_mdwdb.f_customer_service_c TRGT 
  ON (SRC.sys_id =TRGT.row_id  
