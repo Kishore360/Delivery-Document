7f4192a844a8c6a260d4c5536854a318afd0007b
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-01-23 11:44:13 +0530

    OOTB Query Correction

--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/d_incident_over_due_flag_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/d_incident_over_due_flag_mysql.sql
@@ -1,7 +1,7 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.over_due_flag' ELSE 'SUCCESS' END as Message from (
  select ( CASE WHEN  due_date < CONVERT_TZ((SELECT MAX(lastupdated) AS lastupdated
-FROM <<tenant>>_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%' and etl_run_number=TRGT.etl_run_number),"America/Los_Angeles","GMT") and TRGT.active_flag='Y' then 'Y' else 'N' END)abc, 
+FROM <<tenant>>_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%' and etl_run_number=TRGT.etl_run_number),<<DW_TARGET_TIME_ZONE>>,<<TENANT_SSI_TIME_ZONE>>) and TRGT.active_flag='Y' then 'Y' else 'N' END)abc, 
 (TRGT.over_due_flag )def
  FROM <<tenant>>_mdsdb.incident_final SRC 
 LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT 
@@ -10,3 +10,5 @@ LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT
  WHERE due_date is not null and active_flag='Y'
  )a
  WHERE abc<>def
+ 
+ 
