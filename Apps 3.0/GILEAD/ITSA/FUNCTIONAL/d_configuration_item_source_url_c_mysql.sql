5e40f0c3fffa9b69311580c3e5f87272d5fb4953
Kishore Mantha <kishore.mantha@numerify.com>
2017-02-02 17:49:15 +0530

    Updated the QA Query as part of the DELV-4637

--- Apps 3.0/GILEAD/ITSA/FUNCTIONAL/d_configuration_item_source_url_c_mysql.sql
+++ Apps 3.0/GILEAD/ITSA/FUNCTIONAL/d_configuration_item_source_url_c_mysql.sql
@@ -1,1 +1,6 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message from gilead_mdsdb.cmdb_ci_final SRC join gilead_mdwdb.d_configuration_item TGT on SRC.sys_id = TGT.row_id and SRC.sourceinstance = TGT.source_id where TGT.source_url_c <> CONCAT('<a href=\"','https://sparc.service-now.com/nav_to.do?uri=','cmdb_ci_pdu.do?sys_id=',SRC.sys_id,'&sysparm_record_target=cmdb_ci\" target=\"_blank\" >',SRC.name,'</a>');
+SELECT 
+CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message 
+from gilead_mdsdb.cmdb_ci_final SRC join gilead_mdwdb.d_configuration_item TGT on SRC.sys_id = TGT.row_id and SRC.sourceinstance = TGT.source_id 
+where 
+TGT.source_url_c <> CONCAT('<a href=\"','https://sparc.service-now.com/nav_to.do?uri=','cmdb_ci.do?sys_id=',SRC.sys_id,'&sysparm_record_target=cmdb_ci\" target=\"_blank\" >',SRC.name,'</a>');
