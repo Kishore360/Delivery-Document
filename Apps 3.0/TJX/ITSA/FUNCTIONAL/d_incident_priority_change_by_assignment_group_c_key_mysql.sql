6067e728c3e478eebc92f0ff941dfc536313e395
prabhuram <prabhuram.sridharan@numerify.com>
2017-02-02 19:12:08 +0530

    committing the TJX Queries

--- Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_priority_change_by_assignment_group_c_key_mysql.sql
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_priority_change_by_assignment_group_c_key_mysql.sql
@@ -1,15 +1,24 @@
 SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
-FROM (select count(*) as cnt  
-from tjx_mdwdb.d_incident TGT
-inner join  tjx_mdsdb.incident_final incf
+FROM (select count(*) as cnt from 
+tjx_mdsdb.incident_final incf
+inner join tjx_mdwdb.d_incident TGT
 on TGT.row_id=incf.sys_id
-inner join (select documentkey,user,sys_created_on,newvalue,oldvalue , fieldname from tjx_mdsdb.sys_audit_final  
-where fieldname = ('assignment_group') and tablename='incident') src on src.documentkey=incf.sys_id
-inner join ( select documentkey,user,sys_created_on,newvalue,oldvalue , fieldname from tjx_mdsdb.sys_audit_final  
-where fieldname in ('priority') and tablename='incident' ) asg
-on asg.documentkey=src.documentkey
-join tjx_mdwdb.d_internal_organization dic on concat('GROUP~',coalesce(src.oldvalue,incf.assignment_group)) =dic.row_id 
-where priority_change_by_assignment_group_c_key <> coalesce(TGT.row_key,case when concat('GROUP~',coalesce(src.oldvalue,incf.assignment_group)) is null then 0 else -1 end  ) 
-and src.sys_created_on <= asg.sys_created_on)b
+left join (select a.documentkey,user,newvalue,oldvalue , fieldname,a.sys_created_on from  tjx_mdsdb.sys_audit_final a 
+ join (select documentkey , max(sys_created_on) as sys_created_on
+from tjx_mdsdb.sys_audit_final 
+ where fieldname in ('assignment_group') and tablename='incident'
+group by 1) max
+on max.sys_created_on = a.sys_created_on and a.documentkey = max.documentkey
+where  fieldname in ('assignment_group') and tablename='incident') src on src.documentkey=incf.sys_id
+left join ( select a.documentkey,user,newvalue,oldvalue , fieldname,a.sys_created_on from  tjx_mdsdb.sys_audit_final a 
+ join (select documentkey , max(sys_created_on) as sys_created_on
+from tjx_mdsdb.sys_audit_final 
+ where fieldname in ('priority') and tablename='incident'
+group by 1) max
+on max.sys_created_on = a.sys_created_on and a.documentkey = max.documentkey
+where  fieldname in ('priority') and tablename='incident') asg
+on asg.documentkey=src.documentkey 
+join tjx_mdwdb.d_internal_organization dic on concat('GROUP~',src.oldvalue) =dic.row_id 
+where priority_change_by_assignment_group_c_key <> coalesce(TGT.row_key,case when concat('GROUP~',src.oldvalue) is null then 0 else -1 end  ))b
--- Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_priority_change_by_use_c_key_mysql.sql
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_priority_change_by_use_c_key_mysql.sql
@@ -2,15 +2,24 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 FROM (select count(*) as cnt from 
-  tjx_mdsdb.incident_final incf
+tjx_mdsdb.incident_final incf
 inner join tjx_mdwdb.d_incident TGT
 on TGT.row_id=incf.sys_id
-inner join (select documentkey,user,sys_created_on,newvalue,oldvalue , fieldname from tjx_mdsdb.sys_audit_final  
-where fieldname = ('assignment_group') and tablename='incident') src on src.documentkey=incf.sys_id
-inner join ( select documentkey,user,sys_created_on,newvalue,oldvalue , fieldname from tjx_mdsdb.sys_audit_final  
-where fieldname in ('priority') and tablename='incident' ) asg
-on asg.documentkey=src.documentkey
-left join (select sys_id,user_name,sourceinstance from tjx_mdsdb.sys_user_final) sf on  sf.user_name = asg.user 
+left join (select a.documentkey,user,newvalue,oldvalue , fieldname,a.sys_created_on from  tjx_mdsdb.sys_audit_final a 
+ join (select documentkey , max(sys_created_on) as sys_created_on
+from tjx_mdsdb.sys_audit_final 
+ where fieldname in ('assignment_group') and tablename='incident'
+group by 1) max
+on max.sys_created_on = a.sys_created_on and a.documentkey = max.documentkey
+where  fieldname in ('assignment_group') and tablename='incident') src on src.documentkey=incf.sys_id
+left join ( select a.documentkey,user,newvalue,oldvalue , fieldname,a.sys_created_on from  tjx_mdsdb.sys_audit_final a 
+ join (select documentkey , max(sys_created_on) as sys_created_on
+from tjx_mdsdb.sys_audit_final 
+ where fieldname in ('priority') and tablename='incident'
+group by 1) max
+on max.sys_created_on = a.sys_created_on and a.documentkey = max.documentkey
+where  fieldname in ('priority') and tablename='incident') asg
+on asg.documentkey=src.documentkey left 
+join (select sys_id,user_name,sourceinstance from tjx_mdsdb.sys_user_final) sf on  sf.user_name = asg.user 
 join tjx_mdwdb.d_internal_contact dic on  coalesce(concat('INTERNAL_CONTACT~',sf.sys_id),'UNSPECIFIED') =dic.row_id and sf.sourceinstance=dic.source_id
-where priority_change_by_use_c_key <> TGT.row_key
-and src.sys_created_on <= asg.sys_created_on )b
+where priority_change_by_use_c_key <> TGT.row_key)b
