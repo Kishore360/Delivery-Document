c6d886e4afb072997f9955e9a7459dfe9307fdc6
Kishore Mantha <kishore.mantha@numerify.com>
2017-02-09 11:23:50 +0530

    Updated the NBCU _QA queries as part of syntax error (DELV-4376).

--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_task_backlog_flag_c_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_task_backlog_flag_c_mysql.sql
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request.assigned_to_key' ELSE 'SUCCESS' END as Message
-FROM nbcu_mdwdb.d_task d
-      JOIN nbcu_mdwdb.f_task_c f
-          on d.row_key = f.task_key  
-      JOIN nbcu_mdwdb.d_lov_map lov_map
-          on f.state_src_key = lov_map.src_key   
-where case when lov_map.dimension_class  in (
-=======
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
 from
-
 nbcu_mdsdb.task_final a
 join nbcu_mdwdb.d_task d
 on a.sys_id=d.row_id and a.sourceinstance=d.source_id
@@ -21,7 +9,6 @@ on a.sys_id=d.row_id and a.sourceinstance=d.source_id
       JOIN nbcu_mdwdb.d_lov_map lov_map
           on f.state_src_key = lov_map.src_key 
 where case when  lov_map.dimension_class  in (
->>>>>>> aaa1643d087f22d304cecb7a9e916801886b10e5
 'REQUEST_STATE~SC_REQUEST',
 'STATE~CHANGE_REQUEST',
 'STATE~CHANGE_TASK',
@@ -33,12 +20,4 @@ where case when  lov_map.dimension_class  in (
 'STATE~RM_ENHANCEMENT',
 'STATE~RM_DEFECT'
  )
-<<<<<<< HEAD
  and lov_map.dimension_wh_code NOT IN ('RESOLVED','CLOSED') then 'Y' else 'N' end<> d.backlog_flag_c
-=======
-
- and lov_map.dimension_wh_code NOT IN ('RESOLVED','CLOSED') then 'Y' else 'N' end 
- <> backlog_flag_c
- 
- 
->>>>>>> aaa1643d087f22d304cecb7a9e916801886b10e5
