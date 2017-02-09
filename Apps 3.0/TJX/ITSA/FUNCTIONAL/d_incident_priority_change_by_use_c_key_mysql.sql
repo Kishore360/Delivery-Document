0b41fd2149687c39e419d2f4cc1734ef6bcfc5ae
Kishore Mantha <kishore.mantha@numerify.com>
2017-02-09 15:51:39 +0530

    Updated the TJX QA Queries.

--- Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_priority_change_by_use_c_key_mysql.sql
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_priority_change_by_use_c_key_mysql.sql
@@ -1,25 +1,16 @@
 SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM (select count(*) as cnt from 
-tjx_mdsdb.incident_final incf
-inner join tjx_mdwdb.d_incident TGT
-on TGT.row_id=incf.sys_id
-left join (select a.documentkey,user,newvalue,oldvalue , fieldname,a.sys_created_on from  tjx_mdsdb.sys_audit_final a 
- join (select documentkey , max(sys_created_on) as sys_created_on
-from tjx_mdsdb.sys_audit_final 
- where fieldname in ('assignment_group') and tablename='incident'
-group by 1) max
-on max.sys_created_on = a.sys_created_on and a.documentkey = max.documentkey
-where  fieldname in ('assignment_group') and tablename='incident') src on src.documentkey=incf.sys_id
-left join ( select a.documentkey,user,newvalue,oldvalue , fieldname,a.sys_created_on from  tjx_mdsdb.sys_audit_final a 
- join (select documentkey , max(sys_created_on) as sys_created_on
-from tjx_mdsdb.sys_audit_final 
- where fieldname in ('priority') and tablename='incident'
-group by 1) max
-on max.sys_created_on = a.sys_created_on and a.documentkey = max.documentkey
-where  fieldname in ('priority') and tablename='incident') asg
-on asg.documentkey=src.documentkey left 
-join (select sys_id,user_name,sourceinstance from tjx_mdsdb.sys_user_final) sf on  sf.user_name = asg.user 
-join tjx_mdwdb.d_internal_contact dic on  coalesce(concat('INTERNAL_CONTACT~',sf.sys_id),'UNSPECIFIED') =dic.row_id and sf.sourceinstance=dic.source_id
-where priority_change_by_use_c_key <> coalesce(dic.row_key,case when sf.sys_id is null then 0 else -1 end) )b
+ELSE 'Data Matched' END AS Message ,cnt
+FROM (
+select count(*) as cnt from tjx_mdwdb.d_incident TGT 
+inner join  tjx_mdsdb.incident_final incf on TGT.row_id=incf.sys_id and TGT.source_id = incf.sourceinstance
+left join (select a.sourceinstance,a.documentkey,a.newvalue,a.oldvalue,a.user from  tjx_mdsdb.sys_audit_final a 
+                                                join (select documentkey, max(sys_created_on) as sys_created_on from tjx_mdsdb.sys_audit_final where tablename='incident' and fieldname = 'priority' 
+                                                group by documentkey) rec on a.documentkey = rec.documentkey and  a.sys_created_on = rec.sys_created_on
+                                where  tablename='incident' and fieldname = 'priority' ) as pon
+on  pon.documentkey=TGT.row_id and pon.sourceinstance = TGT.source_id
+left join (select a.sys_id,a.user_name,a.sourceinstance from tjx_mdsdb.sys_user_final a join (select max(sys_updated_on) sys_updated_on,user_name from tjx_mdsdb.sys_user_final group by user_name) as b 
+on a.sys_updated_on = b.sys_updated_on  and a.user_name = b.user_name) pcu on  pcu.user_name = pon.user
+left join tjx_mdwdb.d_internal_contact LKP on LKP.row_id = coalesce(concat('INTERNAL_CONTACT~',pcu.sys_id),'UNSPECIFIED') AND LKP.source_id=pcu.sourceinstance
+where TGT.priority_change_by_use_c_key <> coalesce(LKP.row_key,case when pcu.sys_id is null then 0 else -1 end)
+) b;
--- Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_priority_downgraded_c_flag_mysql.sql
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/d_incident_priority_downgraded_c_flag_mysql.sql
@@ -1,26 +1,14 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM (select count(*) as cnt from tjx_mdwdb.d_incident TGT
-inner join  tjx_mdsdb.incident_final incf
-on TGT.row_id=incf.sys_id and TGT.source_id=incf.sourceinstance
-left join (select a.documentkey,user,newvalue,oldvalue , fieldname,a.sys_created_on from  tjx_mdsdb.sys_audit_final a 
- join (select documentkey , max(sys_created_on) as sys_created_on
-from tjx_mdsdb.sys_audit_final 
- where fieldname in ('assignment_group') and tablename='incident'
-group by 1) max
-on max.sys_created_on = a.sys_created_on and a.documentkey = max.documentkey
-where  fieldname in ('assignment_group') and tablename='incident') src on src.documentkey=incf.sys_id
-left join ( select a.documentkey,user,newvalue,oldvalue , fieldname,a.sys_created_on from  tjx_mdsdb.sys_audit_final a 
- join (select documentkey , max(sys_created_on) as sys_created_on
-from tjx_mdsdb.sys_audit_final 
- where fieldname in ('priority') and tablename='incident'
-group by 1) max
-on max.sys_created_on = a.sys_created_on and a.documentkey = max.documentkey
-where  fieldname in ('priority') and tablename='incident') asg
-on asg.documentkey=src.documentkey
-
-where 
-TGT.priority_downgraded_c_flag <> case when (asg.newvalue is not null and asg.oldvalue is not null) then case when  asg.newvalue > asg.oldvalue then 'Y' else 'N' END END
-and src.sys_created_on <= asg.sys_created_on )b
-
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for incident.priority_downgraded_c_flag' ELSE 'SUCCESS' END as Message from (
+select case when locate('Y',b.res)>0 then 'Y' else 'N' end as priority_downgraded_c_flag,c.sys_id,
+d.priority_downgraded_c_flag as target from (
+select group_concat(a.flag) as res,a.documentkey from (
+select case when newvalue>oldvalue then 'Y' else 'N' end as flag,documentkey from tjx_mdsdb.sys_audit_final 
+ where
+tablename = 'incident' 
+AND fieldname =  'priority' and oldvalue is not null and newvalue is not null
+)a group by a.documentkey
+)b
+join tjx_mdsdb.incident_final c on b.documentkey=c.sys_id
+join tjx_mdwdb.d_incident d on c.sys_id = d.row_id and c.sourceinstance = d. source_id
+) e where priority_downgraded_c_flag <> target;
