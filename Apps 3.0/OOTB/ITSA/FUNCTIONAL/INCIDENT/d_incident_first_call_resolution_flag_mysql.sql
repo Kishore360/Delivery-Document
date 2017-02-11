0a50ff287a0785099172b90015dc6a487e8d053d
prabhuram <prabhuram.sridharan@numerify.com>
2017-02-11 22:18:06 +0530

    Committing TJX Syntax error

--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/d_incident_first_call_resolution_flag_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/d_incident_first_call_resolution_flag_mysql.sql
@@ -1,6 +1,6 @@
 SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_incident.first_call_resolution_flag' ELSE 'SUCCESS' END as Message
- FROM (select count(1)cnt <<tenant>>_mdsdb.incident_final SRC
+ FROM (select count(1)cnt from  <<tenant>>_mdsdb.incident_final SRC
   JOIN <<tenant>>_mdwdb.f_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
