64829e1d7d6e22c1f3256d11bcea02bd350c4a68
Kishore Mantha <kishore.mantha@numerify.com>
2017-02-15 10:42:02 +0530

    Updated the ootb _survey and task_sla qa queries.

--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/SURVEY/d_survey_question_survey_key_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/SURVEY/d_survey_question_survey_key_mysql.sql
@@ -1,5 +1,7 @@
- SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_survey_question.survey_key' ELSE 'SUCCESS' END as Message
+ SELECT 
+ CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for d_survey_question.survey_key' ELSE 'SUCCESS' END as Message
+ FROM(SELECT count(1) as CNT 
  FROM <<tenant>>_mdsdb.survey_question_new_final SRC 
  LEFT JOIN <<tenant>>_mdwdb.d_survey_question TRGT 
  ON (SRC.sys_id =TRGT.row_id  
@@ -7,4 +9,5 @@
  LEFT JOIN <<tenant>>_mdwdb.d_survey LKP 
  ON ( SRC.master= LKP.row_id 
 AND SRC.sourceinstance= LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.master IS NULL THEN 0 else -1 end)<> (TRGT.survey_key)
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.master IS NULL THEN 0 else -1 end)<> (TRGT.survey_key)) temp;
+ 
--- Apps 3.0/OOTB/ITSA/FUNCTIONAL/TASK_SLA/f_task_sla_CHANGED_ON_mysql.sql
+++ Apps 3.0/OOTB/ITSA/FUNCTIONAL/TASK_SLA/f_task_sla_CHANGED_ON_mysql.sql
@@ -1,8 +1,11 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.has_breached_update_c_flag' ELSE 'SUCCESS' END as Message from 
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for d_incident.has_breached_update_c_flag' ELSE 'SUCCESS' END as Message from 
+FROM (SELECT count(1) as CNT
 <<tenant>>_mdsdb.task_sla_final task_sla 
 join <<tenant>>_mdwdb.f_task_sla a on a.row_id=task_sla.sys_id and a.source_id=task_sla.sourceinstance
 where 
-convert_tz(task_sla.sys_updated_on,<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>) <> CHANGED_ON
+convert_tz(task_sla.sys_updated_on,<<TENANT_SSI_TIME_ZONE>>,<<DW_TARGET_TIME_ZONE>>) <> CHANGED_ON)temp;
+
 
 
