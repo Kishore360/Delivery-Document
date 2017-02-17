c9991b6527c8728e9598082742e8892d8ae7a542
Kishore Mantha <kishore.mantha@numerify.com>
2017-02-17 11:03:50 +0530

    Updated the cust cardinal _ configuraiton item qa queries

--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_configuration_item_eit_sup_leader_c_key_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_configuration_item_eit_sup_leader_c_key_mysql.sql
@@ -1,9 +1,12 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT >0 THEN 'Failure' ELSE 'Data Matched' END as Message
+FROM (SELECT count(1) as CNT
 from
 cardinalhealth_mdwdb.d_configuration_item a 
 INNER JOIN cardinalhealth_mdsdb.cmdb_ci_application_final b 
 on a.row_id = b.sys_id AND a.source_id = b.sourceinstance
 LEFT OUTER JOIN cardinalhealth_mdwdb.d_internal_contact c 
 ON c.row_id = COALESCE(concat('INTERNAL_CONTACT~',b.u_cah_level_3_ad),'UNSPECIFIED') AND c.source_id = b.sourceinstance
-where a.eit_sup_leader_c_key <> (c.row_key);
+where a.eit_sup_leader_c_key <> (c.row_key))temp;
+
--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_incident_u_system_name_c_key_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_incident_u_system_name_c_key_mysql.sql
@@ -1,5 +1,7 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
+FROM (SELECT count(1) as CNT
  FROM cardinalhealth_mdsdb.incident_final SRC 
  LEFT JOIN cardinalhealth_mdwdb.f_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
@@ -7,4 +9,5 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 LEFT JOIN cardinalhealth_mdwdb.d_configuration_item LKP 
  ON ( SRC.u_system_name= LKP.row_id 
 AND SRC.sourceinstance= LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_system_name IS NULL THEN 0 else -1 end)<> (TRGT.u_system_name_c_key)
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_system_name IS NULL THEN 0 else -1 end)<> (TRGT.u_system_name_c_key))temp;
+ 
