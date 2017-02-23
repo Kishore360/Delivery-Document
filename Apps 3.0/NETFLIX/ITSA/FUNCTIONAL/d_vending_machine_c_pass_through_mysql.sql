c82aaa7da18518da305df03aaa26194577c37b81
prabhuram <prabhuram.sridharan@numerify.com>
2017-02-23 22:35:49 +0530

    Queries modified as part of reg clean up

--- Apps 3.0/NETFLIX/ITSA/FUNCTIONAL/d_vending_machine_c_pass_through_mysql.sql
+++ Apps 3.0/NETFLIX/ITSA/FUNCTIONAL/d_vending_machine_c_pass_through_mysql.sql
@@ -4,8 +4,8 @@
  select count(1)as cnt from netflix_mdsdb.u_vending_machine_final p
  join netflix_mdwdb.d_vending_machine_c f on sys_id=f.row_id and sourceinstance=f.source_id
 where 
-	u_site<>site_c
-u_account<>account_c
+	u_site<>site_c or
+u_account<>account_c or
 u_machine<>machine_c
 	)b
 	
