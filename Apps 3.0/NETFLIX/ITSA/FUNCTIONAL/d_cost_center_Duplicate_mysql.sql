d8361f24905e88afd2ebf94befb14c1b89d37142
prabhuram <prabhuram.sridharan@numerify.com>
2017-02-23 22:03:48 +0530

    Removing Duplicate Queries from Netflix

--- Apps 3.0/NETFLIX/ITSA/FUNCTIONAL/d_cost_center_Duplicate_mysql.sql
+++
@@ -1,9 +0,0 @@
-SELECT CASE WHEN cnt  THEN 'FAILURE' ELSE 'SUCCESS' END AS Result ,CASE WHEN cnt  THEN 'Data did not Match.'  
-ELSE 'Data Matched' END AS Message   
-FROM( Select count(cnt) as cnt 
-from( 
-select row_id,source_id,count(1) as cnt 
-from  
-netflix_mdwdb.d_cost_center  
-group by 1,2 
-having count(1)>1)c )a 
