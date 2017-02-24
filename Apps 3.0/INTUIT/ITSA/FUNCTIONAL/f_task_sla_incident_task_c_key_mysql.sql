5062be39dbbd0da939255da2e500955555c52872
prabhuram <prabhuram.sridharan@numerify.com>
2017-02-24 16:21:50 +0530

    Updating the Query

--- Apps 3.0/INTUIT/ITSA/FUNCTIONAL/f_task_sla_incident_task_c_key_mysql.sql
+++ Apps 3.0/INTUIT/ITSA/FUNCTIONAL/f_task_sla_incident_task_c_key_mysql.sql
@@ -3,7 +3,7 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 from intuit_mdsdb.task_sla_final tsd
 LEFT JOIN intuit_mdsdb.task_final t ON tsd.task = t.sys_id
 LEFT JOIN intuit_mdwdb.f_task_sla dfts on tsd.sys_id=dfts.row_id 
-  AND tsd.sourceinstance=dfts.source_id
+  AND tsd.sourceinstance=dfts.source_id and dfts.soft_deleted_flag='N'
 LEFT JOIN intuit_mdwdb.d_incident_task_c b ON tsd.task = b.row_id and b.source_id = tsd.sourceinstance
 
 WHERE CASE WHEN upper(t.sys_class_name) ='U_INCIDENT_TASK' THEN b.row_key
