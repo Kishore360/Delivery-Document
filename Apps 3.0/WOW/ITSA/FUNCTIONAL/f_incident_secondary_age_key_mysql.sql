9232ba856fbc80e0d5550350e53bb52b841c340b
Kishore Mantha <kishore.mantha@numerify.com>
2017-03-01 17:47:31 +0530

    Updated the QA Queries as part of the DQA-46 for WOW.

--- Apps 3.0/WOW/ITSA/FUNCTIONAL/f_incident_secondary_age_key_mysql.sql
+++ Apps 3.0/WOW/ITSA/FUNCTIONAL/f_incident_secondary_age_key_mysql.sql
@@ -8,5 +8,5 @@ from wow_mdwdb.f_incident f
 LEFT JOIN wow_mdwdb.d_lov L  
 ON ((f.age BETWEEN L.lower_range_value AND L.upper_range_value)
 	AND L.dimension_class = 'SECONDARYAGEBUCKET_WH~INCIDENT' )
-WHERE COALESCE(L.row_key, case when f.age is null or f.age = 0 THEN 0 else -1 end ) 
+WHERE COALESCE(L.row_key, case when f.age is null and f.age = 0 THEN 0 else -1 end ) 
  <> f.secondary_age_c_key) s
