f5e8d663e20cca83ad3a3057bb2bc28df109dbae
prabhuram <prabhuram.sridharan@numerify.com>
2017-03-02 11:13:07 +0530

    updating the Queries

--- Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/d_change_request_completion_status_src_c_key_mysql.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/d_change_request_completion_status_src_c_key_mysql.sql
@@ -5,4 +5,4 @@ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_reque
 FROM molinahealth_mdsdb.change_request_final src 
 LEFT JOIN molinahealth_mdwdb.d_change_request trgt ON src.sys_id = trgt.row_id AND src.sourceinstance = trgt.source_id
 LEFT JOIN molinahealth_mdwdb.d_lov lkp ON CONCAT('COMPLETION_STATUS_C~CHANGE_REQUEST','~~~',UPPER(src.u_completion_status)) = lkp.row_id AND src.sourceinstance = lkp.source_id and  lkp.dimension_class =  'COMPLETION_STATUS_C~CHANGE_REQUEST'
-WHERE soft_deleted_flag='N' and COALESCE(lkp.row_key,CASE WHEN src.u_completion_status IS NULL THEN 0 else -1 end) <> (trgt.completion_status_src_c_key);
+WHERE trgt.soft_deleted_flag='N' and COALESCE(lkp.row_key,CASE WHEN src.u_completion_status IS NULL THEN 0 else -1 end) <> (trgt.completion_status_src_c_key);
---
+++ Apps 3.0/TIVO/ITSA/FUNCTIONAL/f_task_sla_OUTCOME_FLAG_mysql.sql
@@ -0,0 +1,11 @@
+
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for d_incident.has_breached_update_c_flag' ELSE 'SUCCESS' END as Message 
+FROM (SELECT count(1) as CNT
+from 
+tivo_mdsdb.task_sla_final task_sla 
+join tivo_mdwdb.f_task_sla a on a.row_id=task_sla.sys_id and a.source_id=task_sla.sourceinstance
+where 
+case when task_sla.has_breached=TRUE then 'N' else'Y' end <> OUTCOME_FLAG)temp;
+
