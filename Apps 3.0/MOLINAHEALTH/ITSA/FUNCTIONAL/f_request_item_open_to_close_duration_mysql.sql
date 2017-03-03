7e053a02dbb1364b38791dc97e12d388d244c10a
prabhuram <prabhuram.sridharan@numerify.com>
2017-03-03 21:17:14 +0530

    Molina and NBCU Queries corrected

--- Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_request_item_open_to_close_duration_mysql.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_request_item_open_to_close_duration_mysql.sql
@@ -1,3 +1,4 @@
+
 select 
 case 
 
@@ -16,13 +17,14 @@ case when src.open_to_close_duration is not null then 1 else 0 end as src_not_nu
 
 case when trgt.open_to_close_duration is not null then 1 else 0 end as trgt_not_null_count,
 
-case when coalesce(case when lkp.dimension_wh_code = 'OPEN' then null else src.open_to_close_duration end,0)  <> COALESCE(trgt.open_to_close_duration,0) then src.sys_id else '' end as failures,
+case when case when lkp.dimension_wh_code = 'OPEN' then null WHEN  lkp.dimension_wh_code in('FULFILLED','CLOSED') and fulfilled_on is null THEN CASE WHEN opened_at > sys_updated_on THEN NULL ELSE src.open_to_close_duration end end  <> trgt.open_to_close_duration then src.sys_id else '' end as failures,
 
-case when coalesce(case when lkp.dimension_wh_code = 'OPEN' then null else src.open_to_close_duration end,0) <> COALESCE(trgt.open_to_close_duration,0) then 1 else 0 end as failures_cnt
+case when case when lkp.dimension_wh_code = 'OPEN' then null WHEN  lkp.dimension_wh_code in('FULFILLED','CLOSED') and fulfilled_on is null then CASE WHEN opened_at > sys_updated_on THEN NULL ELSE src.open_to_close_duration end end  <> trgt.open_to_close_duration then 1 else 0 end as failures_cnt
 
 from 
 (select 
-ri.sys_id,ri.sourceinstance,timestampdiff(second,ri.opened_at,coalesce(sa_src.fulfilled_on,coalesce(ri.closed_at,ri.sys_updated_on))) as open_to_close_duration
+ri.sys_id,ri.sourceinstance,timestampdiff(second,ri.opened_at,coalesce(ri.closed_at,ri.sys_updated_on)) as open_to_close_duration,fulfilled_on,opened_at,sys_updated_on
+
 from molinahealth_mdsdb.sc_req_item_final ri
 left join (select su.documentkey,su.sourceinstance,max(su.sys_created_on) as fulfilled_on
 from molinahealth_mdsdb.sys_audit_final su 
@@ -31,3 +33,4 @@ where su.tablename = 'sc_req_item' and su.fieldname = 'state' and sc.name = 'sc_
 group by su.documentkey,su.sourceinstance) sa_src on ri.sys_id = sa_src.documentkey) src 
 left join molinahealth_mdwdb.f_request_item trgt on trgt.row_id = src.sys_id and trgt.source_id = src.sourceinstance
 left join molinahealth_mdwdb.d_lov_map lkp on trgt.state_src_key = lkp.src_key and lkp.dimension_class = 'STATE~SC_REQ_ITEM') fnl ;
+
