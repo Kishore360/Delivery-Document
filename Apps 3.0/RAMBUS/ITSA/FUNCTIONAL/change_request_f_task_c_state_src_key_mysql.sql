a30e18a42d5e6c609cb04613555b0562be3df58f
Kishore Mantha <kishore.mantha@numerify.com>
2017-03-15 18:23:28 +0530

    Updated the State_src_key QA Queries as part of the regression issues (DELV-5146)

---
+++ Apps 3.0/RAMBUS/ITSA/FUNCTIONAL/change_request_f_task_c_state_src_key_mysql.sql
@@ -0,0 +1,16 @@
+
+
+
+SELECT 
+CASE WHEN CNT >0 THEN 'FAILURE' ELSE 'SUCCESS' END AS RESULT,
+CASE WHEN CNT >0 THEN 'DATA did not Mactch.' ELSE 'DATA Matched' END AS MESSAGE
+FROM 
+(SELECT count(1) as CNT 
+FROM rambus_mdsdb.change_request_final SRC
+JOIN rambus_mdwdb.f_task_c TRGT ON (SRC.sys_id =TRGT.row_id  AND SRC.sourceinstance= TRGT.source_id)
+JOIN rambus_mdwdb.d_lov LKP ON
+(CASE WHEN SRC.sys_class_name='CHANGE_REQUEST' THEN
+COALESCE( CONCAT('STATE','~','CHANGE_REQUEST','~','~','~',UPPER(SRC.state)),'UNSPECIFIED') END
+= LKP.src_rowid 
+AND SRC.sourceinstance= LKP.source_id)
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.state IS NULL THEN 0 else -1 end)<>TRGT.state_src_key)temp;
