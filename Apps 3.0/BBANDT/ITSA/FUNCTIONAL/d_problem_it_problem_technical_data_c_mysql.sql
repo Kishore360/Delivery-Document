bb67228d5c5fec0c92185427f9a1a47597e8e48e
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-03-16 16:48:15 +0530

    DELV-5162

---
+++ Apps 3.0/BBANDT/ITSA/FUNCTIONAL/d_problem_it_problem_technical_data_c_mysql.sql
@@ -0,0 +1,11 @@
+ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.it_problem_technical_data_c' ELSE 'SUCCESS' END as Message
+ FROM bbandt_mdsdb.problem_final SRC 
+  JOIN   bbandt_mdwdb.d_problem TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ 
+ AND SRC.sourceinstance= TRGT.source_id  )
+ WHERE SRC.u_it_problem_technical_data  TRGT.it_problem_technical_data_c
+ 
+ 
+ 
