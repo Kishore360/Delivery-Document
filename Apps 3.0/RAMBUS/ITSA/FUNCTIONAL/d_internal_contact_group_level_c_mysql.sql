6ea7477d80684465c34ab9d92243013de6ab65fe
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-03-17 15:17:44 +0530

    DELV-4839 Rambus

--- Apps 3.0/RAMBUS/ITSA/FUNCTIONAL/d_internal_contact_group_level_c_mysql.sql
+++ Apps 3.0/RAMBUS/ITSA/FUNCTIONAL/d_internal_contact_group_level_c_mysql.sql
@@ -1,10 +1,11 @@
 
-SELECT 
-CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result,
-CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-from(select count(1) as cnt
-from rambus_mdsdb.sys_user_final b  
-left JOIN rambus_mdwdb.d_internal_contact a 
-ON a.row_id =concat('INTERNAL_CONTACT~',b.sys_id)  
-where coalesce(a.group_level_c,'UNSPECIFIED')<>coalesce(b.u_it_group_level,'UNSPECIFIED'))z; 
+ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_internal_contact.group_level_c' ELSE 'SUCCESS' END as Message
+ FROM rambus_mdsdb.sys_user_final s
+ join rambus_mdwdb.d_internal_contact TRGT 
+ ON (s.sys_id =right(TRGT.row_id,32) 
+ AND s.sourceinstance= TRGT.source_id  )
+ WHERE coalesce(s.u_it_group_level,'UNSPECIFIED') <> (TRGT.group_level_c)
+ ;
+ 
+ 
