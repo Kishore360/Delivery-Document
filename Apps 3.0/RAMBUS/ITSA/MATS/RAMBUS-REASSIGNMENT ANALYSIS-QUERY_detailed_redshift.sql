8a86bd3029ee07a628f853cdecbf63e04006842d
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-03-20 21:14:20 +0530

    Rambus Cust and Mats query updation for support ticket

--- Apps 3.0/RAMBUS/ITSA/MATS/RAMBUS-REASSIGNMENT ANALYSIS-QUERY_detailed_redshift.sql
+++ Apps 3.0/RAMBUS/ITSA/MATS/RAMBUS-REASSIGNMENT ANALYSIS-QUERY_detailed_redshift.sql
@@ -1,6 +1,3 @@
-SELECT CASE WHEN max_count<>min_count THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN  max_count<>min_count THEN 'OOTB has Keys Dropped' ELSE 'SUCCESS'  END as Message FROM (
- select max(Row_Count) max_count,Min(Row_Count) min_count from (
 select'ldb.f_incident_reassignment_analysis_c' as Table_Name, count(a11.incident_key) Row_Count
  from  ldb.f_incident_reassignment_analysis_c       a11 
 union
@@ -49,4 +46,4 @@ select'ldb.d_calendar_month_opened' as Table_Name, count(a11.incident_key) Row_C
 JOIN ldb.d_calendar_date_opened	       a14	ON (a11.opened_on_key = a14.row_key)
 join ldb.d_calendar_month_opened       a20
 on (a14.month_start_date_key = a20.row_key)
-)a)B
+
