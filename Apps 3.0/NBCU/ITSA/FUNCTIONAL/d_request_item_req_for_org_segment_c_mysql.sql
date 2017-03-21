b6770ad38e5bc61780761adf16946518535d088c
vijay rao <vijay.rao@numerify.com>
2017-03-21 15:46:01 +0530

    NBCU cust queries

---
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_request_item_req_for_org_segment_c_mysql.sql
@@ -0,0 +1,8 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message
+from
+nbcu_mdwdb.d_request_item a
+join nbcu_mdsdb.sc_req_item_final b
+ON a.row_id=b.sys_id
+AND a.source_id=b.sourceinstance
+where b.u_req_for_org_segment<>a.req_for_org_segment_c;
