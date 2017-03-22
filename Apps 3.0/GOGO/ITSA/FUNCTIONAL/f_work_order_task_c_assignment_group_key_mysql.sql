70911e5b1bf44c0b0f79449a5c9a8c8b79c6a8e5
prabhuram <prabhuram.sridharan@numerify.com>
2017-03-22 13:26:38 +0530

    committing the query perf changes

--- Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_work_order_task_c_assignment_group_key_mysql.sql
+++ Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_work_order_task_c_assignment_group_key_mysql.sql
@@ -5,9 +5,9 @@
  CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_change_request.work_start_on_key' ELSE 'SUCCESS' END as Message
  FROM (SELECT count(1) as CNT 
  FROM gogo_mdsdb.wm_task_final SRC
- LEFT JOIN gogo_mdwdb.f_work_order_task_c TRGT 
- ON (SRC.sys_id =TRGT.row_id  AND SRC.sourceinstance= TRGT.source_id  ) 
  LEFT JOIN gogo_mdwdb.d_internal_organization LKP on (LKP.row_id =  concat('GROUP~',SRC.assignment_group)) 
+ JOIN gogo_mdwdb.f_work_order_task_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  AND SRC.sourceinstance= TRGT.source_id  ) 
  WHERE COALESCE(LKP.row_key,case when SRC.assignment_group is null then 0 else -1 end )<>(TRGT.assignment_group_key)
  AND DATE_FORMAT(TRGT.pivot_date, '%Y-%m-%d %H:%i:%s') BETWEEN effective_from AND effective_to)temp;
  
--- Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_work_task_order_task_c_actual_travel_duration_mysql.sql
+++ Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_work_task_order_task_c_actual_travel_duration_mysql.sql
@@ -9,7 +9,7 @@
  CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_change_request.time_worked' ELSE 'SUCCESS' END as Message
  FROM (SELECT count(1) as CNT
  FROM gogo_mdsdb.wm_task_final SRC 
- LEFT JOIN gogo_mdwdb.f_work_order_task_c TRGT 
+  JOIN gogo_mdwdb.f_work_order_task_c TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
  WHERE COALESCE( TIMESTAMPDIFF(SECOND,'1970-01-01 00:00:00',SRC.actual_travel_duration),'')<> COALESCE(TRGT.actual_travel_duration ,''))a;
