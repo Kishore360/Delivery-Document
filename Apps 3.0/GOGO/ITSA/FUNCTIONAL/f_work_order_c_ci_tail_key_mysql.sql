41f5ede564bd2d712f2154bc1fcef631003ade28
Kishore Mantha <kishore.mantha@numerify.com>
2017-03-22 13:15:00 +0530

    updated the gogo

--- Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_work_order_c_ci_tail_key_mysql.sql
+++ Apps 3.0/GOGO/ITSA/FUNCTIONAL/f_work_order_c_ci_tail_key_mysql.sql
@@ -1,9 +1,13 @@
- SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.work_start_on_key' ELSE 'SUCCESS' END as Message
+ SELECT 
+ CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_change_request.work_start_on_key' ELSE 'SUCCESS' END as Message
+ FROM (SELECT count(1) as CNT
  FROM gogo_mdsdb.wm_order_final SRC
- LEFT JOIN gogo_mdwdb.f_work_order_c TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  ) LEFT JOIN gogo_mdwdb.d_configuration_item LKP 
+ LEFT JOIN gogo_mdwdb.d_configuration_item LKP 
 on (LKP.row_id = COALESCE(SRC.u_cmdb_ci_tail,'UNSPECIFIED') ) 
- WHERE COALESCE(LKP.row_key,case when SRC.u_cmdb_ci_tail is null then 0 else -1 end )<>(TRGT.ci_tail_key);
+ JOIN gogo_mdwdb.f_work_order_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+ WHERE COALESCE(LKP.row_key,case when SRC.u_cmdb_ci_tail is null then 0 else -1 end )<>(TRGT.ci_tail_key))a;
+ 
  
