2d32823b9bb4e0001b85c7354b8885e885457918
Kishore Mantha <kishore.mantha@numerify.com>
2017-03-24 09:58:48 +0530

    removed the asu folder from 5.0 as latest changes will be added later.

--- Apps 5.0/ASU/ITSA/FUNCTIONAL/f_m2m_kb_task_c_system_modified_count_mysql.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.work_start_on_key' ELSE 'SUCCESS' END as Message
-FROM asu_mdsdb.m2m_kb_task_final
-join asu_mdwdb.f_m2m_kb_task_c on sys_id=row_id and sourceinstance=source_id
-where sys_mod_count<>system_modified_count
-
