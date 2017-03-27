3fc9d4498e1e74ed3f66261f802da02041c72199
Kishore Mantha <kishore.mantha@numerify.com>
2017-03-27 18:26:59 +0530

    Updated the OOTB Queries

--- Apps 5.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/f_incident_closed_by_key_mysql.sql
+++ Apps 5.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/f_incident_closed_by_key_mysql.sql
@@ -1,16 +1,18 @@
- SELECT CASE WHEN cnt THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN cnt THEN 'MDS to DWH data validation failed for f_incident.closed_by_key' ELSE 'SUCCESS' END as Message from (select count(1) cnt 
- FROM <<tenant>>_mdsdb.incident_final SRC 
-  JOIN <<tenant>>_mdwdb.f_incident TRGT 
+SELECT 
+ CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_change_request.closed_by_key' ELSE 'SUCCESS' END as Message
+ FROM (SELECT  count(1) as CNT 
+ FROM <<tenant>>_mdsdb.incident_final SRC
+ JOIN <<tenant>>_mdwdb.f_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
-   JOIN <<tenant>>_mdwdb.d_lov_map dlm 
+	JOIN <<tenant>>_mdwdb.d_lov_map dlm 
 ON TRGT.state_src_key = dlm.src_key AND dlm.dimension_wh_code = 'CLOSED'
-LEFT JOIN <<tenant>>_mdwdb.d_internal_contact LKP 
- ON ( concat('INTERNAL_CONTACT~',closed_by)= LKP.row_id 
-AND SRC.sourceinstance= LKP.source_id 
+ LEFT JOIN <<tenant>>_mdwdb.d_internal_contact LKP 
+ ON ( CONCAT('INTERNAL_CONTACT~',closed_by)= LKP.row_id 
+ AND SRC.sourceinstance= LKP.source_id
 AND TRGT.pivot_date
- BETWEEN LKP.effective_from AND LKP.effective_to) 
-
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.closed_by IS NULL THEN 0 else -1 end)<> (TRGT.closed_by_key) )b
- 
+ BETWEEN LKP.effective_from AND LKP.effective_to)
+LEFT JOIN <<tenant>>_mdwdb.d_internal_contact ic ON (SRC.sys_updated_by = ic.user_name AND SRC.sourceinstance = ic.source_id)
+ WHERE 
+ COALESCE(LKP.row_key,ic.row_key,CASE WHEN SRC.closed_by IS NULL THEN 0 else -1 end)<> (TRGT.closed_by_key)) temp;
