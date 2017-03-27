d064ab8bdd6608d9f8878f386b2d307a8b2319eb
Kishore Mantha <kishore.mantha@numerify.com>
2017-03-27 18:37:52 +0530

    Updated the QA Query

--- Apps 5.0/ASU/ITSA/FUNCTIONAL/f_hr_case_age_key_mysql.sql
+++ Apps 5.0/ASU/ITSA/FUNCTIONAL/f_hr_case_age_key_mysql.sql
@@ -1,11 +1,12 @@
 select 
 CASE WHEN cnt> 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.secondary_age_c_key' 
-ELSE 'SUCCESS' END as Message
+CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.secondary_age_c_key' ELSE 'SUCCESS' END as Message
 from 
 ( select count(1) as cnt
 from asu_mdwdb.f_hr_case f  
 LEFT JOIN asu_mdwdb.d_lov L  
-ON ((f.age BETWEEN L.lower_range_value AND L.upper_range_value) AND L.dimension_class = 'AGE_C~HRCASE' )
+ON floor(f.age/(3600*24)) BETWEEN L.lower_range_value AND L.upper_range_value AND L.dimension_class = 'Agebucket_WH~HR_CASE' 
 WHERE COALESCE(L.row_key, case when f.age is null or f.age = 0 THEN 0 else -1 end ) 
- <> f.age_key) s
+ <> (f.age_key)) s
+ 
+ 
