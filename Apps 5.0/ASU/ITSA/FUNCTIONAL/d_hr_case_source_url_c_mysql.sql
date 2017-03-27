eb8e87a60c372260215336c2adf982309b37b47f
Kishore Mantha <kishore.mantha@numerify.com>
2017-03-27 11:42:49 +0530

    Updated the ASU QA Queries with syntax error

--- Apps 5.0/ASU/ITSA/FUNCTIONAL/d_hr_case_source_url_c_mysql.sql
+++ Apps 5.0/ASU/ITSA/FUNCTIONAL/d_hr_case_source_url_c_mysql.sql
@@ -6,6 +6,7 @@ FROM asu_mdsdb.hr_case_final SRC
 JOIN asu_mdwdb.d_hr_case_c TRGT
 ON (TRGT.row_id=SRC.sys_id and TRGT.source_id=SRC.sourceinstance)
 WHERE 
-CONCAT('<a href=\"','https://asu.service-now.com/nav_to.do?uri=','hr_case.do?sys_id=',SRC.sys_id,'\" target=\"_blank\">',SRC.number,'</a>')<>TRGT.source_url_c
+CONCAT('<a href=\"','https://asu.service-now.com/nav_to.do?uri=','hr_case.do?sys_id=',SRC.sys_id,'\" target=\"_blank\">',SRC.number,'</a>')<>TRGT.source_url_c)temp;
+
 
 
