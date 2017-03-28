b11f6e70733fd0b1c545b79a3802d517b324127a
vijay rao <vijay.rao@numerify.com>
2017-03-28 16:17:52 +0530

    ITAA cust query correction for ITAA

--- Apps 5.0/USF/ITAA/FUNCTIONAL/f_asset_depreciated_doc_amount_mysql.sql
+++ Apps 5.0/USF/ITAA/FUNCTIONAL/f_asset_depreciated_doc_amount_mysql.sql
@@ -1,7 +1,8 @@
-CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset.depreciated_doc_amount' ELSE 'SUCCESS' END as Message
+SELECT CASE WHEN cnt THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN cnt THEN 'MDS to DWH data validation failed for f_asset.depreciated_doc_amount' ELSE 'SUCCESS' END as Message from 
+(select count(1) cnt 
  FROM usf_mdsdb.alm_asset_final SRC 
  LEFT JOIN usf_mdwdb.f_asset TRGT 
  ON ( SRC.sys_id=TRGT.row_id 
  AND SRC.sourceinstance=TRGT.source_id) 
-  WHERE SRC.depreciated_amount<> TRGT.depreciated_doc_amount;
+  WHERE SRC.depreciated_amount<> TRGT.depreciated_doc_amount)b;
--- Apps 5.0/USF/ITAA/FUNCTIONAL/f_asset_history_cost_center_key_mysql.sql
+++ Apps 5.0/USF/ITAA/FUNCTIONAL/f_asset_history_cost_center_key_mysql.sql
@@ -3,9 +3,9 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset_status_history.cost_center_key' ELSE 'SUCCESS' END as Message
  FROM usf_mdsdb.alm_asset_final SRC 
  LEFT JOIN usf_mdwdb.f_asset_status_history TRGT 
-on (convert(CONCAT(SRC.sys_id,'~',DATE_FORMAT(SRC.sys_created_on,'%Y%m%d%H%i%S') ) using utf8) =convert(TRGT.row_id using utf8)
+on (convert(CONCAT(SRC.sys_id,'~',DATE_FORMAT(SRC.sys_created_on,'%Y%m%d%H%i%S') ) ) =convert(TRGT.row_id )
  AND SRC.sourceinstance=TRGT.source_id )
  LEFT JOIN usf_mdwdb.d_cost_center LKP 
- ON ( CONVERT(SRC.cost_center using utf8)= convert(LKP.row_id using utf8)
-AND CONVERT(SRC.sourceinstance using utf8)= convert(LKP.source_id using utf8))
+ ON ( CONVERT(SRC.cost_center )= convert(LKP.row_id )
+AND CONVERT(SRC.sourceinstance )= convert(LKP.source_id ))
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.cost_center IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.cost_center_key,'')
