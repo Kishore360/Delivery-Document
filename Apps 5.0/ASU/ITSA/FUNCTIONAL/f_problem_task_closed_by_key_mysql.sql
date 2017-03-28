737bc46371d5ae8fbd9bbb67c6ef6d9e1974c0ac
Kishore Mantha <kishore.mantha@numerify.com>
2017-03-28 10:12:14 +0530

    Updated the ASU 5.0 QA Queries

--- Apps 5.0/ASU/ITSA/FUNCTIONAL/f_problem_task_closed_by_key_mysql.sql
+++ Apps 5.0/ASU/ITSA/FUNCTIONAL/f_problem_task_closed_by_key_mysql.sql
@@ -1,13 +1,18 @@
-
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match' 
-ELSE 'Data Matched' END AS Message 
-FROM (
-select count(1) as cnt  
-from asu_mdsdb.problem_task_final s
-left join asu_mdwdb.f_problem_task   t
-on s.sys_id=t.row_id and s.sourceinstance=t.source_id
-left join asu_mdwdb.d_internal_contact l
-ON ( concat('INTERNAL_CONTACT~',s.closed_by)= l.row_id AND s.sourceinstance= l.source_id )
- WHERE COALESCE(l.row_key,CASE WHEN s.closed_by IS NULL THEN 0 else '-1' end)<> COALESCE(t.closed_by_key,'')
-) c
+SELECT 
+ CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_problem_task.closed_by_key' ELSE 'SUCCESS' END as Message
+ FROM (SELECT  count(1) as CNT 
+ FROM asu_mdsdb.problem_task_final  SRC
+ JOIN asu_mdwdb.f_problem_task TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+	JOIN asu_mdwdb.d_lov_map dlm 
+ON TRGT.state_src_key = dlm.src_key AND dlm.dimension_wh_code = 'CLOSED'
+ LEFT JOIN asu_mdwdb.d_internal_contact LKP 
+ ON ( CONCAT('INTERNAL_CONTACT~',closed_by)= LKP.row_id 
+ AND SRC.sourceinstance= LKP.source_id
+AND TRGT.pivot_date
+ BETWEEN LKP.effective_from AND LKP.effective_to)
+LEFT JOIN asu_mdwdb.d_internal_contact ic ON (SRC.sys_updated_by = ic.user_name AND SRC.sourceinstance = ic.source_id)
+ WHERE 
+ COALESCE(LKP.row_key,ic.row_key,CASE WHEN SRC.closed_by IS NULL THEN 0 else -1 end)= (TRGT.closed_by_key)) temp;
--- Apps 5.0/OOTB/ITSA/FUNCTIONAL/PROBLEM_TASK/f_problem_task_closed_by_key_mysql.sql
+++ Apps 5.0/OOTB/ITSA/FUNCTIONAL/PROBLEM_TASK/f_problem_task_closed_by_key_mysql.sql
@@ -1,13 +1,18 @@
-SELECT 
-CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_problem_task.closed_by_key' ELSE 'SUCCESS' END as Message
-FROM (SELECT count(1) as CNT
- FROM <<tenant>>_mdsdb.problem_task_final SRC 
- LEFT JOIN <<tenant>>_mdwdb.f_problem_task TRGT 
+ SELECT 
+ CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_problem_task.closed_by_key' ELSE 'SUCCESS' END as Message
+ FROM (SELECT  count(1) as CNT 
+ FROM <<tenant>>_mdsdb.problem_task_final  SRC
+ JOIN <<tenant>>_mdwdb.f_problem_task TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
-LEFT JOIN <<tenant>>_mdwdb.d_internal_contact LKP 
- ON ( concat('INTERNAL_CONTACT~',closed_by)= LKP.row_id 
-AND SRC.sourceinstance= LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.closed_by IS NULL THEN 0 else -1 end)<> (TRGT.closed_by_key))temp;
- 
+	JOIN <<tenant>>_mdwdb.d_lov_map dlm 
+ON TRGT.state_src_key = dlm.src_key AND dlm.dimension_wh_code = 'CLOSED'
+ LEFT JOIN <<tenant>>_mdwdb.d_internal_contact LKP 
+ ON ( CONCAT('INTERNAL_CONTACT~',closed_by)= LKP.row_id 
+ AND SRC.sourceinstance= LKP.source_id
+AND TRGT.pivot_date
+ BETWEEN LKP.effective_from AND LKP.effective_to)
+LEFT JOIN <<tenant>>_mdwdb.d_internal_contact ic ON (SRC.sys_updated_by = ic.user_name AND SRC.sourceinstance = ic.source_id)
+ WHERE 
+ COALESCE(LKP.row_key,ic.row_key,CASE WHEN SRC.closed_by IS NULL THEN 0 else -1 end)<> (TRGT.closed_by_key)) temp;
