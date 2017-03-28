a4fa0271114b00d12a75789d083b7ab4e0e4d1d4
Kishore Mantha <kishore.mantha@numerify.com>
2017-03-28 11:20:06 +0530

    Added the new logic to the query.

--- Apps 5.0/ASU/ITSA/FUNCTIONAL/f_kb_knowledge_c_age_mysql.sql
+++ Apps 5.0/ASU/ITSA/FUNCTIONAL/f_kb_knowledge_c_age_mysql.sql
@@ -4,9 +4,9 @@ CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 from
 (select count(1) cnt FROM (select sys_id,sourceinstance,published from asu_mdsdb.kb_knowledge_final ) SRC 
 join asu_mdwdb.f_kb_knowledge_c f ON (SRC.sys_id =f.row_id  AND SRC.sourceinstance= f.source_id and f.soft_deleted_flag='N' )
-WHERE TIMESTAMPDIFF(second,CONVERT_TZ(CONVERT_TZ(SRC.published,'GMT' ,'US/Mountain'),'US/Mountain','GMT'),
+WHERE CASE WHEN SRC.published > SYSDATE() THEN 0 ELSE 
+(TIMESTAMPDIFF(second,CONVERT_TZ(CONVERT_TZ(SRC.published,'GMT' ,'US/Mountain'),'US/Mountain','GMT'),
 (SELECT CONVERT_TZ(MAX(lastupdated),'US/Mountain','GMT') AS lastupdated FROM asu_mdwdb.d_o_data_freshness 
-WHERE sourcename like 'ServiceNow%')) <> f.age
+WHERE sourcename like 'ServiceNow%')))END <> f.age
  )A ; 
- 
  
