e2cf8544f4571528934ec0f7f02b0222ed4a1ba8
vijay rao <vijay.rao@numerify.com>
2017-03-28 15:38:33 +0530

    correcting syntax errors for ITAA queries

--- Apps 5.0/USF/ITAA/FUNCTIONAL/d_asset_computer_usage_c_mysql.sql
+++ Apps 5.0/USF/ITAA/FUNCTIONAL/d_asset_computer_usage_c_mysql.sql
@@ -1,7 +1,8 @@
-CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_asset.physical_location_c' ELSE 'SUCCESS' END as Message
+SELECT CASE WHEN cnt THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN cnt THEN 'MDS to DWH data validation failed for d_asset.computer_usage_c' ELSE 'SUCCESS' END as Message from
+(select count(1) as cnt
  FROM usf_mdsdb.alm_asset_final SRC 
  LEFT JOIN usf_mdwdb.d_asset TRGT 
  ON ( SRC.sys_id=TRGT.row_id 
  AND SRC.sourceinstance=TRGT.source_id) 
-  WHERE SRC.u_computer_usage<> TRGT.computer_usage_c;
+  WHERE SRC.u_computer_usage<> TRGT.computer_usage_c)t;
