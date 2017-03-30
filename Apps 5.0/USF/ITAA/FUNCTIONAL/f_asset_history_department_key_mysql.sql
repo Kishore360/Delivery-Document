8561ce8573f7046dbaca1d2a2a5c7e95110ff486
vijay rao <vijay.rao@numerify.com>
2017-03-30 15:25:35 +0530

    ITAA ETL queries for USF

--- Apps 5.0/USF/ITAA/FUNCTIONAL/f_asset_history_department_key_mysql.sql
+++ Apps 5.0/USF/ITAA/FUNCTIONAL/f_asset_history_department_key_mysql.sql
@@ -2,9 +2,11 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset_status_history.department_key' ELSE 'SUCCESS' END as Message
  FROM usf_mdsdb.alm_asset_final SRC 
  LEFT JOIN usf_mdwdb.f_asset_status_history TRGT 
- ON (convert(concat(SRC.sys_id,'~',date_format(sys_created_on,'%Y%m%d%H%i%S' ) ) ) =convert(TRGT.row_id ) 
+ ON (SRC.sys_id=TRGT.row_id 
  AND SRC.sourceinstance=TRGT.source_id )
- LEFT JOIN usf_mdwdb.d_internal_organization LKP 
- ON  ( CONVERT(CONCAT('DEPARTMENT~',SRC.department) )= convert(LKP.row_id )
-AND CONVERT(SRC.sourceinstance )= convert(LKP.source_id ))
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.department IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.department_key,'')
+ LEFT JOIN usf_mdsdb.sys_user_final LKP 
+ ON SRC.owned_by= LKP.sys_id 
+AND SRC.sourceinstance = LKP.sourceinstance 
+LEFT JOIN usf_mdwdb.d_internal_organization LKP1
+on LKP1.row_id=COALESCE(CONCAT('DEPARTMENT~',SRC.department),CONCAT('DEPARTMENT~',LKP.department))
+ WHERE COALESCE(LKP1.row_key,CASE WHEN COALESCE(SRC.department, LKP.department)IS NULL then 0 else '-1' end)<> COALESCE(TRGT.department_key,'')
--- Apps 5.0/USF/ITAA/FUNCTIONAL/f_asset_status_key_mysql.sql
+++ Apps 5.0/USF/ITAA/FUNCTIONAL/f_asset_status_key_mysql.sql
@@ -1,12 +1,10 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset.asset_substatus_key' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset.asset_status_key' ELSE 'SUCCESS' END as Message
  FROM usf_mdsdb.alm_asset_final SRC 
  LEFT JOIN usf_mdwdb.f_asset TRGT 
  ON ( SRC.sys_id=TRGT.row_id 
  AND SRC.sourceinstance=TRGT.source_id) 
  LEFT JOIN usf_mdwdb.d_lov LKP 
- ON COALESCE(CONCAT('ASSET~SUBSTATUS~~',SRC.u_install_status),'UNSPECIFIED') =LKP.row_id 
+ ON COALESCE(CONCAT('ASSET~STATUS~~',SRC.u_install_status),'UNSPECIFIED') =LKP.row_id 
 AND (SRC.sourceinstance )= LKP.source_id 
-AND LKP.dimension_class ='ASSET'
-AND LKP.dimension_type = 'SUBSTATUS'
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_install_status IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.asset_substatus_key,'')
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_install_status IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.asset_status_key,'')
