83597b0c8ab545cc0d8bda044e0572e18372797e
vijay rao <vijay.rao@numerify.com>
2017-03-31 11:12:44 +0530

    ITAA USF CUST Query updated

--- Apps 5.0/USF/ITAA/FUNCTIONAL/f_expense_item_cost_center_key_mysql.sql
+++ Apps 5.0/USF/ITAA/FUNCTIONAL/f_expense_item_cost_center_key_mysql.sql
@@ -1,11 +1,12 @@
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset_status_history.cost_center_key' ELSE 'SUCCESS' END as Message
  FROM usf_mdsdb.alm_asset_final SRC 
- LEFT JOIN usf_mdwdb.f_expense TRGT 
-on (convert(CONCAT(SRC.sys_id,'~',DATE_FORMAT(SRC.sys_created_on,'%Y%m%d%H%i%S') ) ) =convert(TRGT.row_id )
+LEFT JOIN usf_mdwdb.f_expense_item TRGT 
+ ON (SRC.sys_id=TRGT.asset_key 
  AND SRC.sourceinstance=TRGT.source_id )
- LEFT JOIN usf_mdwdb.d_cost_center LKP 
- ON ( CONVERT(SRC.cost_center )= convert(LKP.row_id )
-AND CONVERT(SRC.sourceinstance )= convert(LKP.source_id ))
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.cost_center IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.cost_center_key,'')
+ LEFT JOIN usf_mdsdb.sys_user_final LKP 
+ ON SRC.owned_by= LKP.sys_id 
+AND SRC.sourceinstance = LKP.sourceinstance 
+LEFT JOIN usf_mdwdb.d_cost_center LKP1
+on LKP1.row_id=COALESCE( LKP.cost_center,'UNSPECIFIED')
+ WHERE COALESCE(LKP1.row_key,CASE WHEN LKP.cost_center IS NULL THEN 0 else '-1' end)<> TRGT.cost_center_key
