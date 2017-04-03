6bd95891d818d32c2ebf1d7a76cd879af8e1b134
Kishore Mantha <kishore.mantha@numerify.com>
2017-04-03 17:56:17 +0530

    Updated the  VARIABLES_REQUEST_TYPE_mysql Query

--- Apps 5.0/ASU/ITSA/FUNCTIONAL/VARIABLES_REQUEST_TYPE_mysql.sql
+++ Apps 5.0/ASU/ITSA/FUNCTIONAL/VARIABLES_REQUEST_TYPE_mysql.sql
@@ -3,7 +3,7 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 from  
 (select count(1)  as cnt from
 (select type,sourceinstance,sys_id,name  from asu_mdsdb.item_option_new_final  where name='request_type'
-and  sys_id  in 
+and  sys_id  not in 
 (select  row_id from  asu_workdb.lsm_ls_variable_list lvl where 
 lvl.table_name='request_item' and lvl.variable_type!='Reference') 
  ) e
