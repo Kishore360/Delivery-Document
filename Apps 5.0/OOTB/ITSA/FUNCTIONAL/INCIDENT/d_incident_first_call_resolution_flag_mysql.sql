06e2794da744baff6fc32a2cae887b3cc8daa3c1
Kishore Mantha <kishore.mantha@numerify.com>
2017-04-13 14:19:49 +0530

    Updated the QA Queries.

--- Apps 5.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/d_incident_first_call_resolution_flag_mysql.sql
+++ Apps 5.0/OOTB/ITSA/FUNCTIONAL/INCIDENT/d_incident_first_call_resolution_flag_mysql.sql
@@ -1,4 +1,5 @@
- SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ SELECT 
+ CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_incident.first_call_resolution_flag' ELSE 'SUCCESS' END as Message from (select count(1) cnt 
    FROM  <<tenant>>_mdsdb.incident_final SRC 
  LEFT JOIN <<tenant>>_mdwdb.d_incident TRGT 
@@ -13,11 +14,11 @@ LEFT JOIN <<tenant>>_mdwdb.d_lov_map LM ON TRGTF.state_src_key=LM.src_key and LM
 
  WHERE LM.dimension_wh_code IN('RESOLVED') and 
  CASE WHEN
-<<<<<<< HEAD
+-- <<<<<<< HEAD
  TIMESTAMPDIFF(MINUTE,coalesce(SRC.opened_at,'1970-01-01 00:00:00'), SRC.resolved_at)<30
   THEN 'Y' ELSE 'N' END  <> (TRGT.first_call_resolution_flag ))a
-=======
- LM.dimension_wh_code IN('RESOLVED','CLOSED') 
- AND TIMESTAMPDIFF(MINUTE,coalesce(SRC.opened_at,'1970-01-01 00:00:00'), coalesce(SRC.resolved_at,SRC.closed_at,SRC.sys_updated_on))<30
-  THEN 'Y' ELSE 'N' END  <> COALESCE(TRGT.first_call_resolution_flag ,''))temp;
->>>>>>> c7bfeaa6c218a7189df0320b4c059663b1d717ca
+-- =======
+-- LM.dimension_wh_code IN('RESOLVED','CLOSED') 
+-- AND TIMESTAMPDIFF(MINUTE,coalesce(SRC.opened_at,'1970-01-01 00:00:00'), coalesce(SRC.resolved_at,SRC.closed_at,SRC.sys_updated_on))<30
+-- THEN 'Y' ELSE 'N' END  <> COALESCE(TRGT.first_call_resolution_flag ,''))temp;
+-- >>>>>>> c7bfeaa6c218a7189df0320b4c059663b1d717ca
