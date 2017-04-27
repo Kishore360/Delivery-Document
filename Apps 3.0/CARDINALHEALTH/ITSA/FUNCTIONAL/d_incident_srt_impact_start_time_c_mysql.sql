a9ef8f876f8a414642373e040d9d16c162777d83
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-04-27 14:24:22 +0530

    Cardinal SRT related attributes and metrics

---
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_incident_srt_impact_start_time_c_mysql.sql
@@ -0,0 +1,13 @@
+
+
+ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+FROM (
+select count(1) as cnt 
+from cardinalhealth_mdsdb.incident_final a
+JOIN  cardinalhealth_mdwdb.d_incident b
+on  b.row_id = a.sys_id and a.sourceinstance=b.source_id
+where convert_tz(a.u_srt_impact_start_time,'GMT','America/New_York')<> b.srt_impact_start_time_c)c
+
+
