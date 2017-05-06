b0ef0c9639cedc454cebe6f49fc01c1ee69d8706
Shubha <Shubha.rao@numerify.com>
2017-05-06 21:45:45 +0530

    Updating cust queries for USF

--- Apps 5.0/USF/ITAA/FUNCTIONAL/d_asset_deployed_cy_c_flag_mysql.sql
+++ Apps 5.0/USF/ITAA/FUNCTIONAL/d_asset_deployed_cy_c_flag_mysql.sql
@@ -1,8 +1,15 @@
 SELECT CASE WHEN cnt THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
 CASE WHEN cnt THEN 'MDS to DWH data validation failed for d_asset.computer_usage_c' ELSE 'SUCCESS' END as Message from
-(select count(1) as cnt
-FROM usf_mdsdb.alm_asset_final SRC 
- LEFT JOIN usf_mdwdb.d_asset TRGT 
- ON ( CONCAT('ASSET~',SRC.sys_id)=TRGT.row_id 
- AND SRC.sourceinstance=TRGT.source_id) 
-  WHERE 'N'<> TRGT.deployed_cy_c_flag)t;
+(select count(1) as cnt from
+usf_mdwdb.d_asset
+where 
+deployed_cy_c_flag <>
+(
+case
+                when 
+                                (YEAR(deployed_date_c) = (SELECT YEAR(MAX(lastupdated)) 
+                                                FROM usf_mdwdb.d_o_data_freshness))
+                then 'Y'
+                else 'N'
+END
+))a
--- Apps 5.0/USF/ITSA/FUNCTIONAL/d_request_item_in_progress_c_flag_mysql.sql
+++ Apps 5.0/USF/ITSA/FUNCTIONAL/d_request_item_in_progress_c_flag_mysql.sql
@@ -1,7 +1,15 @@
 SELECT CASE WHEN cnt THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
 CASE WHEN cnt THEN 'MDS to DWH data validation failed for d_asset.computer_usage_c' ELSE 'SUCCESS' END as Message from
 (select count(1) as cnt
-FROM usf_mdsdb.sc_req_item_final SRC 
-LEFT JOIN usf_mdwdb.d_request_item TRGT 
- ON SRC.sys_id=TRGT.row_id and TRGT.source_id = SRC.sourceinstance
-WHERE 'N'<> TRGT.in_progress_c_flag)t;
+FROM usf_mdwdb.d_request_item di
+  JOIN usf_mdwdb.f_request_item fi ON di.row_key = fi.request_item_key
+  JOIN usf_mdwdb.d_lov_map dlm ON fi.state_src_key = dlm.src_key
+  JOIN usf_mdwdb.d_master_item m ON fi.catalog_item_key = m.row_key
+  JOIN usf_mdwdb.d_calendar_date c ON fi.delevered_on_c_key = c.row_key
+  JOIN (
+			SELECT lastupdated, 
+				   source_id
+			  FROM usf_mdwdb.d_o_data_freshness
+			
+	) df ON di.source_id = df.source_id
+WHERE case when (dlm.dimension_class = 'STATE~SC_REQ_ITEM' AND dlm.dimension_wh_code = 'OPEN' AND m.item_name like '%refresh%' AND YEAR(df.lastupdated)<c.year_id+1) then 'Y' else'N' end<> di.in_progress_c_flag)t;
