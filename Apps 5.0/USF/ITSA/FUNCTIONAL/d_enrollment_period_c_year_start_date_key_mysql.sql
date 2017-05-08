71ab5c6c2844624113d1b62a74223ffb0d43f01e
Shubha <Shubha.rao@numerify.com>
2017-05-08 17:01:50 +0530

    Updated of ootb and cust queries of USF

--- Apps 5.0/USF/ITSA/FUNCTIONAL/d_enrollment_period_c_year_start_date_key_mysql.sql
+++ Apps 5.0/USF/ITSA/FUNCTIONAL/d_enrollment_period_c_year_start_date_key_mysql.sql
@@ -1,13 +1,10 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match' 
-ELSE 'Data Matched' END AS Message 
-FROM (
-select count(1) as cnt
+SELECT CASE WHEN cnt THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN cnt THEN 'MDS to DWH data validation failed for d_asset.computer_usage_c' ELSE 'SUCCESS' END as Message from
+(select count(1) as cnt
 from  usf_mdsdb.u_enrollment_terms_final s
 left  JOIN usf_mdwdb.d_enrollment_period_c t 
 on  t.ROW_ID=s.SYS_ID and s.sourceinstance=t.source_id  
 join usf_mdwdb.d_calendar_date l
-on COALESCE(DATE_FORMAT(s.u_start_date,'%Y%m%d'),'UNSPECIFIED')= l.row_id
-WHERE COALESCE(l.row_key,CASE WHEN s.u_start_date IS NULL THEN 0 else '-1' end)<>
- t.year_start_date_key
-) temp
+on COALESCE(CAST(CONCAT(LEFT(DATE_FORMAT(s.u_start_date,'%Y%m%d'),4),'0101') as UNSIGNED),'UNSPECIFIED')= l.row_id
+WHERE COALESCE(l.row_key,CASE WHEN s.u_start_date IS NULL THEN 0 else -1 end)<>
+ t.year_start_date_key 
---
+++ Apps 5.0/USF/ITSA/FUNCTIONAL/d_lov_map_row_id_Validation_mysql.sql
@@ -0,0 +1,9 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_item.configuration_item_key' ELSE 'SUCCESS' END as Message
+FROM (
+select row_key from usf_mdwdb.d_lov_map 
+where  (src_rowId<>concat(coalesce(dimension_class,''),'~',coalesce(dimension_type,''),'~',coalesce(dimension_subtype,''),'~',ucase(coalesce(dimension_code)))
+  or
+wh_rowid <> concat(coalesce(wh_dimension_class,''),'~',coalesce(wh_dimension_type,''),'~',coalesce(dimension_subtype,''),'~',ucase(coalesce(dimension_wh_code,''))) )
+and wh_dimension_class<>'UNSPECIFIED'
+and dimension_class not in ('ASSET', 'PROJECT','PORTFOLIO','EXPENSE_ITEM'))a
