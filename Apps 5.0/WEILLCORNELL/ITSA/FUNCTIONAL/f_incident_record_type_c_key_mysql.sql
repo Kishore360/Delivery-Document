e80487fb25f0cac05f9c3907f8020d77f6af234c
vijay rao <vijay.rao@numerify.com>
2017-05-12 16:20:00 +0530

    no message

--- Apps 5.0/WEILLCORNELL/ITSA/FUNCTIONAL/f_incident_record_type_c_key_mysql.sql
+++ Apps 5.0/WEILLCORNELL/ITSA/FUNCTIONAL/f_incident_record_type_c_key_mysql.sql
@@ -1,10 +1,18 @@
- SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM ( select count(1) as cnt from weillcornell_mdsdb.incident_final a
-inner join weillcornell_mdwdb.f_incident b on a.sys_id =b.row_id and a.sourceinstance=b.source_id and b.row_key <100
-left outer join weillcornell_mdwdb.d_lov c on a.u_record_type=c.dimension_code and dimension_class='RECORD_TYPE~INCIDENT'
- where case when  a.u_record_type is null then '0' 
- else case when c.row_key is null then '-1' else c.row_key end end <>b.record_type_c_key
-) c;
-
+ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.urgency_src_key' ELSE 'SUCCESS' END as Message
+ FROM weillcornell_mdsdb.incident_final SRC 
+ LEFT JOIN weillcornell_mdwdb.f_incident TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+LEFT JOIN weillcornell_mdwdb.d_lov LKP 
+ ON ( concat('RECORD_TYPE','~','INCIDENT','~','~','~',upper(u_record_type))= LKP.src_rowid 
+AND SRC.sourceinstance= LKP.source_id )
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_record_type IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.record_type_c_key,'') 
+and    SRC.sys_id not in (select B.sys_id
+from weillcornell_mdsdb.incident_final B
+join weillcornell_mdsdb.sys_user_final C
+on C.sys_id = B.caller_id
+join weillcornell_mdsdb.cmdb_ci_final D
+on D.sys_id = B.u_business_service
+where UPPER(C.user_name) = 'GUEST'
+and UPPER(D.name) = 'ONLINE DIRECTORY' ) 
