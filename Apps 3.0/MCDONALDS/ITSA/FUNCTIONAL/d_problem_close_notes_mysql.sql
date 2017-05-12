be0b1392f8fc97f9ce414d290be25e7b4e06df19
Shubha <Shubha.rao@numerify.com>
2017-05-12 10:49:21 +0530

    Removing the spaces in the  file names of ootb query

--- Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/d_problem_close_notes_mysql.sql
+++ Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/d_problem_close_notes_mysql.sql
@@ -3,5 +3,5 @@ CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_problem.close_
 FROM mcdonalds_mdwdb.d_problem trgt
 RIGHT JOIN mcdonalds_mdsdb.problem_final src
 on src.sys_id = trgt.row_id and src.sourceinstance = trgt.source_id
-WHERE close_notes <>u_close_notes
+WHERE trgt.close_notes <>u_close_notes
 ;
