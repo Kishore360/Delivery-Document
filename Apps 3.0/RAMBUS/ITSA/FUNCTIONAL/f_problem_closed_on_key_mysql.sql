9c87ac85bd504061c06cba184b8b53b2bdf9213a
Kishore Mantha <kishore.mantha@numerify.com>
2017-05-16 14:35:16 +0530

    updated the query 3.0 closed_on_key

--- Apps 3.0/RAMBUS/ITSA/FUNCTIONAL/f_problem_closed_on_key_mysql.sql
+++ Apps 3.0/RAMBUS/ITSA/FUNCTIONAL/f_problem_closed_on_key_mysql.sql
@@ -9,7 +9,7 @@ CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
   JOIN rambus_mdwdb.d_lov_map dlm 
 ON TRGT.state_src_key = dlm.src_key
 LEFT JOIN rambus_mdwdb.d_calendar_date LKP 
-on (LKP.row_id = date_format(convert_tz(coalesce(SRC.sys_updated_on),"GMT","America/Los_Angeles"),'%Y%m%d')  and LKP.source_id=0
+on (LKP.row_id = date_format(convert_tz(coalesce(SRC.closed_at,SRC.sys_updated_on),"GMT","America/Los_Angeles"),'%Y%m%d')  and LKP.source_id=0
 )
 WHERE case when dlm.dimension_wh_code = 'CLOSED' then (LKP.row_key) else null end <> (TRGT.closed_on_key))temp;
 
