b72f14e5a19d3f735f236a8385060ad8d8f5d598
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-06-12 12:29:31 +0530

    nbcu changes

--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_request_task_parent_task_c_key_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_request_task_parent_task_c_key_mysql.sql
@@ -2,7 +2,7 @@
 
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request.assigned_to_key' ELSE 'SUCCESS' END as Message
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_request_task.parent_task_c_key' ELSE 'SUCCESS' END as Message
  FROM nbcu_mdwdb.f_request_task stg
  JOIN nbcu_mdsdb.sc_task_final src ON stg.row_id = src.sys_id AND stg.source_id = src.sourceinstance
 join nbcu_mdsdb.sc_req_item_final ri on ri.sys_id=src.request_item  and ri.sourceinstance=src.sourceinstance
