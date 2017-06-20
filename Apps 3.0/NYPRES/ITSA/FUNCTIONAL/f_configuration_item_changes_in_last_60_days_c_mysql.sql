d2f20299872355711b0c8c92605ddee45a542c70
prabhuram <prabhuram.sridharan@numerify.com>
2017-06-20 08:31:21 +0530

    Committing corrected queries of NYP

--- Apps 3.0/NYPRES/ITSA/FUNCTIONAL/f_configuration_item_changes_in_last_60_days_c_mysql.sql
+++ Apps 3.0/NYPRES/ITSA/FUNCTIONAL/f_configuration_item_changes_in_last_60_days_c_mysql.sql
@@ -3,18 +3,18 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN count(1) >0 THEN 'Failure' ELSE 'Data Matched' END as Message 
 from 
-(select cmdb_ci,count(t.number)cnt
-from (select  number,opened_at,cmdb_ci,sourceinstance,sys_id from nypres_mdsdb.change_request_final 
-join  nypres_mdwdb.d_calendar_date d on d.row_id=date_format(convert_tz(opened_at,'GMT','America/New_york'),'%Y%m%d')
-where  d.lagging_count_of_days between 0 and 59  ) t  
-join  nypres_mdsdb.task_ci_final c on c.task=t.sys_id and t.cmdb_ci=c.ci_item and c.sourceinstance=t.sourceinstance
-group by t.cmdb_ci)src
-join 
-(select row_id,changes_in_last_60_days_c  from nypres_mdwdb.f_configuration_item_c) trgt
-on src.cmdb_ci=trgt.row_id 
-where src.cnt <>trgt.changes_in_last_60_days_c
-
-
+ nypres_mdwdb.f_configuration_item_c as trgt
+inner join(
+select tc.ci_item,tc.sourceinstance,count(tc.task) as source_count 
+from nypres_mdsdb.task_ci_final as tc
+inner join ( select cmdb_ci,sys_id,sourceinstance from nypres_mdsdb.change_request_final 
+join nypres_mdwdb.d_calendar_date d on d.row_id=date_format(convert_tz(opened_at,'GMT','America/New_york'),'%Y%m%d')
+and d.lagging_count_of_days between 0 and 59) as inc
+on tc.task=inc.sys_id and inc.sourceinstance=tc.sourceinstance
+group by tc.ci_item,tc.sourceinstance
+) as src
+on trgt.row_id = src.ci_item and trgt.source_id = src.sourceinstance
+where trgt.changes_in_last_60_days_c <> src.source_count;
 
 
 
