69dc86056cc25c62a4f3ad7b5426e5bc8da2a6fe
Kishore Mantha <kishore.mantha@numerify.com>
2017-06-22 14:13:02 +0530

    Updated the Mcdonald_MACM Application _3.X QA Queries as part of DELV-6332

---
+++ Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/f_macm_c_business_approver_key_MYSQL.sql
@@ -0,0 +1,13 @@
+SELECT
+CASE WHEN CNT  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN CNT THEN 'MDS to DWH data validation failed for f_macm_c_business_approver_key' ELSE 'SUCCESS' END as Message
+FROM
+(
+SELECT count(1) as CNT
+FROM mcdonalds_mdsdb.x_scafe_mcdcr_chan_mcdcr_change_request_final SRC
+JOIN mcdonalds_mdwdb.f_macm_c TRGT 
+ON (SRC.SYS_ID=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id)
+LEFT JOIN mcdonalds_mdwdb.d_internal_contact LKP
+ON(concat('INTERNAL_CONTACT~',SRC.u_business_approver )= LKP.row_id AND SRC.sourceinstance= LKP.source_id )
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_business_approver IS NULL THEN 0 else -1 end)<>TRGT.business_approver_key
+)a;
---
+++ Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/f_macm_c_incident_key_MYSQL.sql
@@ -0,0 +1,15 @@
+SELECT
+CASE WHEN CNT  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN CNT THEN 'MDS to DWH data validation failed for f_macm_c_incident_key' ELSE 'SUCCESS' END as Message
+FROM
+(
+SELECT count(1) as CNT 
+FROM mcdonalds_mdsdb.x_scafe_mcdcr_chan_mcdcr_change_request_final SRC  
+LEFT JOIN mcdonalds_mdwdb.f_macm_c TRGT 
+ON (SRC.SYS_ID=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id)
+LEFT JOIN mcdonalds_mdwdb.d_incident LKP 
+ON COALESCE(SRC.u_incident, 'UNSPECIFIED')=LKP.row_id 
+WHERE COALESCE(LKP.row_key,-1)<>TRGT.incident_key
+)a;
+
+
