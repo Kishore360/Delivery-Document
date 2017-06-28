ad1015bf56bfb6c861475575fa0eed606a2904a9
vijay rao <vijay.rao@numerify.com>
2017-06-28 09:30:20 +0530

    Rambus APPS 5.0 CUST query fixes

--- Apps 5.0/RAMBUS/ITSA/FUNCTIONAL/d_change_request_issue_type_c_mysql.sql
+++ Apps 5.0/RAMBUS/ITSA/FUNCTIONAL/d_change_request_issue_type_c_mysql.sql
@@ -7,4 +7,4 @@ from rambus_mdwdb.d_change_request a
 left outer join rambus_mdsdb.change_request_final b 
 on a.row_id =b.sys_id 
 and a.source_id=b.sourceinstance
-where coalesce(a.issue_type_c,'') <>coalesce(b.u_issue_type,'') and a.soft_deleted_flag='N' ) z;
+where a.issue_type_c <>b.u_issue_type and a.soft_deleted_flag='N' ) z;
---
+++ Apps 5.0/RAMBUS/ITSA/FUNCTIONAL/d_request_item_new_hire_sla_c_flag_mysql.sql
@@ -0,0 +1,10 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_request_item.new_hire_sla_c_flag' ELSE 'SUCCESS' END as Message from
+ (select count(1) cnt   
+ FROM rambus_mdwdb.d_request_item d 
+INNER JOIN ( SELECT f.request_item_key,f.source_id,f.outcome_flag 
+	FROM  rambus_mdwdb.f_task_sla f INNER JOIN ( SELECT request_item_key,source_id,MAX(end_on_key) AS end_on_key FROM  rambus_mdwdb.f_task_sla WHERE task_type = 'SC_REQ_ITEM' GROUP BY 1,2 ) t 
+ON t.request_item_key=f.request_item_key AND t.source_id=f.source_id AND t.end_on_key=f.end_on_key
+ WHERE sla_key IN (SELECT row_key FROM rambus_mdwdb.d_task_sla WHERE sla_name like '%Onboarding%New Hire SLA%') AND task_type ='SC_REQ_ITEM' ) t 
+ ON d.row_key=t.request_item_key AND d.source_id=t.source_id 
+WHERE d.new_hire_sla_c_flag <> t.outcome_flag)a;
