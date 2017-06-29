fda86e27a86feb3fb38e8ce9e841a0ed92f8cba4
DESKTOP-PLTURRV\Shubha <shubha.rao@numerify.com>
2017-06-29 11:37:24 +0530

    Updating qa query

--- Apps 5.0/SVB/ITSA/FUNCTIONAL/f_problem_closed_by_key_mysql.sql
+++ Apps 5.0/SVB/ITSA/FUNCTIONAL/f_problem_closed_by_key_mysql.sql
@@ -6,12 +6,10 @@ CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  LEFT JOIN svb_mdwdb.f_problem TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
-   left join svb_mdwdb.d_lov_map p
-on TRGT.state_src_key=p.src_key and dimension_wh_code='CLOSED' AND p.dimension_class = 'STATE~PROBLEM'
+left join svb_mdwdb.d_lov_map p
+on TRGT.state_src_key=p.src_key 
+AND p.dimension_class = 'STATE~PROBLEM'
 LEFT JOIN svb_mdwdb.d_internal_contact LKP 
- ON ( concat('INTERNAL_CONTACT~',closed_by)= LKP.row_id 
-AND SRC.sourceinstance= LKP.source_id
-AND TRGT.pivot_date
- BETWEEN LKP.effective_from AND LKP.effective_to)
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.closed_by IS NULL THEN 0 else -1 end)<> (TRGT.closed_by_key))temp;
-
+ ON (LKP.user_name = TRGT.changed_by AND LKP.source_id = TRGT.source_id)
+WHERE  dimension_wh_code='CLOSED' and (case when coalesce(TRGT.closed_by_key,0)=0 then coalesce(LKP.row_key,-1) 
+ else TRGT.closed_by_key end) <>(TRGT.closed_by_key) )a
