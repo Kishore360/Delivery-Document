d04a1a7671b80f22874cb496eceaaba445d5c0e3
Kishore Mantha <kishore.mantha@numerify.com>
2017-07-13 14:24:58 +0530

    Added the cube query as part of the DELV-6842 for mcdonalds.

--- Apps 5.0/WOW/ITSA/FUNCTIONAL/f_incident_outage_tech_c_incident_key_mysql.sql
+++ Apps 5.0/WOW/ITSA/FUNCTIONAL/f_incident_outage_tech_c_incident_key_mysql.sql
@@ -1,17 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
  FROM wow_mdsdb.u_incident_outage_final SRC 
