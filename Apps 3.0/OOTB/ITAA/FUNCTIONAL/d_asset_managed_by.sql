200bf08e00dfd6c055543b70acc0e11a1d0d09e4
vijay rao <vijay.rao@numerify.com>
2017-07-19 12:53:17 +0530

    no message

--- Apps 3.0/OOTB/ITAA/FUNCTIONAL/d_asset_managed_by.sql
+++ Apps 3.0/OOTB/ITAA/FUNCTIONAL/d_asset_managed_by.sql
@@ -1,8 +1,11 @@
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_asset.managed_by' ELSE 'SUCCESS' END as Message
- FROM #MDS_TABLE_SCHEMA.alm_asset SRC 
- LEFT JOIN #DWH_TABLE_SCHEMA.d_asset TRGT 
- ON (SRC.sys_id=TRGT.row_id 
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset.managed_by_c_key' ELSE 'SUCCESS' END as Message
+ FROM whirlpool_mdsdb.alm_asset_final SRC 
+ LEFT JOIN whirlpool_mdwdb.d_asset TRGT 
+ ON (SRC.sys_id=right(TRGT.row_id,32)
  AND SRC.sourceinstance=TRGT.source_id )
- WHERE COALESCE( CONVERT(SRC.managed_by using utf8),'')<> COALESCE(CONVERT(TRGT.managed_by using utf8),'')
+ LEFT JOIN whirlpool_mdwdb.d_internal_contact LKP 
+ ON  COALESCE(CONCAT('INTERNAL_CONTACT~',SRC.managed_by),'UNSPECIFIED') = LKP.row_id 
+AND SRC.sourceinstance = LKP.source_id 
+where COALESCE(LKP.row_key,CASE WHEN SRC.managed_by IS NULL THEN 0 else '-1' end) <> TRGT.managed_by_c_key;
