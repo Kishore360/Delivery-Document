87dee1f74652760eb8b3eb36cdb397cbf8f3c1bc
vijay rao <vijay.rao@numerify.com>
2017-07-27 12:35:53 +0530

    no message

--- Apps 5.0/RAMBUS/ITSA/FUNCTIONAL/f_task_c_state_src_key_mysql.sql
+++ Apps 5.0/RAMBUS/ITSA/FUNCTIONAL/f_task_c_state_src_key_mysql.sql
@@ -5,18 +5,21 @@ FROM (
 select count(1) as cnt
 from 
 (select TRGT.state_src_key ,CASE 
-		WHEN i.sys_class_name ='INCIDENT' THEN COALESCE( CONCAT('STATE','~','INCIDENT','~','~','~',UPPER(inc.state)),'UNSPECIFIED')
-		WHEN i.sys_class_name ='PROBLEM' THEN COALESCE( CONCAT('STATE','~','PROBLEM','~','~','~',UPPER(pr.state)),'UNSPECIFIED')
-		WHEN i.sys_class_name ='SC_REQ_ITEM' THEN COALESCE( CONCAT('STATE','~','SC_REQ_ITEM','~','~','~',UPPER(sri.state)),'UNSPECIFIED')
-		WHEN i.sys_class_name ='SC_TASK' THEN COALESCE( CONCAT('STATE','~','SC_TASK','~','~','~',UPPER(sct.state)),'UNSPECIFIED')
-		WHEN i.sys_class_name ='SC_REQUEST' THEN COALESCE( CONCAT('REQUEST_STATE','~','SC_REQUEST','~','~','~',UPPER(sc.request_state)),'UNSPECIFIED')
-		WHEN i.sys_class_name ='CHANGE_REQUEST' THEN COALESCE( CONCAT('STATE','~','CHANGE_REQUEST','~','~','~',UPPER(cr.state)),'UNSPECIFIED')
-		WHEN i.sys_class_name ='U_AD_HOC_REQUEST' THEN COALESCE( CONCAT('STATE','~','ADHOC_REQ','~','~','~',UPPER(i.state)),'UNSPECIFIED')
-		WHEN i.sys_class_name ='U_IT_TASK' THEN COALESCE( CONCAT('STATE','~','TASK','~','~','~',UPPER(i.state)),'UNSPECIFIED')
-		WHEN i.sys_class_name ='CHANGE_TASK' THEN COALESCE( CONCAT('STATE','~','TASK','~','~','~',UPPER(i.state)),'UNSPECIFIED')
-		WHEN i.sys_class_name ='PROBLEM_TASK' THEN COALESCE( CONCAT('STATE','~','PROBLEM_TASK','~','~','~',UPPER(i.state)),'UNSPECIFIED')
-		END
-	AS STATE_SRC_ID 
+WHEN i.sys_class_name ='sc_request' then sc.request_state 
+else i.state end as STATE_SRC_CODE ,
+CASE 
+WHEN i.sys_class_name ='INCIDENT' THEN COALESCE( CONCAT('STATE','','INCIDENT','','','',UPPER(inc.state)),'UNSPECIFIED')
+WHEN i.sys_class_name ='PROBLEM' THEN COALESCE( CONCAT('STATE','','PROBLEM','','','',UPPER(pr.state)),'UNSPECIFIED')
+WHEN i.sys_class_name ='SC_REQ_ITEM' THEN COALESCE( CONCAT('STATE','','SC_REQ_ITEM','','','',UPPER(sri.state)),'UNSPECIFIED')
+WHEN i.sys_class_name ='SC_TASK' THEN COALESCE( CONCAT('STATE','','SC_TASK','','','',UPPER(sct.state)),'UNSPECIFIED')
+WHEN i.sys_class_name ='SC_REQUEST' THEN COALESCE( CONCAT('REQUEST_STATE','','SC_REQUEST','','','',UPPER(sc.request_state)),'UNSPECIFIED')
+WHEN i.sys_class_name ='CHANGE_REQUEST' THEN COALESCE( CONCAT('STATE','','CHANGE_REQUEST','','','',UPPER(cr.state)),'UNSPECIFIED')
+WHEN i.sys_class_name ='U_AD_HOC_REQUEST' THEN COALESCE( CONCAT('STATE','','ADHOC_REQ','','','',UPPER(i.state)),'UNSPECIFIED')
+WHEN i.sys_class_name ='U_IT_TASK' THEN COALESCE( CONCAT('STATE','','TASK','','','',UPPER(i.state)),'UNSPECIFIED')
+WHEN i.sys_class_name ='CHANGE_TASK' THEN COALESCE( CONCAT('STATE','','TASK','','','',UPPER(i.state)),'UNSPECIFIED')
+WHEN i.sys_class_name ='PROBLEM_TASK' THEN COALESCE( CONCAT('STATE','','PROBLEM_TASK','','','',UPPER(i.state)),'UNSPECIFIED')
+END
+AS STATE_SRC_ID
 	from rambus_mdsdb.task_final i 
 	left join rambus_mdsdb.sc_request_final sc on sc.sys_id=i.sys_id and sc.sourceinstance=i.sourceinstance 
 	left join rambus_mdsdb.incident_final inc on inc.sys_id=i.sys_id and inc.sourceinstance=i.sourceinstance 
