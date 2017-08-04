48ddbfe3e6e72d0bf79725d268804e9249b14a05
vijay rao <vijay.rao@numerify.com>
2017-08-04 17:46:44 +0530

    RAMBUS cust queries updated

--- Apps 5.0/RAMBUS/ITSA/FUNCTIONAL/d_change_request_sox_compliance_c_flag_mysql.sql
+++ Apps 5.0/RAMBUS/ITSA/FUNCTIONAL/d_change_request_sox_compliance_c_flag_mysql.sql
@@ -3,7 +3,7 @@ CASE WHEN cnt > 0 THEN 'Data did not Match.'
 ELSE 'Data Matched' END AS Message 
 from(select count(1) cnt
  from rambus_mdwdb.d_change_request a 
-left outer join rambus_mdsdb.change_request_final b 
+join rambus_mdsdb.change_request_final b 
 on a.row_id =b.sys_id 
 and a.source_id=b.sourceinstance
-where coalesce(a.sox_compliance_c_flag,'') <>CASE WHEN b.u_sox_compliance='1' THEN 'Y' ELSE 'N' END  ) z;
+where coalesce(a.sox_compliance_c_flag,'') <>CASE WHEN b.u_sox_compliance=1 THEN 'Y' ELSE 'N' END  ) z;
--- Apps 5.0/RAMBUS/ITSA/FUNCTIONAL/d_incident_dormant_flag_mysql.sql
+++ Apps 5.0/RAMBUS/ITSA/FUNCTIONAL/d_incident_dormant_flag_mysql.sql
@@ -5,9 +5,12 @@ CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for d_incident.dormancy
 FROM (SELECT count(1) as CNT
 FROM rambus_mdwdb.d_incident di
 JOIN rambus_mdwdb.f_incident fi ON di.row_key = fi.incident_key
-JOIN rambus_mdwdb.d_lov_map dlm ON fi.state_src_key = dlm.src_key
-where dlm.dimension_class in ('STATE~INCIDENT','STATE~ADHOC_REQ')
-AND dlm.dimension_wh_code = 'OPEN'
+JOIN rambus_mdwdb.d_lov_map d_lov_map ON fi.state_src_key = d_lov_map.src_key
+where d_lov_map.dimension_class = 'STATE~INCIDENT' 
+AND d_lov_map.dimension_wh_code = 'OPEN'
+AND fi.soft_deleted_flag='Y'
  AND (CASE WHEN timestampdiff(second,di.changed_on, (SELECT MAX(lastupdated) AS lastupdated
 FROM rambus_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%'))>86400 THEN 'Y' ELSE 'N' END) <> di.dormant_flag)temp;
+
+
 
