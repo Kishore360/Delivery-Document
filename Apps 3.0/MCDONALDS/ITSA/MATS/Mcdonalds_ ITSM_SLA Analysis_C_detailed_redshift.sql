27752664eda3c85dfe3670250d676d4dcccc7e91
Kishore Mantha <kishore.mantha@numerify.com>
2017-08-11 17:25:21 +0530

    Updated the f_sla_analysis_c cube query for Mcdonalds_3.X

--- Apps 3.0/MCDONALDS/ITSA/MATS/Mcdonalds_ ITSM_SLA Analysis_C_detailed_redshift.sql
+++ Apps 3.0/MCDONALDS/ITSA/MATS/Mcdonalds_ ITSM_SLA Analysis_C_detailed_redshift.sql
@@ -12,5 +12,15 @@ UNION
 SELECT 'd_lov_sla_priority_c' as Table_Name, count(a.row_key) as Row_count
 FROM ldb.f_sla_analysis_c a 
 JOIN ldb.d_lov_sla_priority_c b ON  a.priority_src_key=b.row_key
+UNION 
+SELECT 'd_sla_analysis_contract_sla_c' as Table_Name, count(a.row_key) as Row_count
+FROM ldb.f_sla_analysis_c a 
+JOIN ldb.d_sla_analysis_contract_sla_c b ON a.sla_key=b.row_key
+UNION 
+SELECT 'd_sla_analysis_incident_c' as Table_Name, count(a.row_key) as Row_count
+FROM ldb.f_sla_analysis_c a 
+JOIN ldb.d_sla_analysis_incident_c b ON a.incident_key=b.row_key
+
+
 
 
