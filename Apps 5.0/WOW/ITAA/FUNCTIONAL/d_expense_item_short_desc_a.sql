0c1d024b099446918b14c9ca3a8f84bd52b68a5c
vijay rao <vijay.rao@numerify.com>
2017-08-18 14:57:19 +0530

    Final ITAA queries for wow

---
+++ Apps 5.0/WOW/ITAA/FUNCTIONAL/d_expense_item_short_desc_a.sql
@@ -0,0 +1,8 @@
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_expense_item.short_desc' ELSE 'SUCCESS' END as Message
+ FROM wow_mdsdb.fm_expense_line_final SRC 
+ LEFT JOIN wow_mdwdb.d_expense_item TRGT 
+ ON (SRC.sys_id=TRGT.row_id 
+ AND SRC.sourceinstance=TRGT.source_id )
+ WHERE COALESCE( CONVERT(SRC.short_description using utf8),'')<> COALESCE(CONVERT(TRGT.short_desc using utf8),'')
---
+++ Apps 5.0/WOW/ITAA/FUNCTIONAL/f_expense_item_expense_status_key_p.sql
@@ -0,0 +1,15 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_expense_item.expense_status_key' ELSE 'SUCCESS' END as Message
+FROM wow_mdsdb.fm_expense_line_final SRC
+JOIN wow_mdsdb.pm_project_final SRC_P ON (SRC_P.sys_id = SRC.task) 
+JOIN wow_mdsdb.fx_currency_instance_final FI on FI.id=SRC_P.sys_id and FI.field='amount'
+ LEFT JOIN wow_mdwdb.f_expense_item TRGT 
+ ON (SRC.sys_id=TRGT.row_id 
+ AND SRC.sourceinstance=TRGT.source_id )
+ LEFT JOIN wow_mdwdb.d_lov LKP 
+ ON ( UCASE(CONCAT('EXPENSE_ITEM~STATUS~~',SRC.state) ) =LKP.row_id
+AND SRC.sourceinstance =LKP.source_id
+AND LKP.dimension_class ='EXPENSE_ITEM'
+AND LKP.dimension_type = 'STATUS')
+WHERE SRC.asset Is Null And SRC.base_expense Is Null
+AND COALESCE(LKP.row_key,CASE WHEN SRC.state IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.expense_status_key,'')
