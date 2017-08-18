f4ad78b30411a510e4b317d595d7a634dbe6ab62
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-08-18 11:07:00 +0530

    updation is DELV-7353

--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_request_item_dispatch_reason_src_c_key_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_request_item_dispatch_reason_src_c_key_mysql.sql
@@ -7,7 +7,7 @@ from   nbcu_mdwdb.d_request_item a11
 left join  nbcu_mdsdb.sc_req_item_final a12
 on a11.row_id=a12.sys_id and a11.source_id=a12.sourceinstance
 join nbcu_mdwdb.d_lov a13
-on COALESCE(CONCAT('U_DISPATCH_REASON~INCIDENT~~~',a12.u_dispatch_reason_req),'UNSPECIFIED')=a13.row_id
+on COALESCE(CONCAT('U_DISPATCH_REASON~SC_REQ_ITEM~~~',a12.u_dispatch_reason_req),'UNSPECIFIED')=a13.row_id
 where COALESCE(a13.row_key,CASE WHEN a12.u_dispatch_reason_req IS NULL THEN 0 else -1 end) <>a11.dispatch_reason_src_c_key)a
 
 
