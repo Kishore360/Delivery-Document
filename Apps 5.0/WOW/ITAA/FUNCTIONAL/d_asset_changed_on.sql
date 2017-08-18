49824653ba0cfff216a517c1d826158a5a67d62e
vijay rao <vijay.rao@numerify.com>
2017-08-18 11:44:27 +0530

    ITAA changes to WOW

--- Apps 5.0/WOW/ITAA/FUNCTIONAL/d_asset_changed_on.sql
+++ Apps 5.0/WOW/ITAA/FUNCTIONAL/d_asset_changed_on.sql
@@ -4,4 +4,4 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  LEFT JOIN wow_mdwdb.d_asset TRGT 
  ON (concat('ASSET~',SRC.sys_id)=TRGT.row_id 
  AND SRC.sourceinstance=TRGT.source_id )
- WHERE CONVERT_TZ(SRC.sys_updated_on,'GMT','MST')<> TRGT.changed_on;
+ WHERE CONVERT_TZ(SRC.sys_updated_on,'GMT','US/Central')<> TRGT.changed_on;
--- Apps 5.0/WOW/ITAA/FUNCTIONAL/d_asset_class_created_on.sql
+++ Apps 5.0/WOW/ITAA/FUNCTIONAL/d_asset_class_created_on.sql
@@ -4,4 +4,4 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
   FROM wow_mdsdb.sys_db_object_final  SRC 
  LEFT JOIN wow_mdwdb.d_asset_class TRGT 
  ON SRC.sys_id=TRGT.row_id  AND SRC.sourceinstance=TRGT.source_id
- WHERE CONVERT_TZ(SRC.sys_created_on,'GMT','MST')<> TRGT.created_on;
+ WHERE CONVERT_TZ(SRC.sys_created_on,'GMT','US/Central')<> TRGT.created_on;
