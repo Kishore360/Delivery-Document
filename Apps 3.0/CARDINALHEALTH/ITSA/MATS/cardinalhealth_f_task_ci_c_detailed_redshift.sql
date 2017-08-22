4802ba35d88c2126057dd4890209363e9a210094
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-08-22 18:14:34 +0530

    DELV-7593

--- Apps 3.0/CARDINALHEALTH/ITSA/MATS/cardinalhealth_f_task_ci_c_detailed_redshift.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/MATS/cardinalhealth_f_task_ci_c_detailed_redshift.sql
@@ -4,17 +4,17 @@ union
 select 'ldb.d_calendar_date a12 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_calendar_date a12 
-on (a11.created_on_c_key = a12.row_key) 
+on (a11.change_created_on_c_key = a12.row_key) 
 union
 select 'ldb.d_calendar_greg_fiscal_c a13 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_calendar_greg_fiscal_c a13 
-on (a11.created_on_c_key = a13.greogrian_calendar_key) 
+on (a11.change_created_on_c_key = a13.greogrian_calendar_key) 
 union
 select 'ldb.d_calendar_date_fiscal a14 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_calendar_greg_fiscal_c a13 
-on (a11.created_on_c_key = a13.greogrian_calendar_key) 
+on (a11.change_created_on_c_key = a13.greogrian_calendar_key) 
 join ldb.d_calendar_date_fiscal a14 
 on (a13.fiscal_key = a14.row_key) 
 union
@@ -26,27 +26,27 @@ union
 select 'ldb.d_configuration_item a16 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_configuration_item a16 
-on (a11.configuration_item_c_key = a16.row_key) 
+on (a11.configuration_item_key = a16.row_key) 
 union
 select 'ldb.d_change_request a17 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_change_request a17 
-on (a11.change_request_c_key = a17.row_key) 
+on (a11.chg_change_request_key = a17.row_key) 
 union
-select 'ldb.d_task a18 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_configuration_item_change_rel_cmdb_ci_configuration_item_c a18 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
-join ldb.d_task a18 
-on (a11.task_c_key = a18.row_key) 
+join ldb.d_configuration_item_change_rel_cmdb_ci_configuration_item_c a18 
+on (a11.configuration_item_c_key = a18.row_key) 
 union
-select 'ldb.d_internal_organization_group a19 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_task a19 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
-join ldb.d_internal_organization_group a19 
-on (a11.assignment_group_key = a19.row_key) 
+join ldb.d_task a19 
+on (a11.task_c_key = a19.row_key) 
 union
-select 'ldb.d_configuration_item_change_rel_cmdb_ci_configuration_item_c a110 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_organization_group a110 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
-join ldb.d_configuration_item_change_rel_cmdb_ci_configuration_item_c a110 
-on (a11.configuration_item_c_key = a110.row_key) 
+join ldb.d_internal_organization_group a110 
+on (a11.assignment_group_key = a110.row_key) 
 union
 select 'ldb.d_change_request_state a111 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
@@ -56,7 +56,7 @@ union
 select 'ldb.d_calendar_fiscal_year a112 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_calendar_greg_fiscal_c a13 
-on (a11.created_on_c_key = a13.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a14 
+on (a11.change_created_on_c_key = a13.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a14 
 on (a13.fiscal_key = a14.row_key) 
 join ldb.d_calendar_fiscal_year a112 
 on (a14.year_start_date_key = a112.row_key) 
@@ -69,7 +69,7 @@ union
 select 'ldb.d_calendar_month a114 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_calendar_date a12 
-on (a11.created_on_c_key = a12.row_key) 
+on (a11.change_created_on_c_key = a12.row_key) 
 join ldb.d_calendar_month a114 
 on (a12.month_start_date_key = a114.row_key) 
 union
@@ -93,34 +93,69 @@ union
 select 'ldb.d_internal_contact_configuration_item_cat_representative_c a118 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_configuration_item a16 
-on (a11.configuration_item_c_key = a16.row_key) 
+on (a11.configuration_item_key = a16.row_key) 
 join ldb.d_internal_contact_configuration_item_cat_representative_c a118 
 on (a16.cat_representative_c_key = a118.row_key) 
 union
 select 'ldb.d_internal_contact_ci_director_c a119 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_configuration_item a16 
-on (a11.configuration_item_c_key = a16.row_key) 
+on (a11.configuration_item_key = a16.row_key) 
 join ldb.d_internal_contact_ci_director_c a119 
 on (a16.director_c_key = a119.row_key) 
 union
 select 'ldb.d_internal_contact_ci_manager_c a120 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_configuration_item a16 
-on (a11.configuration_item_c_key = a16.row_key) 
+on (a11.configuration_item_key = a16.row_key) 
 join ldb.d_internal_contact_ci_manager_c a120 
 on (a16.manager_c_key = a120.row_key) 
 union
 select 'ldb.d_internal_contact_ci_vp_c a121 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_configuration_item a16 
-on (a11.configuration_item_c_key = a16.row_key) 
+on (a11.configuration_item_key = a16.row_key) 
 join ldb.d_internal_contact_ci_vp_c a121 
 on (a16.vp_c_key = a121.row_key) 
 union
-select 'ldb.d_internal_contact_level_5_c a122 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_affected_cat_representative_c a122 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_task_ci_c a11 
+join ldb.d_configuration_item_change_rel_cmdb_ci_configuration_item_c a18 
+on (a11.configuration_item_c_key = a18.row_key) 
+join ldb.d_internal_contact_affected_cat_representative_c a122 
+on (a18.cat_representative_c_key = a122.row_key) 
+union
+select 'ldb.d_internal_contact_affected_director_c a123 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_task_ci_c a11 
+join ldb.d_configuration_item_change_rel_cmdb_ci_configuration_item_c a18 
+on (a11.configuration_item_c_key = a18.row_key) 
+join ldb.d_internal_contact_affected_director_c a123 
+on (a18.director_c_key = a123.row_key) 
+union
+select 'ldb.d_internal_contact_affected_manager_c a124 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_task_ci_c a11 
+join ldb.d_configuration_item_change_rel_cmdb_ci_configuration_item_c a18 
+on (a11.configuration_item_c_key = a18.row_key) 
+join ldb.d_internal_contact_affected_manager_c a124 
+on (a18.manager_c_key = a124.row_key) 
+union
+select 'ldb.d_internal_contact_affected_vp_c a125 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_task_ci_c a11 
+join ldb.d_configuration_item_change_rel_cmdb_ci_configuration_item_c a18 
+on (a11.configuration_item_c_key = a18.row_key) 
+join ldb.d_internal_contact_affected_vp_c a125 
+on (a18.vp_c_key = a125.row_key) 
+union
+select 'ldb.d_internal_contact_level_5_c a126 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_task_ci_c a11 
 join ldb.d_configuration_item a16 
-on (a11.configuration_item_c_key = a16.row_key) 
-join ldb.d_internal_contact_level_5_c a122 
-on (a16.cah_level_5_c_key = a122.row_key) 
+on (a11.configuration_item_key = a16.row_key) 
+join ldb.d_internal_contact_level_5_c a126 
+on (a16.cah_level_5_c_key = a126.row_key) 
+union
+select 'ldb.d_schedule_configuration_item_maintenance_schedule_c a127 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_task_ci_c a11 
+join ldb.d_configuration_item a16 
+on (a11.configuration_item_key = a16.row_key) 
+join ldb.d_schedule_configuration_item_maintenance_schedule_c a127 
+on (a16.maintenance_schedule_c_key = a127.row_key) 
