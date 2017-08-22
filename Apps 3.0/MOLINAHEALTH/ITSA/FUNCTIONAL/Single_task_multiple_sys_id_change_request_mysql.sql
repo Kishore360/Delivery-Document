70c47bc8d88a1a1bcab830ff56f95e8688530175
prabhuram <prabhuram.sridharan@numerify.com>
2017-08-22 21:36:24 +0530

    New Queries added

---
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/Single_task_multiple_sys_id_change_request_mysql.sql
@@ -0,0 +1,10 @@
+-- The Query is verify if there are multiple Sys_id per incident This was found in Molina as source Data issue -- 
+
+SELECT CASE WHEN cnt THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN cnt THEN 'MDS to DWH data validation failed for d_incident.created_on' ELSE 'SUCCESS' END as Message from (
+select count(1) as cnt from 
+(select sys_id,count(1) cnt   
+FROM  molinahealth_mdsdb.change_request_final SRC  
+ group by number having count(1)>1
+
+)b)c
+
