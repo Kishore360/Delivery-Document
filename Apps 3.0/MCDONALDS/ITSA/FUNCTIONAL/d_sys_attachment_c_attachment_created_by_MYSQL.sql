1c93a63b33dc7d7772f27b451079fa80aa907a7b
Kishore Mantha <kishore.mantha@numerify.com>
2017-08-28 11:07:19 +0530

    Updated the McD QA Queries.

--- Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/d_sys_attachment_c_attachment_created_by_MYSQL.sql
+++ Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/d_sys_attachment_c_attachment_created_by_MYSQL.sql
@@ -1,9 +1,9 @@
 
 SELECT 
-CASE WHEN CNT THEN 'FAILURE' ELSE 'SUCCESS' END as RESULT,
-CASE WHEN CNT THEN 'MDS to DW Data Validation Failed for d_sys_attachment_c_attachment_created_by' ELSE 'SUCCESS' END as MESSAGE
+CASE WHEN CNT >0  THEN 'FAILURE' ELSE 'SUCCESS' END as RESULT,
+CASE WHEN CNT >0  THEN 'MDS to DW Data Validation Failed for d_sys_attachment_c_attachment_created_by' ELSE 'SUCCESS' END as MESSAGE
 FROM (SELECT count(1) as CNT
 FROM mcdonalds_mdsdb.sys_attachment_final a 
 JOIN mcdonalds_mdsdb.sys_user_final b ON a.sys_created_by=b.user_name
 JOIN mcdonalds_mdwdb.d_sys_attachment_c X ON a.sys_id=X.row_id and a.sourceinstance=X.source_id
-WHERE Upper(b.name<>X.attachment_created_by)a;
+WHERE Upper(b.name<>X.attachment_created_by))a;
