aed415167987ee1c9df44d45b2b6049a4627c6e2
prabhuram <prabhuram.sridharan@numerify.com>
2017-08-31 12:36:03 +0530

    Committed

--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_incident_met_resolution_with_pause_sla_c_flag_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_incident_met_resolution_with_pause_sla_c_flag_mysql.sql
@@ -5,22 +5,22 @@ FROM (
 select count(*) as cnt from (
 select
 count(1) as cnt
-FROM nbcu_mdwdb.d_incident d_inc
-LEFT JOIN nbcu_mdsdb.task_sla_final tsf ON tsf.TASK = d_inc.row_id
+FROM nbcu_mdsdb.task_sla_final tsf
+JOIN nbcu_mdwdb.d_incident d_inc   ON tsf.TASK = d_inc.row_id and tsf.sourceinstance=d_inc.source_id
 JOIN
   (SELECT Max(a.planned_end_time) planned_end_time,
           a.TASK AS TASK,
-          b.name AS name
+          b.name AS name,a.sourceinstance sourceinstance
    FROM nbcu_mdsdb.task_sla_final a
    JOIN
-     (SELECT sys_id,
+     (SELECT sys_id,sourceinstance sourceinstance,
              collection,
              CASE WHEN name LIKE '%Resolution%(With Pause)%' THEN 'Resolution With Pause' WHEN name LIKE '%Resolution%(No Pause)%' THEN 'Resolution Without Pause' WHEN name LIKE '%Response%' THEN 'Response'  END AS name
-      FROM nbcu_mdsdb.contract_sla_final) b ON a.sla = b.sys_id
+      FROM nbcu_mdsdb.contract_sla_final) b ON a.sla = b.sys_id and a.sourceinstance=b.sourceinstance
    WHERE b.collection = 'incident'
      AND a.stage <> 'Cancelled'
         GROUP BY a.TASK,
-            b.name) AA ON tsf.TASK = AA.TASK
+            b.name,a.sourceinstance) AA ON tsf.TASK = AA.TASK and tsf.sourceinstance=AA.sourceinstance
 AND tsf.planned_end_time = AA.planned_end_time
 GROUP BY tsf.TASK
 HAVING iF(SUM(CASE WHEN tsf.has_breached = 1 THEN 0 ELSE 1 END) > 0
