b53b4fee5fbf56ffa399f6c251f6f4e113caeaa1
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-08-31 15:52:06 +0530

    Updated Query DELV-7655

--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_change_request_change_ever_failed_c_flag_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_change_request_change_ever_failed_c_flag_mysql.sql
@@ -3,16 +3,16 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ELSE 'Data Matched' END AS Message 
 FROM (
  select count(1) cnt
-from cardinalhealth_mdsdb.sys_audit_final a
- left  JOIN  cardinalhealth_mdwdb.f_t_task_activity b
+from cardinalhealth_mdsdb.change_request_final a
+ left  JOIN  cardinalhealth_mdwdb.d_change_request b
 on  a.SYS_ID= b.ROW_ID and a.sourceinstance=b.source_id
-join cardinalhealth_mdwdb.d_change_request d
-on a.documentkey=d.row_key
- where  a.tablename = 'change_request' and  
-case when a.fieldname = 'u_success_status' and b.task_key <> -1 
-and task_attribute_wh_old_value='Failed' or task_attribute_wh_new_value='Failed' then 'Y' 
+join cardinalhealth_mdsdb.sys_audit_final d
+on a.sys_id=d.documentkey
+ where  d.tablename = 'change_request' and  
+case when d.fieldname = 'u_success_status' 
+and oldvalue='Failed' or newvalue='Failed' then 'Y' 
     else 'N' end
 
- <> d.change_ever_failed_c_flag  )b
+ <> b.change_ever_failed_c_flag  )b
  
  
