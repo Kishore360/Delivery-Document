c16639356fef24ca491459d77f09ce1d0e0f0d49
DESKTOP-PLTURRV\Shubha <shubha.rao@numerify.com>
2017-08-31 13:03:12 +0530

    ITCA OOTB Query Updation

--- Apps 3.0/CARDINALHEALTH/ITCA/FUNCTIONAL/f_agent_non_acd_total_agent_calls_silo.sql
+++
@@ -1,7 +0,0 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_agent_non_acd_total_agent_calls' ELSE 'SUCCESS' END as Message
- from cardinalhealth_mdsdb.us_a_agent_fcr_final SRC
-LEFT JOIN cardinalhealth_mdwdb.f_agent_non_acd TRGT
-            ON (CONCAT(SRC.acd_no,'~',SRC.employee_id,'~',DATE_FORMAT(SRC.opened_on,'%Y%m%d'))=TRGT.row_id
-            AND SRC.sourceinstance =TRGT.source_id )
-WHERE coalesce(SRC.total_agent_calls,'') <> coalesce(TRGT.TOTAL_AGENT_CALLS,'')
--- Apps 3.0/CARDINALHEALTH/ITCA/FUNCTIONAL/f_skill_call_summary_target_met_abandon_calls.sql
+++ Apps 3.0/CARDINALHEALTH/ITCA/FUNCTIONAL/f_skill_call_summary_target_met_abandon_calls.sql
@@ -1,8 +1,8 @@
 
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_skill_call_summary.target_met_abandon_calls' ELSE 'SUCCESS' END as Message
-FROM cardinalhealth_mdsdb.hsplit_final SRC 
+FROM cardinalhealth_mdsdb.dsplit_final SRC 
 LEFT JOIN cardinalhealth_mdwdb.f_skill_call_summary TRGT 
-	ON (concat(row_date,'~',starttime,'~',acd,'~',trim(split),'~~',cdchash) =TRGT.row_id 
+	ON (concat(row_date,'~',acd,'~',split) =TRGT.row_id 
 	AND SRC.sourceinstance =TRGT.source_id )
 WHERE COALESCE(SRC.targetabns,'')<> COALESCE(TRGT.target_met_abandon_calls,'')
