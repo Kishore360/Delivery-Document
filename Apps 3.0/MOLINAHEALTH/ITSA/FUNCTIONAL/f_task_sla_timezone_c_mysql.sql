914992da9faa267d696eae7de620d8b51cd6d911
prabhuram <prabhuram.sridharan@numerify.com>
2017-09-01 12:44:44 +0530

    committing Queries

--- Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_task_sla_timezone_c_mysql.sql
+++ Apps 3.0/MOLINAHEALTH/ITSA/FUNCTIONAL/f_task_sla_timezone_c_mysql.sql
@@ -2,12 +2,12 @@
 
 				select CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.age_key' ELSE 'SUCCESS' END as Message
-from mercuryins_mdsdb.task_sla_final tsd 
-JOIN mercuryins_mdwdb.f_task_sla stg
+from molinahealth_mdsdb.task_sla_final tsd 
+JOIN molinahealth_mdwdb.f_task_sla stg
  ON stg.row_id = tsd.sys_id 
  AND stg.source_id = tsd.sourceinstance
 where	
-stg.timezone_c<>coalesce(tsd.timezone,'UNSPECIFIED');
+stg.timzone_c<>coalesce(tsd.timezone,'UNSPECIFIED');
 
 
 
--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_task_completed_on_c_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_task_completed_on_c_mysql.sql
@@ -4,14 +4,14 @@
 ELSE 'Data Matched' END AS Message 
 FROM (
 select count(1) as cnt from  nbcu_mdwdb.d_task a 
-    join nbcu_mdsdb.task_final i on a.row_id=i.sys_id and a.source_id=i.sourceinstance
-     join nbcu_mdsdb.sc_request_final sc on sc.sys_id=i.sys_id and sc.sourceinstance=i.sourceinstance
-                 join nbcu_mdsdb.incident_final inc on inc.sys_id=i.sys_id and inc.sourceinstance=i.sourceinstance
-                 join nbcu_mdsdb.problem_final pr on pr.sys_id=i.sys_id and pr.sourceinstance=i.sourceinstance
-                 join nbcu_mdsdb.problem_task_final prt on prt.sys_id=i.sys_id and prt.sourceinstance=i.sourceinstance
-                 join nbcu_mdsdb.sc_req_item_final sri on sri.sys_id=i.sys_id and sri.sourceinstance=i.sourceinstance
-                 join nbcu_mdsdb.sc_task_final sct on sct.sys_id=i.sys_id and sct.sourceinstance=i.sourceinstance
-                 join nbcu_mdsdb.change_request_final cr on cr.sys_id=i.sys_id and cr.sourceinstance=i.sourceinstance
+    left join nbcu_mdsdb.task_final i on a.row_id=i.sys_id and a.source_id=i.sourceinstance
+     left join nbcu_mdsdb.sc_request_final sc on sc.sys_id=i.sys_id and sc.sourceinstance=i.sourceinstance
+                 left join nbcu_mdsdb.incident_final inc on inc.sys_id=i.sys_id and inc.sourceinstance=i.sourceinstance
+                 left join nbcu_mdsdb.problem_final pr on pr.sys_id=i.sys_id and pr.sourceinstance=i.sourceinstance
+                 left join nbcu_mdsdb.problem_task_final prt on prt.sys_id=i.sys_id and prt.sourceinstance=i.sourceinstance
+                 left join nbcu_mdsdb.sc_req_item_final sri on sri.sys_id=i.sys_id and sri.sourceinstance=i.sourceinstance
+                 left join nbcu_mdsdb.sc_task_final sct on sct.sys_id=i.sys_id and sct.sourceinstance=i.sourceinstance
+                 left join nbcu_mdsdb.change_request_final cr on cr.sys_id=i.sys_id and cr.sourceinstance=i.sourceinstance
                 where  a.completed_on_c<>CASE
                                 WHEN i.sys_class_name ='INCIDENT' THEN  CONVERT_TZ(coalesce(inc.resolved_at,inc.closed_at),'GMT','America/New_York')
                                 WHEN i.sys_class_name ='PROBLEM' THEN CONVERT_TZ (pr.u_validated_at,'GMT','America/New_York')
