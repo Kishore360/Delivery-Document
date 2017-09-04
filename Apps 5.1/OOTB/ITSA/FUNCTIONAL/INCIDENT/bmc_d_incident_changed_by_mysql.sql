786c829f42d4348fcbc566c22aab75f4bed8c753
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2017-09-04 11:58:48 +0530

    push the changes to OOTB schneider

--- Apps 5.1/OOTB/ITSA/FUNCTIONAL/INCIDENT/bmc_d_incident_changed_by_mysql.sql
+++ Apps 5.1/OOTB/ITSA/FUNCTIONAL/INCIDENT/bmc_d_incident_changed_by_mysql.sql
@@ -1,4 +1,4 @@
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
 CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_incident.changed_by' ELSE 'SUCCESS' END as Message from schneider_mdsdb.hpd_help_desk_final  SRC JOIN schneider_mdwdb.d_incident TRGT 
-ON (SRC.request_id = TRGT.row_id  or SRC.instance_id = TRGT.row_id or SRC.entry_id = TRGT.row_id)  AND SRC.sourceinstance = TRGT.source_id 
+ON (SRC.incident_number =  TRGT.row_id)  AND SRC.sourceinstance = TRGT.source_id 
 WHERE SRC.last_modified_by <> (TRGT.changed_by) 
