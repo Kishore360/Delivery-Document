27489ac750efa4d5fa8542ff26ae77990abbd84b
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2017-09-04 18:48:36 +0530

    commiting changes into schneider as part of custom queries for incident*, work order and task

---
+++ Apps 5.1/SCHNEIDER/ITSA/FUNCTIONAL/bmc_d_internal_organization_ctm_Support_Group_organization_name_mysql.sql
@@ -0,0 +1,3 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_service.category' 
+ELSE 'SUCCESS' END as Message from schneider_mdsdb.ctm_support_group_final SRC JOIN schneider_mdwdb.d_internal_organization TRGT 
+ON (SRC.instanceid = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )  WHERE SRC.Support_Group_Name <> (TRGT.organization_name) 
---
+++ Apps 5.1/SCHNEIDER/ITSA/FUNCTIONAL/bmc_f_incident_changed_by_mysql.sql
@@ -0,0 +1,9 @@
+
+
+SELECT CASE WHEN cnt THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt THEN 'MDS to DWH data validation failed for f_incident.changed_by' ELSE 'SUCCESS' END as Message from 
+ (select count(1) cnt 
+ FROM schneider_mdsdb.hpd_help_desk_final SRC 
+JOIN schneider_mdwdb.f_incident TRGT 
+ON SRC.incident_number = TRGT.row_id and  SRC.sourceinstance = TRGT.source_id
+WHERE convert_tz(SRC.last_modified_by,'GMT','America/Los_Angeles')<> TRGT.changed_on )b;
