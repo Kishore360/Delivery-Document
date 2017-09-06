a6c2289ea981dbe0a1746613654e76c139529745
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2017-09-06 22:51:08 +0530

    changed WO

--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/f_work_order_total_duration_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/f_work_order_total_duration_mysql.sql
@@ -1,5 +1,5 @@
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_work_order.total_duration' ELSE 'SUCCESS' END as Message FROM schneider_mdsdb.woi_workorder_final  SRC JOIN schneider_mdwdb.f_work_order TRGT ON (SRC.work_order_id = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )  WHERE 
-where  TIMESTAMPDIFF (SECOND,'EffortTrackingStartTime', 'EffortTrackingEndTime' ) <> total_duration
+where  TIMESTAMPDIFF(SECOND,'EffortTrackingStartTime', 'EffortTrackingEndTime' ) <> total_duration
 
 
 
