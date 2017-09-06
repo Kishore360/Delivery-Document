d526222223364894eeff8fcef1a656e65dddae56
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2017-09-06 20:43:40 +0530

    changes done

--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/f_work_order_task_open_to_close_duration_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/f_work_order_task_open_to_close_duration_mysql.sql
@@ -1,6 +1,6 @@
 
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_work_order_task.open_to_close_duration' ELSE 'SUCCESS' END as Message FROM schneider_mdsdb.tms_task_final  SRC JOIN schneider_mdwdb.f_work_order_task TRGT ON (SRC.task_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id )
-LEFT JOIN schneider_mdwdbdb.d_lov_map LKP 
+LEFT JOIN schneider_mdwdb.d_lov_map LKP 
  ON TRGT.state_src_key = LKP.src_key
 WHERE   LKP.dimension_wh_code IN ('CLOSED') 
 AND  TIMESTAMPDIFF(SECOND,convert_tz(convert_tz( SRC.create_date,'GMT','America/Los_Angeles'),
