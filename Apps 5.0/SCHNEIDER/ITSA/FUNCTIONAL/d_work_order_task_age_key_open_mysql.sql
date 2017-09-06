a8aa4cf7131b4ed0fe6b6242c0621dfb1a560909
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2017-09-06 19:59:24 +0530

    pushing changes made

--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_work_order_task_age_key_open_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_work_order_task_age_key_open_mysql.sql
@@ -1,4 +1,4 @@
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_work_order_task.age' ELSE 'SUCCESS' END as Message FROM schneider_mdsdb.tms_task_final  SRC JOIN schneider_mdwdb.d_work_order_task TRGT ON (SRC.task_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id )
 JOIN schneider_mdwdb.d_lov_map LKP ON TRGT.state_src_key = LKP.src_key
 AND LKP.dimension_wh_code IN ('OPEN') and LKP.dimension_class = 'STATE~WORK_ORDER_TASK'
-WHERE TIMESTAMPDIFF(SECOND, convert_tz(convert_tz(SRC.reported_date,'GMT','America/Los_Angeles'),'America/Los_Angeles', 'GMT' ), (SELECT CONVERT_TZ(max(lastupdated),'America/Los_Angeles', 'GMT') AS lastupdated FROM schneider_mdwdb.d_o_data_freshness WHERE sourcename like '%' and etl_run_number=TRGT.etl_run_number)) <> TRGT.age)a; 
+WHERE TIMESTAMPDIFF(SECOND, convert_tz(convert_tz(SRC.reported_date,'GMT','America/Los_Angeles'),'America/Los_Angeles', 'GMT' ), (SELECT CONVERT_TZ(max(lastupdated),'America/Los_Angeles', 'GMT') AS lastupdated FROM schneider_mdwdb.d_o_data_freshness WHERE sourcename like '%' and etl_run_number=TRGT.etl_run_number)) <> TRGT.age 
--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_work_order_task_age_resolved_closed_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_work_order_task_age_resolved_closed_mysql.sql
@@ -1,7 +1,6 @@
 
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_work_order_task.age' ELSE 'SUCCESS' END as Message FROM schneider_mdsdb.tms_task_final  SRC JOIN schneider_mdwdb.d_work_order_task TRGT ON (SRC.task_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id )
 JOIN schneider_mdwdb.d_lov_map LKP ON TRGT.state_src_key = LKP.src_key
-JOIN schneider_mdwdb.d_lov_map LKP ON TRGT.state_src_key = LKP.src_key
 AND LKP.dimension_wh_code IN ('RESOLVED','CLOSED') and LKP.dimension_class = 'STATE~WORK_ORDER_TASK'
 WHERE TIMESTAMPDIFF(SECOND, convert_tz(convert_tz(SRC.reported_date,'GMT','America/Los_Angeles'),'America/Los_Angeles','GMT'), 
 coalesce(convert_tz(convert_tz(SRC.last_resolved_date,'GMT','America/Los_Angeles'),'America/Los_Angeles','GMT'), 
