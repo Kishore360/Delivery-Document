96bb9a76ec52038aa60ad0eac4a1060377863b63
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2017-09-07 20:50:59 +0530

    pushing modified queries for KSR

--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_ksr_c_short_description_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_ksr_c_short_description_mysql.sql
@@ -1,5 +1,5 @@
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  
 THEN 'MDS to DWH data validation failed for d_ksr_c.short_description' else 'SUCCESS' end as message
-FROM STM_BMC_mdsdb.KS_SRV_CustomerSurvey_base_final SRC
-JOIN STM_BMC_mdwdb.d_ksr_c TRGT ON SRC.CustomerSurveyID  =  TRGT.row_id and  SRC.sourceinstance = TRGT.source_id  
+FROM schneider_mdsdb.ks_srv_customersurvey_base_final SRC
+JOIN schneider_mdwdb.d_ksr_c TRGT ON SRC.customersurveyid  =  TRGT.row_id and  SRC.sourceinstance = TRGT.source_id  
 WHERE coalesce(SRC.short_description,'UNSPECIFIED') <> (TRGT.short_description)
