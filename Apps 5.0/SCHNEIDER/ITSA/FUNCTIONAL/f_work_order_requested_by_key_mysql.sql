6475d45cc6592f6843573538165fe047c1847ae1
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2017-09-07 11:44:13 +0530

    Pushing changes done for WO queries

--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/f_work_order_requested_by_key_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/f_work_order_requested_by_key_mysql.sql
@@ -1,3 +1,5 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_work_order.requested_by_key' ELSE 'SUCCESS' END as Message FROM schneider_mdsdb.woi_workorder_final  SRC JOIN schneider_mdwdb.f_work_order TRGT ON (SRC.work_order_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id )  LEFT JOIN schneider_mdwdb.d_internal_contact LKP ON ( concat( 'INTERNAL_CONTACT~' ,upper( SRC.Requestor_by_id)) = LKP.row_id AND SRC.sourceinstance = LKP.source_id ) WHERE COALESCE(LKP.row_key,CASE WHEN SRC.Requestor_by_id IS NULL THEN 0 else -1 end)<> (TRGT.requested_by_key) 
-
-
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_work_order.requested_by_key' ELSE 'SUCCESS' END as Message from schneider_mdsdb.woi_workorder_final src
+inner join schneider_mdwdb.f_work_order trgt on src.work_order_id = trgt.row_id and src.sourceinstance = trgt.source_id
+left join schneider_mdsdb.ctm_people_final src_lkp on src.Requestor_by_id = src_lkp.remedy_login_id and src.sourceinstance = src_lkp.sourceinstance
+left join schneider_mdwdb.d_internal_contact lkp on  CONCAT('INTERNAL_CONTACT~',src_lkp.person_id) = lkp.row_id and src_lkp.sourceinstance = lkp.source_id 
+where (case when src.Requestor_by_id is null then 0 when src.Requestor_by_id is not null and src_lkp.person_id is null then -1 when src.Requestor_by_id is not null and  src_lkp.person_id is not null then lkp.row_key else -1 end) <> coalesce(trgt.requested_by_key,-1);
--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/f_work_order_requested_for_key_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/f_work_order_requested_for_key_mysql.sql
@@ -1,4 +1,6 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_work_order.requested_by_key' ELSE 'SUCCESS' END as Message FROM schneider_mdsdb.woi_workorder_final  SRC JOIN schneider_mdwdb.f_work_order TRGT ON (SRC.work_order_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id )  LEFT JOIN schneider_mdwdb.d_internal_contact LKP ON ( concat( 'INTERNAL_CONTACT~' ,upper( SRC.Customer_Person_ID)) = LKP.row_id AND SRC.sourceinstance = LKP.source_id ) WHERE COALESCE(LKP.row_key,CASE WHEN SRC.Customer_Person_ID IS NULL THEN 0 else -1 end)<> (TRGT.requested_for_key) 
-
-
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_work_order.requested_for_key' ELSE 'SUCCESS' END as Message from schneider_mdsdb.woi_workorder_final src
+inner join schneider_mdwdb.f_work_order trgt on src.work_order_id = trgt.row_id and src.sourceinstance = trgt.source_id
+left join schneider_mdsdb.ctm_people_final src_lkp on src.Customer_Person_ID = src_lkp.remedy_login_id and src.sourceinstance = src_lkp.sourceinstance
+left join schneider_mdwdb.d_internal_contact lkp on  CONCAT('INTERNAL_CONTACT~',src_lkp.person_id) = lkp.row_id and src_lkp.sourceinstance = lkp.source_id 
+where (case when src.Customer_Person_ID is null then 0 when src.Customer_Person_ID is not null and src_lkp.person_id is null then -1 when src.Customer_Person_ID is not null and  src_lkp.person_id is not null then lkp.row_key else -1 end) <> coalesce(trgt.requested_for_key,-1);
 
