6f57c644b2000ac5cb81de4be584795a03dcecd5
vijay rao <vijay.rao@numerify.com>
2017-09-11 17:08:47 +0530

    Qualcomm Major Incident QA queries

---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_major_incident_c_location_key_mysql.sql
@@ -0,0 +1,10 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_major_incident.location_key' ELSE 'SUCCESS' END as Message
+FROM qualcomm_mdsdb.u_major_incident_final SRC 
+ LEFT JOIN qualcomm_mdwdb.f_major_incident_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+  LEFT JOIN qualcomm_mdwdb.d_location LKP 
+ ON ( SRC.location= LKP.row_id 
+AND SRC.sourceinstance= LKP.source_id )
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.location IS NULL THEN 0 else -1 end)<> (TRGT.location_key);
