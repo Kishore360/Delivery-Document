2a67263bb8f2f9a72dcdcce2d8712c37fc519535
vijay rao <vijay.rao@numerify.com>
2017-09-11 17:25:37 +0530

    qualcomm Major Incident queries

--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_major_incident_c_opened_by_key_mysql.sql
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_major_incident_c_opened_by_key_mysql.sql
@@ -1,7 +1,7 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_major_incident.opened_by_key' ELSE 'SUCCESS' END as Message
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_major_incident.opened_by_key' ELSE 'SUCCESS' END as Message
 FROM qualcomm_mdsdb.u_major_incident_final SRC 
- LEFT JOIN qualcomm_mdwdb.f_major_incident_c TRGT 
+ LEFT JOIN qualcomm_mdwdb.d_major_incident_c TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
  LEFT JOIN qualcomm_mdwdb.d_internal_contact LKP 
---
+++ Apps 5.0/QUALCOMM/f_major_incident_c_major_incident_c_key_mysql.sql
@@ -0,0 +1,12 @@
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_major_incident_c.major_incident_c_key' ELSE 'SUCCESS' END as Message
+ FROM qualcomm_mdsdb.u_major_incident_final SRC 
+ LEFT JOIN qualcomm_mdwdb.f_major_incident_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+ LEFT JOIN qualcomm_mdwdb.d_major_incident_c LKP 
+ ON ( SRC.sys_id= LKP.row_id 
+AND SRC.sourceinstance= LKP.source_id and LKP.soft_deleted_flag='N' )
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.sys_id IS NULL THEN 0 else -1 end)<> (TRGT.major_incident_c_key)
