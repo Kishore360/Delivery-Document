43fc497951ffa7712786242b1967a65eee7481ea
Kishore Mantha <kishore.mantha@numerify.com>
2017-09-14 17:42:35 +0530

    Updated the changes respective to rambus and McD.

--- Apps 3.0/MCDONALDS/ITSA/MATS/Mcdonalds_ ITSM_Incident Weekly Backlog C_detailed_redshift.sql
+++ Apps 3.0/MCDONALDS/ITSA/MATS/Mcdonalds_ ITSM_Incident Weekly Backlog C_detailed_redshift.sql
@@ -21,3 +21,8 @@ UNION
 SELECT 'd_internal_organization_group' as Table_Name, count(1) as Row_count
 FROM ldb.a_incident_weekly_c a 
 JOIN ldb.d_internal_organization_group b ON a.assignment_group_key=b.row_key
+UNION 
+SELECT 'd_configuration_item_support_group_weekly_c' as Table_Name, count(1) as Row_count
+FROM ldb.a_incident_weekly_c a 
+JOIN ldb.d_business_service b ON a.business_service_key=b.row_key
+JOIN ldb.d_configuration_item_support_group_weekly_c c ON a.business_service_key=c.row_key 
