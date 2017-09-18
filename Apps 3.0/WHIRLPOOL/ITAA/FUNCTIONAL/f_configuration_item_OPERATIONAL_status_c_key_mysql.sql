43787d8686cb8e990495f789a70b7c1606b0bb3e
prabhuram <prabhuram.sridharan@numerify.com>
2017-09-18 22:00:52 +0530

    Committing WP Operational status Query

--- Apps 3.0/WHIRLPOOL/ITAA/FUNCTIONAL/f_configuration_item_OPERATIONAL_status_c_key_mysql.sql
+++ Apps 3.0/WHIRLPOOL/ITAA/FUNCTIONAL/f_configuration_item_OPERATIONAL_status_c_key_mysql.sql
@@ -1,17 +1,22 @@
 
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.state_src_key' ELSE 'SUCCESS' END as Message
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.state_src_key' ELSE 'SUCCESS' END as Message
+ FROM (select count(1)cnt  from(
+select  SRC.label lab,LKP.row_key rk from 
+(select distinct  sdof.label label,operational_status
  FROM whirlpool_mdsdb.cmdb_ci_final SRC
  LEFT JOIN whirlpool_mdsdb.sys_db_object_final sdof ON SRC.sys_class_name=sdof.name
 and SRC.SourceInstance=sdof.SourceInstance
+
+ 
+ WHERE  sdof.label in ('AIX Server','Application','DB2 Instance','Oracle Instance','IP Router','IP Switch',
+'Linux Server','Business Service','Windows Server','SAP Instance','Technical Service','Unix Server','Storage',
+'Personal Computer','Server') ) SRC  
 
 LEFT JOIN whirlpool_mdwdb.d_lov LKP 
  ON LKP.dimension_class like '%OPERATIONAL_STATUS~CMDB_CI%' and 
 ( concat('OPERATIONAL_STATUS~CMDB_CI~~~',OPERATIONAL_status)= LKP.src_rowid 
  ) 
-  JOIN whirlpool_mdwdb.f_configuration_item_aggregator_fact_c TRGT 
- ON (sdof.label =TRGT.ci_class_c  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE  -- n_date='2017-09-04 00:00:00' and 
- TRGT.soft_deleted_flag='N' and 
- COALESCE(LKP.row_key,CASE WHEN SRC.OPERATIONAL_status IS NULL THEN 0 else -1 end)<> (TRGT.OPERATIONAL_status_src_c_key) ;
+)A where (lab ,rk) not in 
+ 
+  (select distinct ci_class_c class,operational_status_src_c_key from  whirlpool_mdwdb.f_configuration_item_aggregator_fact_c  where soft_deleted_flag='N'))ABC;
