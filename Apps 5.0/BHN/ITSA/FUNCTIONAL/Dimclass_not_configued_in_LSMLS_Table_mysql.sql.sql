788ace261d31a98bf9d709bf7a3780ee1838b0ac
ashokvelu <ashok.velu@numerify.com>
2017-09-19 19:36:37 +0530

    bhn ootb cust

---
+++ Apps 5.0/BHN/ITSA/FUNCTIONAL/Dimclass_not_configued_in_LSMLS_Table_mysql.sql.sql
@@ -0,0 +1,27 @@
+/* 
+--Please Ignore the dimension classes related to Asset,Finance
+-- APP 1870 is the defect raised for not having LSM LS Dim class not configured
+--Ignore Category and Subcategory for TJX(Populated using M2SI ,Not configured using LSM LS Sytem variable)*/
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'Dimension classes not configured in LSM LS System Variables:Ignore Category and Subcategory for TJX' ELSE 'SUCCESS' END as Message
+ FROM(
+
+select distinct dimension_class,source_id
+from bhn_mdwdb.d_lov lov where  source_id=2 and dimension_class 
+not in (select class_value from bhn_workdb.lsm_ls_system_variables where enable_flag='Y'  )  and dimension_class 
+ like '%~%' and  (dimension_class NOT LIKE '%GEO%') and dimension_class not in (
+
+'ASSET',
+'ASSET_AGE',
+'CITY~GEO',
+'COUNTRY~GEO',
+'CURRENCY',
+'EXPENSE_ITEM',
+'GENDER',
+'STATE~GEO',
+'WARRANTY',
+'CRITICALITY~BUSINESS_SERVICE',
+'NAME~TASK_SLA',
+'TASK_TYPE~TASK'
+) )a;
