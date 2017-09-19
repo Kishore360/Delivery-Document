24970b1e8155bcd72b3d2349c6beebc522d5c258
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2017-09-20 00:36:31 +0530

    removing age

--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/f_work_order_task_age_key_mysql.sql
+++
@@ -1,5 +0,0 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_work_order_task.age_key' ELSE 'SUCCESS' END as Message FROM schneider_mdsdb.tms_task_final  SRC JOIN schneider_mdwdb.f_work_order_task TRGT 
-ON (SRC.task_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id )  LEFT JOIN schneider_mdwdb.d_lov LKP
-ON ((TRGT.age div 86400.0 BETWEEN LKP.lower_range_value AND LKP.upper_range_value)
-AND LKP.dimension_class = 'AGEBUCKET_WH~WORK_ORDER_TASK' )
-WHERE (LKP.row_key )  <> TRGT.age_key; 
