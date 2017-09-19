a31656cb1a7762ecd1ad1f359a1d0dbe654126bb
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2017-09-19 23:08:58 +0530

    Pushing some more changes

--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_business_service_install_status_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_business_service_install_status_mysql.sql
@@ -1,4 +1,8 @@
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_service.install_status' 
 ELSE 'SUCCESS' END as Message FROM schneider_mdsdb.bmc_core_bmc_businessservice_final SRC JOIN schneider_mdwdb.d_service TRGT 
-ON (concat('BUSINESS_SERVICE','~',SRC.requestid) = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )  WHERE SRC.servicelifecycle <> (TRGT.install_status) 
-	
+ON (concat('BUSINESS_SERVICE','~',SRC.requestid)) = TRGT.row_id 
+JOIN (SELECT enum_values.value,enum_values.enumId, enum_values.sourceinstance
+FROM schneider_mdsdb.field_enum_values_final enum_values
+JOIN schneider_mdsdb.field_final field ON (enum_values.fieldId=field.fieldId AND enum_values.schemaId=field.schemaId and enum_values.sourceinstance = field.sourceinstance)
+JOIN schneider_mdsdb.arschema_final schma ON (field.schemaId=schma.schemaId and field.sourceinstance = schma.sourceinstance) where field.fieldName ='ServiceLifeCycle' AND schma.NAME='BMC.CORE:BMC_BusinessService') service_lifecycle on SRC.ServiceLifeCycle = service_lifecycle.enumId and SRC.sourceinstance = service_lifecycle.sourceinstance and SRC.datasetid = 'BMC.ASSET'
+where  service_lifecycle.value  <> (TRGT.install_status) 
--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_business_service_type_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_business_service_type_mysql.sql
@@ -1,3 +1,8 @@
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_service.type' 
 ELSE 'SUCCESS' END as Message FROM schneider_mdsdb.bmc_core_bmc_businessservice_final SRC JOIN schneider_mdwdb.d_service TRGT 
-ON (concat('BUSINESS_SERVICE','~',SRC.requestid) = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )  WHERE SRC.servicetype <> (TRGT.type)
+ON (concat('BUSINESS_SERVICE','~',SRC.requestid)) = TRGT.row_id 
+JOIN (SELECT enum_values.value,enum_values.enumId, enum_values.sourceinstance
+FROM schneider_mdsdb.field_enum_values_final enum_values
+JOIN schneider_mdsdb.field_final field ON (enum_values.fieldId=field.fieldId AND enum_values.schemaId=field.schemaId and enum_values.sourceinstance = field.sourceinstance)
+JOIN schneider_mdsdb.arschema_final schma ON (field.schemaId=schma.schemaId and field.sourceinstance = schma.sourceinstance) where field.fieldName ='ServiceType' AND schma.NAME='BMC.CORE:BMC_BusinessService') service_type on SRC.servicetype = service_type.enumId and SRC.sourceinstance = service_type.sourceinstance and SRC.datasetid = 'BMC.ASSET'
+where  service_type.value  <> (TRGT.type) 
