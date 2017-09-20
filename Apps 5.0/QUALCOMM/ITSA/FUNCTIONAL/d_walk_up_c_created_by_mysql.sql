519c1487ad577dbc453f583e2c761500e7fa402a
vijay rao <vijay.rao@numerify.com>
2017-09-20 12:11:05 +0530

    no message

---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_walk_up_c_created_by_mysql.sql
@@ -0,0 +1,13 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match' 
+ELSE 'Data Matched' END AS Message 
+FROM (
+select count(1) as cnt
+from qualcomm_mdsdb.u_walk_up_final s
+left  JOIN qualcomm_mdwdb.d_walk_up_c t 
+on  t.ROW_ID=s.SYS_ID and s.sourceinstance=t.source_id 
+WHERE t.created_by <> s.sys_created_by
+ ) temp
+ 
+ 
+ 
---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_walk_up_c_created_on_mysql.sql
@@ -0,0 +1,7 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_walk_up_c.created_on' ELSE 'SUCCESS' END as Message
+FROM qualcomm_mdsdb.u_walk_up_final SRC 
+ LEFT JOIN qualcomm_mdwdb.d_walk_up_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+WHERE TRGT.created_on<> CONVERT_TZ(SRC.sys_created_on,'GMT','America/Los_Angeles');
