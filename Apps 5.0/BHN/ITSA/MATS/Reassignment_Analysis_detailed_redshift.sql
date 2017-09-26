6c0dc6a6a6fc3b331d60ea041c085bb273384ad8
prabhuram <prabhuram.sridharan@numerify.com>
2017-09-26 18:05:55 +0530

    Committing changes to the query

--- Apps 5.0/BHN/ITSA/MATS/Reassignment_Analysis_detailed_redshift.sql
+++ Apps 5.0/BHN/ITSA/MATS/Reassignment_Analysis_detailed_redshift.sql
@@ -1,56 +1,223 @@
-
-select 'f_incident_reassignment_analysis_c'as Table_Name,count(a11.incident_key) Row_Count
-from ldb.f_incident_reassignment_analysis_c	a11
-
-
+select 'ldb.f_incident_activity a11 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident_activity a11 
 union
-
-select  'd_incident' as Table_Name,count(a11.incident_key) Row_Count
-from ldb.f_incident_reassignment_analysis_c	a11
-   join        ldb.d_incident	a12
-   on       	(a11.incident_key = a12.row_key)
-
+select 'ldb.d_internal_organization_group a12 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_internal_organization_group a13 
+on (a11.assignment_group_key = a13.row_key) 
 union
-
-select  'd_calendar_date' as Table_Name,count(a11.incident_key) Row_Count
-from ldb.f_incident_reassignment_analysis_c	a11
-   join        ldb.d_calendar_date	a13
-   on       	(a11.created_on_key  = a13.row_key)
-
+select 'ldb.d_incident a13 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_incident a13 
+on (a11.incident_key = a13.row_key) 
 union
-
-select  'd_calendar_date_opened' as Table_Name,count(a11.incident_key) Row_Count
-from ldb.f_incident_reassignment_analysis_c	a11
-   join        ldb.d_calendar_date_opened	a14
-   on       	(a11.opened_on_key = a14.row_key)
-
+select 'ldb.d_incident_class_c a13 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_incident_class_c a13 
+on (a11.class_c_key = a13.row_key) 
 union
-
-select  'd_internal_organization_group' as Table_Name,count(a11.incident_key) Row_Count
-from ldb.f_incident_reassignment_analysis_c	a11
-   join        ldb.d_internal_organization_group	a15
-   on       	(a11.old_assignment_group_key = a15.row_key)
-
+select 'ldb.d_calendar_date a14 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_calendar_date a14 
+on (a11.opened_on_key = a14.row_key) 
 union
-
-select  'd_internal_organization_group_reassignment_c' as Table_Name,count(a11.incident_key) Row_Count
-from ldb.f_incident_reassignment_analysis_c	a11
-   join        ldb.d_internal_organization_group_reassignment_c	a16
-   on       	(a11.new_assignment_group_key = a16.row_key)
-
+select 'ldb.d_calendar_week a15 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_calendar_date a14 
+on (a11.opened_on_key = a14.row_key) 
+join ldb.d_calendar_week a15 
+on (a14.week_start_date_key = a15.row_key) 
 union
-
-select  'd_task_contacttype' as Table_Name,count(a11.incident_key) Row_Count
-from ldb.f_incident_reassignment_analysis_c	a11
-   join        ldb.d_task_contacttype	a17
-   on       	(a11.reported_type_src_key = a17.row_key)
-
+select 'ldb.d_calendar_month a16 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_calendar_date a14 
+on (a11.opened_on_key = a14.row_key) 
+join ldb.d_calendar_week a15 
+on (a14.week_start_date_key = a15.row_key) 
+join ldb.d_calendar_month a16 
+on (a15.month_start_date_key = a16.row_key) 
 union
-
-select  'd_calendar_month_opened' as Table_Name,count(a11.incident_key) Row_Count
-from ldb.f_incident_reassignment_analysis_c	a11
-join	ldb.d_calendar_date_opened	a14
-	  on 	(a11.opened_on_key = a14.row_key)  
-   join        ldb.d_calendar_month_opened	a18
-   on       	(a14.month_start_date_key = a18.row_key)
-
+select 'ldb.d_calendar_quarter a17 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_calendar_date a14 
+on (a11.opened_on_key = a14.row_key) join ldb.d_calendar_week a15 
+on (a14.week_start_date_key = a15.row_key) join ldb.d_calendar_month a16 
+on (a15.month_start_date_key = a16.row_key) 
+join ldb.d_calendar_quarter a17 
+on (a16.quarter_start_date_key = a17.row_key) 
+union
+select 'ldb.d_calendar_year a18 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_calendar_date a14 
+on (a11.opened_on_key = a14.row_key) join ldb.d_calendar_week a15 
+on (a14.week_start_date_key = a15.row_key) join ldb.d_calendar_month a16 
+on (a15.month_start_date_key = a16.row_key) join ldb.d_calendar_quarter a17 
+on (a16.quarter_start_date_key = a17.row_key) 
+join ldb.d_calendar_year a18 
+on (a17.year_start_date_key = a18.row_key) 
+union
+select 'ldb.d_internal_organization_group_to a19 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+  join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_internal_organization_group_to a19 
+on (a12.to_assignment_group_key = a19.row_key) 
+union
+select 'ldb.d_internal_organization_group_from a110 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+  join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_internal_organization_group_from a110 
+on (a12.from_assignment_group_key = a110.row_key) 
+union
+select 'ldb.d_internal_contact_to a114 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+  join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_internal_contact_to a114 
+on (a12.to_assigned_to_key = a114.row_key) 
+union
+select 'ldb.d_internal_contact_from a115 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+  join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_internal_contact_from a115 
+on (a12.from_assigned_to_key = a115.row_key) 
+union
+select 'ldb.d_incident_priority a119 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_incident_priority a119 
+on (a11.priority_src_key = a119.row_key) 
+union
+select 'ldb.d_incident_contacttype a120 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_incident_contacttype a120 
+on (a11.reported_type_src_key = a120.row_key) 
+union
+select 'ldb.d_incident_state a122 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_incident a13 
+on (a11.incident_key = a13.row_key) 
+join ldb.d_incident_state a122 
+on (a13.state_src_key = a122.row_key) 
+union
+select 'ldb.d_incident_call_category_c a124 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_incident_call_category_c a13 
+on (a11.call_categroy_c_key= a13.row_key) 
+union
+select 'ldb.d_incident_lob_c a124 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_incident a11 
+ join          ldb.f_incident_activity            a12
+                  on          (a11.assignment_group_key = a12.current_assginment_group_key and 
+                a11.class_c_key = a12.class_c_key and 
+                a11.incident_key = a12.incident_key and 
+                a11.opened_on_key = a12.assigned_on_key and 
+                a11.priority_src_key = a12.priority_src_key and 
+                a11.reported_type_src_key = a12.reported_type_src_key)
+join ldb.d_incident_lob_c a13 
+on (a11.line_of_business_c_key= a13.row_key) 
