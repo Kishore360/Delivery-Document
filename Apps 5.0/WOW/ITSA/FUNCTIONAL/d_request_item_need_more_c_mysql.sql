f05732123548f07fccbf4d167256379ff10b8284
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-09-28 15:59:46 +0530

    DELV-8269

---
+++ Apps 5.0/WOW/ITSA/FUNCTIONAL/d_request_item_need_more_c_mysql.sql
@@ -0,0 +1,15 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+FROM (
+
+select count(1) from 				
+  wow_mdsdb.sc_req_item_final src 
+  left join wow_mdwdb.d_request_item trgt
+	ON trgt.row_id = src.sys_id
+	AND trgt.source_id = src.sourceinstance
+left JOIN wow_mdsdb.sc_req_item_ext_final sre
+ON src.sys_id = sre.record_id
+where trgt.need_more_c <>sre.need_more)a;
+
+
