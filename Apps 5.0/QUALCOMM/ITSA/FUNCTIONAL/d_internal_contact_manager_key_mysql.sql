5dc26f7f4bcc3c2d199204d14389736ce59c154a
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-10-06 15:59:58 +0530

    DELV-8182

---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_internal_contact_manager_key_mysql.sql
@@ -0,0 +1,11 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_internal_contact.manager_key' ELSE 'SUCCESS' END as Message
+ FROM qualcomm_mdsdb.sys_user_final src 
+ join  qualcomm_mdwdb.d_internal_contact trgt on  coalesce(concat('INTERNAL_CONTACT~',src.sys_id),'UNSPECIFIED')=trgt.row_id 
+ and src.sourceinstance=trgt.source_id
+        join  qualcomm_mdwdb.d_internal_contact lkp on coalesce(concat('INTERNAL_CONTACT~',src.manager),'UNSPECIFIED')=lkp.row_id
+        and src.sourceinstance=lkp.source_id
+        where COALESCE(lkp.row_key,CASE WHEN src.manager IS NULL THEN 0 else -1 end) <> (trgt.manager_key)
+		
+		
+		
