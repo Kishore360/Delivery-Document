967a1b39e36b75fc345023201245b40470010740
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-10-06 15:19:44 +0530

    DELV-8165

--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_incident_u_firt_call_resolution_mysql.sql
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_incident_u_firt_call_resolution_mysql.sql
@@ -1,8 +1,8 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_request.priority_src_key' ELSE 'SUCCESS' END as Message
- FROM  qualcomm_mdsdb.incident_final SRC 
-  JOIN qualcomm_mdwdb.d_incident TRGT 
- ON (SRC.sys_id=TRGT.row_id 
- AND SRC.sourceinstance=TRGT.source_id )
-where CASE WHEN SRC.u_firt_call_resolution='Yes' then 'Yes' else 'No' end <>(TRGT.u_firt_call_resolution )
 
+ SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_incident.u_firt_call_resolution' ELSE 'SUCCESS' END as Message 
+FROM  qualcomm_mdsdb.incident_final SRC
+JOIN qualcomm_mdwdb.d_incident TRGT  
+ON (SRC.sys_id  = TRGT.row_id  
+AND SRC.sourceinstance = TRGT.source_id )
+WHERE case when SRC.u_firt_call_resolution='Yes' then 'Y' else 'N' end <> TRGT.u_firt_call_resolution
--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_incident_u_potential_tier_resolution_mysql.sql
+++
@@ -1,8 +0,0 @@
-
- SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_incident.u_potential_tier_resolution' ELSE 'SUCCESS' END as Message 
-FROM qualcomm_mdsdb.incident_final SRC
-JOIN qualcomm_mdwdb.d_incident TRGT  
-ON (SRC.sys_id  = TRGT.row_id  
-AND SRC.sourceinstance = TRGT.source_id )
-WHERE case when SRC.u_potential_tier_resolution='Yes' then 'Y' else 'N' end <> TRGT.u_potential_tier_resolution
