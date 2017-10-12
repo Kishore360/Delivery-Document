b0e148d630be0348071f883402ac64ea8d4ff10e
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-10-12 18:37:30 +0530

    DELV-8548

--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_incident_asset_name_c_mysql.sql
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_incident_asset_name_c_mysql.sql
@@ -5,8 +5,8 @@ FROM  qualcomm_mdsdb.incident_final SRC
 JOIN qualcomm_mdwdb.f_incident TRGT  
 ON (SRC.sys_id  = TRGT.row_id  
 AND SRC.sourceinstance = TRGT.source_id )
-JOIN qualcomm_mdwdb.d_asset lkp
-on COALESCE(SRC.u_asset,'UNSPECIFIED') =lkp.row_id
-WHERE COALESCE(lkp.row_key,case when SRC.u_asset is null then 0 else -1 end) <> TRGT.asset_name_c
+left JOIN qualcomm_mdsdb.alm_asset_final lkp
+ON  inf.u_asset = alm.sys_id 
+WHERE COALESCE(alm.display_name, 'UNSPECIFIED') <> TRGT.asset_name_c
 
 
