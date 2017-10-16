c4e9850a52aec552a2f1f911014e47084e5409da
DESKTOP-PLTURRV\Shubha <shubha.rao@numerify.com>
2017-10-16 16:52:13 +0530

    Updating the cust queries

---
+++ Apps 5.0/MERCURYINS/ITSA/FUNCTIONAL/D_Change_request_not_on_time_flag_mysql.sql
@@ -0,0 +1,6 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_agent_call_summary_aux_dept_loc_cc' ELSE 'SUCCESS' END as Message
+from  mercuryins_mdwdb.d_change_request d
+JOIN mercuryins_mdsdb.change_request_final f
+on d.row_id =f.sys_id and d.source_id=f.sourceinstance
+where  CASE WHEN (f.CLOSED_AT > f.END_DATE) THEN 'Y' ELSE 'N' END <>  d.not_on_time_flag
