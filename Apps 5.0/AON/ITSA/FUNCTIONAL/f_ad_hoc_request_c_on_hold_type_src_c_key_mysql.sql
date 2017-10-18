337d9a15a8112526a49e93d765c8a55140f43c26
vijay rao <vijay.rao@numerify.com>
2017-10-18 11:44:28 +0530

    AON cust queries changes

--- Apps 5.0/AON/ITSA/FUNCTIONAL/f_ad_hoc_request_c_on_hold_type_src_c_key_mysql.sql
+++ Apps 5.0/AON/ITSA/FUNCTIONAL/f_ad_hoc_request_c_on_hold_type_src_c_key_mysql.sql
@@ -5,5 +5,5 @@ FROM aon_mdsdb.u_ad_hoc_request_final  SRC
 JOIN aon_mdwdb.f_ad_hoc_request_c TRGT 
 ON (SRC.sys_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id ) 
  LEFT JOIN aon_mdwdb.d_lov LKP 
- ON ( concat( '~U_AD_HOC_REQUEST~~~' ,upper( SRC.u_on_hold_type)) = LKP.row_id AND SRC.sourceinstance = LKP.source_id )
+ ON ( concat( 'U_ON_HOLD_TYPE_C~U_AD_HOC_REQUEST~~~' ,upper( SRC.u_on_hold_type)) = LKP.row_id AND SRC.sourceinstance = LKP.source_id )
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_on_hold_type IS NULL THEN 0 else -1 end)<> (TRGT.on_hold_type_src_c_key) 
--- Apps 5.0/AON/ITSA/FUNCTIONAL/f_ad_hoc_request_c_symptom_src_c_key_mysql.sql
+++ Apps 5.0/AON/ITSA/FUNCTIONAL/f_ad_hoc_request_c_symptom_src_c_key_mysql.sql
@@ -5,5 +5,5 @@ FROM aon_mdsdb.u_ad_hoc_request_final  SRC
 JOIN aon_mdwdb.f_ad_hoc_request_c TRGT 
 ON (SRC.sys_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id ) 
  LEFT JOIN aon_mdwdb.d_lov LKP 
- ON ( concat( 'SYMPTON~U_AD_HOC_REQUEST~~~' ,upper( SRC.u_symptom)) = LKP.row_id AND SRC.sourceinstance = LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_symptom IS NULL THEN 0 else -1 end)<> (TRGT.symptom_src_c_key) 
+ ON ( concat( 'SYMPTOM_C~INCIDENT~~~',upper( SRC.u_symptom)) = LKP.row_id AND SRC.sourceinstance = LKP.source_id )
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_symptom IS NULL THEN 0 else -1 end)<> (TRGT.symptom_src_c_key) ;
