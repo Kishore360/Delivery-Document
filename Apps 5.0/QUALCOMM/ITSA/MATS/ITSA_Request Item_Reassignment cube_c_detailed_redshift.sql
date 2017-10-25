b4cdae3ccf92b5f15f65ba8d3ef16cc44f1b49a6
Kishore Mantha <kishore.mantha@numerify.com>
2017-10-25 09:43:40 +0530

    Added the RITM_Reassignment_c cube queries to 5.0_Qual comm

---
+++ Apps 5.0/QUALCOMM/ITSA/MATS/ITSA_Request Item_Reassignment cube_c_detailed_redshift.sql
@@ -0,0 +1,67 @@
+
+select 'ldb.f_request_item_activity_c a11 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+union
+select 'ldb.d_internal_contact_assigned_to a12 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_internal_contact_assigned_to a12 
+on (a11.current_assigned_to_key = a12.row_key) 
+union
+select 'ldb.d_internal_organization_group a13 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_internal_organization_group a13 
+on (a11.current_assignment_group_key = a13.row_key) 
+union
+select 'ldb.d_calendar_date a14 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_calendar_date a14 
+on (a11.changed_on_key = a14.row_key) 
+union
+select 'ldb.d_internal_contact_to a15 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_internal_contact_to a15 
+on (a11.to_assigned_to_key = a15.row_key) 
+union
+select 'ldb.d_internal_organization_group_to a16 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_internal_organization_group_to a16 
+on (a11.to_assignment_group_key = a16.row_key) 
+union
+select 'ldb.d_internal_contact_from a17 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_internal_contact_from a17 
+on (a11.from_assigned_to_key = a17.row_key) 
+union
+select 'ldb.d_internal_organization_group_from a18 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_internal_organization_group_from a18 
+on (a11.from_assignment_group_key = a18.row_key) 
+union
+select 'ldb.d_request_item a19 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_request_item a19 
+on (a11.request_item_key = a19.row_key) 
+union
+select 'ldb.d_sc_req_item_priority a110 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_sc_req_item_priority a110 
+on (a11.priority_src_key = a110.row_key) 
+union
+select 'ldb.d_request_item_state a111 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_request_item_state a111 
+on (a11.current_state_src_key = a111.row_key) 
+union
+select 'ldb.d_calendar_month a112 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_calendar_date a14 
+on (a11.changed_on_key = a14.row_key) 
+join ldb.d_calendar_month a112 
+on (a14.month_start_date_key = a112.row_key) 
+union
+select 'ldb.o_data_freshness a113 ' as Table_name, count(1) Row_Count
+from ldb.f_request_item_activity_c a11 
+join ldb.d_calendar_date a14 
+on (a11.changed_on_key = a14.row_key) 
+join ldb.o_data_freshness a113 
+on (a14.source_id = a113.source_id) 
