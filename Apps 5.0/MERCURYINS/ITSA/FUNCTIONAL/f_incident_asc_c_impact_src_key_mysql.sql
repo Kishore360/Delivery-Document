85e26e9de87221dde6046110c959d01831e7def8
DESKTOP-PLTURRV\Shubha <shubha.rao@numerify.com>
2017-10-28 22:40:57 +0530

    Updating the quesry

--- Apps 5.0/MERCURYINS/ITSA/FUNCTIONAL/f_incident_asc_c_impact_src_key_mysql.sql
+++ Apps 5.0/MERCURYINS/ITSA/FUNCTIONAL/f_incident_asc_c_impact_src_key_mysql.sql
@@ -1,5 +1,3 @@
-
-
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident_asc_c.impact_src_key' ELSE 'SUCCESS' END as Message
  FROM mercuryins_mdsdb.u_asc_ticket_final SRC 
@@ -7,6 +5,6 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
 LEFT JOIN mercuryins_mdwdb.d_lov LKP 
- ON ( concat('IMPACT~TASK~~~',upper(impact))= LKP.src_rowid 
+ ON ( concat('IMPACT~ASC_INCIDENT~~~',upper(impact))= LKP.src_rowid 
 AND SRC.sourceinstance= LKP.source_id )
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.impact IS NULL THEN 0 else -1 end)<> (TRGT.asc_incident_impact_c_key)
