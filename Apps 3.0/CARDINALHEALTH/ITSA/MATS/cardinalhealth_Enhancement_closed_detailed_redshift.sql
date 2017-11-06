af3e4857f3cf8bfb62b58f56f750d09d70d5712a
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-11-06 17:25:04 +0530

    DELV-8849

--- Apps 3.0/CARDINALHEALTH/ITSA/MATS/cardinalhealth_Enhancement_closed_detailed_redshift.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/MATS/cardinalhealth_Enhancement_closed_detailed_redshift.sql
@@ -48,126 +48,181 @@ from ldb.f_enhancement_rm_closed_c a11
 join ldb.d_enhancement_rm_c a110 
 on (a11.enhancement_c_key = a110.row_key) 
 union
-select 'ldb.d_calendar_greg_fiscal_c a111 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_enhancement_opened_by_c a111 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a111 
-on (a11.closed_on_key = a111.greogrian_calendar_key) 
+join ldb.d_enhancement_rm_c a110 
+on (a11.enhancement_c_key = a110.row_key) 
+join ldb.d_internal_contact_enhancement_opened_by_c a111 
+on (a110.opened_by_c_key = a111.row_key) 
 union
-select 'ldb.d_calendar_date_fiscal a112 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_calendar_greg_fiscal_c a112 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a111 
-on (a11.closed_on_key = a111.greogrian_calendar_key) 
-join ldb.d_calendar_date_fiscal a112 
-on (a111.fiscal_key = a112.row_key) 
+join ldb.d_calendar_greg_fiscal_c a112 
+on (a11.closed_on_key = a112.greogrian_calendar_key) 
 union
-select 'ldb.d_rm_change_scope_feature_c a113 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_calendar_date_fiscal a113 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_rm_change_scope_feature_c a113 
-on (a11.feature_change_scope_c_key = a113.row_key) 
+join ldb.d_calendar_greg_fiscal_c a112 
+on (a11.closed_on_key = a112.greogrian_calendar_key) 
+join ldb.d_calendar_date_fiscal a113 
+on (a112.fiscal_key = a113.row_key) 
 union
-select 'ldb.d_configuration_item a114 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_configuration_item a114 
-on (a11.configuration_item_key = a114.row_key) 
-union
-select 'ldb.d_internal_contact_mdm a115 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_mdm a114 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
 join ldb.d_internal_contact a12 
 on (a11.closed_by_key = a12.row_key) 
-join ldb.d_internal_contact_mdm a115 
-on (a12.row_current_key = a115.row_current_key) 
+join ldb.d_internal_contact_mdm a114 
+on (a12.row_current_key = a114.row_current_key) 
 union
-select 'ldb.d_request_item a116 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_rm_change_scope_feature_c a115 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_request_item a116 
-on (a11.request_item_key = a116.row_key) 
+join ldb.d_rm_change_scope_feature_c a115 
+on (a11.feature_change_scope_c_key = a115.row_key) 
 union
-select 'ldb.d_internal_contact_requested_for a117 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_configuration_item a116 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_internal_contact_requested_for a117 
-on (a11.requestor_c_key = a117.row_key) 
+join ldb.d_configuration_item a116 
+on (a11.configuration_item_key = a116.row_key) 
 union
-select 'ldb.d_rm_status_c a118 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_request_item a117 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_rm_status_c a118 
-on (a11.status_rm_src_c_key = a118.row_key) 
+join ldb.d_request_item a117 
+on (a11.request_item_key = a117.row_key) 
 union
-select 'ldb.d_internal_contact_task_closed_by a119 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_requested_for a118 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_internal_contact_task_closed_by a119 
-on (a11.closed_by_key = a119.row_key) 
+join ldb.d_internal_contact_requested_for a118 
+on (a11.requestor_c_key = a118.row_key) 
 union
-select 'ldb.d_task_impact a120 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_rm_status_c a119 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_task_impact a120 
-on (a11.impact_src_key = a120.row_key) 
+join ldb.d_rm_status_c a119 
+on (a11.status_rm_src_c_key = a119.row_key) 
 union
-select 'ldb.d_location a121 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_task_closed_by a120 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_location a121 
-on (a11.location_key = a121.row_key) 
+join ldb.d_internal_contact_task_closed_by a120 
+on (a11.closed_by_key = a120.row_key) 
 union
-select 'ldb.d_task_priority a122 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_task_impact a121 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_task_priority a122 
-on (a11.priority_src_key = a122.row_key) 
+join ldb.d_task_impact a121 
+on (a11.impact_src_key = a121.row_key) 
 union
-select 'ldb.d_task_contacttype a123 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_location a122 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_task_contacttype a123 
-on (a11.reported_type_src_key = a123.row_key) 
+join ldb.d_location a122 
+on (a11.location_key = a122.row_key) 
 union
-select 'ldb.d_task_urgency a124 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_task_priority a123 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_task_urgency a124 
-on (a11.urgency_src_key = a124.row_key) 
+join ldb.d_task_priority a123 
+on (a11.priority_src_key = a123.row_key) 
 union
-select 'ldb.d_rm_lov_type_of_request_c a125 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_task_contacttype a124 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_rm_lov_type_of_request_c a125 
-on (a11.type_of_request_c_key = a125.row_key) 
+join ldb.d_task_contacttype a124 
+on (a11.reported_type_src_key = a124.row_key) 
 union
-select 'ldb.d_internal_organization_legalentity_company_c a126 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_task_urgency a125 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_enhancement_rm_closed_c a11 
+join ldb.d_task_urgency a125 
+on (a11.urgency_src_key = a125.row_key) 
+union
+select 'ldb.d_rm_lov_type_of_request_c a126 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_enhancement_rm_closed_c a11 
+join ldb.d_rm_lov_type_of_request_c a126 
+on (a11.type_of_request_c_key = a126.row_key) 
+union
+select 'ldb.d_internal_organization_legalentity_company_c a127 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
 join ldb.d_internal_contact_assigned_to a15 
 on (a11.assigned_to_key = a15.row_key) 
-join ldb.d_internal_organization_legalentity_company_c a126 
-on (a15.user_company_c1_key = a126.row_key) 
+join ldb.d_internal_organization_legalentity_company_c a127 
+on (a15.user_company_c1_key = a127.row_key) 
 union
-select 'ldb.d_assignment_group_level0_manager_c a127 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_assignment_group_level0_manager_c a128 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
 join ldb.d_assignment_group_level0_manager_c_relation a18 
 on (a11.assignment_group_key = a18.level0_assigment_group_key) 
-join ldb.d_assignment_group_level0_manager_c a127 
-on (a18.level0_manager_c_key = a127.level0_manager_c_key) 
+join ldb.d_assignment_group_level0_manager_c a128 
+on (a18.level0_manager_c_key = a128.level0_manager_c_key) 
 union
-select 'ldb.d_calendar_fiscal_period a128 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_director a129 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a111 
-on (a11.closed_on_key = a111.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a112 
-on (a111.fiscal_key = a112.row_key) 
-join ldb.d_calendar_fiscal_period a128 
-on (a112.period_start_date_key = a128.row_key) 
+join ldb.d_internal_contact a12 
+on (a11.closed_by_key = a12.row_key) join ldb.d_internal_contact_mdm a114 
+on (a12.row_current_key = a114.row_current_key) 
+join ldb.d_internal_contact_director a129 
+on (a114.director_c_key = a129.row_key) 
 union
-select 'ldb.d_calendar_month a129 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_manager a130 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_enhancement_rm_closed_c a11 
+join ldb.d_internal_contact a12 
+on (a11.closed_by_key = a12.row_key) join ldb.d_internal_contact_mdm a114 
+on (a12.row_current_key = a114.row_current_key) 
+join ldb.d_internal_contact_manager a130 
+on (a114.manager_c_key = a130.row_key) 
+union
+select 'ldb.d_internal_contact_vp a131 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_enhancement_rm_closed_c a11 
+join ldb.d_internal_contact a12 
+on (a11.closed_by_key = a12.row_key) join ldb.d_internal_contact_mdm a114 
+on (a12.row_current_key = a114.row_current_key) 
+join ldb.d_internal_contact_vp a131 
+on (a114.vp_c_key = a131.row_key) 
+union
+select 'ldb.d_internal_contact_enhancement_director_c a132 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_enhancement_rm_closed_c a11 
+join ldb.d_enhancement_rm_c a110 
+on (a11.enhancement_c_key = a110.row_key) join ldb.d_internal_contact_enhancement_opened_by_c a111 
+on (a110.opened_by_c_key = a111.row_key) 
+join ldb.d_internal_contact_enhancement_director_c a132 
+on (a111.director_c_key = a132.row_key) 
+union
+select 'ldb.d_internal_contact_enhancement_manager_c a133 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_enhancement_rm_closed_c a11 
+join ldb.d_enhancement_rm_c a110 
+on (a11.enhancement_c_key = a110.row_key) join ldb.d_internal_contact_enhancement_opened_by_c a111 
+on (a110.opened_by_c_key = a111.row_key) 
+join ldb.d_internal_contact_enhancement_manager_c a133 
+on (a111.manager_c_key = a133.row_key) 
+union
+select 'ldb.d_internal_contact_enhancement_vp_c a134 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_enhancement_rm_closed_c a11 
+join ldb.d_enhancement_rm_c a110 
+on (a11.enhancement_c_key = a110.row_key) join ldb.d_internal_contact_enhancement_opened_by_c a111 
+on (a110.opened_by_c_key = a111.row_key) 
+join ldb.d_internal_contact_enhancement_vp_c a134 
+on (a111.vp_c_key = a134.row_key) 
+union
+select 'ldb.d_calendar_fiscal_period a135 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_enhancement_rm_closed_c a11 
+join ldb.d_calendar_greg_fiscal_c a112 
+on (a11.closed_on_key = a112.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a113 
+on (a112.fiscal_key = a113.row_key) 
+join ldb.d_calendar_fiscal_period a135 
+on (a113.period_start_date_key = a135.row_key) 
+union
+select 'ldb.d_calendar_month a136 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
 join ldb.d_calendar_date a19 
 on (a11.closed_on_key = a19.row_key) 
-join ldb.d_calendar_month a129 
-on (a19.month_start_date_key = a129.row_key) 
+join ldb.d_calendar_month a136 
+on (a19.month_start_date_key = a136.row_key) 
 union
-select 'ldb.d_calendar_fiscal_quarter a130 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_calendar_fiscal_quarter a137 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a111 
-on (a11.closed_on_key = a111.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a112 
-on (a111.fiscal_key = a112.row_key) 
-join ldb.d_calendar_fiscal_quarter a130 
-on (a112.quarter_start_date_key = a130.row_key) 
+join ldb.d_calendar_greg_fiscal_c a112 
+on (a11.closed_on_key = a112.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a113 
+on (a112.fiscal_key = a113.row_key) 
+join ldb.d_calendar_fiscal_quarter a137 
+on (a113.quarter_start_date_key = a137.row_key) 
 union
-select 'ldb.d_calendar_fiscal_year a131 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_calendar_fiscal_year a138 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_enhancement_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a111 
-on (a11.closed_on_key = a111.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a112 
-on (a111.fiscal_key = a112.row_key) 
-join ldb.d_calendar_fiscal_year a131 
-on (a112.year_start_date_key = a131.row_key) 
+join ldb.d_calendar_greg_fiscal_c a112 
+on (a11.closed_on_key = a112.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a113 
+on (a112.fiscal_key = a113.row_key) 
+join ldb.d_calendar_fiscal_year a138 
+on (a113.year_start_date_key = a138.row_key) 
--- Apps 3.0/CARDINALHEALTH/ITSA/MATS/cardinalhealth_defect_closed_detailed_redshift.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/MATS/cardinalhealth_defect_closed_detailed_redshift.sql
@@ -1,3 +1,4 @@
+
 select 'ldb.f_defect_rm_closed_c a11 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
 union
@@ -43,145 +44,181 @@ from ldb.f_defect_rm_closed_c a11
 join ldb.d_defect_rm_c a19 
 on (a11.defect_c_key = a19.row_key) 
 union
-select 'ldb.d_calendar_greg_fiscal_c a110 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_defect_opened_by_c a110 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a110 
-on (a11.closed_on_key = a110.greogrian_calendar_key) 
+join ldb.d_defect_rm_c a19 
+on (a11.defect_c_key = a19.row_key) 
+join ldb.d_internal_contact_defect_opened_by_c a110 
+on (a19.opened_by_c_key = a110.row_key) 
 union
-select 'ldb.d_calendar_date_fiscal a111 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_calendar_greg_fiscal_c a111 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a110 
-on (a11.closed_on_key = a110.greogrian_calendar_key) 
-join ldb.d_calendar_date_fiscal a111 
-on (a110.fiscal_key = a111.row_key) 
+join ldb.d_calendar_greg_fiscal_c a111 
+on (a11.closed_on_key = a111.greogrian_calendar_key) 
 union
-select 'ldb.d_internal_contact_mdm a112 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_calendar_date_fiscal a112 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_defect_rm_closed_c a11 
+join ldb.d_calendar_greg_fiscal_c a111 
+on (a11.closed_on_key = a111.greogrian_calendar_key) 
+join ldb.d_calendar_date_fiscal a112 
+on (a111.fiscal_key = a112.row_key) 
+union
+select 'ldb.d_internal_contact_mdm a113 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
 join ldb.d_internal_contact a13 
 on (a11.closed_by_key = a13.row_key) 
-join ldb.d_internal_contact_mdm a112 
-on (a13.row_current_key = a112.row_current_key) 
+join ldb.d_internal_contact_mdm a113 
+on (a13.row_current_key = a113.row_current_key) 
 union
-select 'ldb.d_rm_change_scope_feature_c a113 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_rm_change_scope_feature_c a114 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_rm_change_scope_feature_c a113 
-on (a11.feature_change_scope_c_key = a113.row_key) 
+join ldb.d_rm_change_scope_feature_c a114 
+on (a11.feature_change_scope_c_key = a114.row_key) 
 union
-select 'ldb.d_configuration_item a114 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_configuration_item a115 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_configuration_item a114 
-on (a11.configuration_item_key = a114.row_key) 
+join ldb.d_configuration_item a115 
+on (a11.configuration_item_key = a115.row_key) 
 union
-select 'ldb.d_internal_contact_requested_for a115 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_mdm a116 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_internal_contact_requested_for a115 
-on (a11.requestor_c_key = a115.row_key) 
+join ldb.d_internal_contact_mdm a116 
+on (a11.opened_by_c_key = a116.row_current_key) 
 union
-select 'ldb.d_rm_status_c a116 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_requested_for a117 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_rm_status_c a116 
-on (a11.status_rm_src_c_key = a116.row_key) 
+join ldb.d_internal_contact_requested_for a117 
+on (a11.requestor_c_key = a117.row_key) 
 union
-select 'ldb.d_internal_contact_task_closed_by a117 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_rm_status_c a118 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_internal_contact_task_closed_by a117 
-on (a11.closed_by_key = a117.row_key) 
+join ldb.d_rm_status_c a118 
+on (a11.status_rm_src_c_key = a118.row_key) 
 union
-select 'ldb.d_task_impact a118 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_task_closed_by a119 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_task_impact a118 
-on (a11.impact_src_key = a118.row_key) 
+join ldb.d_internal_contact_task_closed_by a119 
+on (a11.closed_by_key = a119.row_key) 
 union
-select 'ldb.d_location a119 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_task_impact a120 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_location a119 
-on (a11.location_key = a119.row_key) 
+join ldb.d_task_impact a120 
+on (a11.impact_src_key = a120.row_key) 
 union
-select 'ldb.d_task_priority a120 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_location a121 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_task_priority a120 
-on (a11.priority_src_key = a120.row_key) 
+join ldb.d_location a121 
+on (a11.location_key = a121.row_key) 
 union
-select 'ldb.d_task_contacttype a121 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_task_priority a122 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_task_contacttype a121 
-on (a11.reported_type_src_key = a121.row_key) 
+join ldb.d_task_priority a122 
+on (a11.priority_src_key = a122.row_key) 
 union
-select 'ldb.d_task_urgency a122 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_task_contacttype a123 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_task_urgency a122 
-on (a11.urgency_src_key = a122.row_key) 
+join ldb.d_task_contacttype a123 
+on (a11.reported_type_src_key = a123.row_key) 
 union
-select 'ldb.d_rm_lov_type_of_request_c a123 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_task_urgency a124 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_rm_lov_type_of_request_c a123 
-on (a11.type_of_request_c_key = a123.row_key) 
+join ldb.d_task_urgency a124 
+on (a11.urgency_src_key = a124.row_key) 
 union
-select 'ldb.d_internal_organization_legalentity_company_c a124 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_rm_lov_type_of_request_c a125 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_defect_rm_closed_c a11 
+join ldb.d_rm_lov_type_of_request_c a125 
+on (a11.type_of_request_c_key = a125.row_key) 
+union
+select 'ldb.d_internal_organization_legalentity_company_c a126 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
 join ldb.d_internal_contact_assigned_to a14 
 on (a11.assigned_to_key = a14.row_key) 
-join ldb.d_internal_organization_legalentity_company_c a124 
-on (a14.user_company_c1_key = a124.row_key) 
+join ldb.d_internal_organization_legalentity_company_c a126 
+on (a14.user_company_c1_key = a126.row_key) 
 union
-select 'ldb.d_assignment_group_level0_manager_c a125 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_assignment_group_level0_manager_c a127 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
 join ldb.d_assignment_group_level0_manager_c_relation a17 
 on (a11.assignment_group_key = a17.level0_assigment_group_key) 
-join ldb.d_assignment_group_level0_manager_c a125 
-on (a17.level0_manager_c_key = a125.level0_manager_c_key) 
+join ldb.d_assignment_group_level0_manager_c a127 
+on (a17.level0_manager_c_key = a127.level0_manager_c_key) 
 union
-select 'ldb.d_internal_contact_director a126 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_director a128 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
 join ldb.d_internal_contact a13 
-on (a11.closed_by_key = a13.row_key) join ldb.d_internal_contact_mdm a112 
-on (a13.row_current_key = a112.row_current_key) 
-join ldb.d_internal_contact_director a126 
-on (a112.director_c_key = a126.row_key) 
+on (a11.closed_by_key = a13.row_key) join ldb.d_internal_contact_mdm a113 
+on (a13.row_current_key = a113.row_current_key) 
+join ldb.d_internal_contact_director a128 
+on (a113.director_c_key = a128.row_key) 
 union
-select 'ldb.d_internal_contact_manager a127 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_manager a129 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
 join ldb.d_internal_contact a13 
-on (a11.closed_by_key = a13.row_key) join ldb.d_internal_contact_mdm a112 
-on (a13.row_current_key = a112.row_current_key) 
-join ldb.d_internal_contact_manager a127 
-on (a112.manager_c_key = a127.row_key) 
+on (a11.closed_by_key = a13.row_key) join ldb.d_internal_contact_mdm a113 
+on (a13.row_current_key = a113.row_current_key) 
+join ldb.d_internal_contact_manager a129 
+on (a113.manager_c_key = a129.row_key) 
 union
-select 'ldb.d_internal_contact_vp a128 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_vp a130 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
 join ldb.d_internal_contact a13 
-on (a11.closed_by_key = a13.row_key) join ldb.d_internal_contact_mdm a112 
-on (a13.row_current_key = a112.row_current_key) 
-join ldb.d_internal_contact_vp a128 
-on (a112.vp_c_key = a128.row_key) 
+on (a11.closed_by_key = a13.row_key) join ldb.d_internal_contact_mdm a113 
+on (a13.row_current_key = a113.row_current_key) 
+join ldb.d_internal_contact_vp a130 
+on (a113.vp_c_key = a130.row_key) 
 union
-select 'ldb.d_calendar_fiscal_period a129 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_defect_director_c a131 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a110 
-on (a11.closed_on_key = a110.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a111 
-on (a110.fiscal_key = a111.row_key) 
-join ldb.d_calendar_fiscal_period a129 
-on (a111.period_start_date_key = a129.row_key) 
+join ldb.d_defect_rm_c a19 
+on (a11.defect_c_key = a19.row_key) join ldb.d_internal_contact_defect_opened_by_c a110 
+on (a19.opened_by_c_key = a110.row_key) 
+join ldb.d_internal_contact_defect_director_c a131 
+on (a110.director_c_key = a131.row_key) 
 union
-select 'ldb.d_calendar_month a130 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_internal_contact_defect_manager_c a132 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_defect_rm_closed_c a11 
+join ldb.d_defect_rm_c a19 
+on (a11.defect_c_key = a19.row_key) join ldb.d_internal_contact_defect_opened_by_c a110 
+on (a19.opened_by_c_key = a110.row_key) 
+join ldb.d_internal_contact_defect_manager_c a132 
+on (a110.manager_c_key = a132.row_key) 
+union
+select 'ldb.d_internal_contact_defect_vp_c a133 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_defect_rm_closed_c a11 
+join ldb.d_defect_rm_c a19 
+on (a11.defect_c_key = a19.row_key) join ldb.d_internal_contact_defect_opened_by_c a110 
+on (a19.opened_by_c_key = a110.row_key) 
+join ldb.d_internal_contact_defect_vp_c a133 
+on (a110.vp_c_key = a133.row_key) 
+union
+select 'ldb.d_calendar_fiscal_period a134 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_defect_rm_closed_c a11 
+join ldb.d_calendar_greg_fiscal_c a111 
+on (a11.closed_on_key = a111.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a112 
+on (a111.fiscal_key = a112.row_key) 
+join ldb.d_calendar_fiscal_period a134 
+on (a112.period_start_date_key = a134.row_key) 
+union
+select 'ldb.d_calendar_month a135 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
 join ldb.d_calendar_date a18 
 on (a11.closed_on_key = a18.row_key) 
-join ldb.d_calendar_month a130 
-on (a18.month_start_date_key = a130.row_key) 
+join ldb.d_calendar_month a135 
+on (a18.month_start_date_key = a135.row_key) 
 union
-select 'ldb.d_calendar_fiscal_quarter a131 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_calendar_fiscal_quarter a136 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a110 
-on (a11.closed_on_key = a110.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a111 
-on (a110.fiscal_key = a111.row_key) 
-join ldb.d_calendar_fiscal_quarter a131 
-on (a111.quarter_start_date_key = a131.row_key) 
+join ldb.d_calendar_greg_fiscal_c a111 
+on (a11.closed_on_key = a111.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a112 
+on (a111.fiscal_key = a112.row_key) 
+join ldb.d_calendar_fiscal_quarter a136 
+on (a112.quarter_start_date_key = a136.row_key) 
 union
-select 'ldb.d_calendar_fiscal_year a132 ' as Table_name, count(a11.row_key) Row_Count
+select 'ldb.d_calendar_fiscal_year a137 ' as Table_name, count(a11.row_key) Row_Count
 from ldb.f_defect_rm_closed_c a11 
-join ldb.d_calendar_greg_fiscal_c a110 
-on (a11.closed_on_key = a110.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a111 
-on (a110.fiscal_key = a111.row_key) 
-join ldb.d_calendar_fiscal_year a132 
-on (a111.year_start_date_key = a132.row_key) 
+join ldb.d_calendar_greg_fiscal_c a111 
+on (a11.closed_on_key = a111.greogrian_calendar_key) join ldb.d_calendar_date_fiscal a112 
+on (a111.fiscal_key = a112.row_key) 
+join ldb.d_calendar_fiscal_year a137 
+on (a112.year_start_date_key = a137.row_key) 
