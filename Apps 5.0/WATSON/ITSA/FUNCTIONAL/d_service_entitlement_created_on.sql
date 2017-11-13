942b7f83ce40258baa8543482743309f2176e32c
vijay rao <vijay.rao@numerify.com>
2017-11-13 16:53:34 +0530

    Updating ESA Customer service queries for Watson

---
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/d_service_entitlement_created_on.sql
@@ -0,0 +1,8 @@
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_service_entitlement.created_on' ELSE 'SUCCESS' END as Message
+  FROM watson_mdsdb.service_entitlement_final SRC 
+LEFT JOIN watson_mdwdb.d_service_entitlement TRGT 
+	ON (SRC.sys_id =TRGT.row_id 
+	AND SRC.sourceinstance =TRGT.source_id )
+WHERE convert_tz(SRC.sys_created_on,'GMT','America/New_York')<> TRGT.created_on 
---
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/d_service_entitlement_entitlement_name.sql
@@ -0,0 +1,9 @@
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_service_entitlement.entitlement_name' ELSE 'SUCCESS' END as Message
+FROM watson_mdsdb.service_entitlement_final SRC 
+LEFT JOIN watson_mdwdb.d_service_entitlement TRGT 
+	ON (SRC.sys_id =TRGT.row_id 
+	AND SRC.sourceinstance =TRGT.source_id )
+WHERE TRGT.row_key not in (0, -1) and 
+COALESCE(SRC.entitlement_name,'')<> COALESCE(TRGT.entitlement_name,'')
