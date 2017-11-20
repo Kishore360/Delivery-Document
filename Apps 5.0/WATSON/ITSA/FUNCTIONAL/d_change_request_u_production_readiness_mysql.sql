c71e14531c7b492508d674465ca8d0119d37ef96
vijay rao <vijay.rao@numerify.com>
2017-11-20 11:32:00 +0530

    Watson Customization test queries

--- Apps 5.0/WATSON/ITSA/FUNCTIONAL/d_change_request_u_production_readiness_mysql.sql
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/d_change_request_u_production_readiness_mysql.sql
@@ -7,5 +7,5 @@ count(*) as cnt
 from  watson_mdsdb.change_request_final a11
  join watson_mdwdb.d_change_request  a12
 on a12.row_id= a11.sys_id and a11.sourceinstance=a12.source_id
-where case when (a11.u_production_readiness  = 1) then 'Y' else 'N' end <>a11.u_production_readiness_c)a
+where case when (a12.u_production_readiness_c  = 1) then 'Y' else 'N' end <>a11.u_production_readiness)a
 
