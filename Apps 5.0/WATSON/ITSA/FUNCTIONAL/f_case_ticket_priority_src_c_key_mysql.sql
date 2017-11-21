59d3cad5cfedd4e0adfd5a58b922b5c52e1e7702
vijay rao <vijay.rao@numerify.com>
2017-11-21 09:03:43 +0530

    Watson customizations

---
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/f_case_ticket_priority_src_c_key_mysql.sql
@@ -0,0 +1,12 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_case.ticket_priority_src_c_key' ELSE 'SUCCESS' END as Message
+FROM watson_mdsdb.sn_customerservice_case_final SRC 
+LEFT JOIN watson_mdwdb.f_case TRGT 
+	ON (SRC.sys_id =TRGT.row_id 
+	AND SRC.sourceinstance =TRGT.source_id )
+LEFT JOIN watson_mdwdb.d_lov LKP 
+	ON ( coalesce(concat('TICKET_PRIORITY_C~CASE~~~',SRC.u_ticket_priority),'UNSPECIFIED') = LKP.row_id
+	AND SRC.sourceinstance=LKP.source_id)
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_ticket_priority IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.ticket_priority_src_c_key ,'')
+
+
