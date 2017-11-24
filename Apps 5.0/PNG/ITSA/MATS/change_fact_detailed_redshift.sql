23f1dbbeac866d617ad4b885c8eec178e249e33c
prabhuram <prabhuram.sridharan@numerify.com>
2017-11-24 08:31:33 +0530

    Committing PNG PROBLEM AND CHANGE CUBE QUERIES

--- Apps 5.0/PNG/ITSA/MATS/change_fact_detailed_redshift.sql
+++ Apps 5.0/PNG/ITSA/MATS/change_fact_detailed_redshift.sql
@@ -20,3 +20,8 @@ on (a11.change_manager_c_key = a12.row_key)
 union
 select'ldb.f_change_request' as Table_Name, count(a11.row_key) Row_Count
  from  ldb.f_change_request       a11
+ union
+ select'ldb.d_configuration_item_change_request_service_offering_c' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_change_request       a11 
+join ldb.d_configuration_item_change_request_service_offering_c       a12
+on (a11.service_offering_c_key = a12.row_key)
