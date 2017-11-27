8727a7ddf1ef531772bcb824989a98262f04ad17
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-11-27 18:29:29 +0530

    updations

--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_enhancement_rm_opened_by_c_key_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_enhancement_rm_opened_by_c_key_mysql.sql
@@ -2,8 +2,8 @@ SELECT
 CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
 FROM (SELECT count(1) as CNT
- FROM cardinalhealth_mdsdb.rm_enhancement_delta SRC 
- LEFT JOIN cardinalhealth_mdwdb.d_internal_organization LKP
+ FROM cardinalhealth_mdsdb.rm_enhancement_final SRC 
+ LEFT JOIN cardinalhealth_mdwdb.d_internal_contact LKP
  on LKP.row_id = COALESCE(concat('INTERNAL_CONTACT~',SRC.opened_by),'UNSPECIFIED') and LKP.source_id  = SRC.sourceinstance
  LEFT JOIN cardinalhealth_mdwdb.f_enhancement_rm TRGT 
  ON (SRC.sys_id =TRGT.row_id  
