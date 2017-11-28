b42eefa5ce339534a51e52fe2f1ff705abd3d634
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2017-11-28 22:00:31 +0530

    DELV-9265

---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_email_c_incident_key_mysql.sql
@@ -0,0 +1,15 @@
+
+			SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_email_c.incident_key
+
+' ELSE 'SUCCESS' END as Message
+FROM qualcomm_mdsdb.sys_email_final SRC 
+LEFT JOIN qualcomm_mdwdb.f_email_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+   join qualcomm_mdwdb.d_incident LKP 
+	on                SRC.instance=LKP.row_id and SRC.sourceinstance=LKP.source_id and SRC.target_table = 'incident'
+
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.instance IS NULL THEN 0 else -1 end) <> (TRGT.incident_key
+);
+
---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_email_c_request_item_key_mysql.sql
@@ -0,0 +1,14 @@
+
+			SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_email_c.request_item_key
+
+' ELSE 'SUCCESS' END as Message
+FROM qualcomm_mdsdb.sys_email_final SRC 
+LEFT JOIN qualcomm_mdwdb.f_email_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+   join qualcomm_mdwdb.d_request_item LKP 
+	on                SRC.instance=LKP.row_id and SRC.sourceinstance=LKP.source_id and SRC.target_table = 'sc_req_item'
+
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.instance IS NULL THEN 0 else -1 end) <> (TRGT.request_item_key
+);
