f72ee5d81221828b85344900cd11ced4d071f546
Kishore Mantha <kishore.mantha@numerify.com>
2017-11-28 15:47:31 +0530

    Updated the NBCU_QA code to reduce the run time.

--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_task_survey_closure_code_src_c_key_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_task_survey_closure_code_src_c_key_mysql.sql
@@ -1,5 +1,7 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.opened_on_key' ELSE 'SUCCESS' END as Message
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT > 0 THEN 'MDS to DWH data validation failed for f_incident.opened_on_key' ELSE 'SUCCESS' END as Message
+FROM (SELECT Count(1) as CNT
 FROM 
 	nbcu_mdsdb.task_survey_final a11
 left join 
@@ -12,3 +14,5 @@ LEFT JOIN
 	nbcu_mdwdb.d_lov LKP 
 	on concat('SURVEY~CLOSURE_CODE_C~~~',SRC.u_closure_code) = LKP.row_id and LKP.source_id=SRC.sourceinstance
 WHERE  COALESCE(LKP.row_key,CASE WHEN SRC.u_closure_code IS NULL THEN 0 else -1 end)<> TRGT.closure_code_src_c_key
+)temp;
+
--- Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_task_survey_investigation_complete_src_code_c_mysql.sql
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/f_task_survey_investigation_complete_src_code_c_mysql.sql
@@ -1,5 +1,7 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.opened_on_key' ELSE 'SUCCESS' END as Message
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_incident.investigation_complete_src_code_c' ELSE 'SUCCESS' END as Message
+FROM (SELECT count(1) as CNT 
 FROM 
 	nbcu_mdsdb.task_survey_final a11
 left join 
@@ -9,3 +11,4 @@ LEFT JOIN
 	nbcu_mdwdb.f_task_survey TRGT 
 	ON (a11.sys_id =TRGT.row_id AND a11.sourceinstance= TRGT.source_id  )
 WHERE  SRC.u_investigation_complete <> TRGT.investigation_complete_src_code_c
+)temp;
