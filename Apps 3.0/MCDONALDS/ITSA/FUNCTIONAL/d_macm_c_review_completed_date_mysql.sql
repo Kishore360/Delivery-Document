9bbae7d2d577991ec2446cd92290fa1eab7c6d6f
vijay rao <vijay.rao@numerify.com>
2017-11-29 08:09:24 +0530

    MCD customization test queries related to ITSM_MACM_APPLICATION_C

---
+++ Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/d_macm_c_review_completed_date_mysql.sql
@@ -0,0 +1,13 @@
+SELECT
+CASE WHEN CNT  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN CNT THEN 'MDS to DWH data validation failed for d_macm_c_review_completed_date' ELSE 'SUCCESS' END as Message
+FROM
+(
+SELECT count(1) as CNT
+FROM mcdonalds_mdsdb.x_scafe_mcdcr_chan_mcdcr_change_request_final  SRC
+JOIN mcdonalds_mdwdb.d_macm_c TRGT 
+ON (SRC.SYS_ID=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id) 
+WHERE COALESCE(CONVERT_TZ(SRC.u_review_completed_date,'GMT','US/Central'),'UNSPECIFIED')<>TRGT.review_completed_date
+)a;
+
+
