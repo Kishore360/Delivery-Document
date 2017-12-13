109ddefb4758e5d6c5a9d9b18c411db03ab09a71
Kishore Mantha <kishore.mantha@numerify.com>
2017-12-13 10:10:02 +0530

    Updated the 3.0 McD QA Code as part of the Regression Run

--- Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/f_macm_c_request_item_key_mysql.sql
+++ Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/f_macm_c_request_item_key_mysql.sql
@@ -1,15 +1,13 @@
 SELECT
-CASE WHEN CNT  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN CNT THEN 'MDS to DWH data validation failed for f_macm_c_request_item_key' ELSE 'SUCCESS' END as Message
+CASE WHEN CNT > 0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN CNT > 0  THEN 'MDS to DWH data validation failed for f_macm_c_request_item_key' ELSE 'SUCCESS' END as Message
 FROM
 (
-SELECT count(1) as CNT 
-FROM mcdonalds_mdsdb.x_scafe_mcdcr_chan_mcdcr_change_request_final SRC  
-LEFT JOIN mcdonalds_mdwdb.f_macm_c TRGT 
-ON (SRC.SYS_ID=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id)
-LEFT JOIN mcdonalds_mdwdb.d_request_item LKP 
-ON SRC.instance=LKP.row_id and SRC.sourceinstance=LKP.source_id and SRC.target_table = 'sc_req_item'
-WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_ritm IS NULL THEN 0 else -1 end)<>TRGT.request_item_key
+SELECT Count(1) as CNT 
+FROM mcdonalds_mdsdb.x_scafe_mcdcr_chan_mcdcr_change_request_final SRC 
+JOIN mcdonalds_mdwdb.f_macm_c TRGT ON (SRC.sys_id=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id)
+JOIN mcdonalds_mdwdb.d_request_item LKP ON (COALESCE(SRC.u_ritm,'UNSPECIFIED')=LKP.row_id AND SRC.sourceinstance=LKP.source_id)
+WHERE COALESCE(LKP.row_key, CASE WHEN SRC.u_ritm IS NULL THEN 0 ELSE -1 END)<> TRGT.request_item_key
 )a;
 
 
