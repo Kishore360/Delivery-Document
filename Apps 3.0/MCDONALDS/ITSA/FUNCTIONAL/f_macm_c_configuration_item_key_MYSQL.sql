44de35a1475d14d443306036b9c49499f61afd69
Kishore Mantha <kishore.mantha@numerify.com>
2017-12-13 10:21:53 +0530

    Updated the QA code

--- Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/f_macm_c_configuration_item_key_MYSQL.sql
+++ Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/f_macm_c_configuration_item_key_MYSQL.sql
@@ -9,6 +9,6 @@ FROM mcdonalds_mdsdb.x_scafe_mcdcr_chan_mcdcr_change_request_final  SRC
 LEFT JOIN mcdonalds_mdwdb.f_macm_c TRGT 
 ON (SRC.SYS_ID=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id) 
 LEFT JOIN mcdonalds_mdwdb.d_configuration_item LKP
-ON COALESCE(SRC.cmdb_ci, 'UNSPECIFIED')=LKP.row_id
+ON (COALESCE(SRC.cmdb_ci, 'UNSPECIFIED')=LKP.row_id and SRC.sourceinstance=LKP.source_id)
 WHERE COALESCE(LKP.row_key,CASE WHEN SRC.cmdb_ci IS NULL THEN 0 ELSE -1 END)<>TRGT.configuration_item_key
 )a;
