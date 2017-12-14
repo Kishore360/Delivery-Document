c72b19e29aed2b9aaf1746b34d7e53669814c738
vijay rao <vijay.rao@numerify.com>
2017-12-14 12:22:54 +0530

    Qualcomm query fixes

---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_incident_description_mysql.sql
@@ -0,0 +1,8 @@
+
+ SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_incident.description' ELSE 'SUCCESS' END as Message 
+FROM  qualcomm_mdsdb.incident_final SRC
+JOIN qualcomm_mdwdb.d_incident TRGT  
+ON (SRC.sys_id  = TRGT.row_id  
+AND SRC.sourceinstance = TRGT.source_id )
+WHERE SRC.description <> TRGT.description;
