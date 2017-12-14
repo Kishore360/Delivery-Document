377d538a4ad4be2836ec30971584000d51081c85
vijay rao <vijay.rao@numerify.com>
2017-12-14 11:21:52 +0530

    no message

--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_incident_u_initiator_c_key_mysql.sql
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_incident_u_initiator_c_key_mysql.sql
@@ -7,4 +7,5 @@ FROM qualcomm_mdsdb.incident_final SRC
 LEFT JOIN qualcomm_mdwdb.d_internal_contact LKP 
  ON ( concat('INTERNAL_CONTACT~',u_initiator)= LKP.row_id 
 AND SRC.sourceinstance= LKP.source_id )
+AND  TRGT.pivot_date between effective_from and effective_to
 WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_initiator IS NULL THEN 0 else -1 end)<> (TRGT.u_initiator_c_key);
