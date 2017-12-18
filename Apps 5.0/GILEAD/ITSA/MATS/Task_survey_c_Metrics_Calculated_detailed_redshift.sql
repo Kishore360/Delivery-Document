7ce3ac737143a96cebff2e0d1ba733f7564558ba
prabhuram <prabhuram.sridharan@numerify.com>
2017-12-18 08:14:48 +0530

    committing changes  to gilead

---
+++ Apps 5.0/GILEAD/ITSA/MATS/Task_survey_c_Metrics_Calculated_detailed_redshift.sql
@@ -0,0 +1,30 @@
+SELECT 
+'COMPLETED_SURVEY',
+count(distinct a.row_key) as Survey_sent
+FROM ldb.f_task_response_c a 
+JOIN ldb.d_survey_state b ON a.survey_state_src_key=b.row_key
+JOIN ldb.d_calendar_date c ON a.requested_on_key=c.row_key
+WHERE c.lagging_count_of_month between 0 and 11 and b.wh_dimension_code='COMPLETED'
+union 
+SELECT 
+'OUTSTANDING_SURVEY',
+count(distinct a.row_key) as Survey_sent
+FROM ldb.f_task_response_c a 
+JOIN ldb.d_survey_state b ON a.survey_state_src_key=b.row_key
+JOIN ldb.d_calendar_date c ON a.requested_on_key=c.row_key
+WHERE c.lagging_count_of_month between 0 and 11 and b.wh_dimension_code='REQUESTED'
+union
+SELECT 'cSAT_SCORE',avg(case when a.response_value<0 then 0 else a.response_value end  ) as Avg_Incidents_CSAT_Score
+FROM ldb.f_task_response_c a 
+JOIN ldb.d_incident b ON a.incident_key=b.row_key 
+JOIN ldb.d_calendar_date c ON a.requested_on_key=c.row_key
+WHERE c.lagging_count_of_month between 0 and 12
+Union
+
+SELECT 
+'SURVEY_SENT',
+count(distinct a.row_key) as Survey_sent
+FROM ldb.f_task_response_c a 
+JOIN ldb.d_survey_state b ON a.survey_state_src_key=b.row_key
+JOIN ldb.d_calendar_date c ON a.requested_on_key=c.row_key
+WHERE c.lagging_count_of_month between 0 and 11
