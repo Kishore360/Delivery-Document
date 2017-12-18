add3e87361984f1289a10ff7b26ca53334f55a13
Kishore Mantha <kishore.mantha@numerify.com>
2017-12-18 15:12:07 +0530

    Updated the QA Syntax error in 5.0 AON as part of DELV-10060.

--- Apps 5.0/AON/ITSA/FUNCTIONAL/d_pulse_c_impact_mysql.sql
+++ Apps 5.0/AON/ITSA/FUNCTIONAL/d_pulse_c_impact_mysql.sql
@@ -1,8 +1,12 @@
- SELECT CASE WHEN cnt>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_pulse_c.impact' ELSE 'SUCCESS' END as Message  
- FROM (select count(1)cnt 
- from aon_mdsdb.u_aon_pulse_final SRC
- LEFT JOIN aon_mdwdb.d_pulse_c TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
-  WHERE SRC.u_impact <> TRGT.impact)b;
+SELECT 
+CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN cnt > 0 THEN 'MDS to DWH data validation failed for d_pulse_c.impact' ELSE 'SUCCESS' END as Message  
+FROM 
+(
+select count(1) as cnt 
+from aon_mdsdb.u_aon_pulse_final SRC
+LEFT JOIN aon_mdwdb.d_pulse_c TRGT ON (SRC.sys_id =TRGT.row_id  AND SRC.sourceinstance= TRGT.source_id )
+WHERE CHAR_LENGTH(SRC.u_impact)<=4000 and SRC.u_impact <> TRGT.impact
+)b;
+
+
--- Apps 5.0/AON/ITSA/FUNCTIONAL/f_change_task_cmdb_ci_application_service_c_key_mysql.sql
+++ Apps 5.0/AON/ITSA/FUNCTIONAL/f_change_task_cmdb_ci_application_service_c_key_mysql.sql
@@ -1,6 +1,7 @@
- SELECT CASE WHEN cnt>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_change_task.cmdb_ci_application_service_c_key' ELSE 'SUCCESS' END as Message  
- FROM (select count(1)cnt from aon_mdsdb.change_task_final SRC
+ SELECT 
+ CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt > 0 THEN 'MDS to DWH data validation failed for f_change_task.cmdb_ci_application_service_c_key' ELSE 'SUCCESS' END as Message  
+ FROM (select count(1) as cnt from aon_mdsdb.change_task_final SRC
  LEFT JOIN aon_mdsdb.u_cmdb_ci_application_service_final SRC1 
  ON (SRC.cmdb_ci =SRC1.sys_id  
  AND SRC.sourceinstance= SRC1.sourceinstance  )
@@ -11,7 +12,8 @@
  ON TRGT.cmdb_ci_application_service_c_key = LKP.row_key  
  join aon_mdsdb.u_business_unit_lookup_final bu1
 on SRC1.u_business_unit=bu1.sys_id and bu1.sourceinstance= SRC1.sourceinstance
-join aon_mdsdb.u_business_unit_sub_lookup_final bu2 --check this part for subdivision
+join aon_mdsdb.u_business_unit_sub_lookup_final bu2 
+-- --check this part for subdivision
 on SRC1.u_business_unit_subdivision=bu2.sys_id and bu2.sourceinstance= SRC1.sourceinstance
   join aon_mdsdb.u_business_unit_lookup_final bu3
 on bu2.u_bu=bu3.sys_id and bu2.sourceinstance= bu3.sourceinstance
