fa3d9bf3e54aa4909c18550c33a38bdb0d74d3ae
Kishore Mantha <kishore.mantha@numerify.com>
2017-12-21 16:35:28 +0530

    Updated the Query.

--- Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_application_schedule_c_ci_name_MYSQL.sql
+++ Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_application_schedule_c_ci_name_MYSQL.sql
@@ -1,10 +1,8 @@
-
-
-
-
 SELECT 
-CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_application_schedule_c_ci_name' ELSE 'SUCCESS' END as Message 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for d_application_schedule_c_ci_name' ELSE 'SUCCESS' END as Message 
+FROM (SELECT Count(1) as CNT 
 FROM bbandt_mdsdb.us_application_schedule_c_final SRC
 JOIN bbandt_mdwdb.d_application_schedule_c TRGT ON (SRC.sys_id=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id)
 WHERE COALESCE(SRC.name,'UNSPECIFIED')<>COALESCE(TRGT.ci_name,'UNSPECIFIED')
+)temp; 
