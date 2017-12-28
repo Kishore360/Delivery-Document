5fca035bf287866fae72434eb44553998a5841fe
Kishore Mantha <kishore.mantha@numerify.com>
2017-12-28 12:34:12 +0530

    Updated the cube Query as part of DELV-10211.

--- Apps 5.0/BBANDT/ITSA/MATS/BBandT_ITSA_Technical_Health Check_c_detailed_redshift.sql
+++ Apps 5.0/BBANDT/ITSA/MATS/BBandT_ITSA_Technical_Health Check_c_detailed_redshift.sql
@@ -12,5 +12,12 @@ UNION
 SELECT 'ldb.d_internal_contact_division_manager_c' as Table_Name,Count(a11.row_key) as Row_Count 
 FROM ldb.f_technical_health_check_c a11 
 JOIN ldb.d_eit_application_c a12 ON a11.eit_application_c_key=a12.row_key
-JOIN ldb.d_internal_contact_division_manager_c a13 ON a12.ldb.division_manager_key=a13.row_key 
+JOIN ldb.d_internal_contact_division_manager_c a13 ON a12.division_manager_key=a13.row_key
+UNION 
+SELECT 'ldb.d_lov_software_tier_rating_c' as Table_Name,Count(a11.row_key) as Row_Count 
+FROM ldb.f_technical_health_check_c a11 
+JOIN ldb.d_eit_application_c a12 ON a11.eit_application_c_key=a12.row_key
+JOIN ldb.d_lov_software_tier_rating_c a13 ON a12.software_tier_rating_src_c_key=a13.row_key
+
+
 
