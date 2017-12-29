71bf08c80b614eded79b6e6f9a860da5bd77599d
vijay rao <vijay.rao@numerify.com>
2017-12-29 09:43:16 +0530

    CUST QA queries for d_email_c

---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_email_c_checkpoint_mysql.sql
@@ -0,0 +1,11 @@
+
+			SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_email_c.checkpoint
+
+' ELSE 'SUCCESS' END as Message
+FROM qualcomm_mdsdb.sys_email_final SRC 
+LEFT JOIN qualcomm_mdwdb.d_email_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+ WHERE SRC.checkpoint <> TRGT.checkpoint;
+
