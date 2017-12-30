4c835834ef3d555023eb359bc3393eb0e8517908
vijay rao <vijay.rao@numerify.com>
2017-12-30 10:34:17 +0530

    Fixes after latest PROD run

--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_major_incident_u_disruption_end_c_mysql.sql
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_major_incident_u_disruption_end_c_mysql.sql
@@ -4,8 +4,6 @@ FROM qualcomm_mdsdb.u_major_incident_final SRC
  LEFT JOIN qualcomm_mdwdb.d_major_incident_c TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
-<<<<<<< HEAD
 WHERE TRGT.u_disruption_end_c <>  CONVERT_TZ(SRC.u_disruption_end,'GMT','GMT');
-=======
-WHERE TRGT.u_disruption_end_c<> CONVERT_TZ(SRC.u_disruption_end,'GMT','GMT');
->>>>>>> 173a273a5c9ca02e0c3aa4910136dbb1a4fc6c24
+
+
