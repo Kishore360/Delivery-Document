1eed1ebd96b720bddcc3f1e93ec8db86f77b1c8d
vijay rao <vijay.rao@numerify.com>
2017-12-30 09:57:06 +0530

    no message

---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_task_time_worked_c_created_on_key_mysql.sql
@@ -0,0 +1,14 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_task_time_worked_c.created_on_key
+
+' ELSE 'SUCCESS' END as Message
+FROM qualcomm_mdsdb.task_time_worked_final SRC 
+LEFT JOIN qualcomm_mdwdb.f_task_time_worked_c TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+LEFT JOIN qualcomm_mdwdb.d_calendar_date lkp 
+on date_format(CONVERT_TZ (sys_created_on,'GMT','GMT'),'%Y%m%d') =lkp.row_id 
+ WHERE COALESCE(lkp.row_key,CASE WHEN SRC.sys_created_on IS NULL THEN 0 else -1 end) <> (TRGT.created_on_key
+);
+
+
---
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_task_time_worked_c_time_in_seconds_mysql.sql
@@ -0,0 +1,10 @@
+ 
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result,
+CASE WHEN CNT > 0 THEN 'Data validation to MDS f_task_time_worked_c.time_in_seconds' ELSE 'Data Matched' END AS Message 
+FROM (
+Select Count(1) as CNT
+from qualcomm_mdsdb.task_time_worked_final SRC 
+JOIN qualcomm_mdwdb.f_task_time_worked_c TRGT ON SRC.sys_id=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id
+WHERE SRC.time_in_seconds<>TRGT.time_in_seconds
+) temp;
