af177855d2e8c1fde80b9c3f101b5826ba691bc6
SOUMYA-PC\Soumya <soumya.mr@numerify.com>
2018-01-03 15:19:02 +0530

    changed the query from change key to change task key

--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_email_c_change_key_mysql.sql
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_email_c_change_key_mysql.sql
@@ -1,15 +1,9 @@
-
-
-		SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_email_c.change_key
-
-' ELSE 'SUCCESS' END as Message
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_email_c.change_task_key' ELSE 'SUCCESS' END as Message
 FROM qualcomm_mdsdb.sys_email_final SRC 
 LEFT JOIN qualcomm_mdwdb.f_email_c TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
-   join qualcomm_mdwdb.d_change_task LKP 
-	on                SRC.instance=LKP.row_id and SRC.sourceinstance=LKP.source_id and SRC.target_table = 'change_task'
-
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.instance IS NULL THEN 0 else -1 end) = (TRGT.change_key
-);
+join qualcomm_mdwdb.d_change_task LKP 
+on SRC.instance=LKP.row_id and SRC.sourceinstance=LKP.source_id and SRC.target_table = 'change_task'
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.instance IS NULL THEN 0 else -1 end) <> (TRGT.change_task_key);
