fe4e01f683095d2f1bf61856a3e703193341a78f
vijay rao <vijay.rao@numerify.com>
2018-01-05 11:37:10 +0530

    DELV-10248,10249 WATSON metrics for Incident

---
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/f_incident_c_resolved_to_confirmed_cie_duration_c_mysql.sql
@@ -0,0 +1,12 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident_c.resolved_to_confirmed_cie_duration_c' ELSE 'SUCCESS' END as Message
+FROM watson_mdwdb.f_incident f 
+INNER JOIN watson_mdsdb.incident_final a 
+ON f.row_id=a.sys_id and f.source_id=a.sourceinstance
+JOIN watson_mdwdb.d_incident d
+ON f.incident_key=d.row_key
+JOIN watson_mdsdb.sys_audit_final saf 
+ON saf.documentkey=a.sys_id 
+AND saf.tablename='incident' AND saf.fieldname='u_confirmed_cie'
+WHERE f.resolved_to_confirmed_cie_duration_c <> CASE WHEN (a.resolved_at is null  or saf.sys_created_on is null) THEN NULL WHEN TIMESTAMPDIFF(SECOND,saf.sys_created_on,a.resolved_at ) < 0
+										THEN NULL ELSE TIMESTAMPDIFF(SECOND,saf.sys_created_on,a.resolved_at) end
