173c8c007d97a4aaf9b7e170d0e358bc27d5fd6b
vijay rao <vijay.rao@numerify.com>
2018-01-08 20:08:47 +0530

    no message

--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_request_item_variable_c_value_mysql.sql
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_request_item_variable_c_value_mysql.sql
@@ -5,6 +5,19 @@ join qualcomm_mdsdb.sc_item_option_final SRC1
 on SRC.sys_id = SRC1.item_option_new AND SRC.sourceinstance = SRC1.sourceinstance
 join qualcomm_mdsdb.sc_item_option_mtom_final SRC2
 on SRC2.sc_item_option = SRC1.sys_id AND SRC2.sourceinstance = SRC1.sourceinstance
+join qualcomm_mdwdb.d_variable_c LKP
+on SRC1.item_option_new = LKP.Row_id and SRC1.sourceinstance=LKP.source_id and LKP.soft_deleted_flag = 'N'
+join qualcomm_mdsdb.sc_req_item_final d 
+on d.sys_id = SRC2.request_item and d.sourceinstance = SRC2.sourceinstance
+join qualcomm_mdsdb.sc_task_final tf
+on tf.request_item = SRC2.request_item
+and tf.sourceinstance = SRC2.sourceinstance
+join  qualcomm_mdsdb.sc_item_variables_task_final vt 
+on vt.variable = SRC.sys_id and vt.task =  tf.sys_id 
 join qualcomm_mdwdb.f_request_item_variable_c TRGT 
-ON ((concat(SRC1.sys_id,'~',SRC2.request_item) =TRGT.row_id) AND SRC1.sourceinstance= TRGT.source_id  )
+ON ((concat(SRC1.sys_id,'~',vt.task) =TRGT.row_id) AND SRC1.sourceinstance= TRGT.source_id  )
 WHERE  COALESCE(SRC1.value, 'UNSPECIFIED') <> TRGT.value
+and SRC.type  in (1,2,3,5,6,7,8,9,10,16,18,24,4) 
+
+
+
