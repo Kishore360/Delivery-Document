cd3f503624d6203df84b2397067e5f53fc0b9a1d
vijay rao <vijay.rao@numerify.com>
2018-01-16 15:55:24 +0530

    Watson CUST QA queries for 10405,10403

--- Apps 5.0/WATSON/ITSA/FUNCTIONAL/f_case_avg_time_to_final_resolution_c_mysql.sql
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/f_case_avg_time_to_final_resolution_c_mysql.sql
@@ -1,5 +1,5 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_case.resolution_after_first_response_duration_c' ELSE 'SUCCESS' END as Message
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_case.avg_time_to_final_resolution_c' ELSE 'SUCCESS' END as Message
 FROM 
 watson_mdwdb.f_case fi 
 inner join watson_mdwdb.d_case di on di.row_key=fi.case_key
@@ -11,6 +11,6 @@ inner join ( select documentkey ,oldvalue,newvalue, min(sys_created_on) as sys_c
  inner join (select name,element,value,label from watson_mdsdb.sys_choice_final where element='state' and name='sn_customerservice_case' )lov_new
  on lov_new.value=a.newvalue
  where tablename='sn_customerservice_case' and fieldname = 'state' )A
- where oldvalue=10 group by 1,2,3 )r on di.row_id=r.documentkey
-where fi.avg_time_to_respond_c <> timestampdiff(second, di.opened_on, r.sys_created_on)
+ where newvalue=21 group by 1,2,3 )r on di.row_id=r.documentkey
+where fi.avg_time_to_final_resolution_c <> timestampdiff(second, di.opened_on, r.sys_created_on)
  ;
