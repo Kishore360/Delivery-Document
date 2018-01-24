3a4fedcd4f4a4a88fa5c970c536da39bfef23fb0
Kishore Mantha <kishore.mantha@numerify.com>
2018-01-24 11:26:16 +0530

    Updated the QA Codes

--- Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_software_c_archer_id_mysql.sql
+++ Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_software_c_archer_id_mysql.sql
@@ -1,7 +1,10 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_software_c.archer_id' ELSE 'SUCCESS' END as Message
+SELECT 
+CASE WHEN CNT  > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT  > 0 THEN 'MDS to DWH data validation failed for d_software_c.archer_id' ELSE 'SUCCESS' END as Message
+ FROM (SELECT Count(1) as CNT 
  FROM bbandt_mdsdb.cmdb_ci_spkg_final SRC 
  LEFT JOIN bbandt_mdwdb.d_software_c TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.u_archer_id,'')<> COALESCE(TRGT.archer_id ,'')
+ WHERE COALESCE( SRC.u_archer_id,'')<> TRGT.archer_id 
+ ) temp; 
