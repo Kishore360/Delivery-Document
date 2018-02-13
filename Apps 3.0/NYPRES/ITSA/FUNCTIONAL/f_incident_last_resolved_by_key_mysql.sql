bb7622cbe5f9439253b959064a371d63828c2a4c
vijay rao <vijay.rao@numerify.com>
2018-02-13 13:32:30 +0530

    NYPRES delv-11002

---
+++ Apps 3.0/NYPRES/ITSA/FUNCTIONAL/f_incident_last_resolved_by_key_mysql.sql
@@ -0,0 +1,8 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_incident.last_resolved_by_key' ELSE 'SUCCESS' END as Message
+ FROM nypres_mdsdb.incident_final  SRC 
+ JOIN nypres_mdwdb.f_incident TRGT 
+ ON (SRC.sys_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id ) 
+ LEFT JOIN nypres_mdwdb.d_internal_contact LKP
+ ON ( concat( 'INTERNAL_CONTACT~',upper( SRC.resolved_by)) = LKP.row_id 
+ AND SRC.sourceinstance = LKP.source_id ) 
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.resolved_by IS NULL THEN 0 else -1 end)<> (TRGT.last_resolved_by_key) 
