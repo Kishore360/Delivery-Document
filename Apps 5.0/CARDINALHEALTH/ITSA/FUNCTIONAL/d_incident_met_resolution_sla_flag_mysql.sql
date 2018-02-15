988c216417af3756fae418899401535f814b29c9
vijay rao <vijay.rao@numerify.com>
2018-02-15 16:26:04 +0530

    DELV-10965 - Cardinal Health

--- Apps 5.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_incident_met_resolution_sla_flag_mysql.sql
+++ Apps 5.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_incident_met_resolution_sla_flag_mysql.sql
@@ -1,23 +1,13 @@
-SELECT CASE WHEN cnt > 0 THEN 'FaILURE' ELSE 'SUCCESS' END aS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END aS Message 
-FROM (
-select count(1) as cnt  from
-cardinalhealth_mdwdb.d_incident d_inc 
-left join (
-			select a.incident_key,max(case when a.sla_category='Resolution' then a.outcome_flag end) as resolution,
-			max(case when a.sla_category='Response' then a.outcome_flag  end  ) as response 
-			from (SELECT f_sla.incident_key,d_sla.sla_category, group_concat(f_sla.outcome_flag) as outcome_flag  
-					FROM cardinalhealth_mdwdb.f_task_sla f_sla
-					join (select distinct incident_key as incident_key from cardinalhealth_mdwdb.f_task_sla 
-							where task_type='incident' ) tt 
-					on tt.incident_key=f_sla.incident_key
-					join cardinalhealth_mdwdb.d_task_sla d_sla 
-					on d_sla.row_key=f_sla.sla_key 
-					and d_sla.soft_deleted_flag='N' 
-					and f_sla.task_type='incident'
-					group by f_sla.incident_key,d_sla.sla_category) a group by incident_key) tmp 
-	 on d_inc.row_key=tmp.incident_key 
-	 join (select distinct incident_key as incident_key from cardinalhealth_mdwdb.f_task_sla where task_type='incident') tmp1
-	 on tmp1.incident_key=d_inc.row_key
-where d_inc.met_resolution_sla_flag<>coalesce(case when tmp.resolution like '%Y%' then 'N' else 'Y' end,'Y'))c
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
+from cardinalhealth_mdwdb.d_incident a join (
+SELECT  task , group_concat(distinct has_breached) as outcome_flag 
+FROM cardinalhealth_mdsdb.task_sla_final f_sla
+JOIN cardinalhealth_mdsdb.task_final inc
+on f_sla.task = inc.sys_id
+JOIN cardinalhealth_mdsdb.contract_sla_final d_sla ON d_sla.sys_id=f_sla.sla
+WHERE stage <> 'CANCELLED' and d_sla.name like '%Resolution%' and type ='SLA' 
+
+group by 1) b on a.row_id = b.task
+where a.met_resolution_sla_flag <> CASE WHEN outcome_flag like '%1%' THEN 'N' ELSE 'Y' END
