abc5a0bd86abc7bdae041645f21f2feb40ccec62
prabhuram <prabhuram.sridharan@numerify.com>
2018-02-15 17:55:00 +0530

    committing watson queries

---
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/d_ibm_accounts_c_internal_ibm_contact_c_key_mysql.sql
@@ -0,0 +1,11 @@
+
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.caller_state_c' ELSE 'SUCCESS' END as Message
+ FROM    watson_mdsdb.u_ibm_accounts_final s
+left join watson_mdwdb.d_internal_contact d on concat('INTERNAL_CONTACT~',u_internal_ibm_contact)=d.row_id
+left join  watson_mdwdb.d_ibm_accounts_c t on s.sys_id=t.row_id and s.sourceinstance=t.source_id
+where internal_ibm_contact_c_key<> coalesce(d.row_key,case when u_internal_ibm_contact is null then 0 else -1 end )
+
+ 
---
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/d_ibm_accounts_c_support_tier_src_c_key_mysql.sql
@@ -0,0 +1,8 @@
+
+ 
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.caller_state_c' ELSE 'SUCCESS' END as Message
+ FROM   watson_mdsdb.u_ibm_accounts_final s
+left join watson_mdwdb.d_lov d on concat('SUPPORT_TIER_C~U_IBM_ACCOUNTS~~~',u_support_tier)=d.row_id
+left join  watson_mdwdb.d_ibm_accounts_c t on s.sys_id=t.row_id and s.sourceinstance=t.source_id
+where support_tier_src_c_key= coalesce(d.row_key,case when u_support_tier is null then 0 else -1 end )
