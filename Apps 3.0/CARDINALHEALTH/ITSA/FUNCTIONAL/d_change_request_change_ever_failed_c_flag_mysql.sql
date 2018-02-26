5b6f6f980e86f03767169eac8dde0aa56151e473
vijay rao <vijay.rao@numerify.com>
2018-02-26 16:57:18 +0530

    no message

--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_change_request_change_ever_failed_c_flag_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_change_request_change_ever_failed_c_flag_mysql.sql
@@ -2,7 +2,7 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 FROM (
- select number,a.SYS_ID,b.failure_flag,u_success_status,oldvalue,newvalue,change_ever_failed_c_flag,count(1)-- u_success_status, oldvalue, newvalue,a.SYS_ID,b.change_ever_failed_c_flag ,d.documentkey
+ select count(1) as cnt-- u_success_status, oldvalue, newvalue,a.SYS_ID,b.change_ever_failed_c_flag ,d.documentkey
 from cardinalhealth_mdsdb.change_request_final a
  left  JOIN  cardinalhealth_mdwdb.d_change_request b
 on  a.SYS_ID= b.ROW_ID and a.sourceinstance=b.source_id
--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_change_request_failure_flag_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/d_change_request_failure_flag_mysql.sql
@@ -3,17 +3,9 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ELSE 'Data Matched' END AS Message 
 FROM (
  select count(1) cnt
-from
-cardinalhealth_mdwdb.d_change_request d 
-
- JOIN cardinalhealth_mdwdb.f_change_request f ON d.row_key=f.change_request_key
-
- where  d.failure_flag<> CASE WHEN f.success_status_src_code_c LIKE '%fail%' THEN 'Y' ELSE 'N' END 
-    
-
+from cardinalhealth_mdwdb.d_change_request d 
+ JOIN cardinalhealth_mdwdb.f_change_request f 
+ ON d.row_key=f.change_request_key
+ where  d.failure_flag<> CASE WHEN f.success_status_src_code_c LIKE '%fail%' THEN 'Y' WHEN f.success_status_src_code_c 
+ LIKE '%Withdra%' THEN '' ELSE 'N' END 
  )b
- 
- 
- 
- 
- 
