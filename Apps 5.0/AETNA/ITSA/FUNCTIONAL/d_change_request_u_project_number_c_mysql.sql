3a019ab5b187475b39328435f93963e4aab61723
Narmadha-PC\Narmadha <narmadha.d@numerify.com>
2018-02-28 15:27:12 +0530

    Aetna Queries

---
+++ Apps 5.0/AETNA/ITSA/FUNCTIONAL/d_change_request_u_project_number_c_mysql.sql
@@ -0,0 +1,12 @@
+
+ SELECT CASE WHEN cnt>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_change_request.u_project_number_c
+' ELSE 'SUCCESS' END as Message  
+ FROM (select count(1)cnt from aetna_mdsdb.change_request_final SRC
+ LEFT JOIN aetna_mdwdb.d_change_request TRGT 
+ ON (SRC.sys_id =TRGT.row_id  
+ AND SRC.sourceinstance= TRGT.source_id  )
+  WHERE COALESCE( SRC.u_project_number,'UNSPECIFIED') 
+<> (TRGT.u_project_number_c
+
+))b;
