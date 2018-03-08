53c2822cdcc6cf223ce8f1090df5d08741cbb9f5
vijay rao <vijay.rao@numerify.com>
2018-03-08 12:06:55 +0530

    Fidelity ITSA Cust queries

---
+++ Apps 5.0/FIDELITY/ITSA/FUNCTIONAL/d_change_request_outage_type_src_c_key_mysql.sql
@@ -0,0 +1,10 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_change_request.outage_type_src_c_key' ELSE 'SUCCESS' END as Message 
+FROM fidelity_mdsdb.change_request_final src
+JOIN fidelity_mdwdb.d_change_request trgt
+on src.sys_id = trgt.row_id and src.sourceinstance = trgt.source_id
+LEFT JOIN fidelity_mdwdb.d_lov lkp
+ON COALESCE(CONCAT('OUTAGE~CHANGE_REQUEST~~~',src.u_outage_type ),'UNSPECIFIED') =lkp.row_id 
+where COALESCE(lkp.row_key,CASE WHEN src.u_outage_type  IS NULL THEN 0 else -1 end)<> trgt.outage_type_src_c_key
+;
+
