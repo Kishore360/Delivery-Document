ae9a0e51695ab934a77fcd91a311ae543a319144
Kishore Mantha <kishore.mantha@numerify.com>
2018-04-19 12:39:41 +0530

    Updated few changes

--- Apps 5.0/PNG/ITSA/MATS/PNG_ITSA_Incident_c_detailed_redshift.sql
+++ Apps 5.0/PNG/ITSA/MATS/PNG_ITSA_Incident_c_detailed_redshift.sql
@@ -258,3 +258,15 @@ join ldb.d_calendar_date a18
 on (a11.opened_on_key = a18.row_key) 
 join ldb.d_calendar_year a143 
 on (a18.year_start_date_key = a143.row_key) 
+UNION 
+select'ldb.d_configuration_item_incident_service_offering_c' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_incident       a11 
+join ldb.d_configuration_item_incident_service_offering_c       a12
+on (a11.service_offering_c_key = a12.row_key)
+union
+select'ldb.d_parent_location_c' as Table_Name, count(a11.row_key) Row_Count
+ from  ldb.f_incident       a11 
+join ldb.d_location       a12
+on (a11.location_key = a12.row_key)
+join ldb.d_parent_location_c a13
+on (a12.parent_location_c_key=a13.row_key)
