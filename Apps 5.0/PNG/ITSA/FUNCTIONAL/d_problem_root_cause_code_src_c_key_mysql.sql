9fe7928a19e7639d790b12093cdb4ba4113e0164
Kishore Mantha <kishore.mantha@numerify.com>
2018-04-25 12:21:13 +0530

    Updated the structure of the ETL cust Queries in 5.X P&G

--- Apps 5.0/PNG/ITSA/FUNCTIONAL/d_problem_root_cause_code_src_c_key_mysql.sql
+++ Apps 5.0/PNG/ITSA/FUNCTIONAL/d_problem_root_cause_code_src_c_key_mysql.sql
@@ -1,13 +1,15 @@
-
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.root_cause_code_src_c_key' ELSE 'SUCCESS' END as Message
- FROM png_mdsdb.problem_final SRC 
- LEFT JOIN  png_mdwdb.d_problem TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT > 0 THEN 'MDS to DWH data validation failed for d_problem.root_cause_code_src_c_key' ELSE 'SUCCESS' END as Message
+FROM 
+(
+SELECT Count(1) as CNT 
+FROM png_mdsdb.problem_final SRC 
+LEFT JOIN  png_mdwdb.d_problem TRGT ON (SRC.sys_id =TRGT.row_id  AND SRC.sourceinstance= TRGT.source_id  )
 LEFT JOIN png_mdwdb.d_lov LKP 
- ON ( concat('ROOT_CAUSE_CODE_C','~','PROBLEM','~','~','~',upper(u_pg_root_cause_code))= LKP.src_rowid 
-AND SRC.sourceinstance= LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_pg_root_cause_code IS NULL THEN 0 else -1 end) <> (TRGT.root_cause_code_src_c_key)
+ON ( concat('ROOT_CAUSE_CODE_C','~','PROBLEM','~','~','~',upper(u_pg_root_cause_code))= LKP.src_rowid AND SRC.sourceinstance= LKP.source_id )
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_pg_root_cause_code IS NULL THEN 0 else -1 end) <> (TRGT.root_cause_code_src_c_key)
+) temp;
+
  
  
