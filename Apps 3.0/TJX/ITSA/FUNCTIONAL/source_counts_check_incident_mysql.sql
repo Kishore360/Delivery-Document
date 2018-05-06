bd949c1c6c9f47227a3ff9e4357d22c81629117f
prabhuram <prabhuram.sridharan@numerify.com>
2018-05-06 21:39:47 +0530

    committing query for validation

---
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/source_counts_check_incident_mysql.sql
@@ -0,0 +1,5 @@
+select 'change_request' as Result,count(1) Message
+from tjx_mdsdb.incident_final 
+where date_format(opened_at,'%Y%m%d') <= 
+ ( select date_format(max(opened_at),'%Y%m%d') from tjx_mdsdb.incident_final ) and date_format(opened_at,'%Y%m%d') >=
+ date_format(date_add(date_format(( select max(opened_at) from tjx_mdsdb.incident_final ),'%Y%m%d'), INTERVAL -2 month),'%Y%m01')
