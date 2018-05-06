cb03278ea9bffbc16903dad6a97f497afe1447c1
prabhuram <prabhuram.sridharan@numerify.com>
2018-05-06 21:23:09 +0530

    committing to verify the source and target counts

--- Apps 3.0/TJX/ITSA/FUNCTIONAL/source_counts_check_mysql.sql
+++ Apps 3.0/TJX/ITSA/FUNCTIONAL/source_counts_check_mysql.sql
@@ -1,11 +1,16 @@
-select 'change_request' as table_name,count(1)
+select 'change_request' as Result,count(1) Message
 from tjx_mdsdb.change_request_final 
 where date_format(opened_at,'%Y%m%d') <= 
  ( select date_format(max(opened_at),'%Y%m%d') from tjx_mdsdb.change_request_final ) and date_format(opened_at,'%Y%m%d') >=
  date_format(date_add(date_format(( select max(opened_at) from tjx_mdsdb.change_request_final ),'%Y%m%d'), INTERVAL -2 month),'%Y%m01')
  union
- select 'problem' as table_name,count(1)
+ select 'problem' as Result,count(1) Message
 from tjx_mdsdb.problem_final 
 where date_format(opened_at,'%Y%m%d') <= 
  ( select date_format(max(opened_at),'%Y%m%d') from tjx_mdsdb.problem_final ) and date_format(opened_at,'%Y%m%d') >=
  date_format(date_add(date_format(( select max(opened_at) from tjx_mdsdb.problem_final ),'%Y%m%d'), INTERVAL -2 month),'%Y%m01')
+ 
+ 
+ 
+ select CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.age' ELSE 'SUCCESS' END as Message from (select count(1) cnt 
--- Apps 3.0/TJX/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
+++ Apps 3.0/TJX/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
@@ -1,12 +1,13 @@
- select 'incident' as table_name,count(1) from tjx_mdwdb.f_incident
+ select 'incident' as table_name,count(1) Row_Count from tjx_mdwdb.f_incident
 join tjx_mdwdb.d_calendar_date d on opened_on_key=d.row_key
 where lagging_count_of_month between 0 and 2
 union
- select 'problem' as table_name,count(1) from  tjx_mdwdb.f_problem
+ select 'problem' as table_name,count(1) Row_Count from  tjx_mdwdb.f_problem
 join tjx_mdwdb.d_calendar_date d on opened_on_key=d.row_key
 where lagging_count_of_month between 0 and 2
 union
- select 'change_request' as table_name,count(1) from tjx_mdwdb.f_change_request 
+ select 'change_request' as table_name,count(1) Row_Count from tjx_mdwdb.f_change_request 
 join tjx_mdwdb.d_calendar_date d on opened_on_key=d.row_key
 where lagging_count_of_month between 0 and 2
+
 
