7732ebdb78996ad3c5658914138d9077ea83c56f
prabhuram <prabhuram.sridharan@numerify.com>
2018-05-08 14:59:25 +0530

    committing changes

--- Apps 5.0/GILEAD/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
+++ Apps 5.0/GILEAD/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
@@ -1,15 +1,14 @@
- select 'change' as table_name,count(1) Row_Count from ldb.f_change_request 
-join ldb.d_calendar_date d on opened_on_key=d.row_key
-where lagging_count_of_month between 0 and 2
+ select 'change' as table_name,count(a11.row_key) Row_Count from ldb.f_change_request a11
+join ldb.d_calendar_date d on (a11.opened_on_key=d.row_key
+where lagging_count_of_month between 0 and 2)
 union
- select 'incident' as table_name,count(1) Row_Count from ldb.f_incident
-join ldb.d_calendar_date d on opened_on_key=d.row_key
-where lagging_count_of_month between 0 and 2
+ select 'incident' as table_name,count(a11.row_key) Row_Count from ldb.f_incident a11
+join ldb.d_calendar_date d on (a11.opened_on_key=d.row_key
+where lagging_count_of_month between 0 and 2)
 union
- select 'problem' as table_name,count(1) Row_Count from  ldb.f_problem
-join ldb.d_calendar_date d on opened_on_key=d.row_key
-where lagging_count_of_month between 0 and 2
-
+ select 'problem' as table_name,count(a11.row_key) Row_Count from  ldb.f_problem a11
+join ldb.d_calendar_date d on (a11.opened_on_key=d.row_key
+where lagging_count_of_month between 0 and 2)
 
 
 
