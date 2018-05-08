58432e0925b422cb01aa26752d775d0b84b9abfc
prabhuram <prabhuram.sridharan@numerify.com>
2018-05-08 11:29:44 +0530

    commititng changes

--- Apps 5.0/GILEAD/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
+++ Apps 5.0/GILEAD/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
@@ -1,13 +1,15 @@
- select 'incident' as table_name,count(1) Row_Count from gilead_mdwdb.f_incident
-join gilead_mdwdb.d_calendar_date d on opened_on_key=d.row_key
+ select 'change' as table_name,count(1) Row_Count from ldb.f_change_request 
+join ldb.d_calendar_date d on opened_on_key=d.row_key
 where lagging_count_of_month between 0 and 2
 union
- select 'problem' as table_name,count(1) Row_Count from  gilead_mdwdb.f_problem
-join gilead_mdwdb.d_calendar_date d on opened_on_key=d.row_key
+ select 'incident' as table_name,count(1) Row_Count from ldb.f_incident
+join ldb.d_calendar_date d on opened_on_key=d.row_key
 where lagging_count_of_month between 0 and 2
 union
- select 'change_request' as table_name,count(1) Row_Count from gilead_mdwdb.f_change_request 
-join gilead_mdwdb.d_calendar_date d on opened_on_key=d.row_key
+ select 'problem' as table_name,count(1) Row_Count from  ldb.f_problem
+join ldb.d_calendar_date d on opened_on_key=d.row_key
 where lagging_count_of_month between 0 and 2
+union
+
 
 
