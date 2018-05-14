f268736c42d4867d9e9dbc1eebc9001565eedfcd
prabhuram <prabhuram.sridharan@numerify.com>
2018-05-14 20:37:03 +0530

    committing

--- Apps 5.0/GILEAD/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
+++ Apps 5.0/GILEAD/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
@@ -6,9 +6,11 @@ union
 join ldb.d_calendar_date d on (a11.opened_on_key=d.row_key)
 where lagging_count_of_month between 0 and 2
 union
- select 'problem' as table_name,count(a11.row_key) Row_Count from  ldb.f_problem a11
+ select 'problem' as table_name,count(a11.row_key) Row_Count from  ldb.f_request_item a11
 join ldb.d_calendar_date d on (a11.opened_on_key=d.row_key)
 where lagging_count_of_month between 0 and 2
-
-
-
+union
+select 'd_calendar_date' as Table_Name, count(a11.row_key) Row_Count
+FROM ldb.f_request_task a11
+JOIN ldb.d_calendar_date a12  on (a11.opened_on_key = a12.row_key)
+where lagging_count_of_month between 0 and 2
