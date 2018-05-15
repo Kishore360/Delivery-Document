a6df62e6d630eb7b56c0aead077e66ca1e3e75e3
prabhuram <prabhuram.sridharan@numerify.com>
2018-05-15 14:39:09 +0530

    verifying again

--- Apps 5.0/GILEAD/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
+++ Apps 5.0/GILEAD/ITSA/MATS/Redshift_counts_check_detailed_redshift.sql
@@ -1,4 +1,4 @@
- select 'problem' as table_name,count(a11.row_key) Row_Count from  ldb.f_request_item a11
+ select 'request_item' as table_name,count(a11.row_key) Row_Count from  ldb.f_request_item a11
 join ldb.d_calendar_date d on (a11.opened_on_key=d.row_key)
 where lagging_count_of_month between 0 and 2
 union
