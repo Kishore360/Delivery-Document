27f2970159aecb549e3afb6bf1d4470dc9a9cdd3
Kishore Mantha <kishore.mantha@numerify.com>
2018-05-21 16:56:51 +0530

    Updated the QA Code for the BBandT

--- Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_clarity_project_c_financialstatus1017122729_c_mysql.sql
+++ Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_clarity_project_c_financialstatus1017122729_c_mysql.sql
@@ -3,4 +3,4 @@ CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_clarity_projec
 FROM bbandt_mdwdb.d_clarity_project_c trgt
 RIGHT JOIN bbandt_mdsdb.bbt_prj_data_qlkview_final src
 on src.investment_id = trgt.row_id and src.sourceinstance = trgt.source_id
-where trgt.financialstatus1017122729_c <>src.financialstatus10171227
+where trgt.financialstatus1017122729_c <> src.financialstatus1017122729
--- Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_clarity_time_data_c_resource_manager_filter_c_mysql.sql
+++ Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_clarity_time_data_c_resource_manager_filter_c_mysql.sql
@@ -3,4 +3,4 @@ CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_clarity_time_d
 FROM bbandt_mdwdb.d_clarity_time_data_c trgt
 RIGHT JOIN bbandt_mdsdb.bbt_time_data_v_final src
 on src.row_num = trgt.row_id and src.sourceinstance = trgt.source_id
-where trgt.resource_manager_filter_c <>src.resource_manager_fil
+where trgt.resource_manager_filter_c <> src.resource_manager_filter
