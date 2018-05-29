b52fddfea9f22f53b267bbe9d079d811b05cc666
vijay rao <vijay.rao@numerify.com>
2018-05-29 17:15:28 +0530

    BBANDT CUST QA queries for DELV-12240,41

---
+++ Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_clarity_allocation_c_invest_id_c_mysql.sql
@@ -0,0 +1,6 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_clarity_allocation_c.invest_id_c' ELSE 'SUCCESS' END as Message 
+FROM bbandt_mdwdb.d_clarity_allocation_c trgt
+RIGHT JOIN bbandt_mdsdb.bbt_allocation_etc_v_final src
+on trgt.row_id = CONCAT(src.team_user_id,'~',src.invest_id,'~',src.type,'~',src.year,'~',src.month)
+where trgt.invest_id_c <>src.invest_id;
