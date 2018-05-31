de50a83ac8dae9e3f9b8e112de5c6b1b35fd20e4
vijay rao <vijay.rao@numerify.com>
2018-05-31 12:20:28 +0530

    QA queries corrected for PAN 6.0

--- Apps 6.0/PAN6/ITSA/FUNCTIONAL/f_incident_tasks_c_age_mysql.sql
+++ Apps 6.0/PAN6/ITSA/FUNCTIONAL/f_incident_tasks_c_age_mysql.sql
@@ -2,18 +2,22 @@ SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
 ,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
 ELSE 'Data Matched' END AS Message 
 FROM (
-select count(1) as cnt
-from pan6_mdsdb.u_incident_tasks_final s 
-left join pan6_mdwdb.f_incident_tasks_c f 
-on s.sys_id=f.row_id and s.sourceinstance = f.source_id 
-JOIN pan6_mdwdb.d_incident_tasks_c a 
-ON s.sys_id= a.row_id AND s.sourceinstance= a.source_id
-JOIN  pan6_mdwdb.d_lov_map br 
-ON  a.state_src_key = br.src_key AND br.dimension_wh_code ='OPEN' 
-WHERE TIMESTAMPDIFF(second,a.opened_on,(SELECT MAX(lastupdated) AS lastupdated
-FROM pan6_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%')) <> f.age ) temp
-
-
-
-
-
+select count(1) as cnt FROM pan6_mdsdb.u_incident_tasks_final SRC 
+ LEFT JOIN pan6_mdwdb.f_incident_tasks_c TRGT 
+ ON (SRC.sys_id=TRGT.row_id 
+ AND SRC.sourceinstance=TRGT.source_id )
+ JOIN pan6_mdwdb.d_incident_tasks_c a 
+ON SRC.sys_id= a.row_id AND SRC.sourceinstance= a.source_id
+left join pan6_mdwdb.d_lov_map lm 
+  ON (lm.src_key = a.state_src_key)
+ LEFT JOIN (
+ select source_id,max(lastupdated) as lastupdated from  pan6_mdwdb.d_o_data_freshness
+ group by source_id
+ ) df 
+ ON (TRGT.source_id = df.source_id )
+where lm.dimension_class = 'STATE~INCIDENT_TASKS'
+and case when lm.dimension_wh_code in ('OPEN') 
+    then TIMESTAMPDIFF(second, coalesce( SRC.opened_at,0),convert_tz(df.lastupdated, 
+	'America/Los_Angeles','GMT'))
+    else case when SRC.opened_at> SRC.closed_at then 0 else TIMESTAMPDIFF(second,coalesce( SRC.opened_at,0),coalesce( SRC.closed_at,0)) end
+    end <>TRGT.age)Temp;
