961420c2b5c69757dd5947a2f9782dd7c3deea27
DESKTOP-D4G34MN\Narmada <narmadha.d@numerify.com>
2018-06-07 11:02:09 +0530

    Rogers Dev - clenup activity - june (added MATS Query according to scp list and cust coverage )

--- Apps 5.0/ROGERS/ITSA/MATS/ITSA_Task_Ci_c_detailed_redhsift.sql
+++ Apps 5.0/ROGERS/ITSA/MATS/ITSA_Task_Ci_c_detailed_redhsift.sql
@@ -8,7 +8,7 @@ select'ldb.d_task_ci_c' as Table_Name, count(a11.row_key) Row_Count
 union
 select'ldb.d_configuration_item' as Table_Name, count(a11.row_key) Row_Count
  from  ldb.f_task_ci_c       a11 
-  join          ldb.d_configuration_item        a13
+  join          ldb.d_configuration_item_task_ci_c        a13
                   on          (a11.configuration_item_key = a13.row_key)
 union
 select'ldb.d_task' as Table_Name, count(a11.row_key) Row_Count
@@ -18,8 +18,6 @@ join          ldb.d_task               a14
 union
 select'ldb.d_task_type' as Table_Name, count(a11.row_key) Row_Count
  from  ldb.f_task_ci_c       a11 
-join          ldb.d_task               a14
-                  on          (a11.task_key = a14.row_key)
                 join          ldb.d_task_type      a15
-                  on          (a14.task_type_src_key = a15.row_key)
+                  on          (a11.type_src_key = a15.row_key)
 				  
