e128b3d303cc78fadae444686cbf754cff51ad86
DESKTOP-D4G34MN\Narmada <narmadha.d@numerify.com>
2018-06-08 12:03:36 +0530

    cust coverage - prod

---
+++ Apps 5.0/ROGERS/ITSA/FUNCTIONAL/d_configuration_item_environment_c_mysql.sql
@@ -0,0 +1,6 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_configuration_item.id_c' ELSE 'SUCCESS' END as Message 
+FROM rogersdev_mdsdb.cmdb_ci_appl_final  SRC 
+JOIN rogersdev_mdwdb.d_configuration_item TRGT 
+ON SRC.sys_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id 
+ WHERE COALESCE(SRC.u_environment2,'UNSPECIFIED') <> TRGT.environment_c
