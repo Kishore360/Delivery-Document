86d62bf1abfd6f6e2bb01eebdb89042c1aac4002
prabhuram <prabhuram.sridharan@numerify.com>
2018-06-10 22:27:56 +0530

    committing files for mcd

--- Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_macm_c_glide_date_time_MYSQL.sql
+++ Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_macm_c_glide_date_time_MYSQL.sql
@@ -3,8 +3,10 @@ CASE WHEN CNT  THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN CNT THEN 'MDS to DWH data validation failed for d_macm_c_glide_date_time' ELSE 'SUCCESS' END as Message
 FROM
 (
-SELECT count(1) as CNT 
+SELECT count(1) as CNT from from
 (select * FROM mcdonalds_mdsdb.x_scafe_mcdcr_chan_mcdcr_change_request_final where cdctype<>'D') SRC
 JOIN mcdonalds_mdwdb.d_macm_c TRGT ON (SRC.SYS_ID=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id)
+left join (select source_id,max(lastupdated) as lastupdated from mcdonalds_mdwdb.d_o_data_freshness group by source_id) f1 
+on (f1.source_id = SRC.sourceinstance)
 where (SRC.cdctime<=f1.lastupdated) and COALESCE(CONVERT_TZ(SRC.u_glide_date_time_1,'GMT','US/Central'),'UNSPECIFIED')<>TRGT.glide_date_time
 )a;
--- Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_macm_c_regression_test_MYSQL.sql
+++ Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_macm_c_regression_test_MYSQL.sql
@@ -3,7 +3,7 @@ CASE WHEN CNT  THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN CNT THEN 'MDS to DWH data validation failed for d_macm_c_regression_test' ELSE 'SUCCESS' END as Message
 FROM
 (
-SELECT count(1) as CNT 
+SELECT count(1) as CNT from 
 (select * FROM mcdonalds_mdsdb.x_scafe_mcdcr_chan_mcdcr_change_request_final where cdctype<>'D')   SRC
 JOIN mcdonalds_mdwdb.d_macm_c TRGT 
 ON (SRC.SYS_ID=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id) 
