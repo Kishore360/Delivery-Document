67a489b52afb01cf5ae36b12c8f1c14b955c0334
prabhuram <prabhuram.sridharan@numerify.com>
2018-08-06 14:42:46 +0530

    committing changes to queries

---
+++ Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/f_n_request_item_vdr_io_snapshot_weekly_c_age_key_mysql.sql
@@ -0,0 +1,24 @@
+
+
+SELECT CASE WHEN count(1)>0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)>0  THEN 'Age_key not populated correctly' ELSE 'SUCCESS' END as Message 
+FROM (          
+            select vendor_name_group_c,priority_src_key,business_service_key,week_start_key,floor((age/86400)/ending_backlog) as  age_populated ,
+case when floor((age/86400)/ending_backlog) between 0 and 1 then 71178
+     when floor((age/86400)/ending_backlog)  between 2 and 4 then 71179
+     when floor((age/86400)/ending_backlog)  between 5 and 7 then 71180
+     when floor((age/86400)/ending_backlog)  between 8 and 14 then 71181
+     when floor((age/86400)/ending_backlog)  between 15 and 28 then 71182
+     when floor((age/86400)/ending_backlog)  between 29 and 60 then 71183
+     when floor((age/86400)/ending_backlog)  between 61 and 1000000 then 71184 end  age_key_expected ,age_key age_key_actual
+     from  mcdonalds_mdwdb.f_n_request_item_vdr_io_snapshot_weekly_c
+     where   case when floor((age/86400)/ending_backlog) between 0 and 1 then 71178
+     when floor((age/86400)/ending_backlog)  between 2 and 4 then 71179
+     when floor((age/86400)/ending_backlog)  between 5 and 7 then 71180
+     when floor((age/86400)/ending_backlog)  between 8 and 14 then 71181
+     when floor((age/86400)/ending_backlog)  between 15 and 28 then 71182
+     when floor((age/86400)/ending_backlog)  between 29 and 60 then 71183
+     when floor((age/86400)/ending_backlog)  between 61 and 1000000 then 71184 end <>age_key)a;
+	 
+	 
+	 
