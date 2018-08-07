99015240f9a447cfce79ef9b36e491609a241e1e
SARITHAPODALI\Kishore <kishore.mantha@numerify.com>
2018-08-07 11:39:36 +0530

    Updated the QA Codes for Qualcomm

--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_major_incident_active_c_mysql.sql
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/d_major_incident_active_c_mysql.sql
@@ -1,4 +1,5 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+SELECT 
+CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_major_incident.active_c' ELSE 'SUCCESS' END as Message
 FROM qualcomm_mdsdb.u_major_incident_final SRC 
  LEFT JOIN qualcomm_mdwdb.d_major_incident_c TRGT 
--- Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_task_approval_c_changed_on_mysql.sql
+++ Apps 5.0/QUALCOMM/ITSA/FUNCTIONAL/f_task_approval_c_changed_on_mysql.sql
@@ -1,11 +1,11 @@
- SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_task_approval_c.changed_on
-' ELSE 'SUCCESS' END as Message
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for d_task_approval_c.changed_on' ELSE 'SUCCESS' END as Message
+FROM(SELECT Count(1) AS CNT 
 FROM qualcomm_mdsdb.sysapproval_approver_final SRC 
-LEFT JOIN qualcomm_mdsdb.task_final task 
-                ON SRC.sysapproval = task.sys_id 
-LEFT JOIN  qualcomm_mdwdb.d_task_approval_c TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
-WHERE SRC.sys_updated_on <> TRGT.changed_on
+LEFT JOIN qualcomm_mdsdb.task_final task  ON SRC.sysapproval = task.sys_id 
+LEFT JOIN  qualcomm_mdwdb.d_task_approval_c TRGT ON (SRC.sys_id =TRGT.row_id  AND SRC.sourceinstance= TRGT.source_id  )
+WHERE 
+CONVERT_TZ(SRC.sys_updated_on,'GMT','GMT') <> TRGT.changed_on
+) temp
  ;
