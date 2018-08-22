091f66f771c4a5c803b26141f4679b761e5a3308
vijay rao <vijay.rao@numerify.com>
2018-08-22 11:17:19 +0530

    DELV-13665,13667 Watson

--- Apps 5.0/WATSON/ITSA/FUNCTIONAL/f_case_last_meaningful_communication_duration_c_mysql.sql
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/f_case_last_meaningful_communication_duration_c_mysql.sql
@@ -5,15 +5,17 @@ CASE WHEN CNT > 0 THEN 'MDS to DWH data validation failed for f_case.last_meanin
 FROM 
 (
 SELECT Count(1) as CNT 
- FROM watson_mdsdb.sn_customerservice_case_final b
- left join watson_mdwdb.f_case a
-on a.row_id = b.sys_id and a.source_id = b.sourceinstance
+ from watson_mdsdb.sn_customerservice_case_final src 
+join watson_mdwdb.f_case trgt on src.sys_id=trgt.row_id and src.sourceinstance=trgt.source_id
 LEFT JOIN
-(select element_id,max(sys_created_on) as created_on from watson_mdsdb.sys_journal_field_final
-where sys_created_by not in ('system','IMS.SA') and element='comments' 
-group by 1 ) c ON a.row_id = c.element_id
+(select a.element_id,min(a.sys_created_on) as created_on from watson_mdsdb.sys_journal_field_final a
+join watson_mdsdb.sn_customerservice_case_final src2
+ON a.element_id = src2.sys_id and a.sourceinstance = src2.sourceinstance 
+join watson_mdsdb.sys_user_final src3 on src2.contact=src3.sys_id and src2.sourceinstance=src3.sourceinstance
+where a.sys_created_by not in ('system','IMS.SA') and a.element='comments' and src3.user_name<>a.sys_created_by
+group by 1 ) c ON trgt.row_id = c.element_id
 Where 
-a.last_meaningful_communication_duration_c <> timestampdiff(second, b.opened_at,c.created_on)
+trgt.last_meaningful_communication_duration_c <> timestampdiff(second, src.opened_at,c.created_on)
 ) temp;
 
 
