a50ebfc5eb3aa36e5a4b400c2543e9f63d554849
BINDU-PC\Bindu <bindu.k@numerify.com>
2018-09-19 12:39:38 +0530

    PDCHS

--- Apps 6.0/PDCHS/ITSA/FUNCTIONAL/d_category_c_ci_filter_msql.sql
+++ Apps 6.0/PDCHS/ITSA/FUNCTIONAL/d_category_c_ci_filter_msql.sql
@@ -1,8 +1,8 @@
 SELECT CASE WHEN CNT>0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN CNT>0  THEN 'MDS to DWH data validation failed for d_category_c.ci_filter' ELSE 'SUCCESS' END as Message  
+CASE WHEN CNT>0  THEN 'MDS to DWH data validation failed for d_category_c.ci_table' ELSE 'SUCCESS' END as Message  
 FROM 
 (select count(1) as CNT from 
 (select sys_id, sourceinstance, u_ci_filter from pdchs_mdsdb.u_category_final where CDCTYPE<>'D') src
 left join pdchs_mdwdb.d_category_c trgt 
 on src.sys_id=trgt.row_id and src.sourceinstance=trgt.source_id
-where coalesce(src.u_ci_filter, 'UNSPECIFIED') <>  trgt.ci_filter) temp;
+where coalesce(src.u_ci_filter, 'UNSPECIFIED') <>  trgt.ci_table) temp;
