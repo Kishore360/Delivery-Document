ccde59d1f16bfcfc657f7ed7065763a32e6f3525
prabhuram <prabhuram.sridharan@numerify.com>
2018-10-17 18:06:56 +0530

    Committing change request related items

--- Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_change_request_business_justification_c_mysql.sql
+++ Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_change_request_business_justification_c_mysql.sql
@@ -4,8 +4,8 @@ CASE WHEN CNT >0 THEN 'Failure' ELSE 'Data Matched' END as Message
 FROM (SELECT count(1) as CNT
 from
 mcdonalds_mdwdb.d_change_request d
-JOIN mcdonalds_mdsdb.change_request_final i on d.row_id = i.sys_id and d.source_id= i.sourceinstance
-LEFT JOIN mcdonalds_mdsdb.rm_enhancement_final r on i.u_enhancement=r.sys_id and i.sourceinstance=r.sourceinstance
-where d.justification_c <> i.u_business_justification)temp;
-
+JOIN  mcdonalds_mdsdb.change_request_final i on d.row_id = i.sys_id and d.source_id= i.sourceinstance
+-- LEFT JOIN mcdonalds_mdsdb.rm_enhancement_final r on i.u_enhancement=r.sys_id and i.sourceinstance=r.sourceinstance
+where  char_length(i.justification)=250 and
+ d.justification_c <> coalesce(i.justification,'UNSPECIFIED'))temp;
 
