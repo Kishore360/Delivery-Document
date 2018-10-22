15e3f3d55ec62760eafbc34e384a9b8af5fbd5f8
LAPTOP-6CMB9OTI\Admin <ashok.velu@numerify.com>
2018-10-22 14:58:11 +0530

    no message

--- Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_change_request_business_justification_c_mysql.sql
+++ Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_change_request_business_justification_c_mysql.sql
@@ -5,7 +5,5 @@ FROM (SELECT count(1) as CNT
 from
 mcdonalds_mdwdb.d_change_request d
 JOIN  mcdonalds_mdsdb.change_request_final i on d.row_id = i.sys_id and d.source_id= i.sourceinstance
--- LEFT JOIN mcdonalds_mdsdb.rm_enhancement_final r on i.u_enhancement=r.sys_id and i.sourceinstance=r.sourceinstance
-where  char_length(i.justification)=250 and
- d.justification_c <> coalesce(i.justification,'UNSPECIFIED'))temp;
+where  LEFT(d.justification_c,200) <> coalesce(LEFT(i.justification,200),'UNSPECIFIED'))temp;
 
