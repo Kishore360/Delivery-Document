cad483db53e4fcf094b077972bc8b3ea6516ebf2
LAPTOP-6CMB9OTI\Admin <ashok.velu@numerify.com>
2018-10-22 15:25:58 +0530

    incident task qa

---
+++ Apps 5.2/MCDONALDS/ITSA/FUNCTIONAL/d_incident_task_c_description_mysql.sql
@@ -0,0 +1,7 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_incident_task_c.description' ELSE 'SUCCESS' END as Message 
+FROM mcdonalds_mdwdb.d_incident_task_c trgt
+RIGHT JOIN mcdonalds_mdsdb.incident_task_final src
+on src.sys_id = trgt.row_id and src.sourceinstance = trgt.source_id
+where src.description <> trgt.description
+;
---
+++ Apps 5.2/MCDONALDS/ITSA/FUNCTIONAL/f_incident_task_c_dormant_flag_mysql.sql
@@ -0,0 +1,9 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_incident_task_c.dormant_flag' ELSE 'SUCCESS' END as Message 
+FROM mcdonalds_mdwdb.d_incident_task_c trgt
+RIGHT JOIN mcdonalds_mdsdb.incident_task_final src
+on src.sys_id = trgt.row_id and src.sourceinstance = trgt.source_id
+JOIN mcdonalds_mdwdb.d_lov_map lv_mp ON COALESCE(CONCAT('STATE~INCIDENT_TASK~~~',src.state),'UNSPECIFIED') = lv_mp.src_rowid AND lv_mp.dimension_wh_code ='OPEN'
+JOIN (select source_id,max(lastupdated) as lastupdated FROM mcdonalds_workdb.d_o_data_freshness group by source_id) df ON src.sourceinstance = df.source_id
+WHERE IF(TIMESTAMPDIFF(SECOND,CONVERT_TZ(src.sys_updated_on,'GMT','US/Central'),df.lastupdated) > 30 ,'Y','N')  <> trgt.dormant_flag
+;
