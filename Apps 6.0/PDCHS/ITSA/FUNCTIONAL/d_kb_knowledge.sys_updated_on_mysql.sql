11da6727a660ad66130f361ee15780011465f7de
BINDU-PC\Bindu <bindu.k@numerify.com>
2018-10-26 12:52:40 +0530

    PDCHS

--- Apps 6.0/PDCHS/ITSA/FUNCTIONAL/d_kb_knowledge.sys_updated_on_mysql.sql
+++ Apps 6.0/PDCHS/ITSA/FUNCTIONAL/d_kb_knowledge.sys_updated_on_mysql.sql
@@ -1,7 +1,13 @@
-SELECT CASE WHEN count(1)>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)>0 THEN 'MDS to DWH data validation failed for kb_knowledge.sys_updated_on' ELSE 'SUCCESS' END as Message 
-FROM pdchs_mdsdb.kb_knowledge_final  src
+SELECT 
+CASE WHEN cnt>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+
+CASE WHEN cnt>0 THEN 'MDS to DWH data validation failed for kb_knowledge.sys_updated_on' ELSE 'SUCCESS' END as Message 
+
+FROM 
+(select count(1) as cnt from 
+(select * from pdchs_mdsdb.kb_knowledge_final where CDCTYPE<>'D') src
+ 
 JOIN  pdchs_mdwdb.d_kb_knowledge_c trgt
-on src.sys_id = trgt.row_id and src.sourceinstance = trgt.source_id where 
-convert_tz( src.sys_updated_on, "GMT", "America/Los_Angeles") <> trgt.changed_on
-;
+on src.sys_id = trgt.row_id and src.sourceinstance = trgt.source_id 
+where 
+convert_tz( src.sys_updated_on, "GMT", "America/New_York") <> trgt.changed_on) temp;
