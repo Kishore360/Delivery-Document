1ad747b331341c3aa4a162fedd4033b23a8640db
prabhuram <prabhuram.sridharan@numerify.com>
2018-10-31 16:25:45 +0530

    removing pPAN folder

--- Apps 6.0/PAN/ITSA/FUNCTIONAL/f_incident_state_src_code_mysql.sql
+++
@@ -1,7 +0,0 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.state_src_code' ELSE 'SUCCESS' END as Message
- FROM pan6_mdsdb.incident_final SRC 
- LEFT JOIN pan6_mdwdb.f_incident TRGT 
- ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.incident_state,'')<> COALESCE(TRGT.state_src_code ,'')
