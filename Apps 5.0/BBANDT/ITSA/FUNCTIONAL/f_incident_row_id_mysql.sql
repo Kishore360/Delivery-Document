07dc264fff7bb0486f90e0f73816294526639c8a
BINDU-PC\Bindu <bindu.k@numerify.com>
2018-10-31 15:01:55 +0530

    BBANDT

--- Apps 5.0/BBANDT/ITSA/FUNCTIONAL/f_incident_row_id_mysql.sql
+++ Apps 5.0/BBANDT/ITSA/FUNCTIONAL/f_incident_row_id_mysql.sql
@@ -1,12 +1,26 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
- FROM bbandt_mdsdb.incident_final SRC 
+SELECT 
+CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ 
+CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
+ 
+FROM 
+ 
+(select count(1) as cnt 
+from
+ 
+bbandt_mdsdb.incident_final SRC 
+
 left join bbandt_mdsdb.sys_user_group_final lkp 
+
 on SRC.assignment_group = lkp.sys_id 
 and SRC.sourceinstance = lkp.sourceinstance
- LEFT JOIN bbandt_mdwdb.f_incident TRGT 
- ON (SRC.sys_id =TRGT.row_id  
+ 
+LEFT JOIN bbandt_mdwdb.f_incident TRGT 
+ 
+ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.sys_id,'')<> COALESCE(TRGT.row_id ,'') -- 46303
- and ((lkp.name not like 'ITSM-LOB-Branchinfo%' and name not like 'ITSM-LOB-HumanSystems%' and name not like 'ITSM-LOB-CPS%'
-and name not like 'ITSM-LOB-LoanServices%' and name not like 'ITSM-LOB-Wealth%') or lkp.name is null);
+ 
+WHERE COALESCE( SRC.sys_id,'')<> COALESCE(TRGT.row_id ,'') 
+ 
+and ((lkp.name not like 'ITSM-LOB-Branchinfo%' and name not like 'ITSM-LOB-HumanSystems%' and name not like 'ITSM-LOB-CPS%'
+and name not like 'ITSM-LOB-LoanServices%' and name not like 'ITSM-LOB-Wealth%') or lkp.name is null)) temp;
