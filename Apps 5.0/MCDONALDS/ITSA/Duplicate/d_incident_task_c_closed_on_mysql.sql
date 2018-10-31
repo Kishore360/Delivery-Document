8c4403a4130fbfac9b0304d05c634f217468fcf8
Sneha <sneha.v@numerify.com>
2018-10-31 15:59:59 +0530

    no message

--- Apps 5.0/MCDONALDS/ITSA/Duplicate/d_incident_task_c_closed_on_mysql.sql
+++
@@ -1,9 +0,0 @@
-SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
-,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
-ELSE 'Data Matched' END AS Message 
-FROM (
-select count(1) as cnt 
-from mcdonalds_mdsdb.u_incident_task_final s
-left join mcdonalds_mdwdb.d_incident_task_c t
-on s.sys_id=t.row_id and s.sourceinstance = t.source_id
-where CONVERT_TZ(s.closed_at,'GMT','America/Los_Angeles')<>t.closed_on) temp
--- Apps 5.0/MCDONALDS/ITSA/Duplicate/d_request_item_service_category_src_c_code_mysql.sql
+++
@@ -1,8 +0,0 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_request_item.service_category_src_c_code' ELSE 'SUCCESS' END as Message 
-FROM mcdonalds_mdwdb.d_request_item trgt
-RIGHT JOIN (select * from mcdonalds_mdsdb.sc_req_item_final where cdctype<>'D') src
-on src.sys_id = trgt.row_id and src.sourceinstance = trgt.source_id
-left join (select source_id,max(lastupdated) as lastupdated from mcdonalds_mdwdb.d_o_data_freshness group by source_id) f1 on (f1.source_id = src.sourceinstance)
-where (src.cdctime<=f1.lastupdated) and src.u_service_category <> trgt.service_category_src_c_code
-;
