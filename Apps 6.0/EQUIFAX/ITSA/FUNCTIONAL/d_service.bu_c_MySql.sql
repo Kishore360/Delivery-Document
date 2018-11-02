56aaabd43b4f0a7d3d4ee8c71e766f330bfe8573
Sneha <sneha.v@numerify.com>
2018-11-02 12:58:24 +0530

    Tweaked failed Equifax cust queries

--- Apps 6.0/EQUIFAX/ITSA/FUNCTIONAL/d_service.bu_c_MySql.sql
+++ Apps 6.0/EQUIFAX/ITSA/FUNCTIONAL/d_service.bu_c_MySql.sql
@@ -1,6 +1,6 @@
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_service.name' 
 ELSE 'SUCCESS' END as Message 
-from equifax_mdsdb.cmdb_ci_service_final src
+from (select * from equifax_mdsdb.cmdb_ci_service_final where cdctype<>'D') src
  left join equifax_mdwdb.d_service t on RIGHT(t.row_id,32)=src.sys_id and src.sourceinstance=t.source_id
 where coalesce(src.u_bu_coe,'UNSPECIFIED')<>t.bu_c;
 
