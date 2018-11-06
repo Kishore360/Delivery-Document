fd2610d9b70494f99242113a5744e41b4ece26e7
BINDU-PC\Bindu <bindu.k@numerify.com>
2018-11-06 15:16:18 +0530

    FIDELITY_change_failure

--- Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_change_failure/d_change_failure/d_change_failure_opened_on_key.sql
+++ Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_change_failure/d_change_failure/d_change_failure_opened_on_key.sql
@@ -1,12 +1,15 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_failure.opened_on_key' ELSE 'SUCCESS' END as Message
-from (SELECT * FROM fidelity_mdsdb.change_request_final WHERE CDCTYPE<>'D') SRC
-left join (SELECT * FROM fidelity_mdwdb.d_change_failure where year(effective_to)=2999 
-) TRGT 
- ON (SRC.sys_id=TRGT.row_id 
- AND SRC.sourceinstance=TRGT.source_id)
+SELECT 
+CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ 
+CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_change_failure.opened_on_key' ELSE 'SUCCESS' END as Message
+ 
+from (select count(1) as cnt from (SELECT * FROM fidelity_mdsdb.change_request_final WHERE CDCTYPE<>'D') SRC
+
+left join (SELECT * FROM fidelity_mdwdb.d_change_failure where year(effective_to)=2999) TRGT 
+ 
+ON (SRC.sys_id=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id) 
 
 LEFT JOIN fidelity_mdwdb.d_calendar_date LKP 
-on (LKP.row_id  = date_format(convert_tz(SRC.opened_at,'<<TENANT_SSI_TIME_ZONE>>','<<DW_TARGET_TIME_ZONE>>'),'%Y%m%d') and LKP.source_id=0)
-
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.opened_at IS NULL THEN '' else '' end)<> COALESCE(TRGT.opened_on_key,'')
+on (LKP.row_id  = date_format(convert_tz(SRC.opened_at,'GMT','America/New_York'),'%Y%m%d') and LKP.source_id=0)
+ 
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.opened_at IS NULL THEN 0 else -1 end) <> TRGT.opened_on_key) temp;
--- Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_change_failure_oversight_group_name_mysql.sql
+++ Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_change_failure_oversight_group_name_mysql.sql
@@ -1,8 +1,15 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_outage.operational_flag_c' ELSE 'SUCCESS' END as Message FROM 
-fidelity_mdsdb.change_request_final  SRC 
+SELECT 
+CASE WHEN cnt>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+
+CASE WHEN cnt>0  THEN 'MDS to DWH data validation failed for d_change_failure.oversight_group_name_c' ELSE 'SUCCESS' END as Message 
+
+FROM (select count(1) as cnt from fidelity_mdsdb.change_request_final  SRC 
+
 join 
 fidelity_mdsdb.sys_user_group_final SRC1 
-ON SRC.u_bu_oversight_group=SRC1.sys_id
+ON SRC.u_bu_oversight_group=SRC1.sys_id 
+
 JOIN fidelity_mdwdb.d_change_failure TRGT 
 ON (SRC.sys_id = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id ) 
- WHERE COALESCE(SRC1.name,'UNSPECIFIED') <> (TRGT.oversight_group_name) 
+ 
+WHERE COALESCE(SRC1.name,'UNSPECIFIED') <> (TRGT.oversight_group_name_c) ) temp;
