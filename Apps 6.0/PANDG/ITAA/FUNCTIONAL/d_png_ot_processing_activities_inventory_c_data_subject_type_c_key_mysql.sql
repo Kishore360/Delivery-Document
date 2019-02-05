acfe3541730c4567ad2a0c7701c5c44a6d095abe
SARITHAPODALI\Kishore <kishore.mantha@numerify.com>
2019-02-05 12:07:56 +0530

    Updating the QA Codes

--- Apps 6.0/PANDG/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_c_data_subject_type_c_key_mysql.sql
+++ Apps 6.0/PANDG/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_c_data_subject_type_c_key_mysql.sql
@@ -1,5 +1,7 @@
- SELECT CASE WHEN count(1)>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)>0 THEN 'MDS to DWH data validation failed for d_png_ot_processing_activities_inventory_c.data_subject_type_c_key' ELSE 'SUCCESS' END as Message 
+ SELECT 
+ CASE WHEN count(1)> 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)> 0 THEN 'MDS to DWH data validation failed for d_png_ot_processing_activities_inventory_c.data_subject_type_c_key' ELSE 'SUCCESS' END as Message 
+
 FROM pandg_mdsdb.pg_ot_processing_activities_inventory_final  src
 LEFT JOIN  pandg_mdwdb.d_png_ot_processing_activities_inventory_c trgt
 on src.inventory_id = trgt.row_id and src.sourceinstance = trgt.source_id
