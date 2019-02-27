318dd467efe336a9fc5112754a87bd8e55b372e8
DESKTOP-PLTURRV\Shubha <shubha.rao@numerify.com>
2019-02-27 11:52:02 +0530

    Updating NBCU cust queries

---
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_application.ci_enforce_account_lockout_after_failed_logins_c_key_MYSQL.sql
@@ -0,0 +1,15 @@
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT > 0 THEN 'MDS to DWH data validation failed for d_application.ci_enforce_account_lockout_after_failed_logins_c_key' ELSE 'SUCCESS' END as Message 
+FROM (
+SELECT count(1) as CNT
+FROM nbcu_mdsdb.cmdb_ci_appl_final SRC
+LEFT JOIN nbcu_mdwdb.d_lov LKP 
+ON (coalesce(CONCAT('ENFORCE_ACCOUNT_LOCKOUT_AFTER_FAILED_LOGINS','~','APPLICATION~~~',SRC.u_enforce_account_lockout_afte),'UNSPECIFIED')=LKP.row_id  AND
+SRC.sourceinstance=LKP.source_id)
+JOIN nbcu_mdwdb.d_application TRGT
+  ON (concat('APPLICATION','~',SRC.sys_id)= TRGT.row_id AND SRC.sourceinstance=TRGT.source_id)  and TRGT.soft_deleted_flag <>'Y'
+ WHERE COALESCE(LKP.row_key, CASE WHEN SRC.u_enforce_account_lockout_afte IS NULL THEN 0 ELSE -1 END)
+  <>TRGT.ci_enforce_account_lockout_after_failed_logins_c_key
+)temp;
+
---
+++ Apps 3.0/NBCU/ITSA/FUNCTIONAL/d_application.ci_gdpr_c_key_MYSQL.sql
@@ -0,0 +1,15 @@
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT > 0 THEN 'MDS to DWH data validation failed for d_application.ci_gdpr_c_key' ELSE 'SUCCESS' END as Message 
+FROM (
+SELECT count(1) as CNT
+FROM  nbcu_mdsdb.cmdb_ci_appl_final SRC
+LEFT JOIN nbcu_mdwdb.d_lov LKP 
+ON (coalesce(CONCAT('GDPR','~','APPLICATION~~~',SRC.u_safe_harbor),'UNSPECIFIED')=LKP.row_id AND
+SRC.sourceinstance=LKP.source_id)
+JOIN nbcu_mdwdb.d_application TRGT
+  ON (concat('APPLICATION','~',SRC.sys_id)= TRGT.row_id AND SRC.sourceinstance=TRGT.source_id)  and TRGT.soft_deleted_flag <>'Y'
+ WHERE COALESCE(LKP.row_key, CASE WHEN SRC.u_safe_harbor IS NULL THEN 0 ELSE -1 END)
+  <>TRGT.ci_gdpr_c_key
+)temp;
+
