f6b70b6d602bcb8dda413d2c05a5e446072b84f6
prabhuram <prabhuram.sridharan@numerify.com>
2019-03-26 14:44:22 +0530

    allcommitts

--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_incident_reported_type_src_key_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_incident_reported_type_src_key_mysql.sql
@@ -1,11 +1,13 @@
 
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.reported_type_src_key' ELSE 'SUCCESS' END as Message
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.reported_type_src_key' ELSE 'SUCCESS' END as Message from 
+ (select count(1) cnt 
  FROM cardinalhealth_mdsdb.incident_final SRC 
  LEFT JOIN cardinalhealth_mdwdb.f_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
 LEFT JOIN cardinalhealth_mdwdb.d_lov LKP 
- ON ( concat('CONTACT_TYPE','~','INCIDENT','~','~','~',upper(contact_type))= LKP.src_rowid 
-AND SRC.sourceinstance= LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.contact_type IS NULL THEN 0 else -1 end)<> (TRGT.reported_type_src_key)
+ ON ( upper(contact_type)= substring(LKP.row_id,25,length(LKP.row_id)) )
+ WHERE  LKP.soft_deleted_flag='N' and TRGT.soft_deleted_flag='N'
+ and LKP.dimension_class ='CONTACT_TYPE~INCIDENT' and  COALESCE(LKP.row_key,CASE WHEN SRC.contact_type IS NULL THEN 0 else -1 end)<> (TRGT.reported_type_src_key)
+)a
--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_time_entry_c_state_src_key_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_time_entry_c_state_src_key_mysql.sql
@@ -1,11 +1,13 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_time_entry_c.state_src_key' ELSE 'SUCCESS' END as Message
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_time_entry_c.state_src_key' ELSE 'SUCCESS' END as Message
+ FROM (select count(1) cnt
  FROM cardinalhealth_mdsdb.time_card_final SRC
 LEFT JOIN cardinalhealth_mdwdb.d_lov LKP 
  ON  
-( concat('STATE~TIME_CARD~~~',upper(state))= LKP.src_rowid 
+( upper(state)= substring(row_id,19,length(row_id)) 
 AND SRC.sourceinstance= LKP.source_id ) 
- LEFT JOIN cardinalhealth_mdwdb.f_time_entry_c TRGT 
+  JOIN cardinalhealth_mdwdb.f_time_entry_c TRGT 
  ON (SRC.sys_id =LEFT(TRGT.row_id,32 )  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE  LKP.dimension_class = 'STATE~TIME_CARD' and  COALESCE(LKP.row_key,CASE WHEN SRC.state IS NULL THEN 0 else -1 end)<> COALESCE(TRGT.state_src_key)
+ WHERE LKP.soft_deleted_flag='N' and TRGT.soft_deleted_flag='N'
+ and LKP.dimension_class = 'STATE~TIME_CARD' and  COALESCE(LKP.row_key,CASE WHEN SRC.state IS NULL THEN 0 else -1 end)<> COALESCE(TRGT.state_src_key))a
