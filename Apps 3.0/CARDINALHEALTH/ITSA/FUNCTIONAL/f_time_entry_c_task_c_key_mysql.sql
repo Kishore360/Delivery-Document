ff395dea6d1c3a74c6fe2b473dc19a5ff7c38aed
prabhuram <prabhuram.sridharan@numerify.com>
2019-03-27 17:48:36 +0530

    allcommit

--- Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_time_entry_c_task_c_key_mysql.sql
+++ Apps 3.0/CARDINALHEALTH/ITSA/FUNCTIONAL/f_time_entry_c_task_c_key_mysql.sql
@@ -1,12 +1,21 @@
-SELECT 
-CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
-FROM (SELECT count(1) as CNT
+ 
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END AS Result
+,CASE WHEN cnt > 0 THEN 'Data did not Match.' 
+ELSE 'Data Matched' END AS Message 
+from(select count(1)cnt  from
+(SELECT concat(SRC.sys_id,'~',b.day_name) ABC,SRC.sourceinstance src,coalesce(c.row_key,case when SRC.task is null then 0 else -1 end) sr
  FROM cardinalhealth_mdsdb.time_card_final SRC 
- LEFT JOIN cardinalhealth_mdwdb.d_task_c LKP
- on LKP.row_id = COALESCE(SRC.task,'UNSPECIFIED') and LKP.source_id  = SRC.sourceinstance
- JOIN cardinalhealth_mdwdb.d_calendar_date b ON SRC.week_starts_on = b.week_start_date
- LEFT JOIN cardinalhealth_mdwdb.f_time_entry_c TRGT 
- ON (concat(SRC.sys_id,'~',b.day_name) =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )
- WHERE coalesce(LKP.row_key,case when SRC.task is null then 0 else -1 end) <> TRGT.task_c_key )temp;
+ JOIN cardinalhealth_mdwdb.d_calendar_date b ON SRC.week_starts_on = b.week_start_date and SRC.sourceinstance=b.source_id
+ and b.week_start_date>='2018-11-05'
+ left join cardinalhealth_mdwdb.d_task_c c
+on c.row_id=SRC.task and c.soft_deleted_flag='N'  
+and SRC.sourceinstance=c.source_id 
+)a
+
+JOIN 
+
+(select row_id,source_id,task_c_key vp from cardinalhealth_mdwdb.f_time_entry_c where soft_deleted_flag='N' and
+ week_starts_on>='2018-11-05 00:00:00'  ) TRGT 
+ ON  a.ABC =TRGT.row_id  AND a.src= TRGT.source_id  
+ where a.sr<>TRGT.vp 
+  ) a;
