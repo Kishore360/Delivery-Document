d8870197350cccfa81a6a0a63f0fdfa99a61e341
prabhuram <prabhuram.sridharan@numerify.com>
2019-04-02 14:57:17 +0530

    committing

--- Apps 6.0/TJXCO/ITSA/FUNCTIONAL/f_case_company_key_msql.sql
+++ Apps 6.0/TJXCO/ITSA/FUNCTIONAL/f_case_company_key_msql.sql
@@ -3,4 +3,6 @@ case when cnt then 'Data mismatch for f_case.company_key' else 'Matching' end as
 from  tjxco_mdsdb.sn_customerservice_case_final src
 join tjxco_mdwdb.f_case tgt
 on src.sys_id=tgt.row_id and src.sourceinstance=tgt.source_id
-where (case when src.company is null then 'UNSPECIFIED' else concat('SUBSIDIARY~',src.company) end)<>tgt.company_key)a;
+left join tjxco_mdwdb.d_case lkp
+on COALESCE(concat('SUBSIDIARY~',src.company),'UNSPECIFIED')=lkp.row_id and src.sourceinstance=lkp.source_id
+where COALESCE(lkp.row_key,case when src.company is null then 0 else -1 end)<>tgt.company_key)a;
