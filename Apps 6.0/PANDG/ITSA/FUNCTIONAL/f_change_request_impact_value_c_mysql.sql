9e34c5ca9dcbb2081b7631c8e987a017f210fe5f
SARITHAPODALI\Kishore <kishore.mantha@numerify.com>
2019-04-11 11:46:26 +0530

    Updated the QA Code

--- Apps 6.0/PANDG/ITSA/FUNCTIONAL/f_change_request_impact_value_c_mysql.sql
+++ Apps 6.0/PANDG/ITSA/FUNCTIONAL/f_change_request_impact_value_c_mysql.sql
@@ -1,5 +1,6 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.open_to_close_duration' ELSE 'SUCCESS' END as Message
+SELECT 
+CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) > 0 THEN 'MDS to DWH data validation failed for f_change_request.impact_value_c' ELSE 'SUCCESS' END as Message
 FROM png_mdsdb.change_request_final a
  join png_mdwdb.f_change_request b
 on a.sys_id=b.row_id and a.sourceinstance=b.source_id
