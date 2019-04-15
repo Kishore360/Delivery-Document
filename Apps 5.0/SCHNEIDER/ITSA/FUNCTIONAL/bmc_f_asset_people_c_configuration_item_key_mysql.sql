2c99029d6e6af7175a32715f65ef22d55795ad9c
Sneha <sneha.v@numerify.com>
2019-04-15 13:59:19 +0530

    no message

--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/bmc_f_asset_people_c_configuration_item_key_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/bmc_f_asset_people_c_configuration_item_key_mysql.sql
@@ -1,17 +1,16 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset.configuration_item_key' ELSE 'SUCCESS' END as Message
+ SELECT CASE WHEN count(1) > 0 THEN 'SUCCESS' ELSE 'FAILURE' END as Result,
+ CASE WHEN count(1) >0 THEN 'SUCCESS' ELSE 'MDS to DWH data validation failed for f_asset_people_c.configuration_item_key'  END as Message
+ -- SELECT COUNT(1)
  FROM schneider_mdsdb.ast_assetpeople_final SRC 
- left join (select reconciliationidentity reconciliationidentity, sourceinstance sourceinstance, max(requestid) as request_id 
+ left join (select reconciliationidentity, sourceinstance , max(requestid) as request_id 
 from schneider_mdsdb.bmc_core_bmc_baseelement_final bse
 where datasetid = 'BMC.ASSET'
-group by reconciliationidentity , sourceinstance) bmc on SRC.assetinstanceid = bmc.reconciliationidentity and SRC.sourceinstance = bmc.sourceinstance
+group by reconciliationidentity , sourceinstance) bmc 
+on SRC.assetinstanceid = bmc.reconciliationidentity and SRC.sourceinstance = bmc.sourceinstance
   JOIN  schneider_mdwdb.f_asset_people_c TRGT 
  ON (SRC.instanceid=TRGT.row_id 
  AND SRC.sourceinstance=TRGT.source_id )
  LEFT JOIN schneider_mdwdb.d_configuration_item LKP 
- ON ( COALESCE(bmc.request_id,SRC.assetinstanceid)= LKP.row_id 
+ ON ( COALESCE(bmc.request_id,SRC.assetinstanceid,'UNSPECIFIED')= LKP.row_id 
 AND SRC.sourceinstance = LKP.source_id )
  WHERE COALESCE(LKP.row_key,CASE WHEN COALESCE(bmc.request_id,SRC.assetinstanceid) IS NULL THEN 0 else '-1' end)<>(TRGT.configuration_item_key)
-
- 
- 
--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_location_region_c_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_location_region_c_mysql.sql
@@ -5,5 +5,5 @@ CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_location.regio
 FROM schneider_mdsdb.hpd_help_desk_final  SRC 
 JOIN schneider_mdwdb.d_location TRGT 
 ON (SRC.site_id = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )
- WHERE coalesce(SRC.region,'UNSPECIFIED')  = (TRGT.region_c) 
+ WHERE coalesce(SRC.region,'UNSPECIFIED')  <> (TRGT.region_c) 
  and TRGT.soft_deleted_flag='N';
