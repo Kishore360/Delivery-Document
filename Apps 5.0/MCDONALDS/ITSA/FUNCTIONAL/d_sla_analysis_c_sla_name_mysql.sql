fdcd8d557f2178fc4b76397b3b7c94de0ec1ca76
Sneha <sneha.v@numerify.com>
2019-04-23 14:43:43 +0530

    no message

--- Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_sla_analysis_c_sla_name_mysql.sql
+++ Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_sla_analysis_c_sla_name_mysql.sql
@@ -1,24 +1,16 @@
-SELECT CASE WHEN cnt>0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN cnt>0  THEN 'MDS to DWH data validation failed for d_sla_analysis_c.priority_src_code' ELSE 'SUCCESS' END as Message
-from (select count(1)cnt from
-(select a1.priority , trgt.priority_src_code,a1.sys_id,a1.s2,a1.t,a1.s from 
-(select d.sys_name n,CONCAT(src.sys_id,'~',c.sla,'~',c.sys_id) xyz, src1.priority priority,src1.sys_id sys_id,src.sys_id s2,c.task t,c.sla s
-from (select sys_id, sourceinstance,priority from   mcdonalds_mdsdb.incident_final where CDCTYPE<>'D') src1 
-join (select sys_id, sourceinstance,cdctime from   mcdonalds_mdsdb.task_final where sys_class_name='incident' and CDCTYPE<>'D') src 
-on src.sys_id=src1.sys_id and src.sourceinstance=src1.sourceinstance
- join (select sys_id, sla,task,sourceinstance  from   mcdonalds_mdsdb.task_sla_final where CDCTYPE<>'D') c on src.sys_id=c.task 
- and c.sourceinstance=src1.sourceinstance
-join (select sys_id,sourceinstance,sys_name from   mcdonalds_mdsdb.contract_sla_final where CDCTYPE<>'D') d on c.sla=d.sys_id  and d.sourceinstance=src1.sourceinstance
- and d.sourceinstance=c.sourceinstance
+SELECT CASE WHEN count(1)>0  THEN 'FAILURE' ELSE  'SUCCESS'  END as Result, 
+CASE WHEN count(1)>0  THEN  'Data mismatch' ELSE  'SUCCESS'  END as Message 
+from mcdonalds_mdsdb.task_final a
+join mcdonalds_mdsdb.incident_final b 
+ON a.sys_id = b.sys_id and a.sourceinstance = b.sourceinstance and  b.CDCTYPE<>'D'
+LEFT JOIN mcdonalds_mdsdb.task_sla_final c 
+ON a.sys_id = c.task AND a.sourceinstance = c.sourceinstance and  c.CDCTYPE<>'D'
+LEFT JOIN mcdonalds_mdsdb.contract_sla_final d 
+ON c.sla = d.sys_id AND c.sourceinstance = d.sourceinstance  and d.CDCTYPE<>'D'
+join mcdonalds_mdwdb.d_sla_analysis_c e 
+on CONCAT(a.sys_id, '~',COALESCE(c.sla,'UNSPECIFIED'),'~',COALESCE(c.sys_id,'UNSPECIFIED'))=e.row_id and d.sourceinstance=e.source_id
  join (select source_id,max(lastupdated) as lastupdated from mcdonalds_mdwdb.d_o_data_freshness group by source_id) f1
- on (f1.source_id = src.sourceinstance) -- where src1.sys_id='02eb897adb8f1f046a1c790aaf9619d4'
- -- and c.sla='09ccae7c4f311200dd83afdd0210c76f'
- -- and c.sys_id='92eb0dfadb8f1f046a1c790aaf961970'
- and  (src.cdctime<=f1.lastupdated))a1
- join   mcdonalds_mdwdb.d_sla_analysis_c trgt on  a1.xyz =trgt.row_id
-  where trgt.soft_deleted_flag='N'  and  a1.n <> trgt.sla_name)a) temp;
-  
-  
-  
-  
-  
+ on (f1.source_id = c.sourceinstance) 
+ and  (c.cdctime<=f1.lastupdated)
+WHERE c.stage <> 'cancelled'   and a.CDCTYPE ='X'
+and COALESCE(c.sla,'UNSPECIFIED')<>e.sla_name;
--- Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_incident_resolved_by_hd_flag_c_mysql.sql
+++ Apps 5.0/SCHNEIDER/ITSA/FUNCTIONAL/d_incident_resolved_by_hd_flag_c_mysql.sql
@@ -1,10 +1,10 @@
 SELECT CASE WHEN count(1)  THEN 'SUCCESS' ELSE 'FAILURE' END as Result, 
-CASE WHEN count(1) THEN 'SUCCESS' ELSE 'MDS to DWH data validation failed for d_work_order.met_sla_flag' ELSE END as Message
+CASE WHEN count(1) THEN 'SUCCESS' ELSE 'MDS to DWH data validation failed for d_work_order.resolved_by_hd_flag_c' END as Message
 FROM schneider_workdb.d_incident TRGT 
 join schneider_mdsdb.hpd_help_desk_final SRC on SRC.entry_id=TRGT.row_id and SRC.sourceinstance=TRGT.source_id
 JOIN (select * from schneider_mdwdb.d_internal_organization where support_group_role_c='Help Desk' and group_flag='Y') lkp
 ON concat('GROUP~',SRC.assigned_group_id) = lkp.row_id  AND SRC.sourceinstance = lkp.source_id   
 left join (select * from schneider_mdwdb.d_lov_map  where   dimension_class='STATE~INCIDENT') lkp1 
 on lkp1.src_rowid = CONCAT('STATE','~','INCIDENT','~','~','~',UPPER(SRC.status))  and lkp1.source_id = SRC.sourceinstance
- WHERE case when  lkp1.dimension_wh_code = 'RESOLVED' then 'Y' else 'N' end <> TRGT.resolved_by_hd_flag_c ;
- 
+ WHERE case when  lkp1.dimension_wh_code = 'RESOLVED' then 'Y' else 'N' end <> TRGT.resolved_by_hd_flag_c 
+ and TRGT.soft_deleted_flag='N';
