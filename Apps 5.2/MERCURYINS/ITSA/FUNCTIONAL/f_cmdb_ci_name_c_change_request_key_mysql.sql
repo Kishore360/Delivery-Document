9cd3124b1153dbba038d38fc9b65c6e9929f8af0
Narmadha_D <narmadha.d@numerify.com>
2019-05-03 10:57:57 +0530

    creating index on long running query

--- Apps 5.2/MERCURYINS/ITSA/FUNCTIONAL/f_cmdb_ci_name_c_change_request_key_mysql.sql
+++ Apps 5.2/MERCURYINS/ITSA/FUNCTIONAL/f_cmdb_ci_name_c_change_request_key_mysql.sql
@@ -1,11 +1,8 @@
-create index change_fin_index on mercuryins_mdsdb.change_request_final (sys_id,sourceinstance); -- ,u_ci_name: 0.59 sec
-create index cmdb_ci_fact_db_index on mercuryins_mdwdb.f_cmdb_ci_name_c (row_id,source_id);  -- ,change_request_key : 0.18 sec
-
 SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
 CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_task_c.urgency' ELSE 'SUCCESS' END as Message 
 FROM (SELECT CONCAT(cr.sys_id,'~',COALESCE(SUBSTRING_INDEX(SUBSTRING_INDEX(TRIM(cr.u_ci_name), ',', n.n), ',', -1),'UNSPECIFIED')) AS row_id,
        cr.sourceinstance AS source_id,
-      cr.sys_updated_by AS changed_by
+       cr.sys_id AS change_request_id
 FROM mercuryins_mdsdb.change_request_final cr
 CROSS JOIN
   (SELECT a.N + b.N * 10 + c.N * 100 + 1 AS n
@@ -17,7 +14,6 @@ CROSS JOIN
 ON n.n <= 1 + (LENGTH(COALESCE(TRIM(cr.u_ci_name),'UNSPECIFIED')) - LENGTH(REPLACE(COALESCE(TRIM(cr.u_ci_name),'UNSPECIFIED'), ',', '')))) SRC 
 JOIN mercuryins_mdwdb.f_cmdb_ci_name_c TRGT 
 ON SRC.Row_id=TRGT.row_id and SRC.source_id=TRGT.source_id
-WHERE SRC.changed_by<>TRGT.changed_by;
-
-Drop index change_fin_index on mercuryins_mdsdb.change_request_final;
-Drop index cmdb_ci_fact_db_index on mercuryins_mdwdb.f_cmdb_ci_name_c;
+join mercuryins_mdwdb.d_change_request LKP
+on SRC.change_request_id=LKP.row_id
+WHERE coalesce(LKP.row_key,case when SRC.change_request_id is null then  0 else -1 end )<>TRGT.change_request_key
