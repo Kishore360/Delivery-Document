73049174c17615bf95167f6054c2ec7976f088e9
prabhuram <prabhuram.sridharan@numerify.com>
2019-05-09 15:13:45 +0530

    committing

---
+++ Apps 3.0/MCDONALDS/ITSA/FUNCTIONAL/f_incident_priority_dq_c_mysql.sql
@@ -0,0 +1,20 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.dormant_flag' ELSE 'SUCCESS' END as Message
+FROM (
+select number,assignment_group ,priority_dq_c,
+(case when priority  is  null  then 0 
+   else 1 end 
+)  
+from  mcdonalds_mdsdb.incident_final incident
+left join  mcdonalds_mdwdb.f_incident tgt
+on incident.sys_id=tgt.row_id and incident.sourceinstance=tgt.source_id
+where  tgt.priority_dq_c<>(case when priority  is  null  then 0 
+   else 1 end 
+)  
+
+ )a
+ 
+ 
+ 
+ 
+ 
--- Apps 6.0/TJXCO/ITSA/FUNCTIONAL/d_case_ca_standards_c_key_msql.sql
+++ Apps 6.0/TJXCO/ITSA/FUNCTIONAL/d_case_ca_standards_c_key_msql.sql
@@ -1,6 +1,7 @@
 select case when count(1)>1 then 'Not Matching' else 'Matching' end as result,
 case when count(1)>1 then 'Data mismatch for d_case.ca_standards_c' else 'Matching' end as message
 from  tjxco_mdsdb.sn_customerservice_case_final src
+left join tjxco_mdwdb.d_lov d on concat('CA_STANDARD~CASE~',u_ca_standards)=row_id 
 join tjxco_mdwdb.d_case tgt
 on src.sys_id=tgt.row_id and src.sourceinstance=tgt.source_id
-where src.u_ca_standards<>tgt.ca_standards_c;
+where coalesce(d.row_key,case when src.u_ca_standards is null then 0 else -1 end)<>tgt.ca_standard_c_key;
