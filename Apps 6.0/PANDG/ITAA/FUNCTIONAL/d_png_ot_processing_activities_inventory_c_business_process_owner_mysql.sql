796bc637be53b87b478dd9b75fce7f4597fee3ec
SARITHAPODALI\Kishore <kishore.mantha@numerify.com>
2019-05-14 18:24:45 +0530

    Updated as part of DELV-16846

--- Apps 6.0/PANDG/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_c_business_process_owner_mysql.sql
+++ Apps 6.0/PANDG/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_c_business_process_owner_mysql.sql
@@ -1,7 +1,18 @@
-SELECT CASE WHEN count(1)>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)>0 THEN 'MDS to DWH data validation failed for d_png_ot_processing_activities_inventory_c.business_process_owner' ELSE 'SUCCESS' END as Message 
-FROM png_mdsdb.pg_ot_processing_activities_inventory_final  src
-LEFT JOIN  png_mdwdb.d_png_ot_processing_activities_inventory_c trgt
-on src.inventory_id = trgt.row_id and src.sourceinstance = trgt.source_id
-where case when src.Business_Process_Owner IS NULL or src.Business_Process_Owner = ''
-      then 'UNSPECIFIED'  else src.Business_Process_Owner end <> trgt.business_process_owner
+--- DELV-16846
+
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT > 0 THEN 'MDS to DWH data validation failed for d_png_ot_processing_activities_inventory_c.business_process_owner' ELSE 'SUCCESS' END as Message
+FROM
+(
+SELECT 
+Count(1) as CNT 
+FROM png_mdsdb.pg_ot_processing_activities_inventory_final SRC 
+LEFT JOIN png_mdsdb.pg_ot_user_list_c_final SRC1 ON SRC.Business_Process_Owner=SRC1.User_Id
+LEFT JOIN png_mdwdb.d_png_ot_processing_activities_inventory_c TRGT ON SRC.inventory_id=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id
+WHERE 
+CASE WHEN SRC1.User_Name IS NULL OR SRC1.User_Name ='' 
+THEN 'UNSPECIFIED' ELSE SRC1.User_Name END<>TRGT.business_process_owner
+AND SRC.cdctype='X';
+
+) temp;
