a15f37aa5765008b99a71e9a240c0fe9e48d5823
Sneha <sneha.v@numerify.com>
2019-05-28 16:48:53 +0530

    no message

---
+++ Apps 5.0/GILEAD/ITSA/FUNCTIONAL/d_schedule_c_name_mysql.sql
@@ -0,0 +1,9 @@
+select case when count(1) then 'FAILURE' else 'SUCCESS' end as RESULT,
+case when count(1) then 'Data Mismatch' else 'SUCCESS' end as Message
+from ( select a.sys_id,b.row_id,a.sourceinstance,b.source_id,a.name name_src,b.name name_trgt
+from gilead_mdsdb.cmn_schedule_final a
+join gilead_mdwdb.d_schedule_c b
+on a.sys_id=b.row_id and a.sourceinstance=b.source_id
+where coalesce(a.name,'UNSPECIFIED')=b.name
+and b.soft_deleted_flag='N')a;
+
