31ec4993116911f8f1a22b8e2d6df341dbb8eb2a
Sneha <sneha.v@numerify.com>
2019-05-28 11:37:16 +0530

    no message

--- Apps 3.0/WHIRLPOOL/ITSA/FUNCTIONAL/d_configuration_item_operational_status_src_c_key_mysql.sql
+++ Apps 3.0/WHIRLPOOL/ITSA/FUNCTIONAL/d_configuration_item_operational_status_src_c_key_mysql.sql
@@ -1,4 +1,3 @@
-
 SELECT 
 CASE WHEN cnt THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN cnt THEN 'MDS to DWH data validation failed for d_configuration_item.operational_status_src_c_key' ELSE 'SUCCESS' END as Message
@@ -6,10 +5,11 @@ from (select count(1) cnt
 FROM whirlpool_mdsdb.cmdb_ci_final SRC 
 JOIN whirlpool_mdwdb.d_configuration_item TRGT 
 ON (SRC.sys_id =TRGT.row_id  AND SRC.sourceinstance= TRGT.source_id  )
-JOIN whirlpool_mdwdb.d_lov SRC1 ON 
-COALESCE(CONCAT('OPERATIONAL_STATUS~CMDB_CI~~~',UPPER(SRC.operational_status)),'UNSPECIFIED')=SRC1.sys_id and SRC.sourceinstance=SRC1.sourceinstance
+left JOIN whirlpool_mdwdb.d_lov SRC1 ON 
+COALESCE(CONCAT('OPERATIONAL_STATUS~CMDB_CI~~~',UPPER(SRC.operational_status)),'UNSPECIFIED')=SRC1.row_id 
+and SRC.sourceinstance=SRC1.source_id
 WHERE  coalesce(SRC1.row_key,case when SRC.operational_status is null then 0 else -1 end ) <> 
-TRGT.operational_status_src_c_key)b
+TRGT.operational_status_src_c_key)b;
 
  
  
