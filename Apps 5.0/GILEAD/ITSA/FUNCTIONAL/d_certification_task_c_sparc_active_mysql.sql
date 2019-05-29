36d25bcf7afca8be7652e5b187012e78737aab66
bhavani_y <bhavani.y@numerify.com>
2019-05-29 13:21:35 +0530

    no message

--- Apps 5.0/GILEAD/ITSA/FUNCTIONAL/d_certification_task_c_sparc_active_mysql.sql
+++ Apps 5.0/GILEAD/ITSA/FUNCTIONAL/d_certification_task_c_sparc_active_mysql.sql
@@ -2,9 +2,9 @@
 SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.impact_src_code' 
 ELSE 'MDS to DWH data validation passed for f_incident.impact_src_code' END as Message from 
-(select count(1) cnt 
+(select count(1) as cnt 
 FROM gilead_mdsdb.cert_task_final SRC 
 JOIN gilead_mdwdb.d_certification_task_c TRGT 
 ON SRC.sys_id = TRGT.row_id 
 and  SRC.sourceinstance = TRGT.source_id  
-WHERE SRC.u_sparc_active  <> TRGT.sparc_active)b
+WHERE case when SRC.u_sparc_active=1 then 'Y' else 'N' end  <> TRGT.sparc_active)b
--- Apps 5.0/GILEAD/ITSA/FUNCTIONAL/d_configuration_item_aggregator_c_owner_group_c_key_mysql.sql
+++ Apps 5.0/GILEAD/ITSA/FUNCTIONAL/d_configuration_item_aggregator_c_owner_group_c_key_mysql.sql
@@ -7,7 +7,7 @@ FROM gilead_mdsdb.cmdb_ci_final SRC
 JOIN gilead_mdwdb.d_configuration_item_aggregator_c TRGT 
 ON SRC.sys_id = TRGT.row_id 
 and  SRC.sourceinstance = TRGT.source_id 
-join
-gilead_mdwdb.d_internal_organization LKP
+left join gilead_mdwdb.d_internal_organization LKP
 on  COALESCE(CONCAT('GROUP~',SRC.support_group),'UNSPECIFIED')=LKP.row_id
-where coalesce(LKP.row_key,case when SRC.support_group is null then 0 else -1 end ) <> TRGT.owner_group_c_key) b
+where coalesce(LKP.row_key,case when SRC.support_group is null then 0 else -1 end ) <> TRGT.owner_group_c_key
+and TRGT.current_flag='Y') b
