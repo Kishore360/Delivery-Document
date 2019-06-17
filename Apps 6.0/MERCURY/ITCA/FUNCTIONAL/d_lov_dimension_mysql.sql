2f98e912161f091bf95c6a55feb7b2c700ec0b4a
bhavani_y <bhavani.y@numerify.com>
2019-06-17 13:01:04 +0530

    no message

--- Apps 6.0/MERCURY/ITCA/FUNCTIONAL/d_lov_dimension_mysql.sql
+++ Apps 6.0/MERCURY/ITCA/FUNCTIONAL/d_lov_dimension_mysql.sql
@@ -3,7 +3,7 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 FROM 
 mercury_mdsdb.synonyms_final SRC 
 LEFT JOIN mercury_mdwdb.d_lov TRGT 
-	ON (CONCAT('AUX~CCA~~~',acd_no,'~',trim(SRC.value)) =TRGT.row_id 
+	ON (CONCAT('AUX~CCA~',acd_no,'~',trim(SRC.value)) =TRGT.row_id 
 	AND SRC.sourceinstance =TRGT.source_id )
 WHERE 'AUX~CCA'<> COALESCE(TRGT.dimension_class,'')
 AND SRC.item_type = 'aux_rsn'
