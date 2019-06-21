0e7d8e2393c71c4b2bb5a1c950f7d028753b7d87
bhavani_y <bhavani.y@numerify.com>
2019-06-21 14:19:32 +0530

    no message

--- Apps 6.0/PANDG/ITSA/FUNCTIONAL/d_cmdb_ci_website_c_web_business_critical_c_key_mysql.sql
+++ Apps 6.0/PANDG/ITSA/FUNCTIONAL/d_cmdb_ci_website_c_web_business_critical_c_key_mysql.sql
@@ -5,7 +5,7 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  JOIN png_mdwdb.d_cmdb_ci_website_c TRGT
 ON SRC.sys_id = TRGT.row_id and SRC.sourceinstance=TRGT.source_id
 JOIN png_mdwdb.d_lov l
-ON COALESCE(concat('BUSINESS_CRITICAL_C','~','WEBSITE~',a.u_pg_business_criticality),'UNSPECIFIED')=d.row_id 
-AND a.sourceinstance=l.source_id
-where COALESCE(l.row_key,CASE WHEN SRC.u_pg_business_criticality IS NULL THEN 0 else -1 end) <> TRGT.web_business_critical_c_key
+ON COALESCE(concat('BUSINESS_CRITICAL_C','~','WEBSITE~',SRC.u_pg_business_criticality),'UNSPECIFIED')=l.row_id 
+AND SRC.sourceinstance=l.source_id
+where COALESCE(l.row_key,CASE WHEN SRC.u_pg_business_criticality IS NULL THEN 0 else -1 end)<>TRGT.web_business_critical_c_key
 
