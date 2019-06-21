82070d5023b790fbdbbb4bd68c0a1962b0c9e2a5
bhavani_y <bhavani.y@numerify.com>
2019-06-21 12:47:50 +0530

    no message

--- Apps 6.0/PANDG/ITSA/FUNCTIONAL/d_cmdb_ci_website_c_pg_environment_c_key_mysql.sql
+++ Apps 6.0/PANDG/ITSA/FUNCTIONAL/d_cmdb_ci_website_c_pg_environment_c_key_mysql.sql
@@ -5,7 +5,7 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  JOIN png_mdwdb.d_cmdb_ci_website_c TRGT
 ON SRC.sys_id = TRGT.row_id and SRC.sourceinstance=TRGT.source_id
 JOIN png_mdwdb.d_lov l
-ON COALESCE(concat('ENVIRONMENT_C','~','WEBSITE~',a.u_pg_environment),'UNSPECIFIED')=d.row_id 
-AND a.sourceinstance=l.source_id
+ON COALESCE(concat('ENVIRONMENT_C','~','WEBSITE~',SRC.u_pg_environment),'UNSPECIFIED')=l.row_id 
+AND SRC.sourceinstance=l.source_id
 where COALESCE(l.row_key,CASE WHEN SRC.u_pg_environment  IS NULL THEN 0 else -1 end) <> TRGT.pg_environment_c_key
 
--- Apps 6.0/PANDG/ITSA/FUNCTIONAL/d_cmdb_ci_website_c_tier_c_mysql.sql
+++ Apps 6.0/PANDG/ITSA/FUNCTIONAL/d_cmdb_ci_website_c_tier_c_mysql.sql
@@ -1,6 +1,6 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_cmdb_ci_website_c.tier_c' ELSE 'SUCCESS' END as Message
- FROM
+SELECT CASE WHEN cnt> 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt>0 THEN 'MDS to DWH data validation failed for d_cmdb_ci_website_c.tier_c' ELSE 'SUCCESS' END as Message
+ FROM(select count(1) as cnt  from 
   png_mdsdb.cmdb_ci_web_site_final SRC
  JOIN png_mdwdb.d_cmdb_ci_website_c TRGT
 ON SRC.sys_id = TRGT.row_id and SRC.sourceinstance=TRGT.source_id
@@ -8,4 +8,4 @@ where case when SRC.u_pg_business_criticality='1 - most critical'  then 'T1'
            when SRC.u_pg_business_criticality='2 - somewhat critical' then 'T2' 
            when SRC.u_pg_business_criticality='3 - less critical' then 'T3' 
           when SRC.u_pg_business_criticality='4 - not critical' then 'T4' 
-          else 'UNSPECIFIED' end <> TRGT.tier_c
+          else 'UNSPECIFIED' end <>TRGT.tier_c and SRC.cdctype<>'D')ma
