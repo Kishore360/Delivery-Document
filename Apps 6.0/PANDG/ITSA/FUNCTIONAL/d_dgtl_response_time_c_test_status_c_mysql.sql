a0f46ecf165514560af33bd1943be2f420da91a5
bhavani_y <bhavani.y@numerify.com>
2019-06-21 15:44:51 +0530

    no message

--- Apps 6.0/PANDG/ITSA/FUNCTIONAL/d_dgtl_response_time_c_test_status_c_mysql.sql
+++ Apps 6.0/PANDG/ITSA/FUNCTIONAL/d_dgtl_response_time_c_test_status_c_mysql.sql
@@ -1,11 +1,11 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_dgtl_response_time_c.test_status_c' ELSE 'SUCCESS' END as Message
- SELECT count(*)
+SELECT CASE WHEN cnt> 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt>0 THEN 'MDS to DWH data validation failed for d_dgtl_response_time_c.test_status_c' ELSE 'SUCCESS' END as Message
+ from(SELECT count(1) as cnt 
  FROM
    png_mdsdb.us_dgtl_response_time_data_final SRC
  JOIN png_mdwdb.d_dgtl_response_time_c TRGT
 ON concat (COALESCE(SRC.Test_Name,'UNSPECIFIED'),COALESCE(SRC.Date,'UNSPECIFIED')) = TRGT.row_id
-WHERE SRC.Test_Status <> TRGT.test_status_c;
+WHERE SRC.Test_Status <> TRGT.test_status_c)ma;
 
 
 
