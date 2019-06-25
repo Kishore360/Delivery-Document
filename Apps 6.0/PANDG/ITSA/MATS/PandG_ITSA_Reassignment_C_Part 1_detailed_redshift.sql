71885a4fa961ca388c73aae44da169d040d883d4
SARITHAPODALI\Kishore <kishore.mantha@numerify.com>
2019-06-25 10:19:39 +0530

    Updated the QA Code

--- Apps 6.0/PANDG/ITSA/MATS/PandG_ITSA_Reassignment_C_Part 1_detailed_redshift.sql
+++ Apps 6.0/PANDG/ITSA/MATS/PandG_ITSA_Reassignment_C_Part 1_detailed_redshift.sql
@@ -1,76 +1,76 @@
-/* d_calendar_date
-d_calendar_month
-d_configuration_item
-d_incident
-d_incident_priority
-d_incident_state
-d_internal_contact_from
-d_internal_contact_to
-d_internal_organization_group
-d_internal_organization_group_from
-d_internal_organization_group_to
-f_incident
-f_incident_activity */
+	/* d_calendar_date
+	d_calendar_month
+	d_configuration_item
+	d_incident
+	d_incident_priority
+	d_incident_state
+	d_internal_contact_from
+	d_internal_contact_to
+	d_internal_organization_group
+	d_internal_organization_group_from
+	d_internal_organization_group_to
+	f_incident
+	f_incident_activity */
 
 
 
-select 'ldb.f_incident_activity a11 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-union
-select 'ldb.d_calendar_date a12 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_calendar_date a12 on (a11.date_key = a12.row_key) 
-union
-select 'ldb.d_incident a13 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_incident a13 on (a11.incident_key = a13.row_key) 
-union
-select 'ldb.d_internal_organization_group a14 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_internal_organization_group a14 on (a11.current_assignment_group_key = a14.row_key) 
-union
-select 'ldb.d_internal_contact_to a15 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_internal_contact_to a15 on (a11.to_assigned_to_key = a15.row_key) 
-union
-select 'ldb.d_internal_organization_group_to a16 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_internal_organization_group_to a16 on (a11.to_assignment_group_key = a16.row_key) 
-union
-select 'ldb.d_internal_contact_from a17 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_internal_contact_from a17 on (a11.from_assigned_to_key = a17.row_key) 
-union
-select 'ldb.d_internal_organization_group_from a18 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_internal_organization_group_from a18 on (a11.from_assignment_group_key = a18.row_key) 
-union
-select 'ldb.d_calendar_month a19 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_calendar_date a12 on (a11.date_key = a12.row_key) 
-join ldb.d_calendar_month a19 on (a12.month_start_date_key = a19.row_key) 
-union
-select 'ldb.d_incident_priority a110 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_incident a13 on (a11.incident_key = a13.row_key) 
-join ldb.d_incident_priority a110 on (a13.priority_src_key = a110.row_key) 
-union
-select 'ldb.d_incident_state a111 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_incident a13 on (a11.incident_key = a13.row_key) 
-join ldb.d_incident_state a111 on (a13.state_src_key = a111.row_key) 
-UNION 
-select 'ldb.d_calendar_date_fiscal a13 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_calendar_date_fiscal a13 on (a11.fiscal_date_key = a13.row_key) 
-UNION 
-select 'ldb.d_calendar_date_opened' as Table_Name, count(1) Row_Count
-from  ldb.f_incident_activity  a11 
-join  ldb.d_calendar_date_opened a110 on (a11.date_key = a110.row_key)
-UNION 
-select 'ldb.d_configuration_item a13 ' as Table_name, count(a11.row_key) Row_Count
-from ldb.f_incident_activity a11 
-join ldb.d_configuration_item a12 ON a11.configuration_item_key=a12.row_key
+	select 'ldb.f_incident_activity a11 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	union
+	select 'ldb.d_calendar_date a12 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_calendar_date a12 on (a11.date_key = a12.row_key) 
+	union
+	select 'ldb.d_incident a13 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_incident a13 on (a11.incident_key = a13.row_key) 
+	union
+	select 'ldb.d_internal_organization_group a14 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_internal_organization_group a14 on (a11.current_assignment_group_key = a14.row_key) 
+	union
+	select 'ldb.d_internal_contact_to a15 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_internal_contact_to a15 on (a11.to_assigned_to_key = a15.row_key) 
+	union
+	select 'ldb.d_internal_organization_group_to a16 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_internal_organization_group_to a16 on (a11.to_assignment_group_key = a16.row_key) 
+	union
+	select 'ldb.d_internal_contact_from a17 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_internal_contact_from a17 on (a11.from_assigned_to_key = a17.row_key) 
+	union
+	select 'ldb.d_internal_organization_group_from a18 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_internal_organization_group_from a18 on (a11.from_assignment_group_key = a18.row_key) 
+	union
+	select 'ldb.d_calendar_month a19 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_calendar_date a12 on (a11.date_key = a12.row_key) 
+	join ldb.d_calendar_month a19 on (a12.month_start_date_key = a19.row_key) 
+	union
+	select 'ldb.d_incident_priority a110 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_incident a13 on (a11.incident_key = a13.row_key) 
+	join ldb.d_incident_priority a110 on (a13.priority_src_key = a110.row_key) 
+	union
+	select 'ldb.d_incident_state a111 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_incident a13 on (a11.incident_key = a13.row_key) 
+	join ldb.d_incident_state a111 on (a13.state_src_key = a111.row_key) 
+	UNION 
+	select 'ldb.d_calendar_date_fiscal a13 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_calendar_date_fiscal a13 on (a11.fiscal_date_key = a13.row_key) 
+	UNION 
+	select 'ldb.d_calendar_date_opened' as Table_Name, count(1) Row_Count
+	from  ldb.f_incident_activity  a11 
+	join  ldb.d_calendar_date_opened a110 on (a11.date_key = a110.row_key)
+	UNION 
+	select 'ldb.d_configuration_item a13 ' as Table_name, count(a11.row_key) Row_Count
+	from ldb.f_incident_activity a11 
+	join ldb.d_configuration_item a12 ON a11.configuration_item_key=a12.row_key
 
 
 
---
+++ Apps 6.0/png/ITAA/MATS/RITA_Inventory_C_detailed_redshift.sql
@@ -0,0 +1,197 @@
+select 'ldb.f_png_ot_processing_activities_inventory_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+union
+select'ldb.d_png_ot_processing_activities_inventory_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+union
+select'ldb.d_lov_pa_breach_resp_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11
+join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_breach_resp_stats_c       a13
+on (a12.breach_resp_stats_c = a13.row_key)
+union
+select'ldb.d_lov_pa_priv_imp_asmt_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_priv_imp_asmt_stats_c       a14
+on (a12.priv_imp_asmt_stats_c = a14.row_key)
+union
+select'ldb.d_lov_pa_ret_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_ret_stats_c       a15
+on (a12.ret_stats_c = a15.row_key)
+union
+select'ldb.d_lov_pa_sub_rgt_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_sub_rgt_stats_c       a16
+on (a12.sub_rgt_stats_c = a16.row_key)
+union
+select'ldb.d_lov_pa_inventory_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_inventory_stats_c       a17
+on (a12.inventory_stats_c = a17.row_key)
+union
+select'ldb.d_lov_pa_law_basis_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11
+join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_law_basis_stats_c       a18
+on (a12.law_basis_stats_c = a18.row_key)
+union
+select'ldb.d_lov_pa_mini_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_mini_stats_c       a19
+on (a12.mini_stats_c = a19.row_key)
+union
+select'ldb.d_lov_pa_notice_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11
+join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_notice_stats_c       a110
+on (a12.notice_stats_c = a110.row_key)
+union
+select'ldb.d_lov_pa_it_sec_ctrl_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11
+join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_it_sec_ctrl_stats_c       a111
+on (a12.it_sec_ctrl_stats_c = a111.row_key)
+union
+select'ldb.d_lov_pa_vendors_stats_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11
+join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_pa_vendors_stats_c       a112
+on (a12.vendors_stats_c = a112.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_eu_data_stakeholders_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_eu_data_stakeholders_c       a116
+on (a12.ot_eu_data_stakeholders_c_key = a116.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_age_class_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_age_class_c       a117
+on (a12.ot_age_class_c_key = a117.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_life_cycle_status_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_life_cycle_status_c       a118
+on (a12.ot_life_cycle_status_c_key = a118.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_compliance_prioritization_two_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_compliance_prioritization_two_c       a119
+on (a12.ot_compliance_priority2_c_key = a119.row_key)
+union
+select'ldb.d_lov_processing_activities_data_subject_type_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_data_subject_type_c       a120
+on (a12.data_subject_type_c_key = a120.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_pg_users_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11
+join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_pg_users_c       a121
+on (a12.ot_pg_users_c_key = a121.row_key)
+union
+select'ldb.d_lov_processing_activities_pii_data_type_final_formula_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_pii_data_type_final_formula_c       a122
+on (a12.pii_data_type_final_formula_c_key = a122.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_repository_sub_type_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_repository_sub_type_c       a123
+on (a12.ot_repository_sub_type_c_key = a123.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_repository_type_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_repository_type_c       a124
+on (a12.ot_repository_type_c_key = a124.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_scope_number_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_scope_number_c       a125
+on (a12.ot_scope_number_c_key = a125.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_third_party_users_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_third_party_users_c       a126
+on (a12.ot_third_party_users_c_key = a126.row_key)
+union
+select'ldb.d_lov_processing_activities_vendor_qualification_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_vendor_qualification_c       a127
+on (a12.vendor_qualification_c_key = a127.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_volume_risk_level_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_volume_risk_level_c       a128
+on (a12.ot_volume_risk_level_c_key = a128.row_key)
+union
+select'ldb.d_lov_processing_activities_ot_volume_of_records_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+ join ldb.d_png_ot_processing_activities_inventory_c       a12
+on (a11.png_ot_inventory_c_key = a12.row_key)
+join ldb.d_lov_processing_activities_ot_volume_of_records_c       a129
+on (a12.ot_volume_of_records_c_key = a129.row_key)
+union
+select'ldb.d_processing_activities_managing_organization_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+join ldb.d_processing_activities_managing_organization_c       a113
+on (a11.managing_organization_c_key = a113.row_key)
+union
+select'ldb.d_processing_activities_owning_organization_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+join ldb.d_processing_activities_owning_organization_c       a114
+on (a11.png_ot_organization_c_key = a114.row_key)
+union
+select'ldb.d_processing_activities_sap_vendor_c' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c       a11 
+join ldb.d_processing_activities_sap_vendor_c       a115
+on (a11.sap_vendor_c_key = a115.row_key)
+
+UNION 
+select 'ldb.d_calendar_date' as Table_Name, count(a11.row_key) Row_Count
+from  ldb.f_png_ot_processing_activities_inventory_c a11 
+JOIN ldb.d_png_ot_processing_activities_inventory_c a12 ON a11.png_ot_inventory_c_key=a12.row_key
+JOIN ldb.d_rita_application_source_type_c a13 ON a12.rita_application_source_type_c_key=a13.row_key
+JOIN ldb.d_calendar_date b ON a11.created_on_key=b.row_key
