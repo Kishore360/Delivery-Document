d68c45e68d0e26cf59421a9808af0c511a316b14
Narmadha_D <narmadha.d@numerify.com>
2019-06-26 10:38:36 +0530

    moxie queries

--- Apps 6.0/MERITSA/MOXIE/FUNCTIONAL/f_agent_log_ais_status_code_c_mysql.sql
+++ Apps 6.0/MERITSA/MOXIE/FUNCTIONAL/f_agent_log_ais_status_code_c_mysql.sql
@@ -1,8 +1,8 @@
 
 SELECT CASE WHEN cnt>0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN cnt>0  THEN 'MDS to DWH data validation failed for f_agent_log.agent_c' ELSE 'SUCCESS' END as Message 
+CASE WHEN cnt>0  THEN 'MDS to DWH data validation failed for f_agent_log_c_c.agent_c' ELSE 'SUCCESS' END as Message 
 FROM
 (select count(1) as cnt
- from mercury_mdsdb.us_ais_agent_report_final  a
-left join mercury_mdwdb.f_agent_log f on concat('AIS~',a.row_id)=f.row_id
+ from mercury_mdsdb.mox_ais_agent_report_final  a
+left join mercury_mdwdb.f_agent_log_c_c f on concat('AIS~',a.row_id)=f.row_id
 where  a.Status_code<>f.Status_code_c )a 
--- Apps 6.0/MERITSA/MOXIE/FUNCTIONAL/f_agent_log_mercury_time_in_c_mysql.sql
+++ Apps 6.0/MERITSA/MOXIE/FUNCTIONAL/f_agent_log_mercury_time_in_c_mysql.sql
@@ -1,10 +1,10 @@
 
 SELECT CASE WHEN cnt>0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN cnt>0  THEN 'MDS to DWH data validation failed for f_agent_log.agent_c' ELSE 'SUCCESS' END as Message 
+CASE WHEN cnt>0  THEN 'MDS to DWH data validation failed for f_agent_log_c_c.agent_c' ELSE 'SUCCESS' END as Message 
 FROM
 (select count(1) as cnt
- from mercury_mdsdb.us_mercury_agent_report_final  a
-left join mercury_mdwdb.f_agent_log f on concat('MERCURY~',a.row_id)=f.row_id
+ from mercury_mdsdb.mox_mercury_agent_report_final  a
+left join mercury_mdwdb.f_agent_log_c_c f on concat('MERCURY~',a.row_id)=f.row_id
 where str_to_date(Time_in,'%m/%d/%Y %h:%i:%s %p')<>f.time_in_c)a;
 
 
