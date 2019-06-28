1bb7b26203597246f80cdf4c7a011ee2de242e5b
SARITHAPODALI\Kishore <kishore.mantha@numerify.com>
2019-06-28 12:45:05 +0530

    Updated the QA code as part of DELV-17901

--- Apps 6.0/png/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_c_control_criteria_mysql.sql
+++ Apps 6.0/png/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_c_control_criteria_mysql.sql
@@ -10,9 +10,13 @@ LEFT JOIN  png_mdwdb.d_png_ot_processing_activity_validation_control_c trgt
 on CONCAT(src.Inventory_Id,'~',sha1(lower(src.Compliance_control)))=trgt.row_id 
 and src.sourceinstance = trgt.source_id
 where 
-COALESCE(CASE   
-WHEN src.Compliance_control = 'Data breach response'  THEN IF(src.Data_breach_response_Criteria <> '',src.Data_breach_response_Criteria,NULL)  
-WHEN src.Compliance_control = 'Data Retention' THEN IF(src.Data_retention_Criteria <> '',src.Data_retention_Criteria,NULL)  
+left(COALESCE(CASE   
+                    WHEN src.Compliance_control = 'Data breach response'       THEN IF(src.Data_breach_response_Criteria <> '',
+                    src.Data_breach_response_Criteria ,
+                    NULL)  
+                    WHEN src.Compliance_control = 'Data Retention'                              THEN IF(src.Data_retention_Criteria <> '',
+                    src.Data_retention_Criteria ,
+                    NULL)  
                     WHEN src.Compliance_control = 'Data Subject Rights'                         THEN IF(src.Data_Subject_Rights_Criteria <> '',
                     src.Data_Subject_Rights_Criteria ,
                     NULL)  
@@ -34,7 +38,12 @@ WHEN src.Compliance_control = 'Data Retention' THEN IF(src.Data_retention_Criter
                     WHEN src.Compliance_control = 'Vendors'                                     THEN IF(src.Vendors_Criteria <> '',
                     src.Vendors_Criteria ,
                     NULL)  
+                    WHEN src.Compliance_control = 'Data Privacy Impact Assessment'              THEN IF(src.DPIA_Criteria <> '',
+                    src.DPIA_Criteria ,
+                    NULL)  
                     ELSE 'UNSPECIFIED' 
                 END,
-                'UNSPECIFIED')<>trgt.control_criteria 
+                'UNSPECIFIED')
+,255)
+<>trgt.control_criteria 
 				;
