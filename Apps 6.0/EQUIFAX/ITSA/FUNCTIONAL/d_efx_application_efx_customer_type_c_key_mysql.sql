244a17a11a112829a8f6563b8b876807d27f7211
Shubha_Rao <Shubha.Rao@numerify.com>
2019-07-02 16:36:56 +0530

    Updating Cust queries for Equifax

---
+++ Apps 6.0/EQUIFAX/ITSA/FUNCTIONAL/d_efx_application_efx_customer_type_c_key_mysql.sql
@@ -0,0 +1,12 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.impact_src_code' 
+ELSE 'MDS to DWH data validation passed for f_incident.impact_src_code' END as Message from 
+(select count(1) cnt 
+FROM equifax_mdsdb.u_efx_application_final SRC 
+JOIN equifax_mdwdb.d_efx_application_c TRGT 
+ON SRC.sys_id = TRGT.row_id 
+and  SRC.sourceinstance = TRGT.source_id 
+join
+equifax_mdwdb.d_lov lkp
+on COALESCE(CONCAT('CUSTOMER_TYPE~EFX_APPLICATION~',SRC.u_efx_customer_type),'UNSPECIFIED') =lkp.row_id
+WHERE COALESCE(lkp.row_key, case when SRC.u_efx_customer_type is null then 0 else -1 end) <> TRGT.efx_customer_type_c_key)b
---
+++ Apps 6.0/EQUIFAX/ITSA/FUNCTIONAL/f_efx_application_risk_profile_submitted_by_c_key_mysql.sql
@@ -0,0 +1,12 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for f_incident.impact_src_code' 
+ELSE 'MDS to DWH data validation passed for f_incident.impact_src_code' END as Message from 
+(select count(1) cnt 
+FROM equifax_mdsdb.u_efx_application_final SRC 
+JOIN equifax_mdwdb.f_efx_application_c TRGT 
+ON SRC.sys_id = TRGT.row_id 
+and  SRC.sourceinstance = TRGT.source_id 
+join
+equifax_mdwdb.d_internal_contact lkp
+on COALESCE(CONCAT('INTERNAL_CONTACT','~',SRC.u_risk_profile_submitted_by),'UNSPECIFIED') =lkp.row_id
+WHERE COALESCE(lkp.row_key, case when SRC.u_risk_profile_submitted_by is null then 0 else -1 end) <> TRGT.risk_profile_submitted_by_c_key)b
