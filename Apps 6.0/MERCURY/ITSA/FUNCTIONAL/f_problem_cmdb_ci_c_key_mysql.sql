f8f30cf640a8df09a315635f16162143f99fa788
Narmadha_D <narmadha.d@numerify.com>
2019-07-06 15:38:55 +0530

    updated queries

--- Apps 6.0/MERCURY/ITSA/FUNCTIONAL/f_problem_cmdb_ci_c_key_mysql.sql
+++ Apps 6.0/MERCURY/ITSA/FUNCTIONAL/f_problem_cmdb_ci_c_key_mysql.sql
@@ -5,8 +5,9 @@ SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  FROM mercury_mdsdb.problem_final SRC 
  LEFT JOIN mercury_mdwdb.f_problem TRGT 
  ON (SRC.sys_id =TRGT.row_id  
- AND SRC.sourceinstance= TRGT.source_id  )  and SRC.CDCTYPE<>'D'
+ AND SRC.sourceinstance= TRGT.source_id  )  
  LEFT JOIN mercury_mdwdb.d_configuration_item LKP 
  ON ( SRC.u_ci_name= LKP.row_id 
 AND SRC.sourceinstance= LKP.source_id )and LKP.soft_deleted_flag='N'
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_ci_name IS NULL THEN 0 else -1 end)<> (TRGT.cmdb_ci_c_key)
+and  SRC.CDCTYPE= 'X'
