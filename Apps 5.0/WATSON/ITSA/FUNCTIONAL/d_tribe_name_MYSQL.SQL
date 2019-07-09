62af1cb47feb0ea43025e29d729679ca868d8e2e
bhavani_y <bhavani.y@numerify.com>
2019-07-09 10:47:28 +0530

    no message

---
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/d_tribe_name_MYSQL.SQL
@@ -0,0 +1,6 @@
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_tribe_c.name ' ELSE 'SUCCESS' END as Message
+ from(select count(1) as cnt from watson_mdsdb.u_tribe_final src
+ left join watson_mdwdb.d_tribe_c trgt 
+ on src.sys_id=trgt.row_id and src.sourceinstance=trgt.source_id
+ where src.u_name <>trgt.name)ma	
