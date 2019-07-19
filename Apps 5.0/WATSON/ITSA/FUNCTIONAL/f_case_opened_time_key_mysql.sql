a965c6fafd719208e4424d7de04de9e026feb094
bhavani_y <bhavani.y@numerify.com>
2019-07-19 16:35:07 +0530

    no message

--- Apps 5.0/WATSON/ITSA/FUNCTIONAL/f_case_opened_time_key_mysql.sql
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/f_case_opened_time_key_mysql.sql
@@ -1,10 +1,10 @@
 
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_case.opened_time_key' ELSE 'SUCCESS' END as Message
-FROM watson_mdsdb.sn_customerservice_case_final SRC
+SELECT CASE WHEN cnt> 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt>0 THEN 'MDS to DWH data validation failed for f_case.opened_time_key' ELSE 'SUCCESS' END as Message
+FROM (select count(1) as cnt from watson_mdsdb.sn_customerservice_case_final SRC
 LEFT JOIN watson_mdwdb.f_case TRGT 
 	ON (SRC.sys_id =TRGT.row_id 
 	AND SRC.sourceinstance =TRGT.source_id )
 LEFT JOIN watson_mdwdb.d_calendar_time LKP 
-	ON(LKP.row_id = date_format(convert_tz(SRC.opened_at,'GMT','America/New_York'),'%H%i') and LKP.source_id=0)
-WHERE COALESCE(LKP.row_key,CASE WHEN SRC.opened_at IS NULL THEN 0 else '-1' end)  <> COALESCE(TRGT.opened_time_key,'') 
+	ON(LKP.row_id = date_format(convert_tz(SRC.opened_at,'GMT','UTC'),'%H%i') and LKP.source_id=0)
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.opened_at IS NULL THEN 0 else '-1' end)<>TRGT.opened_time_key and TRGT.soft_deleted_flag='N')ma 
