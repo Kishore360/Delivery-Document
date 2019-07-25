d28d8666afa5d73fac93f04e195548028dd4ab9b
Kishore_Mantha <kishore.mantha@numerify.com>
2019-07-25 14:35:44 +0530

    Updated the Folder Details

--- Apps 6.0/TJXCO/ITSA/FUNCTIONAL/d_change_failure_change_reason.sql
+++
@@ -1,8 +0,0 @@
-select CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_failure.change_reason' ELSE 'SUCCESS' END as Message
-from (SELECT * FROM tjxco_mdsdb.change_request_final WHERE CDCTYPE<>'D') SRC
-left join (SELECT * FROM tjxco_mdwdb.d_change_failure where year(effective_to)=2999 
-) TRGT 
- ON SRC.sys_id =TRGT.row_id 
- AND SRC.sourceinstance = TRGT.source_id  
- where coalesce(SRC.reason,'UNSPECIFIED') <> coalesce(TRGT.change_reason,'')
--- Apps 6.0/TJXCO/ITSA/FUNCTIONAL/d_change_failure_production_change_flag.sql
+++
@@ -1,9 +0,0 @@
-
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_failure.production_change_flag' ELSE 'SUCCESS' END as Message
-from (SELECT * FROM tjxco_mdsdb.change_request_final WHERE CDCTYPE<>'D') SRC
-left join (SELECT * FROM tjxco_mdwdb.d_change_failure where year(effective_to)=2999 
-) TRGT 
- ON (SRC.sys_id=TRGT.row_id 
- AND SRC.sourceinstance=TRGT.source_id )
- WHERE COALESCE( CASE WHEN  SRC.production_system= TRUE THEN 'Y' ELSE 'N' END ,'')<> COALESCE(TRGT.production_change_flag ,'')
