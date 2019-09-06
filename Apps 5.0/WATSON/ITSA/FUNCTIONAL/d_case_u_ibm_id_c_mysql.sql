b2dbf6166ef4de8a2d7d946f6b2c41a5f378c4e2
bhavani_y <bhavani.y@numerify.com>
2019-09-06 11:33:42 +0530

    no message

--- Apps 5.0/WATSON/ITSA/FUNCTIONAL/d_case_u_ibm_id_c_mysql.sql
+++ Apps 5.0/WATSON/ITSA/FUNCTIONAL/d_case_u_ibm_id_c_mysql.sql
@@ -1,8 +1,11 @@
-SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
- CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_case.u_ibm_id_c' ELSE 'SUCCESS' END as Message
-FROM watson_mdsdb.sn_customerservice_case_final SRC 
+SELECT CASE WHEN cnt > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN cnt >0 THEN 'MDS to DWH data validation failed for d_case.u_ibm_id_c' ELSE 'SUCCESS' END as Message
+FROM (select count(1) as cnt
+from watson_mdsdb.sn_customerservice_case_final SRC
+left join watson_mdsdb.sys_user_final lkp on SRC.contact=lkp.sys_id 
 LEFT JOIN watson_mdwdb.d_case TRGT 
 	ON (SRC.sys_id =TRGT.row_id 
 	AND SRC.sourceinstance =TRGT.source_id )
-WHERE TRGT.row_key not in (0, -1) and 
-COALESCE(SRC.u_ibmid,'UNSPECIFIED')  <> COALESCE(TRGT.u_ibmid_c,'')
+WHERE  COALESCE(lkp.email,'UNSPECIFIED')<>TRGT.u_ibmid_c and SRC.cdctype<>'D')ma;
+
+
