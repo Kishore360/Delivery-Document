b46b4c4e3dbccf35511fc66cd47e4be60ac6e31b
Shubha_Rao <Shubha.Rao@numerify.com>
2019-09-12 17:03:38 +0530

    commit

--- Apps 6.0/EQUIFAX/ITSA/FUNCTIONAL/d_efx_application_application_logs_contain_user_id_c_flag_mysql.sql
+++
@@ -1,9 +0,0 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_efx_application_c.application_logs_c_flag' ELSE 'SUCCESS' END as Message 
-FROM equifax_mdsdb.u_efx_application_final SRC
-JOIN equifax_mdwdb.d_efx_application_c TRGT  
-ON (SRC.sys_id  = TRGT.row_id  
-AND SRC.sourceinstance = TRGT.source_id ) and SRC.cdctype<>'D'
-WHERE case when SRC.u_application_logs_contain_user_id= 1 then 'Y' else 'N' end<> TRGT.application_logs_contain_user_id_c_flag
-
-
--- Apps 6.0/EQUIFAX/ITSA/FUNCTIONAL/d_efx_application_test_environment_mimics_production_c_flag_mysql.sql
+++
@@ -1,7 +0,0 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_efx_application_c.test_environment_mimics_production_c_flag' ELSE 'SUCCESS' END as Message 
-FROM equifax_mdsdb.u_efx_application_final SRC
-JOIN equifax_mdwdb.d_efx_application_c TRGT  
-ON (SRC.sys_id  = TRGT.row_id  
-AND SRC.sourceinstance = TRGT.source_id ) and SRC.cdctype<>'D'
-WHERE case when SRC.u_test_environment_mimics_production= 1 then 'Y' else 'N' end<> TRGT.test_environment_mimics_production_c_flag
