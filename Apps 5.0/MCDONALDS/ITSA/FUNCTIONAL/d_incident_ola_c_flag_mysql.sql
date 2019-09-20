510c42f6d34f0d4689194ca2c6e3a6b28fdf7b86
bhavani_y <bhavani.y@numerify.com>
2019-09-20 11:53:26 +0530

    no message

--- Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_incident_ola_c_flag_mysql.sql
+++ Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_incident_ola_c_flag_mysql.sql
@@ -1,7 +1,7 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_incident.ola_c_flag' ELSE 'SUCCESS' END as Message 
-FROM mcdonalds_mdsdb.incident_final  SRC 
+SELECT CASE WHEN cnt>0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN cnt>0 THEN 'MDS to DWH data validation failed for d_incident.ola_c_flag' ELSE 'SUCCESS' END as Message 
+FROM (select count(1) as cnt from mcdonalds_mdsdb.incident_final  SRC 
 JOIN mcdonalds_mdwdb.d_incident TRGT 
 ON (SRC.sys_id = TRGT.row_id  
 AND SRC.sourceinstance = TRGT.source_id )  
-WHERE (CASE WHEN SRC.u_ola = 1 then 'Y' else 'N' END) <> (TRGT.ola_c_flag) 
+WHERE (CASE WHEN SRC.u_ola = 1 then 'Y' else 'N' END)<>TRGT.ola_c_flag and SRC.cdctype='X') ma
--- Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_problem_priority_upgraded_c_flag_mysql.sql
+++ Apps 5.0/MCDONALDS/ITSA/FUNCTIONAL/d_problem_priority_upgraded_c_flag_mysql.sql
@@ -1,7 +1,8 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_problem.priority_upgraded_c_flag' ELSE 'SUCCESS' END as Message 
-FROM mcdonalds_mdwdb.d_problem trgt
-RIGHT JOIN mcdonalds_mdsdb.problem_final src
+SELECT CASE WHEN cnt>0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN cnt>0  THEN 'MDS to DWH data validation failed for d_problem.priority_upgraded_c_flag' ELSE 'SUCCESS' END as Message 
+FROM(select count(1) as cnt from  
+ mcdonalds_mdsdb.problem_final src
+left join mcdonalds_mdwdb.d_problem trgt
 on src.sys_id = trgt.row_id and src.sourceinstance = trgt.source_id
-WHERE CASE WHEN src.u_priority_upgraded = 1 THEN 'Y' ELSE 'N' END <>priority_upgraded_c_flag
+WHERE CASE WHEN src.u_priority_upgraded = 1 THEN 'Y' ELSE 'N' END <>priority_upgraded_c_flag and src.cdctype='X')ma
 ;
