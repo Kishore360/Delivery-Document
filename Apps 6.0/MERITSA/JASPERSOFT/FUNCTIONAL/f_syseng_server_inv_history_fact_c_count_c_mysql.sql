ffcc991bba7cf35f1a436a9196dffcb3dab29d21
Narmadha_D <narmadha.d@numerify.com>
2019-10-30 18:23:09 +0530

    Validation of DELV-19988 in Meritsa

--- Apps 6.0/MERITSA/JASPERSOFT/FUNCTIONAL/f_syseng_server_inv_history_fact_c_count_c_mysql.sql
+++ Apps 6.0/MERITSA/JASPERSOFT/FUNCTIONAL/f_syseng_server_inv_history_fact_c_count_c_mysql.sql
@@ -4,5 +4,5 @@ CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_syseng_server_
 
 FROM  meritsa_mdsdb.syseng_server_inv_history_final SRC
 join meritsa_mdwdb.f_syseng_server_inv_history_fact_c TRGT on 
-concat(SRC.date_time,'~',SRC.server_type,'~',SRC.platform,'~',SRC.environment)=TRGT.row_id and SRC.sourceinstance=TRGT.source_id
+concat(SRC.date_time,'~',SRC.server_type,'~',SRC.platform,'~',SRC.environment)=TRGT.row_id and SRC.sourceinstance=TRGT.source_id and SRC.cdctype<>'D'
 where count<>count_c
