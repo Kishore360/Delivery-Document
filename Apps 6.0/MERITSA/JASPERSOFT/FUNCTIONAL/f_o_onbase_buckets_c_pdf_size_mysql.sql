2aa3724ed121c78b9d0860fe9a1443ac79ead1be
Narmadha_D <narmadha.d@numerify.com>
2019-10-30 17:28:36 +0530

    DELV-19812 related changes

---
+++ Apps 6.0/MERITSA/JASPERSOFT/FUNCTIONAL/f_o_onbase_buckets_c_pdf_size_mysql.sql
@@ -0,0 +1,13 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_o_onbase_buckets_c.pdf_size' ELSE 'SUCCESS' END as Message
+
+FROM meritsa_mdsdb.o_onbase_buckets_c_final SRC
+
+JOIN meritsa_mdwdb.f_o_onbase_buckets_c TRGT 
+
+ON ((CONCAT(DATE(SRC.row_date),'~', SRC.folder))  = TRGT.row_id
+
+AND SRC.sourceinstance = TRGT.source_id ) and SRC.cdctype<>'D'
+
+WHERE SRC.pdf_size<>TRGT.pdf_size;
