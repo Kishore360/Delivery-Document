29da06e557c71be133a48558b587046bc7259741
bhavani_y <bhavani.y@numerify.com>
2019-11-06 17:02:07 +0530

    no message

---
+++ Apps 6.0/WHIRLPOOL/ITSA/FUNCTIONAL/d_incident_u_time_to_repair_c_mysql.sql
@@ -0,0 +1,9 @@
+SELECT
+CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
+FROM whirlpool_mdsdb.incident_final SRC 
+LEFT JOIN whirlpool_mdwdb.d_incident TRGT 
+ON (SRC.sys_id =TRGT.row_id  
+AND SRC.sourceinstance= TRGT.source_id  )
+WHERE CONVERT_TZ (SRC.u_time_to_repair,'GMT','America/New_York') = TRGT.u_time_to_repair_c
+and SRC.CDCTYPE<>'D'
---
+++ Apps 6.0/WHIRLPOOL/ITSA/FUNCTIONAL/f_asset_status_history_support_vendor_c_key_mysql.sql
@@ -0,0 +1,12 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_asset_status_history.support_vendor_c_key' ELSE 'SUCCESS' END as Message
+ FROM whirlpool_mdsdb.alm_asset_final SRC 
+ LEFT JOIN whirlpool_mdwdb.f_asset_status_history TRGT 
+ on (CONCAT(SRC.sys_id,'~',DATE_FORMAT(SRC.sys_created_on,'%Y%m%d%H%i%S') )  =right(TRGT.row_id,32)
+ AND SRC.sourceinstance=TRGT.source_id )
+JOIN whirlpool_mdwdb.d_internal_organization LKP 
+  ON (COALESCE(CONCAT('SUBSIDIARY~',SRC.u_support_vendor),'UNSPECIFIED'))= LKP.row_id
+  AND DATE_FORMAT(TRGT.status_start_on, '%Y-%m-%d %H:%i:%s') 
+									BETWEEN effective_from AND effective_to
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_support_vendor IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.support_vendor_c_key,'')
+ and SRC.cdctype<>'D' 
