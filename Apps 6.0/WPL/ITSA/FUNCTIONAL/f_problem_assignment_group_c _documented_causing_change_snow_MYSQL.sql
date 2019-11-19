722b1b920a00d152e38b65f0599046bfa331b58b
TriveniGN <triveni.n@numerify.com>
2019-11-19 12:36:35 +0530

    no message

---
+++ Apps 6.0/WPL/ITSA/FUNCTIONAL/f_problem_assignment_group_c _documented_causing_change_snow_MYSQL.sql
@@ -0,0 +1,13 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_problem_assignment_group_c .authorized_change'
+ ELSE 'SUCCESS' END as Message 
+ FROM wpl_mdsdb.us_NODCS1718_2018PRBCollectionSheet_final SRC
+ JOIN wpl_mdwdb.f_problem_assignment_group_c TRGT 
+ ON concat(coalesce(SRC.numbersnow,
+                'unspecified'),
+                '~',
+                coalesce(SRC.dateandtimesnow,
+                'unspecified'))  = TRGT.row_id  
+ AND SRC.sourceinstance = TRGT.source_id  
+ WHERE coalesce(SRC.documentedcausingchangesnow,'UNSPECIFIED') <> (TRGT.documented_causing_change_snow)
+ and SRC.CDCTYPE='X';
