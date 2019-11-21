a044c21f0b8baf57b7b72d37d391ad9a7ac0fdf9
bhavani_y <bhavani.y@numerify.com>
2019-11-21 16:33:34 +0530

    no message

--- Apps 6.0/WPL/ITSA/FUNCTIONAL/f_task_survey_sd_categorization_state_src_c_key_mysql.sql
+++ Apps 6.0/WPL/ITSA/FUNCTIONAL/f_task_survey_sd_categorization_state_src_c_key_mysql.sql
@@ -6,7 +6,7 @@ JOIN wpl_mdsdb.asmt_assessment_instance_final tsd ON ts.row_id=tsd.sys_id and ts
 LEFT JOIN wpl_mdsdb.task_final tf 
 on tf.sys_id = tsd.u_parent_ticket
  LEFT JOIN wpl_mdwdb.d_lov LKP 
- ON ( COALESCE( CONCAT('U_SD_CATEGORIZATION_STATE~SURVEY~~~',upper(tsd.u_sd_categorization_state)),'UNSPECIFIED') = LKP.row_id 
+ ON ( COALESCE( CONCAT('U_SD_CATEGORIZATION_STATE~SURVEY~',upper(tsd.u_sd_categorization_state)),'UNSPECIFIED') = LKP.row_id 
 AND tsd.sourceinstance= LKP.source_id )
  WHERE COALESCE(LKP.row_key,CASE WHEN tsd.u_sd_categorization_state IS NULL THEN 0 else -1 end)<> (ts.sd_categorization_state_src_c_key)
  and tf.CDCTYPE<>'D'
