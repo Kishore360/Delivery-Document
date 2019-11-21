b759181857718eaa54b33c5d13f1c411946e21e5
TriveniGN <triveni.n@numerify.com>
2019-11-21 19:18:57 +0530

    no message

---
+++ Apps 6.0/WPL/ITSA/FUNCTIONAL/f_incident_opened_on_key_mysql.sql
@@ -0,0 +1,9 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_change_request.opened_on_key' ELSE 'SUCCESS' END as Message 
+FROM wpl_mdsdb.incident_final  SRC 
+JOIN wpl_mdwdb.f_incident TRGT 
+ON (SRC.sys_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id )  
+LEFT JOIN wpl_mdwdb.d_calendar_date LKP 
+ON  (COALESCE(date_format(convert_tz(SRC.opened_at,'GMT','America/New_York'),'%Y%m%d'),'UNSPECIFIED') = LKP.row_id ) 
+WHERE COALESCE(LKP.row_key,CASE WHEN SRC.opened_at IS NULL THEN 0 else -1 end)<> (TRGT.opened_on_key)
+ and SRC.cdctype='X'
