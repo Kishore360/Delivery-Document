86c3e973741e1ecddfca11ab72565fa4c8579970
Narmadha_D <narmadha.d@numerify.com>
2020-02-01 00:08:58 +0530

    modification

--- Apps 6.0/MERCURY/ITSA/FUNCTIONAL/f_incident_asc_c_age_open_mysql.sql
+++ Apps 6.0/MERCURY/ITSA/FUNCTIONAL/f_incident_asc_c_age_open_mysql.sql
@@ -9,7 +9,7 @@ from
 join mercury_mdwdb.d_incident_asc_c d ON (f.incident_asc_c_key=d.row_key and f.soft_deleted_flag='N' )
 JOIN mercury_mdwdb.d_lov_map br ON f.asc_incident_state_c_key = br.src_key
 AND br.dimension_wh_code = 'OPEN' and br.dimension_class = 'STATE~ASC_INCIDENT'
-WHERE TIMESTAMPDIFF(second,d.opened_on,(SELECT MAX(lastupdated)
+WHERE TIMESTAMPDIFF(second,convert_tz(d.opened_on,'America/Los_Angeles','GMT'),(SELECT convert_tz(MAX(lastupdated),'America/Los_Angeles','GMT')
 FROM mercury_mdwdb.d_o_data_freshness WHERE sourcename like 'ServiceNow%')
 )<> f.age
 
