802e5837daab5c5ed440e92b1b310988cf7caf59
Shubha_Rao <Shubha.Rao@numerify.com>
2020-02-03 18:32:55 +0530

    Cust coverage updates for fedility

--- Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_application_fmr_product_c_key_mysql.sql
+++ Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_application_fmr_product_c_key_mysql.sql
@@ -1,7 +1,13 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_change_request.mf_online_inst_db2_module_src_code_c' ELSE 'SUCCESS' END as Message 
-FROM fidelity_mdsdb.cmdb_ci_appl_final  SRC 
-JOIN fidelity_mdwdb.d_application TRGT 
-ON (CONCAT('APPLICATION~',SRC.sys_id) = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id ) 
-join fidelity_mdwdb.d_fmr_product_c LKP
-on COALESCE(SRC.u_product,'UNSPECIFIED') = LKP.row_id and SRC.sourceinstance=LKP.source_id
- WHERE  COALESCE(LKP.row_key,case when SRC.u_product is null then 0 else -1 end ) <> TRGT.fmr_product_c_key; 
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.u_raise_impact_c' ELSE 'SUCCESS' END as Message
+FROM
+fidelity_mdsdb.cmdb_ci_appl_final SRC
+JOIN fidelity_mdwdb.d_application trgt1
+on SRC.sys_id = trgt1.row_id and SRC.sourceinstance = trgt1.source_id
+join
+fidelity_mdwdb.d_fmr_product_c lkp
+on  COALESCE(SRC.u_product, 'UNSPECIFIED')=lkp.row_id and SRC.sourceinstance=lkp.source_id
+WHERE coalesce(lkp.row_key,case when SRC.u_product is null then 0 else -1 end)  <>trgt1.fmr_product_c_key
+
+
+
---
+++ Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_change_failure_environment_details_src_c_key_mysql.sql
@@ -0,0 +1,14 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.u_raise_impact_c' ELSE 'SUCCESS' END as Message
+FROM
+fidelity_mdsdb.change_request_final SRC
+JOIN
+ fidelity_mdwdb.d_change_failure trgt1
+on SRC.sys_id = trgt1.row_id and SRC.sourceinstance = trgt1.source_id
+join
+fidelity_mdwdb.d_lov lkp
+on  COALESCE(CONCAT('U_ENVIRONMENT_DETAILS~CHANGE_REQUEST~',SRC.u_environment_details),'UNSPECIFIED') =lkp.row_id and SRC.sourceinstance=lkp.source_id
+WHERE coalesce(lkp.row_key,case when SRC.u_environment_details is null then 0 else -1 end)  <>trgt1.environment_details_src_c_key
+
+
+
