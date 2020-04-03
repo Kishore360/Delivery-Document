36e3a8188d4f94e035b20ae126b457f9c3b5f672
prabhuram.sridharan <prabhuram.sridharan@ad.numerify.com>
2020-04-03 16:30:47 +0530

    committing

--- Apps 5.0/AETNA/ITSA/FUNCTIONAL/d_incident.close_notes_c_mysql.sql
+++ Apps 5.0/AETNA/ITSA/FUNCTIONAL/d_incident.close_notes_c_mysql.sql
@@ -1,7 +1,9 @@
 SELECT CASE WHEN CNT>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
 CASE WHEN CNT>0 THEN 'MDS to DWH data validation failed for d_incident.close_notes_c' ELSE 'SUCCESS' END as Message 
 FROM(select count(1) as CNT from 
-(select sys_id,sourceinstance, close_notes  from aetna_mdsdb.incident_final where CDCTYPE<>'D') src
-left JOIN  aetna_mdwdb.d_incident trgt
+(select sys_id,sourceinstance, close_notes,cdctime  from aetna_mdsdb.incident_final where CDCTYPE<>'D') src
+join (select source_id,max(lastupdated) as lastupdated from aetna_mdwdb.d_o_data_freshness group by source_id) f1 on
+ (f1.source_id = b.sourceinstance) and  (src.cdctime<=f1.lastupdated)
+JOIN  aetna_mdwdb.d_incident trgt
 on src.sys_id = trgt.row_id and src.sourceinstance = trgt.source_id 
-where coalesce(src.close_notes) <> trgt.close_notes_c )temp;
+where src.close_notes <> trgt.close_notes_c )temp;
