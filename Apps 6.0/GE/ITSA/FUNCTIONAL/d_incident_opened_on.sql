8a9c971d429a969ba088dd87842abbeff6b5b72c
Shubha_Rao <Shubha.Rao@numerify.com>
2020-04-11 20:56:55 +0530

    Updating qa Queries in GE

---
+++ Apps 6.0/GE/ITSA/FUNCTIONAL/d_incident_opened_on.sql
@@ -0,0 +1,9 @@
+
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_incident.opened_on' ELSE 'SUCCESS' END as Message
+  FROM ( SELECT * FROM ge_mdsdb.incident_final WHERE CDCTYPE<>'D') SRC 
+ LEFT JOIN ge_mdwdb.d_incident TRGT 
+ ON (SRC.sys_id=TRGT.row_id 
+ AND SRC.sourceinstance=TRGT.source_id )
+ WHERE convert_tz(SRC.opened_at,'<<TENANT_SSI_TIME_ZONE>>','<<DW_TARGET_TIME_ZONE>>') <> TRGT.opened_on 
---
+++ Apps 6.0/GE/ITSA/FUNCTIONAL/f_change_credit_score_assigned_to_key_ITSA.sql
@@ -0,0 +1,6 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_change_credit_score.assigned_to_key' ELSE 'SUCCESS' END as Message
+ FROM ( SELECT * FROM ge_mdsdb.f_change_credit_score_base) SRC 
+ LEFT JOIN ge_mdwdb.f_change_credit_score TRGT 
+ ON (SRC.row_id=TRGT.row_id AND SRC.source_id=TRGT.source_id AND SRC.task_type = TRGT.task_type)
+ where COALESCE( SRC.assigned_to_key,'')<> COALESCE(TRGT.assigned_to_key,'')
