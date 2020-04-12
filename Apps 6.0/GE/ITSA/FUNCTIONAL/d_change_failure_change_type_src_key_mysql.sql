aeeef73520f4102884c30f736c12587e84342d9d
Shubha_Rao <Shubha.Rao@numerify.com>
2020-04-12 22:56:11 +0530

    Updating the GE queries

---
+++ Apps 6.0/GE/ITSA/FUNCTIONAL/d_change_failure_change_type_src_key_mysql.sql
@@ -0,0 +1,13 @@
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_failure.change_type_src_key' ELSE 'SUCCESS' END as Message
+from (SELECT * FROM ge_mdsdb.change_request_final WHERE CDCTYPE<>'D') SRC
+left join (SELECT * FROM ge_mdwdb.d_change_failure where year(effective_to)=2999 
+) TRGT 
+ ON (SRC.sys_id=TRGT.row_id 
+ AND SRC.sourceinstance=TRGT.source_id )
+ LEFT JOIN ge_mdwdb.d_lov LKP 
+ ON ( CONCAT('TYPE','~','CHANGE_REQUEST','~',UPPER(type)) = LKP.src_rowid 
+ 
+AND SRC.sourceinstance = LKP.source_id )
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.type IS NULL THEN 0 else '-1' end)<> COALESCE(TRGT.change_type_src_key,'')
--- Apps 6.0/GE/ITSA/FUNCTIONAL/f_n_change_credit_score_monthly_minor_incident_deduction_ITSA_mysql.sql
+++ Apps 6.0/GE/ITSA/FUNCTIONAL/f_n_change_credit_score_monthly_minor_incident_deduction_ITSA_mysql.sql
@@ -1,6 +1,6 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_n_change_credit_score_monthly.minor_incident_deduction' ELSE 'SUCCESS' END as Message
-FROM ge_mdsdb.f_n_change_credit_score_monthly_base SRC
+FROM #STG_TABLE_SCHEMA.f_n_change_credit_score_monthly_base SRC
  
  LEFT JOIN ge_mdwdb.f_n_change_credit_score_monthly TRGT 
  ON (SRC.sys_id = TRGT.row_id 
