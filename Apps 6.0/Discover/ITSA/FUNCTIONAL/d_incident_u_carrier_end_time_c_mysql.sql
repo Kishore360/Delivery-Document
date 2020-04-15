32cb97fd9eed1875f1ff4bbb7cc3c9d9240db4c5
bhavani_y <bhavani.y@numerify.com>
2020-04-15 17:26:01 +0530

    no message

--- Apps 6.0/Discover/ITSA/FUNCTIONAL/d_incident_u_carrier_end_time_c_mysql.sql
+++ Apps 6.0/Discover/ITSA/FUNCTIONAL/d_incident_u_carrier_end_time_c_mysql.sql
@@ -1,4 +1,4 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN
- 'MDS to DWH data validation failed for d_incident.u_carrier_end_time_c' ELSE 'SUCCESS' END as Message FROM discover_mdsdb.incident_final  SRC 
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_incident.u_carrier_end_time_c' ELSE 'SUCCESS' END as Message FROM discover_mdsdb.incident_final  SRC 
  JOIN discover_mdwdb.d_incident TRGT ON (SRC.sys_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id ) 
-WHERE CONVERT_TZ (SRC.u_carrier_end_time, 'GMT','America/Los_Angeles') <> (TRGT.u_carrier_end_time_c) 
+WHERE CONVERT_TZ (SRC.u_carrier_end_time, 'GMT','America/New_York') <> (TRGT.u_carrier_end_time_c) 
--- Apps 6.0/Discover/ITSA/FUNCTIONAL/f_incident_resolver_group_c_key_mysql.sql
+++ Apps 6.0/Discover/ITSA/FUNCTIONAL/f_incident_resolver_group_c_key_mysql.sql
@@ -6,7 +6,7 @@ FROM
 SELECT Count(1) AS CNT 
 FROM discover_mdsdb.incident_final SRC 
 LEFT JOIN discover_mdwdb.f_incident TRGT ON SRC.sys_id=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id
-JOIN discover_mdwdb.d_internal_organization LKP ON COALESCE(concat('GROUP~',SRC.u_resolver_group),'UNSPECIFIED')=LKP.row_id AND SRC.sourceinstance=TRGT.source_id)
+JOIN discover_mdwdb.d_internal_organization LKP ON COALESCE(concat('GROUP~',SRC.u_resolver_group),'UNSPECIFIED')=LKP.row_id AND SRC.sourceinstance=TRGT.source_id
 WHERE COALESCE(LKP.row_key,CASE WHEN SRC.u_resolver_group IS NULL THEN 0 ELSE -1 END)<>TRGT.resolver_group_c_key
-AND SRC.cdctype='X'
+AND SRC.cdctype='X' and SRC.opened_at is not null
 ) temp; 
