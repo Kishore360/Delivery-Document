32da038019ba06cc73d8db71fcd67e6ccb447f4f
prabhuram.sridharan <prabhuram.sridharan@ad.numerify.com>
2020-04-15 16:53:05 +0530

    committing

--- Apps 6.0/INGRAMMICRO/ITSA/FUNCTIONAL/d_problem_u_root_cause_c_mysql.sql
+++ Apps 6.0/INGRAMMICRO/ITSA/FUNCTIONAL/d_problem_u_root_cause_c_mysql.sql
@@ -6,5 +6,5 @@ FROM
 SELECT  count(1) as CNT
 FROM ingrammicro_mdsdb.problem_final SRC 
 LEFT JOIN ingrammicro_mdwdb.d_problem TRGT ON (SRC.sys_id=TRGT.row_id AND SRC.sourceinstance=TRGT.source_id)
-WHERE  substring(COALESCE(SRC.u_root_cause,'UNSPECIFIED'),1,700) <> TRGT.u_root_cause_c
+WHERE COALESCE(SRC.u_root_cause,'UNSPECIFIED') <> TRGT.u_root_cause_c
 AND SRC.cdctype='X' ) temp; 
