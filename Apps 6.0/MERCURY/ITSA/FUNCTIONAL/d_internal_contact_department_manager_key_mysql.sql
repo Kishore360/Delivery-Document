7b0dd411175182e2a8b0e32b82e10f27eb5e1e79
Narmadha_D <narmadha.d@numerify.com>
2020-04-16 11:44:10 +0530

    updated DELV-23226

---
+++ Apps 6.0/MERCURY/ITSA/FUNCTIONAL/d_internal_contact_department_manager_key_mysql.sql
@@ -0,0 +1,18 @@
+SELECT CASE WHEN CNT>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN CNT>0 THEN 'MDS to DWH data validation failed for d_internal_contact.department_key' ELSE 'SUCCESS' END as Message
+FROM
+(select count(1) as CNT FROM mercury_mdsdb.incident_final src
+join mercury_mdwdb.f_incident a
+on src.sys_id=a.row_id and src.sourceinstance=a.source_id
+ JOIN mercury_mdwdb.d_incident i 
+ ON a.incident_key = i.row_key
+ join mercury_mdwdb.d_internal_contact ic
+ on i.caller_key=ic.row_key
+ join mercury_mdwdb.d_internal_organization io
+ on ic.department_key=io.row_key
+ join mercury_mdwdb.d_internal_contact mgr
+on ic.manager_key=mgr.row_key
+join mercury_mdwdb.d_internal_organization md
+on mgr.department_key=md.row_key
+ where mgr.department_key <> md.row_key
+ ) der;
