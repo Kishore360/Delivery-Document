97773e8d369f7ccc8682cf066c586505b77296d9
bhavani_y <bhavani.y@numerify.com>
2020-04-17 13:35:59 +0530

    no message

--- Apps 5.0/GILEAD/ITSA/FUNCTIONAL/f_certification_task_c_closed_by_key_mysql.sql
+++ Apps 5.0/GILEAD/ITSA/FUNCTIONAL/f_certification_task_c_closed_by_key_mysql.sql
@@ -1,10 +1,11 @@
 SELECT CASE WHEN CNT >0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
 CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_certification_task_c.closed_by_key' ELSE 'SUCCESS' END as Message 
 FROM (
-select count(*) as CNT from gilead_mdsdb.cert_task_final SRC
+select count(1) as CNT from gilead_mdsdb.cert_task_final SRC
 LEFT JOIN  gilead_mdwdb.f_certification_task_c trgt
 on SRC.sys_id = trgt.row_id and SRC.sourceinstance = trgt.source_id
 LEFT JOIN gilead_mdwdb.d_internal_contact LKP 
  ON ( concat('INTERNAL_CONTACT~',SRC.closed_by)= LKP.row_id 
 AND SRC.sourceinstance= LKP.source_id )
- WHERE COALESCE(LKP.row_key,CASE WHEN SRC.closed_by IS NULL THEN 0 else -1 end)<> (trgt.closed_by_key))t;
+ WHERE COALESCE(LKP.row_key,CASE WHEN SRC.closed_by IS NULL THEN 0 else -1 end)<> (trgt.closed_by_key) and SRC.cdctype='X'
+ and trgt.soft_deleted_flag='Y')t;
--- Apps 5.0/GILEAD/ITSA/FUNCTIONAL/f_certification_task_c_closed_on_key_mysql.sql
+++ Apps 5.0/GILEAD/ITSA/FUNCTIONAL/f_certification_task_c_closed_on_key_mysql.sql
@@ -1,11 +1,13 @@
 SELECT CASE WHEN CNT >0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
 CASE WHEN CNT >0 THEN 'MDS to DWH data validation failed for f_certification_task_c.closed_on_key' ELSE 'SUCCESS' END as Message 
 FROM (
-select count(*) as CNT from gilead_mdsdb.cert_task_final SRC
+select count(1) as CNT from gilead_mdsdb.cert_task_final SRC
 LEFT JOIN  gilead_mdwdb.f_certification_task_c trgt
 on SRC.sys_id = trgt.row_id and SRC.sourceinstance = trgt.source_id
+left join gilead_mdwdb.d_lov_map map on map.src_key=trgt.state_src_key
 LEFT JOIN gilead_mdwdb.d_calendar_date LKP 
 on (LKP.row_id = COALESCE(date_format(convert_tz(SRC.closed_at,'GMT','America/New_York'),'%Y%m%d'), 'UNSPECIFIED') and LKP.source_id=0
-) WHERE COALESCE(LKP.row_key, CASE WHEN SRC.closed_at is NULL THEN 0 ELSE -1 END) <> (trgt.closed_on_key))t 
+) WHERE case when map.dimension_wh_code<>'CLOSED' then 0 else LKP.row_key end <> (trgt.closed_on_key) and SRC.cdctype='X'
+)t 
 
 ;
