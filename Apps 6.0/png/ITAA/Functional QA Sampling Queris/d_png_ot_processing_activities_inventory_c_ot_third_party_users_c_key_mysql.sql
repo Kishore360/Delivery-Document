9f4c8f2ecb90caaa0f158d329c353a7198585bfc
Kishore_Mantha <kishore.mantha@numerify.com>
2020-04-20 12:33:19 +0530

    Updated the QA Codes.

--- Apps 6.0/png/ITAA/Functional QA Sampling Queris/d_png_ot_processing_activities_inventory_c_ot_third_party_users_c_key_mysql.sql
+++ Apps 6.0/png/ITAA/Functional QA Sampling Queris/d_png_ot_processing_activities_inventory_c_ot_third_party_users_c_key_mysql.sql
@@ -1,8 +1,13 @@
-SELECT CASE WHEN count(1)>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)>0 THEN 'MDS to DWH data validation failed for d_png_ot_processing_activities_inventory_c.ot_third_party_users_c_key' ELSE 'SUCCESS' END as Message 
+SELECT 
+CASE WHEN CNT > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN CNT > 0 THEN 'MDS to DWH data validation failed for d_png_ot_processing_activities_inventory_c.ot_third_party_users_c_key' ELSE 'SUCCESS' END as Message 
+FROM 
+( 
+SELECT Count(1) as CNT 
 FROM png_mdsdb.pg_ot_processing_activities_inventory_final  src
 LEFT JOIN  png_mdwdb.d_png_ot_processing_activities_inventory_c trgt
 on src.inventory_id = trgt.row_id and src.sourceinstance = trgt.source_id
 JOIN png_mdwdb.d_lov lkp
 ON COALESCE(CONCAT('OT_THIRD_PARTY_USERS_C~PROCESSING_ACTIVITIES~',src.Thirdparty_Users),'UNSPECIFIED') =lkp.row_id
 where COALESCE(lkp.row_key,CASE WHEN src.Thirdparty_Users IS NULL THEN 0 else -1 end)<> trgt.ot_third_party_users_c_key
+) temp;
