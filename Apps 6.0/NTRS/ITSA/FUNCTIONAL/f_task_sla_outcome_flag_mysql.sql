8c2659fbe91cbca17f80d0ca7592c8515c4f48c7
Narmadha_D <narmadha.d@numerify.com>
2020-04-22 03:17:56 +0530

    updates

--- Apps 6.0/NTRS/ITSA/FUNCTIONAL/f_task_sla_outcome_flag_mysql.sql
+++ Apps 6.0/NTRS/ITSA/FUNCTIONAL/f_task_sla_outcome_flag_mysql.sql
@@ -1,5 +1,9 @@
 SELECT CASE WHEN count(1) >1 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN count(1)>1 THEN 'MDS to DWH data validation failed for f_task_sla.outcome_flag' ELSE 'SUCCESS' END as Message
-FROM ntrust_mds_viewdb.task_sla_final SRC
+FROM ntrust_mdsdb.task_sla_final SRC
+JOIN
+                ntrust_mdsdb.task_final task 
+                    ON task.sys_id=SRC.task 
+                    and task.sourceinstance=SRC.sourceinstance 
 JOIN ntrust_mdwdb.f_task_sla TRGT ON (SRC.sys_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id )
 WHERE case when SRC.has_breached=TRUE then 'Y' else 'N' end <> (TRGT.outcome_flag) and SRC.cdctype<>'D';
--- Apps 6.0/NTRS/ITSA/FUNCTIONAL/f_task_sla_task_key_mysql.sql
+++ Apps 6.0/NTRS/ITSA/FUNCTIONAL/f_task_sla_task_key_mysql.sql
@@ -1,7 +1,11 @@
 SELECT CASE WHEN count(1) >1 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
 CASE WHEN count(1)>1 THEN 'MDS to DWH data validation failed for f_task_sla.task_key' ELSE 'SUCCESS' END as Message
 FROM  ntrust_mds_viewdb.task_sla_final SRC
+JOIN
+                ntrust_mdsdb.task_final task 
+                    ON task.sys_id=SRC.task 
+                    and task.sourceinstance=SRC.sourceinstance 
 JOIN ntrust_mdwdb.f_task_sla TRGT ON (SRC.sys_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id)
-join ntrust_mdwdb.d_task Lkp on (SRC.task=Lkp.row_id)
+join ntrust_mdwdb.d_task Lkp on (task.sys_id=Lkp.row_id)
 WHERE COALESCE(Lkp.row_key,CASE WHEN SRC.task is NULL THEN 0 ELSE -1 END) <> TRGT.task_key
 and SRC.cdctype<>'D';
