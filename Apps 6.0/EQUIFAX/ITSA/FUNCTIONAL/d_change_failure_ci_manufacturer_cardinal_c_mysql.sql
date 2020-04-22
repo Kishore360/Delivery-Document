d5dc8104ca9a40eeb2f4ae3151bfee0c9f6f19df
Shubha_Rao <Shubha.Rao@numerify.com>
2020-04-22 17:58:07 +0530

    updating the QA queries

--- Apps 6.0/EQUIFAX/ITSA/FUNCTIONAL/d_change_failure_ci_manufacturer_cardinal_c_mysql.sql
+++ Apps 6.0/EQUIFAX/ITSA/FUNCTIONAL/d_change_failure_ci_manufacturer_cardinal_c_mysql.sql
@@ -4,6 +4,6 @@ FROM equifax_mdsdb.change_request_final SRC
 JOIN equifax_mdwdb.d_change_failure TRGT  
 ON (SRC.sys_id  = TRGT.row_id  
 AND SRC.sourceinstance = TRGT.source_id ) and SRC.cdctype<>'D' and TRGT.current_flag='Y'
-LEFT JOIN equifax_mdsdb.us_predictor_variable_c_final LKP
+LEFT JOIN (select * from equifax_mdsdb.us_predictor_variable_c_final where cdctype<>'D')LKP
 ON TRGT.ci_manufacturer_c=LKP.value and TRGT.source_id=LKP.sourceinstance and LKP.predictor_variable='manufacturer'
 WHERE CASE WHEN LKP.value is null then ci_manufacturer_c else 'OTHERS' end <>ci_manufacturer_cardinal_c and SRC.cdctype<>'D';
