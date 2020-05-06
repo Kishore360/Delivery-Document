55fb042c98f23c96efe36947b308d4b599aa3502
snehav_0807 <sneha.v@numerify.com>
2020-05-06 14:17:33 +0530

    BB&T cust coverage

---
+++ Apps 5.0/BBANDT/ITSA/FUNCTIONAL/d_problem_assignment_organization_c_mysql.sql
@@ -0,0 +1,9 @@
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_problem.assignment_organization_c' ELSE 'SUCCESS' END as Message
+ -- select CONVERT_TZ (src.sys_updated_on,'GMT','America/New_York'), trgt.sys_updated_on
+ FROM bbandt_mdsdb.problem_final src 
+ join bbandt_mdwdb.d_problem trgt
+  ON   src.sys_id=trgt.row_id 
+  and trgt.source_id = src.sourceinstance
+ WHERE COALESCE(src.u_organization_problem_assignment,'UNSPECIFIED')<>trgt.assignment_organization_c
+ and src.cdctype='X';
