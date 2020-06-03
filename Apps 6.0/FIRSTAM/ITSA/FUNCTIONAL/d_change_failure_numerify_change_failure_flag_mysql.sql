e9d1bd09d6713f94e0e0da519f035920105a02ad
Shubha_Rao <Shubha.Rao@numerify.com>
2020-06-03 11:44:24 +0530

    Updating the queries in the tenants

--- Apps 6.0/FIRSTAM/ITSA/FUNCTIONAL/d_change_failure_numerify_change_failure_flag_mysql.sql
+++ Apps 6.0/FIRSTAM/ITSA/FUNCTIONAL/d_change_failure_numerify_change_failure_flag_mysql.sql
@@ -1,30 +1,34 @@
-SELECT CASE WHEN count(1)>0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)
-THEN 'MDS to DWH data validation failed for d_change_request.failure_flag' ELSE 'SUCCESS' END as Message
-FROM firstam_mdsdb.change_request_final a
-JOIN firstam_mdwdb.d_change_request d ON a.sys_id=d.row_id AND a.sourceinstance=d.source_id
-LEFT JOIN
-(SELECT DISTINCT c.sourceinstance AS source_id,c.sys_id AS row_id,c.cdctype
-FROM firstam_mdsdb.change_request_final c
-JOIN firstam_mdsdb.incident_final i ON c.sys_id=i.caused_by AND c.sourceinstance=i.sourceinstance
-WHERE i.caused_by IS NOT NULL AND (i.opened_at > c.end_date OR i.opened_at < c.start_date)) b
-ON a.sys_id = b.row_id and a.sourceinstance = b.source_id
-LEFT JOIN
-(SELECT DISTINCT c.sourceinstance AS source_id,c.sys_id AS row_id,c.cdctype
-FROM firstam_mdsdb.change_request_final c
-JOIN firstam_mdsdb.label_final l ON c.number = l.name AND c.sourceinstance = l.sourceinstance
-JOIN firstam_mdsdb.label_entry_final le ON le.label = l.sys_id AND le.sourceinstance = l.sourceinstance
-JOIN firstam_mdsdb.incident_final i ON le.table_key = i.sys_id AND le.sourceinstance = i.sourceinstance
-WHERE l.name LIKE 'GECHG%' AND le.id_type = 'Incident' AND (i.opened_at > c.end_date OR i.opened_at < c.start_date)) c
-ON a.sys_id = c.row_id and a.sourceinstance = c.source_id
-LEFT JOIN firstam_mdwdb.d_lov_map lm ON lm.dimension_class = 'STATE~CHANGE_REQUEST' AND a.state=lm.dimension_code
-AND lm.source_id=a.sourceinstance
-WHERE a.cdctype<>'D' AND ((CASE
-WHEN COALESCE(a.close_code,'UNSPECIFIED') in ('successful_issues','unsuccessful')
-OR b.row_id is not null OR c.row_id is not null THEN 'Y'
-WHEN (COALESCE(a.close_code,'UNSPECIFIED') not in ('successful_issues','unsuccessful')
-AND b.row_id is null AND c.row_id is null) AND lm.dimension_wh_code <> ('CANCELED')
-AND (a.end_date< '2020-03-25 00:00:00' OR lm.dimension_wh_code = ('CLOSED')) THEN 'N'
-WHEN (COALESCE(a.close_code,'UNSPECIFIED') not in ('successful_issues','unsuccessful')
-AND b.row_id is null AND c.row_id is null) AND (lm.dimension_wh_code = ('CANCELED')
-OR ((a.end_date >= '2020-03-25 00:00:00' AND lm.dimension_wh_code <> ('CLOSED')))) THEN 'X'
-ELSE 'X' END) <> d.failure_flag);
+
+SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+ CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_failure.numerify_change_failure_flag' ELSE 'SUCCESS' END as Message
+from (SELECT * FROM firstam_mdsdb.change_request_final WHERE CDCTYPE<>'D') SRC
+left join (SELECT * FROM firstam_mdwdb.d_change_failure where year(effective_to)=2999 
+) TRGT 
+ ON (SRC.sys_id=TRGT.row_id 
+ AND SRC.sourceinstance=TRGT.source_id )
+
+ left outer join
+( 
+select cr.row_id, inc.sourceinstance, count(*) as incident_count 
+from firstam_mdsdb.incident_final inc
+left outer join firstam_mdwdb.d_change_request cr
+on inc.caused_by = cr.row_id and 
+inc.sourceinstance = cr.source_id
+where inc.caused_by is not null
+group by cr.row_id, inc.sourceinstance
+) inc_count
+on (SRC.sys_id=inc_count.row_id 
+ AND SRC.sourceinstance=inc_count.sourceinstance )
+ 
+ LEFT JOIN firstam_mdwdb.d_lov_map LKP1
+ ON ( CONCAT('STATE','~','CHANGE_REQUEST','~',UPPER(state)) = LKP1.row_id 
+ AND SRC.sourceinstance = LKP1.source_id )
+ 
+ LEFT JOIN firstam_mdwdb.d_lov_map LKP2
+ ON ( CONCAT('CLOSE_CODE','~','CHANGE_REQUEST','~',UPPER(close_code)) = LKP2.row_id 
+ AND SRC.sourceinstance = LKP2.source_id )
+ 
+ WHERE COALESCE(CASE WHEN LKP1.dimension_wh_code is null or LKP2.dimension_wh_code is null then 'X'
+	   WHEN LKP1.dimension_wh_code = 'CLOSED' THEN (CASE WHEN (LKP2.dimension_wh_code IN ('SUCCESSFUL_WITH_ISSUES','UNSUCCESSFUL')
+ or inc_count.incident_count>0) THEN 'Y' ELSE 'N' END)
+	   ELSE 'X' END, '')<> COALESCE(TRGT.numerify_change_failure_flag,'')
