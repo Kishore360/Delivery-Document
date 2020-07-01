cfb9da904d3724a51242293d895c749aa0044bc1
Shubha_Rao <Shubha.Rao@numerify.com>
2020-07-01 13:39:14 +0530

    updating the cust queries

--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_archer_application_c_slo_c_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_archer_application_c_slo_c_mysql.sql
@@ -1,7 +1,18 @@
-SELECT 
-CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_archer_application_c_slo_c' ELSE 'SUCCESS' END as Message 
-from truist_mdwdb.f_archer_application_c trgt
-join truist_mdsdb.us_Third_Party_SLA_SLO_Targets_final src
-on trgt.application_name_c=src.application_name
-where slo_c<>src.Target_perc_2 and src.cdctype='X';
+select case when count(1)>1 then 'FAILURE' else 'SUCCESS'  END as result ,
+CASE WHEN count(1)>1 then 'Data mismatch'  ELSE 'SUCCESS'  END as Message
+from 
+truist_mdwdb.f_archer_application_c f_archer_application_c   
+    INNER JOIN
+       ( SELECT
+            MAX(d_o_data_freshness.lastupdated) AS lastupdated,
+            d_o_data_freshness.source_id AS source_id,
+            MAX(d_o_data_freshness.etl_run_number) AS etl_run_number    
+        FROM
+           truist_mdwdb.d_o_data_freshness d_o_data_freshness             
+        GROUP BY
+            2  ) t_d_o_data_freshness_c 
+            ON f_archer_application_c.source_id=t_d_o_data_freshness_c.source_id  
+    INNER JOIN
+        truist_mdwdb.d_archer_application_c d_archer_application_c 
+            ON f_archer_application_c.archer_application_c_key=d_archer_application_c.row_key  
+            where f_archer_application_c.slo_c <> d_archer_application_c.slo_c
