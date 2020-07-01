5853f70e1c6aa8a15666e662e4d5aa4191d9bd29
Shubha_Rao <Shubha.Rao@numerify.com>
2020-07-01 18:28:52 +0530

    latest updates on queries

--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_application_infra_c_configuration_item_key_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_application_infra_c_configuration_item_key_mysql.sql
@@ -6,5 +6,5 @@ join truist_mdwdb.f_application_infra_c a12 on
 (concat(a11.app_id,'~',a11.app_name,'~',a11.infra_resource_name,'~',a11.source)=a12.row_id and a11.sourceinstance=a12.source_id)
 left join truist_mdwdb.d_configuration_item a13 
 on COALESCE(a11.u_cmdb_sys_id,'UNSPECIFIED')=a13.row_id -- and a11.sourceinstance=a13.source_id
-where COALESCE(a13.row_key,CASE WHEN a11.u_cmdb_sys_id IS NULL THEN 0 ELSE -1 END)<>a12.cmdb_ci_key 
+where COALESCE(a13.row_key,CASE WHEN a11.u_cmdb_sys_id IS NULL THEN 0 ELSE -1 END)<>a12.configuration_item_key 
 and a11.cdctype='X';
--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_application_infra_c_infra_eos_score_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_application_infra_c_infra_eos_score_mysql.sql
@@ -1,7 +1,7 @@
 select 
 CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_application_infra_c.infra_eos_score' ELSE 'SUCCESS' END as Message
- from truist_mdwdb.f_application_infra_c 
+ from truist_mdwdb.d_application_infra_c 
  where CASE WHEN db_end_of_support_health_code='White' and ws_end_of_support_health_code='White' and os_end_of_support_health_code='White' 
 then 'White'
 WHEN db_end_of_support_health_code='Red' or ws_end_of_support_health_code='Red' or os_end_of_support_health_code='Red' 
