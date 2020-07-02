965dde9bfa62d60b06869b3ece815e0c3e7d419b
Shubha_Rao <Shubha.Rao@numerify.com>
2020-07-02 09:44:14 +0530

    Upgrade cust query updates

--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_configuration_item_server_c_key_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_configuration_item_server_c_key_mysql.sql
@@ -1,9 +1,9 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_request.close_code_c_key' ELSE 'SUCCESS' END as Message
- FROM truist_mdsdb.cmdb_ci_final SRC 
+ FROM (select * from truist_mdsdb.cmdb_ci_final where CDCTYPE<>'D') SRC 
 LEFT JOIN truist_mdwdb.d_configuration_item TRGT 
  ON (SRC.sys_id= TRGT.row_id AND SRC.sourceinstance= TRGT.source_id )
  LEFT JOIN truist_mdwdb.d_server_c LKP 
- ON (SRC.sys_id =LKP.row_id AND SRC.sourceinstance= LKP.source_id)
+ ON (COALESCE(SRC.sys_id,'UNSPECIFIED')  =LKP.row_id AND SRC.sourceinstance= LKP.source_id)
  WHERE COALESCE(LKP.row_key,CASE WHEN SRC.sys_id IS NULL THEN 0 else -1 end)<> (TRGT.server_c_key);
  
--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_server_c_created_on_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_server_c_created_on_mysql.sql
@@ -1,6 +1,6 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_server_c.created_on' ELSE 'SUCCESS' END as Message
- FROM truist_mdsdb.cmdb_ci_server_final SRC 
+ FROM (select * from truist_mdsdb.cmdb_ci_server_final where cdctype <>'D') SRC 
  LEFT JOIN truist_mdwdb.d_server_c TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
