0794b61d7ca61f6e3dec42f2cbfcec31d74d722d
Shubha_Rao <Shubha.Rao@numerify.com>
2020-07-08 15:54:14 +0530

    Updating cust queries

--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_change_request_group_coordinated_with_c_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_change_request_group_coordinated_with_c_mysql.sql
@@ -1,11 +1,18 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
 CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for d_change_request.group_coordinated_with_c' ELSE 'SUCCESS' END as Message 
- from truist_mdwdb.d_change_request trgt 
-left join (select grpcord.change_request_key, group_concat(distinct grp.organization_name) as res
-from truist_mdwdb.f_group_coordinated_with_c grpcord
-join truist_mdwdb.d_internal_organization grp 
-on grpcord.group_coordinated_with_c_key = grp.row_key 
-and grpcord.etl_run_number = 0 
-and grpcord.pivot_date between grp.effective_from and grp.effective_to group by change_request_key) src 
-on trgt.row_key = src.change_request_key
-where trgt.group_coordinated_with_c <> LEFT(COALESCE(src.res,'UNSPECIFIED'),4000);
+ from truist_mdwdb.d_change_request d_change_request   
+    LEFT OUTER JOIN
+        (SELECT
+            f_group_coordinated_with_c.change_request_key AS change_request_key,
+            group_concat(distinct d_internal_organization.organization_name) AS res,
+            f_group_coordinated_with_c.etl_run_number AS etl_run_number        
+        FROM
+            truist_mdwdb.f_group_coordinated_with_c f_group_coordinated_with_c      
+        LEFT OUTER JOIN
+            truist_mdwdb.d_internal_organization d_internal_organization 
+                ON f_group_coordinated_with_c.group_coordinated_with_c_key = d_internal_organization.row_key 
+                and f_group_coordinated_with_c.pivot_date between d_internal_organization.effective_from and d_internal_organization.effective_to    
+        group by
+            f_group_coordinated_with_c.change_request_key  ) t_group_coordinated_with_c 
+            ON d_change_request.row_key = t_group_coordinated_with_c.change_request_key 
+where d_change_request.group_coordinated_with_c <> LEFT(COALESCE(t_group_coordinated_with_c.res,'UNSPECIFIED'),4000);
--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_incident_age_key_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_incident_age_key_mysql.sql
@@ -2,7 +2,23 @@
  select CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed for f_incident.age_key' ELSE 'SUCCESS' END as Message
 from truist_mdwdb.f_incident f  
-LEFT JOIN truist_mdwdb.d_lov L  
-ON ((f.age div 86400 BETWEEN L.lower_range_value AND L.upper_range_value)
-	AND L.dimension_class = 'AGEBUCKET_WH~INCIDENT' )
-	WHERE  coalesce(L.row_key,case when age is null then 0 else -1 end )  <> f.age_key  and f.age is not null
+join
+(SELECT
+            d_incident.opened_on AS opened_on,
+            d_incident.age AS age,
+            d_incident.over_due_flag AS over_due_flag,
+            d_incident.changed_on AS changed_on,
+            d_incident.row_key AS row_key,
+            d_incident.active_flag AS active_flag,
+            d_incident.due_on AS due_on,
+            d_incident.source_id AS source_id,
+            d_incident.etl_run_number AS etl_run_number        
+        FROM
+            truist_mdwdb.d_incident d_incident      
+        INNER JOIN
+            truist_mdwdb.d_lov_map d_lov_map 
+                ON d_incident.state_src_key = d_lov_map.src_key 
+                AND d_lov_map.dimension_class = 'STATE~INCIDENT' 
+                AND d_lov_map.dimension_wh_code = 'OPEN'  )t 
+                 ON f.incident_key = t.row_key  
+where coalesce((select x.row_key  from truist_mdwdb.d_lov x  where (x.dimension_class = 'WH_AGEBUCKET~INCIDENT')   AND floor(t.age) between x.lower_range_value and x.upper_range_value),  -1)  <> f.age_key  and f.age is not null
