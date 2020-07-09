473ed654653fd6993193d7e4acca8fe28e4df36c
Kishore_Mantha <kishore.mantha@numerify.com>
2020-07-09 18:36:08 +0530

    Updated the QA Codes

--- Apps 6.0/png_uat/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_ext_c_it_sec_ctrl_vendor_scope_c_mysql.sql
+++ Apps 6.0/png_uat/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_ext_c_it_sec_ctrl_vendor_scope_c_mysql.sql
@@ -1,7 +1,12 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
-CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_png_ot_processing_activities_inventory_ext_c_it_sec_ctrl_vendor_scope_c' ELSE 'SUCCESS' END as Message 
- 
+SELECT 
+CASE WHEN CNT > 0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN CNT > 0  THEN 'MDS to DWH data validation failed for d_png_ot_processing_activities_inventory_ext_c_it_sec_ctrl_vendor_scope_c' ELSE 'SUCCESS' END as Message 
+FROM 
+(
+SELECT Count(1) as CNT 
 FROM png_mdsdb.pg_ot_processing_activities_inventory_final  SRC 
-JOIN png_jul_mdwdb.d_png_ot_processing_activities_inventory_ext_c TRGT ON 
+JOIN png_mdwdb.d_png_ot_processing_activities_inventory_ext_c TRGT ON 
 SRC.Inventory_Id = TRGT.row_id and SRC.sourceinstance = TRGT.source_id
-WHERE IF(Vendor_Qualification ='DATA PROCESSOR','TRUE','FALSE')  <> (TRGT.it_sec_ctrl_vendor_scope_c) and TRGT.soft_deleted_flag ='N';
+WHERE IF(Vendor_Qualification ='DATA PROCESSOR','TRUE','FALSE')  <> (TRGT.it_sec_ctrl_vendor_scope_c) and TRGT.soft_deleted_flag ='N'
+) temp;
+ 
