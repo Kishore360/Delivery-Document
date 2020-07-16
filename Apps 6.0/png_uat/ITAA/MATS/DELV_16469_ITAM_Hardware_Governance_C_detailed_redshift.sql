ead338fa72bdc16a1381807fdcab5e54b8e59324
Kishore_Mantha <kishore.mantha@numerify.com>
2020-07-16 11:44:36 +0530

    Updated the QA Codes

--- Apps 6.0/png_uat/ITAA/MATS/DELV_16469_ITAM_Hardware_Governance_C_detailed_redshift.sql
+++ Apps 6.0/png_uat/ITAA/MATS/DELV_16469_ITAM_Hardware_Governance_C_detailed_redshift.sql
@@ -13,56 +13,81 @@ d_owned_by_department
 f_configuration_item
  */
  
-SELECT 'f_configuration_item' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-UNION 
-SELECT 'd_configuration_item' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_configuration_item b ON a.configuration_item_key=b.row_key
-UNION 
-SELECT 'd_location' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_configuration_item b ON a.configuration_item_key=b.row_key
-JOIN ldb.d_location c ON b.location_key=c.row_key
-UNION 
-SELECT 'd_calendar_date' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_calendar_date b ON a.date_key_c=b.row_key
-UNION 
-SELECT 'd_calendar_month' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_calendar_date b ON a.date_key_c=b.row_key
-JOIN ldb.d_calendar_month c ON b.month_start_date_key=c.row_key
-UNION 
-SELECT 'd_owned_by' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_owned_by b ON a.owned_by_key=b.row_key
-UNION 
-SELECT 'd_owned_by_department' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_owned_by b ON a.owned_by_key=b.row_key
-JOIN ldb.d_owned_by_department c ON b.owned_by_department=c.row_key
-UNION 
-SELECT 'd_internal_organization_group' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_internal_organization_group b ON a.assignment_group_key=b.row_key
-UNION 
-SELECT 'd_lov_cmdb_ci_install_status_c' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_configuration_item b ON a.configuration_item_key=b.row_key
-JOIN ldb.d_lov_cmdb_ci_install_status_c c ON b.cmdb_ci_install_status_c_key=c.row_key
-UNION 
-SELECT 'd_lov_cmdb_ci_hardware_status_c' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_configuration_item b ON a.configuration_item_key=b.row_key
-JOIN ldb.d_lov_cmdb_ci_hardware_status_c c ON b.cmdb_ci_hardware_status_c_key=c.row_key
-UNION 
-SELECT 'd_lov_cmdb_ci_discovery_source_c' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_configuration_item b ON a.configuration_item_key=b.row_key
-JOIN ldb.d_lov_cmdb_ci_discovery_source_c c ON b.cmdb_ci_discovery_source_c_key=c.row_key
-UNION 
-SELECT 'd_organization_manager_c' AS Table_Name, Count(1) as Row_Count 
-FROM ldb.f_configuration_item a
-JOIN ldb.d_internal_organization_group b ON a.assignment_group_key=b.row_key
-JOIN ldb.d_organization_manager_c c ON b.organization_manager_c_key=c.row_key
+select 'ldb.f_configuration_item a11 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+union
+select 'ldb.d_configuration_item a12 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_configuration_item a12
+on (a11.configuration_item_key = a12.row_key)
+union
+select 'ldb.d_configitem_location_c a13 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_configuration_item a12
+on (a11.configuration_item_key = a12.row_key)
+join ldb.d_configitem_location_c a13
+on (a12.location_key = a13.row_key)
+union
+select 'ldb.d_calendar_date a14 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_calendar_date a14
+on (a11.date_key_c = a14.row_key)
+union
+select 'ldb.d_owned_by a15 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_owned_by a15
+on (a11.owned_by_key = a15.row_key)
+union
+select 'ldb.d_internal_organization_group a16 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_internal_organization_group a16
+on (a11.assignment_group_key = a16.row_key)
+union
+select 'ldb.d_organization_manager_c a17 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_internal_organization_group a16
+on (a11.assignment_group_key = a16.row_key)
+join ldb.d_organization_manager_c a17
+on (a16.organization_manager_c_key = a17.row_key)
+union
+select 'ldb.d_calendar_month a18 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_calendar_date a14
+on (a11.date_key_c = a14.row_key)
+join ldb.d_calendar_month a18
+on (a14.month_start_date_key = a18.row_key)
+union
+select 'ldb.d_owned_by_department a19 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_owned_by a15
+on (a11.owned_by_key = a15.row_key)
+join ldb.d_owned_by_department a19
+on (a15.owned_by_department = a19.row_key)
+union
+select 'ldb.d_product_model_c a110 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_configuration_item a12
+on (a11.configuration_item_key = a12.row_key)
+join ldb.d_product_model_c a110
+on (a12.product_model_c_key = a110.row_key)
+union
+select 'ldb.d_lov_cmdb_ci_discovery_source_c a111 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_configuration_item a12
+on (a11.configuration_item_key = a12.row_key)
+join ldb.d_lov_cmdb_ci_discovery_source_c a111
+on (a12.cmdb_ci_discovery_source_c_key = a111.row_key)
+union
+select 'ldb.d_lov_cmdb_ci_hardware_status_c a112 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_configuration_item a12
+on (a11.configuration_item_key = a12.row_key)
+join ldb.d_lov_cmdb_ci_hardware_status_c a112
+on (a12.cmdb_ci_hardware_status_c_key = a112.row_key)
+union
+select 'ldb.d_lov_cmdb_ci_install_status_c a113 ' as Table_name, count(a11.row_key) Row_Count
+from ldb.f_configuration_item a11
+join ldb.d_configuration_item a12
+on (a11.configuration_item_key = a12.row_key)
+join ldb.d_lov_cmdb_ci_install_status_c a113
+on (a12.cmdb_ci_install_status_c_key = a113.row_key)
