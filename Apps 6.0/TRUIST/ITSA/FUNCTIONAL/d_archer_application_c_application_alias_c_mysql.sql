319b13f0220a3ba71b6970584f27e7d01dcf6548
Shubha_Rao <Shubha.Rao@numerify.com>
2020-07-19 21:14:28 +0530

    Updating QA query

--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_archer_application_c_application_alias_c_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_archer_application_c_application_alias_c_mysql.sql
@@ -1,6 +1,6 @@
-select case when count(1)>1 then 'FAILURE' else 'SUCCESS'  END as result ,
-CASE WHEN count(1)>1 then 'Data mismatch'  ELSE 'SUCCESS'  END as Message
-from truist_mdsdb.app_vwarchertonumerifyfeed_final a
+select case when count(1)>0 then 'FAILURE' else 'SUCCESS'  END as result ,
+CASE WHEN count(1)>0 then 'Data mismatch'  ELSE 'SUCCESS'  END as Message
+from (select * from truist_mdsdb.app_vwarchertonumerifyfeed_final where cdctype<>'D') a
 join truist_mdwdb.d_archer_application_c b
 on a.application_id=b.row_id and a.sourceinstance=b.source_id
 where COALESCE(a.application_alias,'UNSPECIFIED')<>b.application_alias_c;
--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_application_infra_c_archer_application_c_key_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/f_application_infra_c_archer_application_c_key_mysql.sql
@@ -1,6 +1,6 @@
 select case when count(1)>1 then 'FAILURE'  else 'SUCCESS'  END as result ,
 CASE WHEN count(1)>1 then 'Data mismatch for f_application_infra_c.archer_application_c_key'  ELSE 'SUCCESS'  END as Message
-from  truist_mdsdb.v_app_to_server_to_product_final a11
+from (select * from  truist_mdsdb.v_app_to_server_to_product_final where cdctype<>'D') a11
 join truist_mdwdb.f_application_infra_c a12 on 
 (concat(a11.app_id,'~',a11.app_name,'~',a11.infra_resource_name,'~',a11.source)=a12.row_id and a11.sourceinstance=a12.source_id)
 join truist_mdwdb.d_archer_application_c a13
