e52ac8b0e4e084cb18cf83074e06cda1091bfc35
Kishore_Mantha <kishore.mantha@numerify.com>
2020-07-20 10:31:06 +0530

    Updated the QA Codes

--- Apps 6.0/png_uat/ITAA/FUNCTIONAL/d_brazil_inventory_c__of_Data_Stakeholders_from_Jurisdiction_c_mysql.sql
+++ Apps 6.0/png_uat/ITAA/FUNCTIONAL/d_brazil_inventory_c__of_Data_Stakeholders_from_Jurisdiction_c_mysql.sql
@@ -1,1 +1,14 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_brazil_inventory_c._of_Data_Stakeholders_from_Jurisdiction_c' ELSE 'SUCCESS' END as Message FROM png_mdsdb.pg_ot_brazil_processing_activity_inventory_c_final  SRC JOIN png_mdwdb.d_brazil_inventory_c TRGT ON (SRC.Inventory_Id = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )  WHERE SRC._of_Data_Stakeholders_from_Jurisdiction <> (TRGT._of_Data_Stakeholders_from_Jurisdiction_c) 
+SELECT 
+CASE WHEN CNT > 0   THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN CNT > 0   THEN 'MDS to DWH data validation failed for d_brazil_inventory_c.of_data_stakeholders_from_jurisdiction_c' ELSE 'SUCCESS' END as Message 
+FROM 
+(
+SELECT Count(1) as CNT
+FROM png_mdsdb.pg_ot_brazil_processing_activity_inventory_c_final  SRC 
+LEFT JOIN png_mdwdb.d_brazil_inventory_c TRGT ON (SRC.Inventory_Id = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )  
+WHERE SRC._of_Data_Stakeholders_from_Jurisdiction <> (TRGT.of_data_stakeholders_from_jurisdiction_c) 
+AND SRC.cdctype='X'
+) temp;
+
+
+
