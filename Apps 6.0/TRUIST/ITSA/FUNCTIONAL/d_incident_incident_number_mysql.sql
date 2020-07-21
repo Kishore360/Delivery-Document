477df635f12726901f415b7e092fc80a1b4ba961
Shubha_Rao <Shubha.Rao@numerify.com>
2020-07-21 12:57:00 +0530

    Updating QA queries

--- Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_incident_incident_number_mysql.sql
+++ Apps 6.0/TRUIST/ITSA/FUNCTIONAL/d_incident_incident_number_mysql.sql
@@ -1,12 +1,12 @@
 SELECT CASE WHEN count(1) > 0 THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
  CASE WHEN count(1) >0 THEN 'MDS to DWH data validation failed' ELSE 'SUCCESS' END as Message
- FROM truist_mdsdb.incident_final SRC 
+ FROM (select  * from  truist_mdsdb.incident_final where cdctype<>'D')SRC 
 left join truist_mdsdb.sys_user_group_final lkp 
 on SRC.assignment_group = lkp.sys_id 
 and SRC.sourceinstance = lkp.sourceinstance
  LEFT JOIN truist_mdwdb.d_incident TRGT 
  ON (SRC.sys_id =TRGT.row_id  
  AND SRC.sourceinstance= TRGT.source_id  )
- WHERE COALESCE( SRC.number,'')<> COALESCE(TRGT.incident_number ,'') -- 46303
+ WHERE COALESCE( SRC.number,'unspecified')<> TRGT.incident_number  -- 46303
  and ((lkp.name not like 'ITSM-LOB-Branchinfo%' and name not like 'ITSM-LOB-HumanSystems%' and name not like 'ITSM-LOB-CPS%'
 and name not like 'ITSM-LOB-LoanServices%' and name not like 'ITSM-LOB-Wealth%') or lkp.name is null);
