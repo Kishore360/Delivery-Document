e2bef7a9f6eed407714755e08b6edb66ac42e570
Kishore_Mantha <kishore.mantha@numerify.com>
2020-07-22 23:23:00 +0530

    Added the QA Codes as part of DELV-24672

--- Apps 6.0/png/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_c_vendors_stats_c_key_mysql.sql
+++ Apps 6.0/png/ITAA/FUNCTIONAL/d_png_ot_processing_activities_inventory_c_vendors_stats_c_key_mysql.sql
@@ -10,5 +10,6 @@ on src.inventory_id = trgt.row_id and src.sourceinstance = trgt.source_id
 JOIN png_mdwdb.d_lov lkp
 ON COALESCE(CONCAT('OT_CONTROL_STATUS_C~PROCESSING_ACTIVITIES~',src.Vendors_Status),'UNSPECIFIED') =lkp.row_id
 where COALESCE(lkp.row_key,CASE WHEN src.Vendors_Status IS NULL THEN 0 else -1 end)<> trgt.vendors_stats_c_key
+AND src.cdctype='X'
 ) temp;
 
