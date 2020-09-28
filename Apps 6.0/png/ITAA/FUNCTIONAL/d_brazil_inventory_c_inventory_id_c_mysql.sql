7c61704a06b3d788f882a0a6f2ed4fbd526d73bb
Kishore_Mantha <kishore.mantha@numerify.com>
2020-09-28 19:43:29 +0530

    Updated the QA Code

--- Apps 6.0/png/ITAA/FUNCTIONAL/d_brazil_inventory_c_inventory_id_c_mysql.sql
+++ Apps 6.0/png/ITAA/FUNCTIONAL/d_brazil_inventory_c_inventory_id_c_mysql.sql
@@ -1,1 +1,11 @@
-SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_brazil_inventory_c.inventory_id_c' ELSE 'SUCCESS' END as Message FROM png_mdsdb.pg_ot_brazil_processing_activity_inventory_c_final  SRC JOIN png_mdwdb.d_brazil_inventory_c TRGT ON (SRC.Inventory_Id = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )  WHERE SRC.Inventory_Id <> (TRGT.inventory_id_c) 
+SELECT 
+CASE WHEN CNT > 0  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, 
+CASE WHEN CNT > 0  THEN 'MDS to DWH data validation failed for d_brazil_inventory_c.inventory_id_c' ELSE 'SUCCESS' END as Message 
+(
+Select Count(1) as CNT 
+FROM png_mdsdb.pg_ot_brazil_processing_activity_inventory_c_final  SRC 
+LEFT JOIN png_mdwdb.d_brazil_inventory_c TRGT ON (SRC.Inventory_Id = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )  
+WHERE SRC.Inventory_Id <> (TRGT.inventory_id_c) 
+AND SRC.cdctype='X' 
+)
+
