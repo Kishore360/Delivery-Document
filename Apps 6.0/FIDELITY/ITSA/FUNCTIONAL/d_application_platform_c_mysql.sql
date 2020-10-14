c26efece4ca6c4178020a9c4a3fc8e4c9c7bf37c
Shubha_Rao <Shubha.Rao@numerify.com>
2020-10-14 13:09:56 +0530

    Updating the QA queries

--- Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_application_platform_c_mysql.sql
+++ Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_application_platform_c_mysql.sql
@@ -2,18 +2,23 @@ SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHE
 from
 fidelity_mdwdb.d_application a
 LEFT JOIN (
-select group_concat(b.u_value SEPARATOR  ', ') as platform ,CONCAT('Application~',a.app_id) as app_id,a.sourceinstance  from (SELECT cast(COALESCE(SUBSTRING_INDEX(SUBSTRING_INDEX(TRIM(app.u_platform), ',', n.n), ',', -1),'UNSPECIFIED')as char(32))  AS platform,app.sys_id as app_id,app.sourceinstance
+select group_concat(b.u_value SEPARATOR  ', ') as platform ,CONCAT('Application~',a.app_id) as app_id,a.sourceinstance  from
+(SELECT cast(COALESCE(SUBSTRING_INDEX(SUBSTRING_INDEX(TRIM(app.u_platform), ',', n.n), ',', -1),'UNSPECIFIED')as char(32))
+  AS platform,app.sys_id as app_id,app.sourceinstance
 from fidelity_mdsdb.cmdb_ci_appl_final app
 JOIN
 (SELECT a.N + b.N * 10 + 1 AS n
 FROM
- (SELECT 0 AS N UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) a,
- (SELECT 0 AS N UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) b
+ (SELECT 0 AS N UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6
+ UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) a,
+ (SELECT 0 AS N UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6
+ UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) b
 ORDER BY n) n
-ON n.n <= 1 + (LENGTH(COALESCE(TRIM(app.u_platform),'UNSPECIFIED')) - LENGTH(REPLACE(COALESCE(TRIM(app.u_platform),'UNSPECIFIED'), ',', ''))) where app.cdctype<>'D') a
+ON n.n <= 1 + (LENGTH(COALESCE(TRIM(app.u_platform),'UNSPECIFIED'))
+- LENGTH(REPLACE(COALESCE(TRIM(app.u_platform),'UNSPECIFIED'), ',', ''))) where app.cdctype<>'D') a
 join fidelity_mdsdb.u_fmr_tech_stack_attributes_final b
-ON a.platform = b.sys_id
+ON a.platform COLLATE utf8_general_ci = b.sys_id
 group by 2
 ) b ON a.row_id = b.app_id and a.source_id = b.sourceinstance
 where
-a.platform_c <> COALESCE(b.platform,'UNSPECIFIED') COLLATE utf8_unicode_ci
+a.platform_c COLLATE utf8_general_ci <> COALESCE(b.platform,'UNSPECIFIED')
--- Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_incident_u_manual_escalation_c_mysql.sql
+++ Apps 6.0/FIDELITY/ITSA/FUNCTIONAL/d_incident_u_manual_escalation_c_mysql.sql
@@ -1,3 +1,4 @@
-SELECT CASE WHEN count(1) THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
-CASE WHEN count(1) THEN 'MDS to DWH data validation failed for d_incident.u_manual_escalation_c' ELSE 'SUCCESS' END as Message
-FROM fidelity_mdsdb.incident_final SRC JOIN fidelity_mdwdb.d_incident TRGT ON (SRC.sys_id = TRGT.row_id AND SRC.sourceinstance = TRGT.source_id ) WHERE SRC.cdctype <> 'D' and SRC.u_manual_escalation <> (TRGT.u_manual_escalation_c)
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result, CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for d_incident.u_manual_escalation_c' ELSE 'SUCCESS' END as Message
+FROM fidelity_mdsdb.incident_final  SRC JOIN fidelity_mdwdb.d_incident TRGT ON (SRC.sys_id = TRGT.row_id  AND SRC.sourceinstance = TRGT.source_id )
+WHERE SRC.cdctype <> 'D' and
+case when SRC.u_manual_escalation =0 then 'N'  else 'Y' end  <> (TRGT.u_manual_escalation_c) ;
