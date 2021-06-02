c3d2927e5cb5eb36b56e3bdfbe03d25668fe8bb2
Kishore_Mantha <kishore.mantha@numerify.com>
2021-06-02 15:06:18 +0530

    Updated the QA Code in to the Bitbucket as part of the DELV-29118

---
+++ Apps 6.0/jhi/ITSA/FUNCTIONAL/UNIT TEST ROADMAPS_IN_AHA_C DELV-29118.sql
@@ -0,0 +1,15 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_work_item.roadmaps_in_aha_c' ELSE 'SUCCESS' END as Message
+FROM jhi_mdsdb.issue_final  SRC
+JOIN jhi_jira_sb_mdwdb.d_work_item_ext_c TRGT ON
+SRC.id = TRGT.row_id and SRC.sourceinstance = TRGT.source_id
+JOIN jhi_jira_sb_mdwdb.d_work_item TRGT2 on TRGT.work_item_c_key=TRGT2.row_key
+JOIN jhi_jira_sb_mdwdb.f_work_item LKP on LKP.work_item_key=TRGT2.row_key
+WHERE  CASE             
+            WHEN LKP.roadmaps_created_in_aha_c = 1 
+            AND LKP.roadmaps_reviewed_with_product_family_c = 1 
+            AND LKP.roadmaps_reviewed_with_business_c = 1 
+            AND LKP.roadmaps_reviewed_with_tech_org_c = 1
+            THEN 1             
+            ELSE 0          
+        END  <> (LKP.roadmaps_in_aha_c) and LKP.soft_deleted_flag ='N';
---
+++ Apps 6.0/jhi/ITSA/FUNCTIONAL/UNIT TEST ROADMAPS_REVIEWED_WITH_BUSINESS_C DELV-29118.sql
@@ -0,0 +1,12 @@
+SELECT CASE WHEN count(1)  THEN 'FAILURE' ELSE 'SUCCESS' END as Result,
+CASE WHEN count(1)  THEN 'MDS to DWH data validation failed for f_work_item.roadmaps_reviewed_with_business_c' ELSE 'SUCCESS' END as Message
+FROM jhi_mdsdb.issue_final  SRC
+JOIN jhi_jira_sb_mdwdb.d_work_item_ext_c TRGT ON
+SRC.id = TRGT.row_id and SRC.sourceinstance = TRGT.source_id
+JOIN jhi_jira_sb_mdwdb.d_work_item TRGT2 on TRGT.work_item_c_key=TRGT2.row_key
+JOIN jhi_jira_sb_mdwdb.f_work_item LKP on LKP.work_item_key=TRGT2.row_key
+WHERE  CASE             
+            WHEN UPPER(TRGT.product_roadmaps_12_18_month_view_in_aha_c) LIKE '%REVIEWED WITH BUSINESS STAKEHOLDERS%'
+            THEN 1             
+            ELSE 0          
+        END  <> (LKP.roadmaps_reviewed_with_business_c) and LKP.soft_deleted_flag ='N';
